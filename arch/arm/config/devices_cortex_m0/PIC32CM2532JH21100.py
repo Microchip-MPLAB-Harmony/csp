e9a310f4d920a7a3c809f87d87f323df828ccd71
Leonard Elevich <leonard.elevich@microchip.com>
2017-12-14 03:19:47 -0700

    removed mips devices added M0+ devices changed bootstrap of peripherals

---
+++ arch/arm/config/devices_cortex_m0/PIC32CM2532JH21100.py
@@ -0,0 +1,22 @@
+print("Loading System Services for " + Variables.get("__PROCESSOR"))
+
+# load clock manager information
+execfile(Variables.get("__CORE_DIR") + "/../peripheral/pmc/config/pmc.py")
+
+# load pin manager information
+execfile(Variables.get("__CORE_DIR") + "/../peripheral/pio/config/pio.py")
+
+devMenu = coreComponent.createMenuSymbol(None, None)
+devMenu.setLabel("Device & Project Configuration")
+
+devCfgMenu = coreComponent.createMenuSymbol(None, devMenu)
+devCfgMenu.setLabel(Variables.get("__PROCESSOR") + " Device Configuration")
+devCfgMenu.setDescription("Hardware Configuration Bits")
+
+devCfgComment = coreComponent.createCommentSymbol("Comment1", devCfgMenu)
+devCfgComment.setLabel("Note: Set Device Configuration Bits via Programming Tool")
+
+prjMenu = coreComponent.createMenuSymbol(None, devMenu)
+prjMenu.setLabel("Project Configuration")
+prjMenu.setDescription("Project Specific Configuration")
+
