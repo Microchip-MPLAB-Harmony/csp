e52f8a87940d8eb76f65487d3aefa72bd0c2ae82
Jeevith <http://support.microchip.com>
2017-12-14 12:28:24 +0530

    [USART] Basic menu is created for the USART PLIB and necessary files are added. MH-8407

--- peripheral/usart/config/usart.py
+++ peripheral/usart/config/usart.py
@@ -1,24 +1,75 @@
+regModule = Register.getRegisterModule("USART")
+regGroup = regModule.getRegisterGroup("USART")
+
+modeRegister = regGroup.getRegister("US_MR")
+parityBitField = modeRegister.getBitfield("PAR")
+parityValueGroup = regModule.getValueGroup(parityBitField.getValueGroupName())
+bit9Field = modeRegister.getBitfield("MODE9")
+bit9ValueGroup = regModule.getValueGroup(bit9Field.getValueGroupName())
+stopBitsField = modeRegister.getBitfield("NBSTOP")
+stopBitsValueGroup = regModule.getValueGroup(stopBitsField.getValueGroupName())
+syncField = modeRegister.getBitfield("SYNC")
+syncValueGroup = regModule.getValueGroup(stopBitsField.getValueGroupName())
+
 def instantiateComponent(usartComponent):
 
 	num = usartComponent.getID()[-1:]
 	print("Running USART" + str(num))
-
+	
 	usartMenu = usartComponent.createMenuSymbol(None, None)
 	usartMenu.setLabel("Hardware Settings ")
+	
+	usartEnable = usartComponent.createBooleanSymbol("enable", usartMenu)
+	print(usartEnable)
+	usartEnable.setLabel("Enable")
+	usartEnable.setDefaultValue(True)
+	
+	usartInterrupt = usartComponent.createBooleanSymbol("interruptMode", usartMenu)
+	print(usartInterrupt)
+	usartInterrupt.setLabel("Enable Interrupts")
+	usartInterrupt.setDefaultValue(True)
+	
+	usartBaud = usartComponent.createIntegerSymbol("baud", usartMenu)
+	print(usartBaud)
+	usartBaud.setLabel("Baud Rate")
+	usartBaud.setDefaultValue(9600)
+	
+	usartDataBits = usartComponent.createBooleanSymbol("bit9", usartMenu)
+	print(usartDataBits)
+	usartDataBits.setLabel(bit9Field.getDescription())
+	usartDataBits.setDefaultValue(False)
+	
+	usartParity = usartComponent.createComboSymbol("parityBit", usartMenu, parityValueGroup.getValueNames())
+	print(usartParity)
+	usartParity.setLabel(parityBitField.getDescription())
+	usartParity.setDefaultValue("NO")
+	
+	usartStopBits = usartComponent.createComboSymbol("stopBit", usartMenu, stopBitsValueGroup.getValueNames())
+	print(usartStopBits)
+	usartStopBits.setLabel(stopBitsField.getDescription())
+	usartStopBits.setDefaultValue("_1_BIT")
 
-	usartConfig1 = usartComponent.createBooleanSymbol("Config1", usartMenu)
-	print(usartConfig1)
-	usartConfig1.setLabel("Config 1")
-
+	usartSyncMode = usartComponent.createBooleanSymbol("sync", usartMenu)
+	print(usartSyncMode)
+	usartSyncMode.setLabel(syncField.getDescription())
+	usartSyncMode.setDefaultValue(False)
+	
 	usartIndex = usartComponent.createIntegerSymbol("INDEX", usartMenu)
 	usartIndex.setVisible(False)
 	usartIndex.setDefaultValue(int(num))
 
 	configName = Variables.get("__CONFIGURATION_NAME")
 
+	usartHeader1File = usartComponent.createFileSymbol(None, None)
+	usartHeader1File.setSourcePath("../peripheral/usart/templates/plib_usart.h.ftl")
+	usartHeader1File.setOutputName("plib_usart" + str(num) + ".h")
+	usartHeader1File.setDestPath("system_config/" + configName +"/peripheral/usart/")
+	usartHeader1File.setProjectPath("system_config/" + configName +"/peripheral/usart/")
+	usartHeader1File.setType("HEADER")
+	
 	usartSource1File = usartComponent.createFileSymbol(None, None)
-	usartSource1File.setSourcePath("../peripheral/usart/templates/usart.c.ftl")
-	usartSource1File.setOutputName("usart" + str(num) + ".c")
+	usartSource1File.setSourcePath("../peripheral/usart/templates/plib_usart.c.ftl")
+	usartSource1File.setOutputName("plib_usart" + str(num) + ".c")
 	usartSource1File.setDestPath("system_config/" + configName +"/peripheral/usart/")
 	usartSource1File.setProjectPath("system_config/" + configName +"/peripheral/usart/")
 	usartSource1File.setType("SOURCE")
---
+++ peripheral/usart/templates/plib_usart.c.ftl
@@ -0,0 +1,7 @@
+//Source File
+Usart${INDEX?string} Plib for ${__PROCESSOR}
+<#if interruptMode == true>
+Interrupt is Enabled
+<#else>
+Interrupt is Disabled
+</#if>
