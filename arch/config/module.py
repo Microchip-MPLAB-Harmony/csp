7256c374dff4bd43132f2fb6b2d53f36c6213f25
Ryan <http://support.microchip.com>
2017-12-19 13:55:48 -0700

    Dev Update
    Added Module.execfile interface to execute files from the module root
    instead of the process root
    
    Moved peripheral execfile to a module-type script instead of a component
    one.  Components can only be created from a module-type script.

--- arch/config/module.py
+++ arch/config/module.py
@@ -1,3 +1,5 @@
 def loadModule():
 	coreComponent = Module.CreateSharedComponent("core", "System", "/", "config/core.py")
 
+	# load device specific peripherals
+	Module.execfile("../../csp/peripheral/config/peripheral.py")
