2485007ff4e093cbf924ba449103c5148351a3c3
Leonard <http://support.microchip.com>
2017-12-24 15:42:26 -0700

    renamed eefc to efc to confirm with atdf file

--- peripheral/efc_6450/config/efc.py
+++ peripheral/efc_6450/config/efc.py
@@ -14,7 +14,7 @@ def instantiateComponent(eefcComponent):
 	configName = Variables.get("__CONFIGURATION_NAME")
 	#Generate Output Header
 	eefcHeaderFile = eefcComponent.createFileSymbol(None, None)
-	eefcHeaderFile.setSourcePath("../peripheral/eefc_6450/templates/plib_eefc.h.ftl")
+	eefcHeaderFile.setSourcePath("../peripheral/efc_6450/templates/plib_eefc.h.ftl")
 	eefcHeaderFile.setOutputName("plib_eefc.h")
 	eefcHeaderFile.setOverwrite(True)
 	eefcHeaderFile.setDestPath("system_config/" + configName +"/peripheral/eefc/")
@@ -22,7 +22,7 @@ def instantiateComponent(eefcComponent):
 	eefcHeaderFile.setType("HEADER")
 	#Generate Output source
 	eefcSourceFile = eefcComponent.createFileSymbol(None, None)
-	eefcSourceFile.setSourcePath("../peripheral/eefc_6450/templates/plib_eefc.c.ftl")
+	eefcSourceFile.setSourcePath("../peripheral/efc_6450/templates/plib_eefc.c.ftl")
 	eefcSourceFile.setOutputName("plib_eefc.c")
 	eefcSourceFile.setOverwrite(True)
 	eefcSourceFile.setDestPath("system_config/" + configName +"/peripheral/eefc/")
