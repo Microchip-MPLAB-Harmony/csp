e2c612b64fdf72ed2c014abcfa3f8cc09eaf0f53
Leonard <http://support.microchip.com>
2017-12-24 15:22:27 -0700

    make peripheral instantiation fully dynamic

--- peripheral/config/peripheral.py
+++ peripheral/config/peripheral.py
@@ -1,73 +1,32 @@
-# add TC PLib =============================================================
-if (Peripheral.moduleExists("TC")):
-	tcModule = Register.getRegisterModule("TC")
-	for tcCount in range(0, Peripheral.getModuleInstanceCount("TC")):
-		print("create component: Peripheral TC" + str(tcCount) + " (ID = " + tcModule.getID() + ")")
-		tcComponent = Module.CreateComponent("tc"+str(tcCount), "TC"+str(tcCount), "/Peripherals/TC/", "../peripheral/tc_"+tcModule.getID()+"/config/tc.py")
-		tcComponent.addCapability("TC")
-else:
-	print("No TC peripheral")
-# add USART PLib ==========================================================
-if (Peripheral.moduleExists("UART")):
-	uartModule = Register.getRegisterModule("UART")
-	for uartCount in range(0, Peripheral.getModuleInstanceCount("UART")):
-		print("create component: Peripheral UART" + str(uartCount) + " (ID = " + uartModule.getID() + ")")
-		uartComponent = Module.CreateComponent("uart"+str(uartCount), "UART"+str(uartCount), "/Peripherals/UART/", "../peripheral/uart_"+uartModule.getID()+"/config/uart.py")
-		uartComponent.addCapability("USART")
-else:
-	print("No UART peripheral")
-# add USART PLib ==========================================================
-if (Peripheral.moduleExists("USART")):
-	usartModule = Register.getRegisterModule("USART")
-	for usartCount in range(0, Peripheral.getModuleInstanceCount("USART")):
-		print("create component: Peripheral USART" + str(usartCount) + " (ID = " + usartModule.getID() + ")")
-		usartComponent = Module.CreateComponent("usart"+str(usartCount), "USART"+str(usartCount), "/Peripherals/USART/", "../peripheral/usart_"+usartModule.getID()+"/config/usart.py")
-		usartComponent.addCapability("USART")
-else:
-	print("No USART peripheral")
+import os.path
 
-# add EEFC PLib ==========================================================
-if (Peripheral.moduleExists("EFC")):
-	eefcModule = Register.getRegisterModule("EFC")
-	print("create component: Peripheral EEFC" + " (ID = " + eefcModule.getID() + ")")
-	eefcComponent = Module.CreateComponent("EEFC", "EEFC", "/Peripherals/EEFC/", "../peripheral/eefc_" + eefcModule.getID() + "/config/eefc.py")
-	eefcComponent.addCapability("EEFC")
-else:
-	print("No EEFC peripheral")
+periphlNames = Register.getRegisterModuleNames()
 
-# add WDT PLib ==========================================================
-if (Peripheral.moduleExists("WDT")):
-	wdtModule = Register.getRegisterModule("WDT")
-	print("create component: Peripheral WDT" + " (ID = " + wdtModule.getID() + ")")
-	wdtComponent = Module.CreateComponent("WDT", "WDT", "/Peripherals/WDT/", "../peripheral/wdt_" + wdtModule.getID() + "/config/wdt.py")
-	wdtComponent.addCapability("WDT")
-else:
-	print("No WDT peripheral")
+for periphCount in range(0, Register.getRegisterModuleCount()):
+	periphName = str(periphlNames[periphCount])
+	periphModule =  Register.getRegisterModule(periphName)
+	periphInstanceCount = Peripheral.getModuleInstanceCount(periphName)
+	periphName = periphName.lower()
+	periphID = periphModule.getID()
+	periphScript = Variables.get("__CSP_DIR") + "/peripheral/" + periphName + "_" + periphID + "/config/" + periphName + ".py"
 
-#add RSWDT PLib ==========================================================
-if (Peripheral.moduleExists("RSWDT")):
-	rswdtModule = Register.getRegisterModule("RSWDT")
-	print("create component: Peripheral RSWDT" + " (ID = " + rswdtModule.getID() + ")")
-	rswdtComponent = Module.CreateComponent("RSWDT", "RSWDT", "/Peripherals/RSWDT/", "../peripheral/rswdt_" + rswdtModule.getID() + "/config/rswdt.py")
-	rswdtComponent.addCapability("RSWDT")
-else:
-	print("No RSWDT peripheral")
+	# port/pio is instantiated as part of system service and not part of peripherals
+	if (periphName == "port" or periphName == "pio"):
+		print("CSP: System Peripheral [" + periphName + " id=" + periphID + "]")
+		continue
 
-# add TRNG PLib ==========================================================
-if (Peripheral.moduleExists("TRNG")):
-	trngModule = Register.getRegisterModule("TRNG")
-	print("create component: Peripheral TRNG" + " (ID = " + trngModule.getID() + ")")
-	trngComponent = Module.CreateComponent("TRNG", "TRNG", "/Peripherals/TRNG/", "../peripheral/trng_" + trngModule.getID() + "/config/trng.py")
-	trngComponent.addCapability("TRNG")
-else:
-	print("No TRNG peripheral")
+	# Check if peripheral has implementation
+	if (os.path.isfile(periphScript)):
+		for periphInstance in range(0, periphInstanceCount):
+			print("CSP: create component: Peripheral " + periphName + str(periphInstance) + " (ID = " + periphID + ")")
+			periphComponent = Module.CreateComponent(periphName+str(periphInstance), periphName.upper()+str(periphInstance), "/Peripherals/"+periphName.upper()+"/", "../peripheral/"+periphName+"_"+periphID+"/config/"+periphName+".py")
 
-# add RTCC PLib ==========================================================
-if (Peripheral.moduleExists("RTC")):
-	rtccModule = Register.getRegisterModule("RTC")
-	print("create component: Peripheral RTCC" + " (ID = " + rtccModule.getID() + ")")
-	rtccComponent = Module.CreateComponent("RTCC", "RTCC", "/Peripherals/RTCC/", "../peripheral/rtcc_" + rtccModule.getID() + "/config/rtcc.py")
-	rtccComponent.addCapability("RTCC")
-else:
-	print("No RTCC peripheral")
+			if (periphName == "uart"):
+				periphComponent.addCapability("USART")
+			else:
+				periphComponent.addCapability(periphName.upper())
+
+	else:
+		print("CSP: Peripheral [" + periphName + " id=" + periphID + "] is not supported in MCC")
+
 
