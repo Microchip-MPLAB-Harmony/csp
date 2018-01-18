c3f3e7285ef89e28dbecfb5a633d4801f6f94d7a
Leonard Elevich <leonard.elevich@microchip.com>
2018-01-18 15:41:45 -0700

    migrated arch from harmony v2 (for m7 devices)

--- arch/config/module.py
+++ arch/config/module.py
@@ -1,5 +1,18 @@
 def loadModule():
 	coreComponent = Module.CreateSharedComponent("core", "System", "/", "config/core.py")
 
+	print("Load Module: Device Family Pack (DFP)")
+
+	#supportedProcessors = [] 
+	selectedProcessor = Variables.get("__PROCESSOR")
+
+	#if selectedProcessor.lower() in supportedProcessors:
+	dfpComponent = Module.CreateComponent("dfp", "Device Family Pack (DFP)", "/Packs/", "config/dfp.py")
+	#else:
+	#	print("Can't Load PIC32CZ-CA70 DFP: " + selectedProcessor + " is not supported.")
+
+	cmsisComponent = Module.CreateComponent("cmsis", "CMSIS Pack", "/Packs/", "config/cmsis.py")
+
+
 	# load device specific peripherals
 	Module.execfile("../../csp/peripheral/config/peripheral.py")
