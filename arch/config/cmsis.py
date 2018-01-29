7cf93771ff5096870c925f511a85b90063094363
Leonard Elevich <leonard.elevich@microchip.com>
2018-01-29 09:25:32 -0700

    update CMMSIS to support version 5.2.0 [MH-9796] update clk, so it compiles

--- arch/config/cmsis.py
+++ arch/config/cmsis.py
@@ -1,23 +1,47 @@
 def instantiateComponent(cmsisComponent):
 
+	cmsisSupportedVersion = "5.2.0"
+
 	cmsisInformation = cmsisComponent.createCommentSymbol("cmsisInformation", None)
+	cmsisError = cmsisComponent.createCommentSymbol("cmsisError", None)
+	cmsisError.setLabel("Error: This version of CMSIS Pack is not supported! Supported version is " + cmsisSupportedVersion)
+	cmsisError.setVisible(False)
 
 	import xml.etree.ElementTree as ET
 	cmsisDescriptionFile = open(Variables.get("__CMSIS_PACK_DIR") + "/ARM.CMSIS.pdsc", "r")
 	cmsisDescription = ET.fromstring(cmsisDescriptionFile.read())
 	for release in cmsisDescription.iter("release"):
 		cmsisInformation.setLabel("Release Information: " + str(release.attrib))
+		if release.attrib['version'] != cmsisSupportedVersion:
+			print("Error: This version of CMSIS Pack is not supported! Supported version is " + cmsisSupportedVersion)
+			cmsisError.setVisible(True)
+			return
 		break
 
-	headerFileNames = ["cmsis_compiler.h", "cmsis_gcc.h", "tz_context.h", "arm_common_tables.h", "arm_const_structs.h", "arm_math.h"]
+	#add core header files
+	headerFileNames = ["cmsis_compiler.h", "cmsis_gcc.h", "tz_context.h"]
 
 	for headerFileName in headerFileNames:
 		headerFile = cmsisComponent.createFileSymbol(None, None)
 		headerFile.setRelative(False)
-		headerFile.setSourcePath(Variables.get("__CMSIS_PACK_DIR") + "/CMSIS/Include/" + headerFileName)
+		headerFile.setSourcePath(Variables.get("__CMSIS_PACK_DIR") + "/CMSIS/Core/Include/" + headerFileName)
 		headerFile.setOutputName(headerFileName)
 		headerFile.setMarkup(False)
 		headerFile.setOverwrite(True)
-		headerFile.setDestPath("../../packs/CMSIS/CMSIS/Include/")
-		headerFile.setProjectPath("packs/CMSIS/CMSIS/Include/")
+		headerFile.setDestPath("../../packs/CMSIS/CMSIS/Core/Include/")
+		headerFile.setProjectPath("packs/CMSIS/CMSIS/Core/Include/")
+		headerFile.setType("HEADER")
+
+	#add dsp header files
+	headerFileNames = ["arm_common_tables.h", "arm_const_structs.h", "arm_math.h"]
+
+	for headerFileName in headerFileNames:
+		headerFile = cmsisComponent.createFileSymbol(None, None)
+		headerFile.setRelative(False)
+		headerFile.setSourcePath(Variables.get("__CMSIS_PACK_DIR") + "/CMSIS/DSP/Include/" + headerFileName)
+		headerFile.setOutputName(headerFileName)
+		headerFile.setMarkup(False)
+		headerFile.setOverwrite(True)
+		headerFile.setDestPath("../../packs/CMSIS/CMSIS/DSP/Include/")
+		headerFile.setProjectPath("packs/CMSIS/CMSIS/DSP/Include/")
 		headerFile.setType("HEADER")
