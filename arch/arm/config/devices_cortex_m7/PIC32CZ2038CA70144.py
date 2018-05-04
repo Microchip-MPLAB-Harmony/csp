ccc08aac431eff5f4c2de686ac5c2b738dafb035
Thibaut VIARD <thibaut.viard@microchip.com>
2018-05-04 19:11:02 +0200

    [arch] Removing PIC32C remnants
    Signed-off-by: Thibaut VIARD <thibaut.viard@microchip.com>

--- arch/arm/config/devices_cortex_m7/PIC32CZ2038CA70144.py
+++
@@ -1,7 +0,0 @@
-# load device specific configurations (fuses) 
-devCfgComment = coreComponent.createCommentSymbol("CoreCfgComment1", devCfgMenu)
-devCfgComment.setLabel("Note: Set Device Configuration Bits via Programming Tool")
-
-# load family specific configuration
-execfile(Variables.get("__ARCH_DIR") + "/PIC32CZ_CA70.py")
-
