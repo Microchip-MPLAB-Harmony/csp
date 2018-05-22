d6e247dfe075478818177fa127a4c8a3e4f4f954
Settu Duraisamy <settu.duraisamy@microchip.com>
2018-05-22 16:41:57 +0530

    [arch] Startup code and core configuration updates 1. Added menu to configure TCM and Cache in MHC, and set compiler options 2. Simplified startup file 3. Added configuration fuse support 4. Added code to program GPNVM fuse to configure TCM size at run time

--- arch/arm/config/devices_cortex_m7/DS60001527.py
+++ arch/arm/config/devices_cortex_m7/DS60001527.py
@@ -1,30 +1,136 @@
+global xc32StackInTCMSym
+global xc32DTCMSizeSym
+global xc32ITCMSizeSym
+global tcmSize
+global tcmEnable
+
+def xc32SetTcmSize(symbol, event):
+    symObj=event["symbol"]
+    if (symObj.getSelectedKey() == "0KB"):
+        xc32DTCMSizeSym.setValue("")
+        xc32ITCMSizeSym.setValue("")
+    elif (symObj.getSelectedKey() == "32KB"):
+        xc32DTCMSizeSym.setValue("0x8000")
+        xc32ITCMSizeSym.setValue("0x8000")
+    elif (symObj.getSelectedKey() == "64KB"):
+        xc32DTCMSizeSym.setValue("0x10000")
+        xc32ITCMSizeSym.setValue("0x10000")
+    elif (symObj.getSelectedKey() == "128KB"):
+        xc32DTCMSizeSym.setValue("0x20000")
+        xc32ITCMSizeSym.setValue("0x20000")
+
+def xc32SetStackInTcm(symbol, event):
+    if (event["value"] == True):
+        xc32StackInTCMSym.setValue("true")
+    else:
+        xc32StackInTCMSym.setValue("false")
+
+def setTcmSize(symbol, event):
+    symObj=event["symbol"]
+    if (symObj.getSelectedKey()  == "0KB"):
+        tcmSize.setValue("0 KB",2)
+        tcmEnable.setValue(False,2)
+    elif (symObj.getSelectedKey()  == "32KB"):
+        tcmSize.setValue("32 KB",2)
+        tcmEnable.setValue(True,2)
+    elif (symObj.getSelectedKey()  == "64KB"):
+        tcmSize.setValue("64 KB",2)
+        tcmEnable.setValue(True,2)
+    elif (symObj.getSelectedKey()  == "128KB"):
+        tcmSize.setValue("128 KB",2)
+        tcmEnable.setValue(True,2)
+
+
 # load family specific configurations
 print("Loading System Services for " + Variables.get("__PROCESSOR"))
 
-# load clock manager information
-execfile(Variables.get("__CORE_DIR") + "/../peripheral/clk_sam_e70/config/clk.py")
-coreComponent.addPlugin("../peripheral/clk_sam_e70/plugin/clockmanager.jar")
+# Device Configuration
+deviceSecurity = coreComponent.createKeyValueSetSymbol("DEVICE_SECURITY", devCfgMenu)
+deviceSecurity.setLabel("Security")
+deviceSecurity.setOutputMode("Key")
+deviceSecurity.setDisplayMode("Description")
+deviceSecurity.addKey("CLEAR", "0", "Disable (Code Protection Disabled)" )
+deviceSecurity.addKey("SET", "1", "Enable (Code Protection Enabled)")
+deviceSecurity.setSelectedKey("CLEAR",1)
 
-# load dma manager information
-execfile(Variables.get("__CORE_DIR") + "/../peripheral/xdmac_11161/config/xdmac.py")
-coreComponent.addPlugin("../peripheral/xdmac_11161/plugin/dmamanager.jar")
+deviceBoot = coreComponent.createKeyValueSetSymbol("DEVICE_BOOT", devCfgMenu)
+deviceBoot.setLabel("Boot Mode")
+deviceBoot.setOutputMode("Key")
+deviceBoot.setDisplayMode("Description")
+deviceBoot.addKey("CLEAR", "0", "Boot from ROM")
+deviceBoot.addKey("SET", "1", "Boot from Flash")
+deviceBoot.setSelectedKey("SET",1)
 
-# load device specific nvic manager information
-execfile(Variables.get("__CORE_DIR") + "/../peripheral/nvic_m7/config/nvic.py")
-coreComponent.addPlugin("../peripheral/nvic_m7/plugin/ARM_M7_NVICmanager.jar")
+deviceTCMsize = coreComponent.createKeyValueSetSymbol("DEVICE_TCM_SIZE", devCfgMenu)
+deviceTCMsize.setLabel("TCM Size")
+deviceTCMsize.setOutputMode("Value")
+deviceTCMsize.setDisplayMode("Description")
+deviceTCMsize.addKey("0KB", "0", "0 KB" )
+deviceTCMsize.addKey("32KB", "1", "32 KB")
+deviceTCMsize.addKey("64KB", "2", "64 KB")
+deviceTCMsize.addKey("128KB", "3", "128 KB")
+deviceTCMsize.setSelectedKey("0KB",1)
+
+coreArch = coreComponent.createStringSymbol("CoreArchitecture", devCfgMenu)
+coreArch.setLabel("Core Architecture")
+coreArch.setDefaultValue("CORTEX-M7")
+coreArch.setReadOnly(True)
+coreArch.setVisible(False)
+
+deviceFamily = coreComponent.createStringSymbol("DeviceFamily", devCfgMenu)
+deviceFamily.setLabel("Device Family")
+deviceFamily.setDefaultValue("SAM_E70_S70_V70_V71")
+deviceFamily.setReadOnly(True)
+deviceFamily.setVisible(False)
+
+cortexMenu = coreComponent.createMenuSymbol("CORTEX_MENU", None)
+cortexMenu.setLabel("Cortex-M7 Configuration")
+cortexMenu.setDescription("Configuration for Cortex M7")
 
 # load device specific pin manager information
 execfile(Variables.get("__CORE_DIR") + "/../peripheral/pio_11004/config/pio.py")
 coreComponent.addPlugin("../peripheral/pio_11004/plugin/SAME70pinmanager.jar")
 
-# load device specific adc manager information
-coreComponent.addPlugin("../peripheral/afec_11147/plugin/ARM_M7_ADCmanager.jar")
+# load clock manager information
+execfile(Variables.get("__CORE_DIR") + "/../peripheral/clk_sam_e70/config/clk.py")
+coreComponent.addPlugin("../peripheral/clk_sam_e70/plugin/clockmanager.jar")
 
-# load rswdt
-execfile(Variables.get("__CORE_DIR") + "/../peripheral/rswdt_11110/config/rswdt.py")
+# Cortex-M7 IP Configuration
+tcmMenu = coreComponent.createMenuSymbol("TCM_MENU", cortexMenu)
+tcmMenu.setLabel("TCM")
+tcmMenu.setDescription("TCM Configuration")
 
-# load wdt
-execfile(Variables.get("__CORE_DIR") + "/../peripheral/wdt_6080/config/wdt.py")
+tcmEnable = coreComponent.createBooleanSymbol("TCM_ENABLE", tcmMenu)
+tcmEnable.setLabel("Enable TCM")
+tcmEnable.setDefaultValue(False)
+tcmEnable.setVisible(False)
+
+tcmSize = coreComponent.createStringSymbol("TCM_SIZE", tcmMenu)
+tcmSize.setLabel("TCM Size Selected through configuration Fuse")
+tcmSize.setDefaultValue("0 KB")
+tcmSize.setReadOnly(True)
+tcmSize.setDependencies(setTcmSize, ["DEVICE_TCM_SIZE"])
+
+stackTCM = coreComponent.createBooleanSymbol("STACK_IN_TCM", tcmMenu)
+stackTCM.setLabel("Locate Stack in TCM")
+stackTCM.setDefaultValue(False)
+
+cacheMenu = coreComponent.createMenuSymbol("CACHE_MENU", cortexMenu)
+cacheMenu.setLabel("CACHE")
+cacheMenu.setDescription("CACHE Configuration")
+
+dcacheEnable = coreComponent.createBooleanSymbol("DATA_CACHE_ENABLE", cacheMenu)
+dcacheEnable.setLabel("Enable Data Cache")
+dcacheEnable.setDefaultValue(True)
+
+icacheEnable = coreComponent.createBooleanSymbol("INSTRUCTION_CACHE_ENABLE", cacheMenu)
+icacheEnable.setLabel("Enable Instruction Cache")
+icacheEnable.setDefaultValue(True)
+
+
+# load NVIC
+execfile(Variables.get("__CORE_DIR") + "/../peripheral/nvic_m7/config/nvic.py")
+coreComponent.addPlugin("../peripheral/nvic_m7/plugin/ARM_M7_NVICmanager.jar")
 
 #load mpu
 execfile(Variables.get("__CORE_DIR") + "/../peripheral/mpu/config/mpu.py")
@@ -33,18 +139,21 @@ coreComponent.addPlugin("../peripheral/mpu/plugin/MPUmanager.jar")
 #load systick
 execfile(Variables.get("__CORE_DIR") + "/../peripheral/systick/config/systick.py")
 
-coreArch = coreComponent.createStringSymbol("CoreArchitecture", devCfgMenu)
-coreArch.setLabel("Core Architecture")
-coreArch.setDefaultValue("CORTEX-M7")
-coreArch.setReadOnly(True)
+# load dma manager information
+execfile(Variables.get("__CORE_DIR") + "/../peripheral/xdmac_11161/config/xdmac.py")
+coreComponent.addPlugin("../peripheral/xdmac_11161/plugin/dmamanager.jar")
 
-deviceFamily = coreComponent.createStringSymbol("DeviceFamily", devCfgMenu)
-deviceFamily.setLabel("Device Family")
-deviceFamily.setDefaultValue("SAM_E70_S70_V70_V71")
-deviceFamily.setReadOnly(True)
+# load rswdt
+execfile(Variables.get("__CORE_DIR") + "/../peripheral/rswdt_11110/config/rswdt.py")
+
+# load wdt
+execfile(Variables.get("__CORE_DIR") + "/../peripheral/wdt_6080/config/wdt.py")
+
+# load device specific adc manager information
+coreComponent.addPlugin("../peripheral/afec_11147/plugin/ARM_M7_ADCmanager.jar")
 
 
-#generate startup_xc32.c file
+# generate startup_xc32.c file
 armSysStartSourceFile = coreComponent.createFileSymbol("STARTUP_C", None)
 armSysStartSourceFile.setSourcePath("arm/templates/startup_xc32.c.ftl")
 armSysStartSourceFile.setOutputName("startup.c")
@@ -54,7 +163,7 @@ armSysStartSourceFile.setDestPath("")
 armSysStartSourceFile.setProjectPath("config/" + configName + "/")
 armSysStartSourceFile.setType("SOURCE")
 
-#generate libc_syscalls.c file
+# generate libc_syscalls.c file
 armLibCSourceFile = coreComponent.createFileSymbol("LIBC_SYSCALLS_C", None)
 armLibCSourceFile.setSourcePath("arm/templates/libc_syscalls.c.ftl")
 armLibCSourceFile.setOutputName("libc_syscalls.c")
@@ -64,3 +173,29 @@ armLibCSourceFile.setDestPath("")
 armLibCSourceFile.setProjectPath("config/" + configName + "/")
 armLibCSourceFile.setType("SOURCE")
 
+# set XC32 ITCM Size
+#xc32ITCMSizeSym = coreComponent.createSettingSymbol("XC32_ITCM_SIZE", None)
+#xc32ITCMSizeSym.setCategory("C32Global")
+#xc32ITCMSizeSym.setKey("mitcm")
+#xc32ITCMSizeSym.setValue("")
+#xc32ITCMSizeSym.setDependencies(xc32SetTcmSize, ["DEVICE_TCM_SIZE"])
+
+# set XC32 DTCM Size
+#xc32DTCMSizeSym = coreComponent.createSettingSymbol("XC32_DTCM_SIZE", None)
+#xc32DTCMSizeSym.setCategory("C32Global")
+#xc32DTCMSizeSym.setKey("mdtcm")
+#xc32DTCMSizeSym.setValue("")
+#xc32DTCMSizeSym.setDependencies(xc32SetTcmSize, ["DEVICE_TCM_SIZE"])
+
+# set XC32 Stack in TCM: True or False
+#xc32StackInTCMSym = coreComponent.createSettingSymbol("XC32_STACK_IN_TCM", None)
+#xc32StackInTCMSym.setCategory("C32Global")
+#xc32StackInTCMSym.setKey("mstacktcm")
+#xc32StackInTCMSym.setValue("false")
+#xc32StackInTCMSym.setDependencies(xc32SetStackInTcm, ["STACK_IN_TCM"])
+
+#devconSystemInitFile = coreComponent.createFileSymbol("DEVICE_CONFIG_SYSTEM_INIT", None)
+#devconSystemInitFile.setType("STRING")
+#devconSystemInitFile.setOutputName("core.LIST_SYSTEM_INIT_C_CONFIG_BITS_INITIALIZATION")
+#devconSystemInitFile.setSourcePath("arm/templates/DS60001527.c.ftl")
+#devconSystemInitFile.setMarkup(True)
--- arch/arm/templates/startup_xc32_SAM_E70_S70_V70_V71.c.ftl
+++ arch/arm/templates/startup_xc32_SAM_E70_S70_V70_V71.c.ftl
@@ -1,65 +1,65 @@
-/*
- *  Constants used for setting the GPNVM bits for TCM Size.
- */
+
 #define GPNVM_TCM_SIZE_Pos        7u
 #define GPNVM_TCM_SIZE_Msk        (0x3u << GPNVM_TCM_SIZE_Pos)
-#define GPNVM_TCM_SIZE_0K_Val     (0x0u << GPNVM_TCM_SIZE_Pos)
-#define GPNVM_TCM_SIZE_32K_Val    (0x1u << GPNVM_TCM_SIZE_Pos)
-#define GPNVM_TCM_SIZE_64K_Val    (0x2u << GPNVM_TCM_SIZE_Pos)
-#define GPNVM_TCM_SIZE_128K_Val   (0x3u << GPNVM_TCM_SIZE_Pos)
 
-/** \brief  TCM memory enable
+/** Program GPNVM fuse for TCM configuration */
+__STATIC_INLINE void TCM_Configure(uint32_t neededGpnvmValue)
+{
+    static uint32_t gpnvmReg;
+    static uint32_t cmd;
 
- The function enables TCM memories
- */
+    /* Read GPNVM fuse setting  */
+    EFC_REGS->EEFC_FCR = (EEFC_FCR_FKEY_PASSWD | EEFC_FCR_FCMD_GGPB);
+
+    while (!(EFC_REGS->EEFC_FSR & EEFC_FSR_FRDY_Msk))
+    {
+    }
+
+    gpnvmReg=EFC_REGS->EEFC_FRR;
+
+    /* Program only if change is needed */
+    if (((gpnvmReg & GPNVM_TCM_SIZE_Msk)>>GPNVM_TCM_SIZE_Pos) != neededGpnvmValue)
+    {
+        if(neededGpnvmValue & 0x2)
+            cmd=EEFC_FCR_FCMD_SGPB;
+        else
+            cmd=EEFC_FCR_FCMD_CGPB;
+
+        EFC_REGS->EEFC_FCR = (EEFC_FCR_FKEY_PASSWD | cmd | EEFC_FCR_FARG(8));
+        while (!(EFC_REGS->EEFC_FSR & EEFC_FSR_FRDY_Msk))
+        {
+        }
+
+        if(neededGpnvmValue & 0x1)
+            cmd=EEFC_FCR_FCMD_SGPB;
+        else
+            cmd=EEFC_FCR_FCMD_CGPB;
+
+        EFC_REGS->EEFC_FCR = (EEFC_FCR_FKEY_PASSWD | cmd | EEFC_FCR_FARG(7));
+        while (!(EFC_REGS->EEFC_FSR & EEFC_FSR_FRDY_Msk))
+        {
+        }
+
+        /* Reset the device for the programmed fuse value to take effect */
+        RSTC_REGS->RSTC_CR = RSTC_CR_KEY_PASSWD | RSTC_CR_PROCRST_Msk;
+    }
+}
+
+
+/** Enable TCM memory */
 __STATIC_INLINE void __attribute__((optimize("-O1"))) TCM_Enable(void)
 {
     __DSB();
     __ISB();
-    SCB->ITCMCR = (SCB_ITCMCR_EN_Msk  | SCB_ITCMCR_RMW_Msk
-                    | SCB_ITCMCR_RETEN_Msk);
-    SCB->DTCMCR = (SCB_DTCMCR_EN_Msk | SCB_DTCMCR_RMW_Msk
-                    | SCB_DTCMCR_RETEN_Msk);
+    SCB->ITCMCR = (SCB_ITCMCR_EN_Msk  | SCB_ITCMCR_RMW_Msk | SCB_ITCMCR_RETEN_Msk);
+    SCB->DTCMCR = (SCB_DTCMCR_EN_Msk | SCB_DTCMCR_RMW_Msk | SCB_DTCMCR_RETEN_Msk);
     __DSB();
     __ISB();
 }
 
-/** \brief  TCM memory Disable
-
- The function disables TCM memories
- */
+/* Disable TCM memory */
 __STATIC_INLINE void __attribute__((optimize("-O1"))) TCM_Disable(void)
 {
-    /* Define the __XC32_SKIP_STARTUP_GPNVM preprocessor macro when you do not
-     * want this code to modify the GPNVM bits at runtime.
-     * Define the __XC32_SKIP_STARTUP_GPNVM_WAIT preprocessor macro when you do
-     * not want the code to poll the FRDY bit before continuing.
-     */
-#if !defined(__XC32_SKIP_STARTUP_GPNVM)
-    static uint32_t gpnvm_value;
-    EFC_REGS->EEFC_FCR = (EEFC_FCR_FKEY_PASSWD | EEFC_FCR_FCMD_GGPB);
-#if !defined(__XC32_SKIP_STARTUP_GPNVM_WAIT)
-    while (!(EFC_REGS->EEFC_FSR & EEFC_FSR_FRDY_Msk));
-#endif
-    gpnvm_value=EFC_REGS->EEFC_FRR;
-
-    /* 0K size of ITCM and DTCM */
-    /* GPNVM bits[8:7] == b'00 */
-    if ((gpnvm_value & GPNVM_TCM_SIZE_Msk) != GPNVM_TCM_SIZE_0K_Val)
-    {
-            EFC_REGS->EEFC_FCR = (EEFC_FCR_FKEY_PASSWD | EEFC_FCR_FCMD_CGPB
-                            | EEFC_FCR_FARG(8));
-#if !defined(__XC32_SKIP_STARTUP_GPNVM_WAIT)
-            while (!(EFC_REGS->EEFC_FSR & EEFC_FSR_FRDY_Msk));
-#endif
-            EFC_REGS->EEFC_FCR = (EEFC_FCR_FKEY_PASSWD | EEFC_FCR_FCMD_CGPB
-                            | EEFC_FCR_FARG(7));
-#if !defined(__XC32_SKIP_STARTUP_GPNVM_WAIT)
-            while (!(EFC_REGS->EEFC_FSR & EEFC_FSR_FRDY_Msk));
-            RSTC_REGS->RSTC_CR = RSTC_CR_KEY_PASSWD | RSTC_CR_PROCRST_Msk;
-#endif
-    }
-#endif /* !defined(__XC32_SKIP_STARTUP_GPNVM) */
     __DSB();
     __ISB();
     SCB->ITCMCR &= ~(uint32_t)SCB_ITCMCR_EN_Msk;
@@ -68,72 +68,8 @@ __STATIC_INLINE void __attribute__((optimize("-O1"))) TCM_Disable(void)
     __ISB();
 }
 
-/** \brief  TCM memory configure size
 
- The function configures size of TCM memory based on __XC32_ITCM_LENGTH macro
- */
-__STATIC_INLINE void __attribute__((optimize("-O1"))) TCM_ConfigureSize(void)
-{
-    /* Define the __XC32_SKIP_STARTUP_GPNVM preprocessor macro when you do not
-     * want this code to modify the GPNVM bits at runtime.
-     * Define the __XC32_SKIP_STARTUP_GPNVM_WAIT preprocessor macro when you do
-     * not want the code to poll the FRDY bit before continuing.
-     */
-#if !defined(__XC32_SKIP_STARTUP_GPNVM)
-#if defined(__XC32_ITCM_LENGTH)
-    static uint32_t gpnvm_value;
-    EFC_REGS->EEFC_FCR = (EEFC_FCR_FKEY_PASSWD | EEFC_FCR_FCMD_GGPB);
-#if !defined(__XC32_SKIP_STARTUP_GPNVM_WAIT)
-    while (!(EFC_REGS->EEFC_FSR & EEFC_FSR_FRDY_Msk));
-#endif
-    gpnvm_value=EFC_REGS->EEFC_FRR;
 
-# if (__XC32_ITCM_LENGTH == 0x8000)
-    /* 32K default size of ITCM and DTCM */
-    /* GPNVM bits[8:7] == b'01 */
-    if ((gpnvm_value & GPNVM_TCM_SIZE_Msk) != GPNVM_TCM_SIZE_32K_Val)
-    {
-        EFC_REGS->EEFC_FCR = (EEFC_FCR_FKEY_PASSWD | EEFC_FCR_FCMD_CGPB | EEFC_FCR_FARG(8));
-#if !defined(__XC32_SKIP_STARTUP_GPNVM_WAIT)
-        while (!(EFC_REGS->EEFC_FSR & EEFC_FSR_FRDY_Msk));
-#endif
-        EFC_REGS->EEFC_FCR = (EEFC_FCR_FKEY_PASSWD | EEFC_FCR_FCMD_SGPB | EEFC_FCR_FARG(7));
-#if !defined(__XC32_SKIP_STARTUP_GPNVM_WAIT)
-        while (!(EFC_REGS->EEFC_FSR & EEFC_FSR_FRDY_Msk));
-        RSTC_REGS->RSTC_CR = RSTC_CR_KEY_PASSWD | RSTC_CR_PROCRST_Msk;
-#endif
-    }
-# elif (__XC32_ITCM_LENGTH == 0x10000)
-    /* 64K */
-    /* GPNVM bits[8:7] == b'10 */
-    if ((gpnvm_value & GPNVM_TCM_SIZE_Msk) != GPNVM_TCM_SIZE_64K_Val)
-    {
-        EFC_REGS->EEFC_FCR = (EEFC_FCR_FKEY_PASSWD | EEFC_FCR_FCMD_SGPB | EEFC_FCR_FARG(8));
-#if !defined(__XC32_SKIP_STARTUP_GPNVM_WAIT)
-        while (!(EFC_REGS->EEFC_FSR & EEFC_FSR_FRDY_Msk));
-#endif
-        EFC_REGS->EEFC_FCR = (EEFC_FCR_FKEY_PASSWD | EEFC_FCR_FCMD_CGPB | EEFC_FCR_FARG(7));
-#if !defined(__XC32_SKIP_STARTUP_GPNVM_WAIT)
-        while (!(EFC_REGS->EEFC_FSR & EEFC_FSR_FRDY_Msk));
-        RSTC_REGS->RSTC_CR = RSTC_CR_KEY_PASSWD | RSTC_CR_PROCRST_Msk;
-#endif
-    }
-# elif (__XC32_ITCM_LENGTH == 0x20000)
-    /* 128K */
-    /* GPNVM bits[8:7] == b'11 */
-    if ((gpnvm_value & GPNVM_TCM_SIZE_Msk) != GPNVM_TCM_SIZE_128K_Val)
-    {
-        EFC_REGS->EEFC_FCR = (EEFC_FCR_FKEY_PASSWD | EEFC_FCR_FCMD_SGPB | EEFC_FCR_FARG(8));
-#if !defined(__XC32_SKIP_STARTUP_GPNVM_WAIT)
-      while (!(EFC_REGS->EEFC_FSR & EEFC_FSR_FRDY_Msk));
-#endif
-        EFC_REGS->EEFC_FCR = (EEFC_FCR_FKEY_PASSWD | EEFC_FCR_FCMD_SGPB | EEFC_FCR_FARG(7));
-#if !defined(__XC32_SKIP_STARTUP_GPNVM_WAIT)
-        while (!(EFC_REGS->EEFC_FSR & EEFC_FSR_FRDY_Msk));
-        RSTC_REGS->RSTC_CR = RSTC_CR_KEY_PASSWD | RSTC_CR_PROCRST_Msk;
-#endif
-    }
-# endif /* __XC32_ITCM_LENGTH == value */
-#endif /* defined(__XC32_ITCM_LENGTH) */
-#endif /* !defined(__XC32_SKIP_STARTUP_GPNVM) */
-}
+
+
+
