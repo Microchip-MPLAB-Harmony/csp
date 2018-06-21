d2bb4db67b0e0b924b64fb37b3d89ca0bf77ef4d
Manjunath Hiremath <manjunath.hiremath@microchip.com>
2018-06-21 16:02:33 +0530

    [H3/RSTC/PLIB/SAME70]: Interrupt handler lock is made dynamic for RSTC module MH-10904

--- peripheral/rstc_11009/config/rstc.py
+++ peripheral/rstc_11009/config/rstc.py
@@ -5,6 +5,7 @@ from math import ceil
 ################################################################################
 global NVICVector
 global NVICHandler
+global NVICHandlerLock
 global instance
 global peripId
 
@@ -18,21 +19,24 @@ def calcExtResetAssertTime(symbol,event):
     numSlck=2**(event["value"]+1)
     time= float(numSlck)/32768
     timeUsInt=int(time*1e6)
-    timeMs=float(timeUsInt)/1000   
+    timeMs=float(timeUsInt)/1000
     symbol.setLabel("External Reset Assertion Time is: " + str(timeMs) + " ms ( "+str(numSlck)+" SLCK Cycles )")
 
 def NVICControl(symbol, event):
     symObj=event["symbol"]
-    
+
     Database.clearSymbolValue("core", NVICVector)
     Database.clearSymbolValue("core", NVICHandler)
-  
+    Database.clearSymbolValue("core", NVICHandlerLock)
+
     if (symObj.getSelectedKey() == "INTERRUPT"):
         Database.setSymbolValue("core", NVICVector, True, 2)
         Database.setSymbolValue("core", NVICHandler, "RSTC" + str(instance) +  "_InterruptHandler", 2)
+        Database.setSymbolValue("core", NVICHandlerLock, True, 2)
     else:
         Database.setSymbolValue("core", NVICVector, False, 2)
-        Database.setSymbolValue("core", NVICHandler, "RSTC" + str(instance) +  "_Handler", 2)   
+        Database.setSymbolValue("core", NVICHandler, "RSTC" + str(instance) +  "_Handler", 2)
+        Database.setSymbolValue("core", NVICHandlerLock, False, 2)
 
 ################################################################################
 #### Component ####
@@ -41,6 +45,7 @@ def instantiateComponent(rstcComponent):
 
     global NVICVector
     global NVICHandler
+    global NVICHandlerLock
     global instance
     global peripId
 
@@ -59,7 +64,7 @@ def instantiateComponent(rstcComponent):
     rstcSym_MR_UserReset.setOutputMode("Key")
     rstcSym_MR_UserReset.setDisplayMode("Description")
     rstcSym_MR_UserReset.setSelectedKey("User Reset",1)
-                                 
+
     rstcSym_MR_ERSTL = rstcComponent.createIntegerSymbol("RSTC_MR_ERSTL", None)
     rstcSym_MR_ERSTL.setLabel("External Reset Assertion duration on WDT/SW Reset")
     rstcSym_MR_ERSTL.setMin(0)
@@ -69,27 +74,27 @@ def instantiateComponent(rstcComponent):
     extResetAssertTimeMessage = rstcComponent.createCommentSymbol("EXT_RESET_LENGTH_MESSAGE", None)
     extResetAssertTimeMessage.setVisible(True)
     extResetAssertTimeMessage.setLabel("External Reset Assertion Time is: 0.061 ms ( 2 SLCK Cycles )")
-    extResetAssertTimeMessage.setDependencies(calcExtResetAssertTime, ["RSTC_MR_ERSTL"])    
+    extResetAssertTimeMessage.setDependencies(calcExtResetAssertTime, ["RSTC_MR_ERSTL"])
 
     rstcSym_MR_ERSTL.setDefaultValue(0)
 
-    
+
 
     ############################################################################
     #### Dependency ####
     ############################################################################
-    
+
     # Setup Peripheral Interrupt in Interrupt manager
     peripId = Interrupt.getInterruptIndex("RSTC")
     NVICVector = "NVIC_" + str(peripId) + "_ENABLE"
     NVICHandler = "NVIC_" + str(peripId) + "_HANDLER"
     NVICHandlerLock = "NVIC_" + str(peripId) + "_HANDLER_LOCK"
-      
+
     # NVIC Dynamic settings
     rstcNVICControl = rstcComponent.createBooleanSymbol("NVIC_RSTC_ENABLE", None)
     rstcNVICControl.setDependencies(NVICControl, ["RSTC_MR_URSTEN"])
     rstcNVICControl.setVisible(False)
-    
+
 ############################################################################
 #### Code Generation ####
 ############################################################################
@@ -101,10 +106,10 @@ def instantiateComponent(rstcComponent):
     rstcHeader1File.setDestPath("peripheral/rstc/")
     rstcHeader1File.setProjectPath("config/" + configName + "/peripheral/rstc/")
     rstcHeader1File.setType("HEADER")
-    rstcHeader1File.setOverwrite(True)  
-    
+    rstcHeader1File.setOverwrite(True)
+
     rstcHeader2File = rstcComponent.createFileSymbol("RSTC_HEADER2", None)
-    rstcHeader2File.setMarkup(True)    
+    rstcHeader2File.setMarkup(True)
     rstcHeader2File.setSourcePath("../peripheral/rstc_11009/templates/plib_rstc.h.ftl")
     rstcHeader2File.setOutputName("plib_rstc" + str(instance) + ".h")
     rstcHeader2File.setDestPath("peripheral/rstc/")
@@ -113,14 +118,14 @@ def instantiateComponent(rstcComponent):
     rstcHeader2File.setOverwrite(True)
 
     rstcSource1File = rstcComponent.createFileSymbol("RSTC_SOURCE1", None)
-    rstcSource1File.setMarkup(True)    
+    rstcSource1File.setMarkup(True)
     rstcSource1File.setSourcePath("../peripheral/rstc_11009/templates/plib_rstc.c.ftl")
     rstcSource1File.setOutputName("plib_rstc" + str(instance) + ".c")
     rstcSource1File.setDestPath("peripheral/rstc/")
     rstcSource1File.setProjectPath("config/" + configName + "/peripheral/rstc/")
     rstcSource1File.setType("SOURCE")
     rstcSource1File.setOverwrite(True)
-    
+
     rstcSystemInitFile = rstcComponent.createFileSymbol("RSTC_INIT", None)
     rstcSystemInitFile.setType("STRING")
     rstcSystemInitFile.setOutputName("core.LIST_SYSTEM_INIT_C_SYS_INITIALIZE_PERIPHERALS")
@@ -132,5 +137,4 @@ def instantiateComponent(rstcComponent):
     rstcSystemDefFile.setOutputName("core.LIST_SYSTEM_DEFINITIONS_H_INCLUDES")
     rstcSystemDefFile.setSourcePath("../peripheral/rstc_11009/templates/system/system_definitions.h.ftl")
     rstcSystemDefFile.setMarkup(True)
-    
-    
+
