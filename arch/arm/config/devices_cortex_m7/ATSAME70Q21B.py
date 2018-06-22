66c7ec4364984d58c23454234f70c0ab64df1d2d
Thibaut VIARD <thibaut.viard@microchip.com>
2018-06-22 10:40:58 +0200

    [arch] Renaming DS60001527 to SAM_E70_S70_V70_V71
    Signed-off-by: Thibaut VIARD <thibaut.viard@microchip.com>

--- arch/arm/config/devices_cortex_m7/ATSAME70Q21B.py
+++ arch/arm/config/devices_cortex_m7/ATSAME70Q21B.py
@@ -1,3 +1,6 @@
-# load family specific configuration
-execfile(Variables.get("__ARCH_DIR") + "/DS60001527.py")
+# load device specific configurations (fuses)
+devCfgComment = coreComponent.createCommentSymbol("CoreCfgComment1", devCfgMenu)
+devCfgComment.setLabel("Note: Set Device Configuration Bits via Programming Tool")
 
+# load family specific configuration
+execfile(Variables.get("__ARCH_DIR") + "/SAM_E70_S70_V70_V71.py")
--- arch/arm/config/devices_cortex_m7/ATSAMV70Q19B.py
+++ arch/arm/config/devices_cortex_m7/ATSAMV70Q19B.py
@@ -3,5 +3,4 @@ devCfgComment = coreComponent.createCommentSymbol("CoreCfgComment1", devCfgMenu)
 devCfgComment.setLabel("Note: Set Device Configuration Bits via Programming Tool")
 
 # load family specific configuration
-execfile(Variables.get("__ARCH_DIR") + "/DS60001527.py")
-
+execfile(Variables.get("__ARCH_DIR") + "/SAM_E70_S70_V70_V71.py")
