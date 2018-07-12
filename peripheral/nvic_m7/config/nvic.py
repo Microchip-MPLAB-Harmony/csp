edd3dd46f1e7817609b524cd91c4f07b20b6b69c
Settu <http://support.microchip.com>
2018-07-12 18:48:47 +0530

    [NVIC] Fixed initialisation value

--- peripheral/nvic_m7/config/nvic.py
+++ peripheral/nvic_m7/config/nvic.py
@@ -33,12 +33,12 @@ global vectorSettings
 vectorSettings = {"Reset"         : [True,   True,    False,    "-3",    True,    False,    True],
                 "NonMaskableInt"  : [True,   True,    False,    "-2",    True,    False,    True],
                 "HardFault"       : [True,   True,    False,    "-1",    True,    False,    True],
-                "MemoryManagement": [True,   True,    False,    "0",     False,   True,     True],
-                "BusFault"        : [True,   False,   False,    "0",     False,   True,     True],
-                "UsageFault"      : [True,   False,   False,    "0",     False,   True,     True],
-                "SVCall"          : [False,  True,    False,    "0",     False,   True,     True],
-                "DebugMonitor"    : [True,   True,    False,    "0",     False,   True,     True],
-                "PendSV"          : [False,  True,    False,    "0",     False,   True,     True],
+                "MemoryManagement": [False,  True,    False,    "0",     False,   True,     True],
+                "BusFault"        : [False,  False,   False,    "0",     False,   True,     True],
+                "UsageFault"      : [False,  False,   False,    "0",     False,   True,     True],
+                "SVCall"          : [True,   True,    False,    "0",     False,   True,     True],
+                "DebugMonitor"    : [False,  False,   False,    "0",     False,   True,     True],
+                "PendSV"          : [True,   True,    False,    "0",     False,   True,     True],
                 "SysTick"         : [False,  True,    False,    "0",     False,   True,     True],
                 "Peripheral"      : [False,  False,   True,     "7",     False,   True,     False]}
 
@@ -78,37 +78,6 @@ def getInterruptDescription(index):
                 else:
                     return interrupts[interrupt].getAttribute("caption")
 
-def configureVectorSettings(nvicSym, event):
-    global vectorSettings
-    global vector
-    settingsIndex = 0
-
-    symId = nvicSym.getID()
-    symId = ''.join([i for i in symId if not i.isdigit()])
-    # Following operation is needed for negative vectors
-    symId = symId.replace("-", "")
-
-    nvicSym.clearValue()
-
-    if symId == "NVIC__ENABLE":
-        settingsIndex = 0
-        if (event["value"] == "None"):
-            nvicSym.setVisible(False)
-    elif symId == "NVIC__ENABLE_LOCK":
-        settingsIndex = 1
-    elif symId == "NVIC__ENABLE_GENERATE":
-        settingsIndex = 2
-    elif symId == "NVIC__PRIORITY":
-        settingsIndex = 3
-    elif symId == "NVIC__PRIORITY_LOCK":
-        settingsIndex = 4
-    elif symId == "NVIC__PRIORITY_GENERATE":
-        settingsIndex = 5
-    elif symId == "NVIC__HANDLER_LOCK":
-        settingsIndex = 6
-
-    nvicSym.setValue(vectorSettings[vector][settingsIndex], 2)
-
 ################################################################################
 #### Component ####
 ################################################################################
@@ -137,76 +106,73 @@ for i in range(0,len(interrupts)):
     interrupt_index[int(i)] = interrupts[i].getAttribute("index")
 
 for index in interrupt_index:
-    nvicNumber = int(interrupt_index.get(index))
-    nvicVectorNumber.append(index)
-    nvicVectorNumber[index]= coreComponent.createIntegerSymbol("NVIC_" + str(nvicNumber), nvicMenu)
-    nvicVectorNumber[index].setLabel("Vector Number")
-    nvicVectorNumber[index].setDefaultValue(nvicNumber)
-    nvicVectorNumber[index].setVisible(False)
 
-    nvicVectorName.append(index)
-    nvicVectorName[index]= coreComponent.createStringSymbol("NVIC_" + str(nvicNumber) + "_VECTOR", nvicMenu)
-    nvicVectorName[index].setLabel("Vector Name")
-    nvicVectorName[index].setVisible(False)
-    vector = str(getInterruptName(nvicNumber))
-    # Default value is set later to trigger business logic for the first time
+    nvicNumber = int(interrupt_index.get(index))
+    nvicName=str(getInterruptName(nvicNumber))
+
+    vector = nvicName
+    if vector not in vectorSettings:
+        vector = "Peripheral"
 
     nvicVectorEnable.append(index)
     nvicVectorEnable[index] = coreComponent.createBooleanSymbol("NVIC_" + str(nvicNumber) + "_ENABLE", nvicMenu)
     nvicVectorEnable[index].setLabel("Enable " + str(getInterruptDescription(nvicNumber)) + " Interrupt")
-    nvicVectorEnable[index].setDependencies(configureVectorSettings, ["NVIC_" + str(nvicNumber) + "_VECTOR"])
-    nvicVectorEnable[index].setDefaultValue(False)
+    nvicVectorEnable[index].setDefaultValue(vectorSettings[vector][0])
+
+    nvicVectorNumber.append(index)
+    nvicVectorNumber[index]= coreComponent.createIntegerSymbol("NVIC_" + str(nvicNumber), nvicVectorEnable[index])
+    nvicVectorNumber[index].setLabel("Vector Number")
+    nvicVectorNumber[index].setDefaultValue(nvicNumber)
+    nvicVectorNumber[index].setVisible(True)
+
+    nvicVectorName.append(index)
+    nvicVectorName[index]= coreComponent.createStringSymbol("NVIC_" + str(nvicNumber) + "_VECTOR", nvicVectorEnable[index])
+    nvicVectorName[index].setLabel("Vector Name")
+    nvicVectorName[index].setVisible(True)
+    nvicVectorName[index].setDefaultValue(nvicName)
+
 
     nvicVectorEnableLock.append(index)
     nvicVectorEnableLock[index] = coreComponent.createBooleanSymbol("NVIC_" + str(nvicNumber) + "_ENABLE_LOCK", nvicVectorEnable[index])
     nvicVectorEnableLock[index].setLabel("Enable Lock")
-    nvicVectorEnableLock[index].setDependencies(configureVectorSettings, ["NVIC_" + str(nvicNumber) + "_VECTOR"])
-    nvicVectorEnableLock[index].setDefaultValue(False)
+    nvicVectorEnableLock[index].setDefaultValue(vectorSettings[vector][1])
     nvicVectorEnableLock[index].setVisible(False)
 
     nvicVectorEnableGenerate.append(index)
     nvicVectorEnableGenerate[index] = coreComponent.createBooleanSymbol("NVIC_" + str(nvicNumber) + "_ENABLE_GENERATE", nvicVectorEnable[index])
     nvicVectorEnableGenerate[index].setLabel("Enable Generate")
-    nvicVectorEnableGenerate[index].setDependencies(configureVectorSettings, ["NVIC_" + str(nvicNumber) + "_VECTOR"])
-    nvicVectorEnableGenerate[index].setDefaultValue(False)
+    nvicVectorEnableGenerate[index].setDefaultValue(vectorSettings[vector][2])
     nvicVectorEnableGenerate[index].setVisible(False)
 
     nvicVectorPriority.append(index)
     nvicVectorPriority[index] = coreComponent.createComboSymbol("NVIC_" + str(nvicNumber) + "_PRIORITY", nvicVectorEnable[index], nvicPriorityGroup)
     nvicVectorPriority[index].setLabel("Priority")
-    nvicVectorPriority[index].setDependencies(configureVectorSettings, ["NVIC_" + str(nvicNumber) + "_VECTOR"])
-    nvicVectorPriority[index].setDefaultValue("0")
+    nvicVectorPriority[index].setDefaultValue(vectorSettings[vector][3])
 
     nvicVectorPriorityLock.append(index)
     nvicVectorPriorityLock[index] = coreComponent.createBooleanSymbol("NVIC_" + str(nvicNumber) + "_PRIORITY_LOCK", nvicVectorEnable[index])
     nvicVectorPriorityLock[index].setLabel("Priority Lock")
-    nvicVectorPriorityLock[index].setDependencies(configureVectorSettings, ["NVIC_" + str(nvicNumber) + "_VECTOR"])
-    nvicVectorPriorityLock[index].setDefaultValue(False)
+    nvicVectorPriorityLock[index].setDefaultValue(vectorSettings[vector][4])
     nvicVectorPriorityLock[index].setVisible(False)
 
     nvicVectorPriorityGenerate.append(index)
     nvicVectorPriorityGenerate[index] = coreComponent.createBooleanSymbol("NVIC_" + str(nvicNumber) + "_PRIORITY_GENERATE", nvicVectorEnable[index])
     nvicVectorPriorityGenerate[index].setLabel("Priority Generate")
-    nvicVectorPriorityGenerate[index].setDependencies(configureVectorSettings, ["NVIC_" + str(nvicNumber) + "_VECTOR"])
-    nvicVectorPriorityGenerate[index].setDefaultValue(False)
+    nvicVectorPriorityGenerate[index].setDefaultValue(vectorSettings[vector][5])
     nvicVectorPriorityGenerate[index].setVisible(False)
 
     nvicVectorHandler.append(index)
     nvicVectorHandler[index] = coreComponent.createStringSymbol("NVIC_" + str(nvicNumber) + "_HANDLER", nvicVectorEnable[index])
     nvicVectorHandler[index].setLabel("Handler")
-    nvicVectorHandler[index].setDefaultValue(vector + "_Handler")
+    nvicVectorHandler[index].setDefaultValue(nvicName + "_Handler")
 
     nvicVectorHandlerLock.append(index)
     nvicVectorHandlerLock[index] = coreComponent.createBooleanSymbol("NVIC_" + str(nvicNumber) + "_HANDLER_LOCK", nvicVectorEnable[index])
     nvicVectorHandlerLock[index].setLabel("Handler Lock")
-    nvicVectorHandlerLock[index].setDependencies(configureVectorSettings, ["NVIC_" + str(nvicNumber) + "_VECTOR"])
-    nvicVectorHandlerLock[index].setDefaultValue(False)
+    nvicVectorHandlerLock[index].setDefaultValue(vectorSettings[vector][6])
     nvicVectorHandlerLock[index].setVisible(False)
 
-    if vector not in vectorSettings:
-        vector = "Peripheral"
 
-    nvicVectorName[index].setDefaultValue(str(getInterruptName(nvicNumber)))
 
 ############################################################################
 #### Code Generation ####
