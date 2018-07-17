a34b1335174258a8e1e110fd1b110c217d080e8c
Daniel <http://support.microchip.com>
2018-07-17 09:04:20 -0700

    [core] Add SAMA5D2 core support

---
+++ arch/arm/config/devices_cortex_a5/ATSAMA5D21.py
@@ -0,0 +1,1 @@
+execfile(Variables.get("__ARCH_DIR") + "/SAMA5D2.py")
---
+++ arch/arm/config/devices_cortex_a5/SAMA5D2.py
@@ -0,0 +1,57 @@
+print ("Loading System Services for " + Variables.get("__PROCESSOR"))
+
+deviceFamily = coreComponent.createStringSymbol("DeviceFamily", devCfgMenu)
+deviceFamily.setLabel("Device Family")
+deviceFamily.setDefaultValue("SAMA5D2")
+deviceFamily.setReadOnly(True)
+deviceFamily.setVisible(False)
+
+cortexMenu = coreComponent.createMenuSymbol("CORTEX_MENU", None)
+cortexMenu.setLabel("Cortex-A5 Configuration")
+cortexMenu.setDescription("Configuration for Cortex A5")
+
+# load clock manager information
+
+# load device specific pin manager information
+
+cacheMenu = coreComponent.createMenuSymbol("CACHE_MENU", cortexMenu)
+cacheMenu.setLabel("CACHE")
+cacheMenu.setDescription("CACHE Configuration")
+
+dcacheEnable = coreComponent.createBooleanSymbol("DATA_CACHE_ENABLE", cacheMenu)
+dcacheEnable.setLabel("Enable Data Cache")
+dcacheEnable.setDefaultValue(True)
+
+icacheEnable = coreComponent.createBooleanSymbol("INSTRUCTION_CACHE_ENABLE", cacheMenu)
+icacheEnable.setLabel("Enable Instruction Cache")
+icacheEnable.setDefaultValue(True)
+
+# load AIC
+
+# load PIT
+
+# load dma manager information
+#execfile(Variables.get("__CORE_DIR") + "/../peripheral/xdmac_11161/config/xdmac.py")
+#coreComponent.addPlugin("../peripheral/xdmac_11161/plugin/dmamanager.jar")
+
+# load wdt
+execfile(Variables.get("__CORE_DIR") + "/../peripheral/wdt_6080/config/wdt.py")
+
+armSysStartSourceFile = coreComponent.createFileSymbol("STARTUP_C", None)
+armSysStartSourceFile.setSourcePath("arm/templates/a5_cstartup.s.ftl")
+armSysStartSourceFile.setOutputName("cstartup.s")
+armSysStartSourceFile.setMarkup(True)
+armSysStartSourceFile.setOverwrite(True)
+armSysStartSourceFile.setDestPath("")
+armSysStartSourceFile.setProjectPath("config/" + configName + "/")
+armSysStartSourceFile.setType("SOURCE")
+
+#default exception handlers.
+faultSourceFile = coreComponent.createFileSymbol("DFLT_FAULT_HANDLER_C", None)
+faultSourceFile.setSourcePath("arm/templates/a5_vectortrap.s.ftl")
+faultSourceFile.setOutputName("vectortrap.s")
+faultSourceFile.setMarkup(True)
+faultSourceFile.setOverwrite(True)
+faultSourceFile.setDestPath("")
+faultSourceFile.setProjectPath("config/" + configName + "/")
+faultSourceFile.setType("SOURCE")
