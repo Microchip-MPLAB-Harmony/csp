2cfff068ab3704727d3144abbd196d18f3cb90f7
Settu <http://support.microchip.com>
2018-09-24 23:27:50 +0530

    [SYSTICK] Added comment to indicate RTOS is using SysTick MH-11344

--- peripheral/systick/config/systick.py
+++ peripheral/systick/config/systick.py
@@ -7,36 +7,32 @@ global  systickSystemDefFile
 global  systickSystemInitFile
 global  systickinterruptVector
 global  systickinterruptHandlerLock
+global  systickComment
 
 Log.writeInfoMessage("Loading SYSTICK for " + Variables.get("__PROCESSOR"))
 
+################################################################################
+#### Business Logic ####
+################################################################################
 
 def systickUse(systickEnable, osal):
     if osal["value"] == "BareMetal":
         systickEnable.setVisible(True)
+        systickComment.setVisible(False)
     else:
         systickEnable.setVisible(False)
+        systickComment.setVisible(True)
+
         systickHeaderFile.setEnabled(False)
         systickSourceFile.setEnabled(False)
         systickSystemDefFile.setEnabled(False)
         systickSystemInitFile.setEnabled(False)
 
-sysTickMenu = coreComponent.createMenuSymbol("SYSTICK_MENU", cortexMenu)
-sysTickMenu.setLabel("SysTick")
-
-systickEnable = coreComponent.createBooleanSymbol("systickEnable", sysTickMenu)
-systickEnable.setLabel("Enable SysTick")
-systickEnable.setDependencies(systickUse, ["HarmonyCore.SELECT_RTOS"])
-
-
-
-def showMenu(menu, show):
-    menu.setVisible(show["value"])
 
 def sysTickEnableCfgMenu(CfgMenu, event):
     CfgMenu.setVisible(event["value"])
 
-    if(event["value"] == True):
+    if event["value"] == True:
         systickHeaderFile.setEnabled(True)
         systickSourceFile.setEnabled(True)
         systickSystemDefFile.setEnabled(True)
@@ -51,23 +47,89 @@ def sysTickinterruptControl(symbol, event):
     Database.clearSymbolValue("core", systickinterruptVector)
     Database.clearSymbolValue("core", systickinterruptHandlerLock)
 
-    if (event["value"] == True):
+    if event["value"] == True:
         Database.setSymbolValue("core", systickinterruptVector, True, 2)
         Database.setSymbolValue("core", systickinterruptHandlerLock, True, 2)
     else:
         Database.setSymbolValue("core", systickinterruptVector, False, 2)
         Database.setSymbolValue("core", systickinterruptHandlerLock, False, 2)
 
-systickMenu = coreComponent.createMenuSymbol("SYSTICK_MENU_0", systickEnable)
-systickMenu.setLabel("SysTick Configuration")
-systickMenu.setDependencies(showMenu, ["systickEnable"])
-systickMenu.setVisible(False)
-systickMenu.setDependencies(sysTickEnableCfgMenu, ["systickEnable"])
+def sysTickMax(systick, event):
+    clock = 0
+    freq_ext = 0
+    freq_proc = 0
+    max = 0
 
-systickInterrupt = coreComponent.createBooleanSymbol("USE_SYSTICK_INTERRUPT", systickMenu)
+    if Database.getSymbolValue("core", "SYSTICK_EXTERNAL_CLOCK"):
+        freq_ext = Database.getSymbolValue("core", "SYSTICK")
+        clock = Database.getSymbolValue("core", "SYSTICK_CLOCK")
+    else:
+        clock = 1
+
+    freq_proc = Database.getSymbolValue("core", "CPU_CLOCK_FREQUENCY")
+
+    systickCVRNode = ATDF.getNode('/avr-tools-device-file/modules/module@[name="SysTick"]/register-group@[name="SysTick"]/register@[name="CVR"]/bitfield@[name="CURRENT"]')
+    currentValueMask = str(systickCVRNode.getAttribute("mask"))
+
+    if clock == 0:
+        if freq_ext != 0 and freq_ext != None:
+            max = ((float(1) / int(freq_ext)) * int(currentValueMask, 0) * 1000)
+    else:
+        if freq_proc != 0 and freq_proc != None:
+            max = ((float(1) / int(freq_proc)) * int(currentValueMask, 0) * 1000)
+
+    systick.setMax(float(max))
+
+def systickCal(symbol, event):
+    clock = 0
+    freq_ext = 0
+    freq_proc = 0
+    value = 0
+
+    if Database.getSymbolValue("core","SYSTICK_EXTERNAL_CLOCK"):
+        clock = Database.getSymbolValue("core", "SYSTICK_CLOCK")
+        freq_ext = Database.getSymbolValue("core", "SYSTICK")
+    else:
+        clock = 1
+
+    period = Database.getSymbolValue("core", "SYSTICK_PERIOD_MS")
+    freq_proc = Database.getSymbolValue("core", "CPU_CLOCK_FREQUENCY")
+
+    if clock == 0:
+        if freq_ext != 0 and freq_ext != None:
+            value = int((float(freq_ext) / 1000) * period)
+    else:
+        if freq_proc != 0 and freq_proc != None:
+            value = int((float(freq_proc) / 1000) * period)
+
+    symbol.setValue(str(hex(value)),2)
+    Database.setSymbolValue("core","SYSTICK_PERIOD_US", int(period * 1000), 2)
+
+################################################################################
+#### Menu ####
+################################################################################
+
+sysTickMenu = coreComponent.createMenuSymbol("SYSTICK_MENU", cortexMenu)
+sysTickMenu.setLabel("SysTick")
+
+# Interrupt Warning status
+systickComment = coreComponent.createCommentSymbol("SYSTICK_COMMENT", sysTickMenu)
+systickComment.setVisible(False)
+systickComment.setLabel("RTOS is using SysTick for Timebase")
+
+systickEnable = coreComponent.createBooleanSymbol("systickEnable", sysTickMenu)
+systickEnable.setLabel("Enable SysTick")
+systickEnable.setDependencies(systickUse, ["HarmonyCore.SELECT_RTOS"])
+
+systickConfigMenu = coreComponent.createMenuSymbol("SYSTICK_MENU_0", systickEnable)
+systickConfigMenu.setLabel("SysTick Configuration")
+systickConfigMenu.setVisible(False)
+systickConfigMenu.setDependencies(sysTickEnableCfgMenu, ["systickEnable"])
+
+systickInterrupt = coreComponent.createBooleanSymbol("USE_SYSTICK_INTERRUPT", systickConfigMenu)
 systickInterrupt.setLabel("Enable Interrupt")
 
-systickClock = coreComponent.createKeyValueSetSymbol("SYSTICK_CLOCK", systickMenu)
+systickClock = coreComponent.createKeyValueSetSymbol("SYSTICK_CLOCK", systickConfigMenu)
 systickClock.setLabel("SysTick Clock")
 systickClock.setOutputMode("Value")
 systickClock.setDisplayMode("Description")
@@ -75,49 +137,8 @@ if Database.getSymbolValue("core","SYSTICK_EXTERNAL_CLOCK"):
     systickClock.addKey("HCLK/2", str(0) , "SysTick External clock (HCLK/2)" )
 systickClock.addKey("HCLK", str(1) , "Processor clock (HCLK)" )
 
-def sysTickMax(systick, event):
-	clock = 0
-	freq_ext = 0
-	freq_proc = 0
-	max = 0
-	if Database.getSymbolValue("core","SYSTICK_EXTERNAL_CLOCK"):
-		freq_ext = Database.getSymbolValue("core", "SYSTICK")
-		clock = Database.getSymbolValue("core", "SYSTICK_CLOCK")
-	else:
-		clock = 1
-	freq_proc = Database.getSymbolValue("core", "CPU_CLOCK_FREQUENCY")
-
-	if clock == 0:
-		if (int(freq_ext) != 0):
-			max = ((float(1) / int(freq_ext)) * 16777215 * 1000)
-	else:
-		if (int(freq_proc) !=0):
-			max = ((float(1) / int(freq_proc)) * 16777215 * 1000)
-	systick.setMax(float(max))
-	
-
-
-def systickCal(symbol, event):	
-	if Database.getSymbolValue("core","SYSTICK_EXTERNAL_CLOCK"):
-		clock = Database.getSymbolValue("core", "SYSTICK_CLOCK")
-		freq_ext = Database.getSymbolValue("core", "SYSTICK")
-	else:
-		clock = 1
-	period = Database.getSymbolValue("core", "SYSTICK_PERIOD_MS")
-	freq_proc = Database.getSymbolValue("core", "CPU_CLOCK_FREQUENCY")
-	
-	if clock == 0:
-		if (int(freq_ext) != 0):
-			value = int((float(freq_ext) / 1000) * period)
-	else:
-		if (int(freq_proc) !=0):
-			value = int((float(freq_proc) / 1000) * period)
-			
-	symbol.setValue(str(hex(value)),2)
-	Database.setSymbolValue("core","SYSTICK_PERIOD_US", int(period * 1000), 2)
-	
-systickPeriodMS = coreComponent.createFloatSymbol("SYSTICK_PERIOD_MS", systickMenu)
-systickPeriodMS.setLabel("Systick Period(Milli sec)")
+systickPeriodMS = coreComponent.createFloatSymbol("SYSTICK_PERIOD_MS", systickConfigMenu)
+systickPeriodMS.setLabel("Systick Period(Milliseconds)")
 systickPeriodMS.setVisible(True)
 systickPeriodMS.setDefaultValue(float(1.0))
 systickPeriodMS.setMin(0)
@@ -125,24 +146,23 @@ systickPeriodMS.setDependencies(sysTickMax, ["core.CPU_CLOCK_FREQUENCY", "SYSTIC
 systickClock.setDefaultValue(int(Database.getSymbolValue("core","SYSTICK_EXTERNAL_CLOCK")))
 
 systickDefault = int(Database.getSymbolValue("core", "CPU_CLOCK_FREQUENCY")) / 1000
-systickPeriod = coreComponent.createStringSymbol("SYSTICK_PERIOD", systickMenu)
+systickPeriod = coreComponent.createStringSymbol("SYSTICK_PERIOD", systickConfigMenu)
 systickPeriod.setLabel("SysTick Period")
 systickPeriod.setVisible(False)
 systickPeriod.setDefaultValue(str(hex(systickDefault)))
 systickPeriod.setDependencies(systickCal, ["SYSTICK_PERIOD_MS", "SYSTICK_CLOCK", "core.CPU_CLOCK_FREQUENCY", "core.SYSTICK_CLOCK_FREQUENCY"])
 
-systickPeriodUS = coreComponent.createIntegerSymbol("SYSTICK_PERIOD_US", systickMenu)
+systickPeriodUS = coreComponent.createIntegerSymbol("SYSTICK_PERIOD_US", systickConfigMenu)
 systickPeriodUS.setVisible(False)
 systickPeriodUS.setDefaultValue(1000)
 systickPeriodUS.setMin(0)
-
 
 # Setup Peripheral Interrupt in Interrupt manager
 systickinterruptVector = "SysTick_INTERRUPT_ENABLE"
 systickinterruptHandlerLock = "SysTick_INTERRUPT_HANDLER_LOCK"
 
 # NVIC Dynamic settings
-SYSTICK_interruptControl = coreComponent.createBooleanSymbol("NVIC_SYSTICK_ENABLE", systickMenu)
+SYSTICK_interruptControl = coreComponent.createBooleanSymbol("NVIC_SYSTICK_ENABLE", systickConfigMenu)
 SYSTICK_interruptControl.setDependencies(sysTickinterruptControl, ["USE_SYSTICK_INTERRUPT"])
 SYSTICK_interruptControl.setVisible(False)
 
