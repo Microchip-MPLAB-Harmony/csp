19e6056fea45c75722789507b6fb720ad27d8386
Prachi <http://support.microchip.com>
2018-09-28 13:54:34 +0530

    [AFEC] Regenerated and tested afec_polled_mode application MH-11382

---
+++ apps/afec/afec_polled_mode/firmware/src/config/sam_e70_xult/peripheral/afec/plib_afec1.h
@@ -0,0 +1,127 @@
+/*******************************************************************************
+  AFEC Peripheral Library Interface Header File
+
+  Company
+    Microchip Technology Inc.
+
+  File Name
+    plib_afec1.h
+
+  Summary
+    AFEC1 peripheral library interface.
+
+  Description
+    This file defines the interface to the AFEC peripheral library.  This
+    library provides access to and control of the associated peripheral
+    instance.
+
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+Copyright (c) 2017 released Microchip Technology Inc.  All rights reserved.
+
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+#ifndef PLIB_AFEC1_H    // Guards against multiple inclusion
+#define PLIB_AFEC1_H
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+/*  This section lists the other files that are included in this file.
+*/
+
+#include "plib_afec_common.h"
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+extern "C" {
+
+#endif
+
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Data Types
+// *****************************************************************************
+// *****************************************************************************
+/*  The following data type definitions are used by the functions in this
+    interface and should be considered part it.
+*/
+#define AFEC1_CH0_MAX_OUTPUT (65535U)
+#define AFEC1_CH0_MIN_OUTPUT (0U)
+#define CHANNEL_0 (0U)
+/***********************************************************************/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Interface Routines
+// *****************************************************************************
+// *****************************************************************************
+/* The following functions make up the methods (set of possible operations) of
+   this interface.
+*/
+
+void AFEC1_Initialize (void);
+
+void AFEC1_ChannelsEnable (AFEC_CHANNEL_MASK channelsMask);
+
+void AFEC1_ChannelsDisable (AFEC_CHANNEL_MASK channelsMask);
+
+void AFEC1_ChannelsInterruptEnable (AFEC_INTERRUPT_MASK channelsInterruptMask);
+
+void AFEC1_ChannelsInterruptDisable (AFEC_INTERRUPT_MASK channelsInterruptMask);
+
+void AFEC1_ConversionStart(void);
+
+bool AFEC1_ChannelResultIsReady(AFEC_CHANNEL_NUM channel);
+
+uint16_t AFEC1_ChannelResultGet(AFEC_CHANNEL_NUM channel);
+
+void AFEC1_ConversionSequenceSet(AFEC_CHANNEL_NUM *channelList, uint8_t numChannel);
+
+void AFEC1_ChannelGainSet(AFEC_CHANNEL_NUM channel, AFEC_CHANNEL_GAIN gain);
+
+void AFEC1_ChannelOffsetSet(AFEC_CHANNEL_NUM channel, uint16_t offset);
+
+// *****************************************************************************
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+}
+
+#endif
+// DOM-IGNORE-END
+
+#endif //PLIB_AFEC1_H
+
+/**
+ End of File
+*/
+
---
+++ apps/afec/afec_polled_mode/firmware/src/config/sam_e70_xult/peripheral/pio/plib_pio.c
@@ -0,0 +1,412 @@
+/*******************************************************************************
+  SYS PORTS Static Functions for PORTS System Service
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    plib_pio.c
+
+  Summary:
+    PIO function implementations for the PIO PLIB.
+
+  Description:
+    The PIO PLIB provides a simple interface to manage peripheral
+    input-output controller.
+
+*******************************************************************************/
+
+//DOM-IGNORE-BEGIN
+/*******************************************************************************
+Copyright (c) 2018 released Microchip Technology Inc.  All rights reserved.
+
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED AS IS WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+*******************************************************************************/
+//DOM-IGNORE-END
+
+#include "plib_pio.h"
+
+
+#define PIO_MAX_NUM_OF_CHANNELS     5
+
+
+/* Array to store callback objects of each configured interrupt */
+PIO_PIN_CALLBACK_OBJ portPinCbObj[2 + 0 + 0 + 0 + 0];
+
+/* Array to store number of interrupts in each PORT Channel + previous interrupt count */
+uint8_t portNumCb[PIO_MAX_NUM_OF_CHANNELS + 1] = {0, 2, 2, 2, 2, 2};
+
+/******************************************************************************
+  Function:
+    PIO_Initialize ( void )
+
+  Summary:
+    Initialize the PIO library.
+
+  Remarks:
+    See plib_pio.h for more details.
+*/
+void PIO_Initialize ( void )
+{
+    /* Selected System IO pins are configured as GPIO */
+    MATRIX_REGS->CCFG_SYSIO |= 0x10;
+
+    /************************ PIO A Initialization ************************/
+    /* PORTA PIO Disable and Peripheral Enable*/
+    ((pio_registers_t*)PIO_PORT_A)->PIO_PDR = 0x200000;
+    /* PORTA Pull Up Enable/Disable as per MHC selection */
+    ((pio_registers_t*)PIO_PORT_A)->PIO_PUDR = ~0x800;
+    ((pio_registers_t*)PIO_PORT_A)->PIO_PUER = 0x800;
+    /* PORTA Pull Down Enable/Disable as per MHC selection */
+    ((pio_registers_t*)PIO_PORT_A)->PIO_PPDDR = 0xFFFFFFFF;
+    /* PORTA Output Write Enable */
+    ((pio_registers_t*)PIO_PORT_A)->PIO_OWER = PIO_OWER_Msk;
+    /* PORTA Additional interrupt mode Enable */
+    ((pio_registers_t*)PIO_PORT_A)->PIO_AIMER = 0x800;
+    /* PORTA Rising Edge or High Level Interrupt Enable */
+    ((pio_registers_t*)PIO_PORT_A)->PIO_REHLSR = 0x800;
+    /************************ PIO B Initialization ************************/
+    /* PORTB Peripheral Function Selection */
+    ((pio_registers_t*)PIO_PORT_B)->PIO_ABCDSR[0]= 0x10;
+    ((pio_registers_t*)PIO_PORT_B)->PIO_ABCDSR[1]= 0x10;
+    /* PORTB PIO Disable and Peripheral Enable*/
+    ((pio_registers_t*)PIO_PORT_B)->PIO_PDR = 0x10;
+    /* PORTB Pull Up Enable/Disable as per MHC selection */
+    ((pio_registers_t*)PIO_PORT_B)->PIO_PUDR = 0xFFFFFFFF;
+    /* PORTB Pull Down Enable/Disable as per MHC selection */
+    ((pio_registers_t*)PIO_PORT_B)->PIO_PPDDR = 0xFFFFFFFF;
+    /* PORTB Output Write Enable */
+    ((pio_registers_t*)PIO_PORT_B)->PIO_OWER = PIO_OWER_Msk;
+    /************************ PIO C Initialization ************************/
+    /* PORTC Pull Up Enable/Disable as per MHC selection */
+    ((pio_registers_t*)PIO_PORT_C)->PIO_PUDR = 0xFFFFFFFF;
+    /* PORTC Pull Down Enable/Disable as per MHC selection */
+    ((pio_registers_t*)PIO_PORT_C)->PIO_PPDDR = 0xFFFFFFFF;
+    /* PORTC Output Write Enable */
+    ((pio_registers_t*)PIO_PORT_C)->PIO_OWER = PIO_OWER_Msk;
+    /************************ PIO D Initialization ************************/
+    /* PORTD Pull Up Enable/Disable as per MHC selection */
+    ((pio_registers_t*)PIO_PORT_D)->PIO_PUDR = 0xFFFFFFFF;
+    /* PORTD Pull Down Enable/Disable as per MHC selection */
+    ((pio_registers_t*)PIO_PORT_D)->PIO_PPDDR = 0xFFFFFFFF;
+    /* PORTD Output Write Enable */
+    ((pio_registers_t*)PIO_PORT_D)->PIO_OWER = PIO_OWER_Msk;
+    /************************ PIO E Initialization ************************/
+    /* PORTE Pull Up Enable/Disable as per MHC selection */
+    ((pio_registers_t*)PIO_PORT_E)->PIO_PUDR = 0xFFFFFFFF;
+    /* PORTE Pull Down Enable/Disable as per MHC selection */
+    ((pio_registers_t*)PIO_PORT_E)->PIO_PPDDR = 0xFFFFFFFF;
+    /* PORTE Output Write Enable */
+    ((pio_registers_t*)PIO_PORT_E)->PIO_OWER = PIO_OWER_Msk;
+
+    uint32_t i;
+    /* Initialize Interrupt Pin data structures */
+    portPinCbObj[0].pin = PIO_PIN_PA11;
+    
+    for(i=0; i<2; i++)
+    {
+        portPinCbObj[i].callback = NULL;
+    }
+
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: PIO APIs which operates on multiple pins of a port
+// *****************************************************************************
+// *****************************************************************************
+
+// *****************************************************************************
+/* Function:
+    uint32_t PIO_PortRead ( PIO_PORT port )
+
+  Summary:
+    Read all the I/O lines of the selected port.
+
+  Description:
+    This function reads the live data values on all the I/O lines of the
+    selected port.  Bit values returned in each position indicate corresponding
+    pin levels.
+    1 = Pin is high.
+    0 = Pin is low.
+
+    This function reads the value regardless of pin configuration, whether it is
+    set as as an input, driven by the PIO Controller, or driven by a peripheral.
+
+  Remarks:
+    If the port has less than 32-bits, unimplemented pins will read as
+    low (0).
+    Implemented pins are Right aligned in the 32-bit return value.
+*/
+uint32_t PIO_PortRead(PIO_PORT port)
+{
+    return ((pio_registers_t*)port)->PIO_PDSR;
+}
+
+// *****************************************************************************
+/* Function:
+    void PIO_PortWrite (PIO_PORT port, uint32_t mask, uint32_t value);
+
+  Summary:
+    Write the value on the masked I/O lines of the selected port.
+
+  Remarks:
+    See plib_pio.h for more details.
+*/
+void PIO_PortWrite(PIO_PORT port, uint32_t mask, uint32_t value)
+{
+    ((pio_registers_t*)port)->PIO_ODSR = (((pio_registers_t*)port)->PIO_ODSR & (~mask)) | (mask & value);
+}
+
+// *****************************************************************************
+/* Function:
+    uint32_t PIO_PortLatchRead ( PIO_PORT port )
+
+  Summary:
+    Read the latched value on all the I/O lines of the selected port.
+
+  Remarks:
+    See plib_pio.h for more details.
+*/
+uint32_t PIO_PortLatchRead(PIO_PORT port)
+{
+    return ((pio_registers_t*)port)->PIO_ODSR;
+}
+
+// *****************************************************************************
+/* Function:
+    void PIO_PortSet ( PIO_PORT port, uint32_t mask )
+
+  Summary:
+    Set the selected IO pins of a port.
+
+  Remarks:
+    See plib_pio.h for more details.
+*/
+void PIO_PortSet(PIO_PORT port, uint32_t mask)
+{
+    ((pio_registers_t*)port)->PIO_SODR = mask;
+}
+
+// *****************************************************************************
+/* Function:
+    void PIO_PortClear ( PIO_PORT port, uint32_t mask )
+
+  Summary:
+    Clear the selected IO pins of a port.
+
+  Remarks:
+    See plib_pio.h for more details.
+*/
+void PIO_PortClear(PIO_PORT port, uint32_t mask)
+{
+    ((pio_registers_t*)port)->PIO_CODR = mask;
+}
+
+// *****************************************************************************
+/* Function:
+    void PIO_PortToggle ( PIO_PORT port, uint32_t mask )
+
+  Summary:
+    Toggles the selected IO pins of a port.
+
+  Remarks:
+    See plib_pio.h for more details.
+*/
+void PIO_PortToggle(PIO_PORT port, uint32_t mask)
+{
+    /* Write into Clr and Set registers */
+    ((pio_registers_t*)port)->PIO_ODSR^= mask;
+}
+
+// *****************************************************************************
+/* Function:
+    void PIO_PortInputEnable ( PIO_PORT port, uint32_t mask )
+
+  Summary:
+    Enables selected IO pins of a port as input.
+
+  Remarks:
+    See plib_pio.h for more details.
+*/
+void PIO_PortInputEnable(PIO_PORT port, uint32_t mask)
+{
+    ((pio_registers_t*)port)->PIO_ODR = mask;
+}
+
+// *****************************************************************************
+/* Function:
+    void PIO_PortOutputEnable ( PIO_PORT port, uint32_t mask )
+
+  Summary:
+    Enables selected IO pins of a port as output(s).
+
+  Remarks:
+    See plib_pio.h for more details.
+*/
+void PIO_PortOutputEnable(PIO_PORT port, uint32_t mask)
+{
+    ((pio_registers_t*)port)->PIO_OER = mask;
+}
+
+// *****************************************************************************
+/* Function:
+    void PIO_PortInterruptEnable(PIO_PORT port, uint32_t mask)
+
+  Summary:
+    Enables IO interrupt on selected IO pins of a port.
+
+  Remarks:
+    See plib_pio.h for more details.
+*/
+void PIO_PortInterruptEnable(PIO_PORT port, uint32_t mask)
+{
+    ((pio_registers_t*)port)->PIO_IER = mask;
+}
+
+// *****************************************************************************
+/* Function:
+    void PIO_PortInterruptDisable(PIO_PORT port, uint32_t mask)
+
+  Summary:
+    Disables IO interrupt on selected IO pins of a port.
+
+  Remarks:
+    See plib_pio.h for more details.
+*/
+void PIO_PortInterruptDisable(PIO_PORT port, uint32_t mask)
+{
+    ((pio_registers_t*)port)->PIO_IDR = mask;
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: PIO APIs which operates on one pin at a time
+// *****************************************************************************
+// *****************************************************************************
+
+// *****************************************************************************
+/* Function:
+    void PIO_PinInterruptCallbackRegister(
+        PIO_PIN pin,
+        const PIO_PIN_CALLBACK callback,
+        uintptr_t context
+    );
+
+  Summary:
+    Allows application to register callback for every pin.
+
+  Remarks:
+    See plib_pio.h for more details.
+*/
+void PIO_PinInterruptCallbackRegister(
+    PIO_PIN pin,
+    const PIO_PIN_CALLBACK callback,
+    uintptr_t context
+)
+{
+    uint8_t i;
+    uint8_t portIndex;
+
+    portIndex = pin >> 5;
+
+    for(i = portNumCb[portIndex]; i < portNumCb[portIndex +1]; i++)
+    {
+        if (portPinCbObj[i].pin == pin)
+        {
+            portPinCbObj[i].callback = callback;
+            portPinCbObj[i].context  = context;
+            break;
+        }
+    }
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Interrupt Service Routine (ISR) Implementation(s)
+// *****************************************************************************
+// *****************************************************************************
+
+// *****************************************************************************
+/* Function:
+    void PIOA_InterruptHandler (void)
+
+  Summary:
+    Interrupt handler for PORTA.
+
+  Description:
+    This function defines the Interrupt service routine for PORTA.
+    This is the function which by default gets into Interrupt Vector Table.
+
+  Remarks:
+    User should not call this function.
+*/
+void PIOA_InterruptHandler(void)
+{
+    /* Local PIO Interrupt Handler */
+    _PIO_Interrupt_Handler(PIO_PORT_A);
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Local Function Implementation
+// *****************************************************************************
+// *****************************************************************************
+
+// *****************************************************************************
+/* Function:
+    void _PIO_Interrupt_Handler ( PIO_PORT port )
+
+  Summary:
+    Interrupt handler for a selected port.
+
+  Description:
+    This function defines the Interrupt handler for a selected port.
+
+  Remarks:
+	It is an internal function used by the library, user need not call it.
+*/
+void _PIO_Interrupt_Handler ( PIO_PORT port )
+{
+    uint32_t status;
+    uint32_t i, portIndex;
+
+    status  = ((pio_registers_t*)port)->PIO_ISR;
+    status &= ((pio_registers_t*)port)->PIO_IMR;
+
+    /* get the index of the port channel: PIO_PORT_A--> 0, PIO_PORT_B--> 1 ... */
+    portIndex = (port - PIOA_BASE_ADDRESS) >> 9;
+
+    /* Check pending events and call callback if registered */
+    for(i = portNumCb[portIndex]; i < portNumCb[portIndex +1]; i++)
+    {
+        if((status & (1 << (portPinCbObj[i].pin & 0x1F))) && (portPinCbObj[i].callback != NULL))
+        {
+            portPinCbObj[i].callback (portPinCbObj[i].pin, portPinCbObj[i].context);
+        }
+    }
+
+}
+
+
+
+/*******************************************************************************
+ End of File
+*/
