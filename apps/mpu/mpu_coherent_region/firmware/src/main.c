c22467b4f12ce0d3a63ad2f649dcec5e0a19a150
Amit Raddi <amit.raddi@microchip.com>
2018-10-01 12:30:26 +0530

    [MPU] Regenerated and tested mpu_coherent_region application
    MH-11395
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

---
+++ apps/mpu/mpu_coherent_region/firmware/src/main.c
@@ -0,0 +1,168 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+Copyright (c) 2017 released Microchip Technology Inc.  All rights reserved.
+
+//Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+#include "string.h"
+
+#define LED_ON                      LED_Clear
+#define LED_OFF                     LED_Set
+#define LED_TOGGLE                  LED_Toggle
+
+#define USART1_TRANSMIT_CHANNEL     XDMAC_CHANNEL_0
+#define USART1_RECEIVE_CHANNEL      XDMAC_CHANNEL_1
+
+#define USART1_TRANSMIT_ADDRESS     (&USART1_REGS->US_THR)
+#define USART1_RECEIVE_ADDRESS      (&USART1_REGS->US_RHR)
+
+#define WRITE_SIZE                  400
+#define READ_SIZE                   10
+
+/* Keeping buffers in the non-cacheable memory region or coherent memory region.
+ * The memory section is created by linker script and MPU region is defined to
+ * set it as non-cacheable. Since this is user defined section and these are
+ * global variables, initialization has to be done later. */
+char __attribute__  ((space(data), section (".ram_nocache"))) writeBuffer[WRITE_SIZE] = {};
+char __attribute__  ((space(data), section (".ram_nocache"))) readBuffer[READ_SIZE] = {};
+char __attribute__  ((space(data), section (".ram_nocache"))) echoBuffer[2*READ_SIZE] = {};
+
+/* Application Status Data */
+char failureMessage[] = "\r\n**** Data transfer error ****\r\n";
+bool errorStatus = false;
+bool writeStatus = false;
+bool readStatus = false;
+
+void XDMAC_Callback(XDMAC_TRANSFER_EVENT status, uintptr_t context)
+{
+    if(status == XDMAC_TRANSFER_COMPLETE)
+    {
+        if(context == 0)
+        {
+            writeStatus = true;
+        }
+        else if(context == 1)
+        {
+            readStatus = true;
+        }
+    }
+    else
+    {
+        errorStatus = true;
+    }
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+    LED_OFF();
+
+   /* Initialize the write buffer */
+   strcpy(writeBuffer, "**** MPU Example to Create Coherent or Non-Cacheable \
+Memory Region****\r\n**** Demo uses coherent or no-cache memory region to \
+handle cache coherency ****\r\n**** Type a buffer of 10 characters and observe \
+it echo back ****\r\n**** LED toggles on each time buffer is echoed ****\r\n");
+
+    /* Register callback functions for both write and read contexts */
+    XDMAC_ChannelCallbackRegister(USART1_TRANSMIT_CHANNEL, XDMAC_Callback, 0);
+    XDMAC_ChannelCallbackRegister(USART1_RECEIVE_CHANNEL, XDMAC_Callback, 1);
+
+    XDMAC_ChannelTransfer(USART1_TRANSMIT_CHANNEL, &writeBuffer, (const void *)USART1_TRANSMIT_ADDRESS, sizeof(writeBuffer));
+
+    while ( true )
+    {
+        if(errorStatus == true)
+        {
+            /* Send error message to console.
+             * Using USART directly to since DMA is in error condition */
+            errorStatus = false;
+            USART1_Write(&failureMessage, sizeof(failureMessage));
+        }
+        else if(readStatus == true)
+        {
+            /* Echo back received buffer and Toggle LED */
+            readStatus = false;
+
+            strcpy(echoBuffer, readBuffer);
+            strcat(echoBuffer, "\r\n");
+
+            XDMAC_ChannelTransfer(USART1_TRANSMIT_CHANNEL, &echoBuffer, (const void *)USART1_TRANSMIT_ADDRESS, READ_SIZE+strlen("\r\n"));
+            LED_TOGGLE();
+        }
+        else if(writeStatus == true)
+        {
+            /* Submit buffer to read user data */
+            writeStatus = false;
+            XDMAC_ChannelTransfer(USART1_RECEIVE_CHANNEL, (const void *)USART1_RECEIVE_ADDRESS, &readBuffer, READ_SIZE);
+        }
+        else
+        {
+            /* Repeat the loop */
+            ;
+        }
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+
