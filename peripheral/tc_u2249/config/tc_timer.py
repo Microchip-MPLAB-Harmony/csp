08ec5f084d1dba5ac8e43b6057f49aeec11ae32e
Manjunath <http://support.microchip.com>
2018-10-04 23:40:24 +0530

    [License] Updated Copyright symbol with "Copyright (C)" text MH3-76

--- peripheral/tc_u2249/config/tc_timer.py
+++ peripheral/tc_u2249/config/tc_timer.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 """*****************************************************************************
-* © 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
