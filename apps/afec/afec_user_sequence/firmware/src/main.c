55bc5200131d23ae71effb443f236b352cc95e36
Prachi <http://support.microchip.com>
2018-10-08 15:34:52 +0530

    [AFEC] Regenerated and tested user_sequence application for E70 XULT MH3-83

--- apps/afec/afec_user_sequence/firmware/src/main.c
+++ apps/afec/afec_user_sequence/firmware/src/main.c
@@ -16,28 +16,30 @@
     machines of all modules in the system
  *******************************************************************************/
 
+// DOM-IGNORE-BEGIN
 /*******************************************************************************
-Copyright (c) 2017 released Microchip Technology Inc.  All rights reserved.
-
-Microchip licenses to you the right to use, modify, copy and distribute
-Software only when embedded on a Microchip microcontroller or digital signal
-controller that is integrated into your product or third party product
-(pursuant to the sublicense terms in the accompanying license agreement).
-
-You should refer to the license agreement accompanying this Software for
-additional information regarding your rights and obligations.
-
-SOFTWARE AND DOCUMENTATION ARE PROVIDED AS IS  WITHOUT  WARRANTY  OF  ANY  KIND,
-EITHER EXPRESS  OR  IMPLIED,  INCLUDING  WITHOUT  LIMITATION,  ANY  WARRANTY  OF
-MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A  PARTICULAR  PURPOSE.
-IN NO EVENT SHALL MICROCHIP OR  ITS  LICENSORS  BE  LIABLE  OR  OBLIGATED  UNDER
-CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION,  BREACH  OF  WARRANTY,  OR
-OTHER LEGAL  EQUITABLE  THEORY  ANY  DIRECT  OR  INDIRECT  DAMAGES  OR  EXPENSES
-INCLUDING BUT NOT LIMITED TO ANY  INCIDENTAL,  SPECIAL,  INDIRECT,  PUNITIVE  OR
-CONSEQUENTIAL DAMAGES, LOST  PROFITS  OR  LOST  DATA,  COST  OF  PROCUREMENT  OF
-SUBSTITUTE  GOODS,  TECHNOLOGY,  SERVICES,  OR  ANY  CLAIMS  BY  THIRD   PARTIES
-(INCLUDING BUT NOT LIMITED TO ANY DEFENSE  THEREOF),  OR  OTHER  SIMILAR  COSTS.
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
 *******************************************************************************/
+// DOM-IGNORE-END
 
 // *****************************************************************************
 // *****************************************************************************
@@ -49,6 +51,12 @@ SUBSTITUTE  GOODS,  TECHNOLOGY,  SERVICES,  OR  ANY  CLAIMS  BY  THIRD   PARTIES
 #include <stdbool.h>                    // Defines true
 #include <stdlib.h>                     // Defines EXIT_FAILURE
 #include "definitions.h"                // SYS function prototypes
+
+/*****************************************************
+ AFEC CH0 - PB01 - Connect to DACC output PB13
+ AFEC CH5 - PC30 - connect to Vcc
+ AFEC_CH6 - PC31 - Connect to GND
+ *****************************************************/
 
 #define AFEC_VREF               (3.3f)
 #define DAC_COUNT_INCREMENT     (124U)  // Equivalent to 0.1V 
@@ -67,7 +75,7 @@ uint16_t dac_count=0x800;
 void switch_handler( PIO_PIN pin, uintptr_t context )
 {
     /* Write next data sample */
-    dac_count=dac_count + DAC_COUNT_INCREMENT;
+    dac_count = dac_count + DAC_COUNT_INCREMENT;
     
     if (dac_count > DAC_COUNT_MAX)
             dac_count=0;    
@@ -98,7 +106,7 @@ int main ( void )
     /* Initialize all modules */
     SYS_Initialize ( NULL );
     
-    PIO_PinInterruptCallbackRegister(PIO_PIN_PA11, &switch_handler, (uintptr_t) NULL );
+    PIO_PinInterruptCallbackRegister(SWITCH_PIN, &switch_handler, (uintptr_t) NULL );
     PIO_PinInterruptEnable(SWITCH_PIN);
 
     /* Register callback function for AFEC end of conversion interrupt*/
@@ -110,8 +118,9 @@ int main ( void )
     /* Start the timer to trigger ADC conversion every 50 ms*/
     TC1_CH0_CompareStart();
 
-    printf("\r\n\n\n");
-    printf ("********** AFEC user sequence mode ****************\r\n");
+    printf("\n\r---------------------------------------------------------");
+    printf("\n\r                    AFEC User Sequence Demo                 ");
+    printf("\n\r---------------------------------------------------------\n\r");
     printf("CH0 Count  CH0 Voltage  CH5 Count  CH5 Voltage  CH6 Count  CH6 Voltage \n\r");           
 
     while ( true )
