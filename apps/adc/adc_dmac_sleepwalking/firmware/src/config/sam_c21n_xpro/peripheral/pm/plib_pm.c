2f144700478b9e79850f74e9d2a7c0f74954de5e
Jain <http://support.microchip.com>
2018-10-11 15:23:08 +0530

    [ADC] adc_dmac_sleepwalk demo regenerated MH3-1415

---
+++ apps/adc/adc_dmac_sleepwalking/firmware/src/config/sam_c21n_xpro/peripheral/pm/plib_pm.c
@@ -0,0 +1,72 @@
+/*******************************************************************************
+  Power Manager(PM) PLIB
+
+  Company
+    Microchip Technology Inc.
+
+  File Name
+    plib_pm.c
+
+  Summary
+    PM PLIB Implementation File.
+
+  Description
+    This file defines the interface to the PM peripheral library. This
+    library provides access to and control of the associated peripheral
+    instance.
+
+  Remarks:
+    None.
+
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+/* This section lists the other files that are included in this file.
+*/
+
+#include "device.h"
+#include "plib_pm.h"
+
+void PM_Initialize( void )
+{
+    /* Configure back biasing & VREG switching mode */
+    PM_REGS->PM_STDBYCFG = PM_STDBYCFG_BBIASHS(0) | PM_STDBYCFG_VREGSMOD_AUTO;
+}
+
+void PM_SleepModeEnter( PM_SLEEP_MODE sleepMode )
+{
+
+    PM_REGS->PM_SLEEPCFG = sleepMode;
+
+    /* Wait for interrupt instruction execution */
+    __WFI();
+}
