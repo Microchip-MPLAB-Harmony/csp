2dc4740ea52c48e009f17c8ba4059edc46406f4c
Prachi <http://support.microchip.com>
2018-10-12 14:40:23 +0530

    [clock] Added clock application for SAMV71 XULT
    MH3-1180

---
+++ apps/clock/clock_config/firmware/src/config/sam_v71_xult/peripheral/clk/plib_clk.c
@@ -0,0 +1,99 @@
+
+#include "device.h"
+#include "plib_clk.h"
+
+
+
+
+
+
+
+/*********************************************************************************
+Initialize PLLA (PLLACK)
+*********************************************************************************/
+
+static void CLK_PLLAInitialize(void)
+{
+    /* Configure and Enable PLLA */
+    PMC_REGS->CKGR_PLLAR = CKGR_PLLAR_ONE_Msk | CKGR_PLLAR_PLLACOUNT(0x3f) |
+                              CKGR_PLLAR_MULA(25 - 1) |
+                              CKGR_PLLAR_DIVA(1);
+
+    while ( (PMC_REGS->PMC_SR & PMC_SR_LOCKA_Msk) != PMC_SR_LOCKA_Msk);
+
+}
+
+
+
+/*********************************************************************************
+Initialize Master clock (MCK)
+*********************************************************************************/
+
+static void CLK_MasterClockInitialize(void)
+{
+    /* Program PMC_MCKR.PRES and wait for PMC_SR.MCKRDY to be set   */
+    PMC_REGS->PMC_MCKR = (PMC_REGS->PMC_MCKR & ~PMC_MCKR_PRES_Msk) | PMC_MCKR_PRES_CLK_1;
+    while ((PMC_REGS->PMC_SR & PMC_SR_MCKRDY_Msk) != PMC_SR_MCKRDY_Msk);
+
+    /* Program PMC_MCKR.MDIV and Wait for PMC_SR.MCKRDY to be set   */
+    PMC_REGS->PMC_MCKR = (PMC_REGS->PMC_MCKR & ~PMC_MCKR_MDIV_Msk) | PMC_MCKR_MDIV_PCK_DIV2;
+    while ((PMC_REGS->PMC_SR & PMC_SR_MCKRDY_Msk) != PMC_SR_MCKRDY_Msk);
+
+    /* Program PMC_MCKR.CSS and Wait for PMC_SR.MCKRDY to be set    */
+    PMC_REGS->PMC_MCKR = (PMC_REGS->PMC_MCKR & ~PMC_MCKR_CSS_Msk) | PMC_MCKR_CSS_PLLA_CLK;
+    while ((PMC_REGS->PMC_SR & PMC_SR_MCKRDY_Msk) != PMC_SR_MCKRDY_Msk);
+
+}
+
+
+
+
+
+
+/*********************************************************************************
+Initialize Programmable Clock (PCKx)
+*********************************************************************************/
+
+static void CLK_ProgrammableClockInitialize(void)
+{
+    /* Disable selected programmable clock  */
+    PMC_REGS->PMC_SCDR = PMC_SCDR_PCK2_Msk;
+
+    /* Configure selected programmable clock    */
+    PMC_REGS->PMC_PCK[2]= PMC_PCK_CSS_PLLA_CLK | PMC_PCK_PRES(59);
+
+    /* Enable selected programmable clock   */
+    PMC_REGS->PMC_SCER =    PMC_SCER_PCK2_Msk;
+
+    /* Wait for clock to be ready   */
+    while( (PMC_REGS->PMC_SR & (PMC_SR_PCKRDY2_Msk) ) != (PMC_SR_PCKRDY2_Msk));
+
+
+}
+
+
+/*********************************************************************************
+Clock Initialize
+*********************************************************************************/
+void CLK_Initialize( void )
+{
+    /* Set Flash Wait States and  Enable Code Loop Optimization */
+    EFC_REGS->EEFC_FMR = EEFC_FMR_FWS(6) | EEFC_FMR_CLOE_Msk;
+
+
+
+    /* Initialize PLLA */
+    CLK_PLLAInitialize();
+
+
+    /* Initialize Master Clock */
+    CLK_MasterClockInitialize();
+
+
+
+    /* Initialize Programmable Clock */
+    CLK_ProgrammableClockInitialize();
+
+    /* Enable Peripheral Clock */
+    PMC_REGS->PMC_PCER0=0x31c00;
+}
