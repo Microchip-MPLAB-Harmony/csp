7cd653fe7369a7362b3fb7178397f04cb53b7ef7
Rajat Batra <rajat.batra@microchip.com>
2018-10-12 12:48:41 +0530

    [WDT] Created demo for SAMV71 MH3-1192
    Signed-off-by: Rajat Batra - I41609 <rajat.batra@microchip.com>

--- apps/wdt/wdt_timeout/firmware/src/config/sam_v71_xult/definitions.h
+++ apps/wdt/wdt_timeout/firmware/src/config/sam_v71_xult/definitions.h
@@ -1,21 +1,18 @@
 /*******************************************************************************
-  Main Source File
+  System Definitions
 
-  Company:
-    Microchip Technology Inc.
-  
   File Name:
-    main.c
+    definitions.h
 
   Summary:
-    This file contains the "main" function for a project.
+    project system definitions.
 
   Description:
-    This file contains the "main" function for a project.  The
-    "main" function calls the "SYS_Initialize" function to initialize the state 
-    machines of all modules in the system
+    This file contains the system-wide prototypes and definitions for a project.
+
  *******************************************************************************/
 
+//DOM-IGNORE-BEGIN
 /*******************************************************************************
 * Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
 *
@@ -37,70 +34,103 @@
 * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
 * ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
 * THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
-*******************************************************************************/
+ *******************************************************************************/
+//DOM-IGNORE-END
+
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
 
 // *****************************************************************************
 // *****************************************************************************
 // Section: Included Files
 // *****************************************************************************
 // *****************************************************************************
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "peripheral/clk/plib_clk.h"
+#include "peripheral/pio/plib_pio.h"
+#include "peripheral/nvic/plib_nvic.h"
+#include "peripheral/systick/plib_systick.h"
+#include "peripheral/wdt/plib_wdt.h"
+#include "peripheral/usart/plib_usart1.h"
 
-#include <stddef.h>                     // Defines NULL
-#include <stdbool.h>                    // Defines true
-#include <stdlib.h>                     // Defines EXIT_FAILURE
-#include "definitions.h"               // SYS function prototypes
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
 
+extern "C" {
 
+#endif
+// DOM-IGNORE-END
 
-volatile bool switch_pressed = false;
-
-void switch_handler( PIO_PIN pin, uintptr_t context )
-{
-    switch_pressed = true;
-}
 // *****************************************************************************
 // *****************************************************************************
-// Section: Main Entry Point
+// Section: System Functions
 // *****************************************************************************
 // *****************************************************************************
 
-int main ( void )
-{
+// *****************************************************************************
+/* System Initialization Function
 
-    /* Initialize all modules */
+  Function:
+    void SYS_Initialize( void *data )
+
+  Summary:
+    Function that initializes all modules in the system.
+
+  Description:
+    This function initializes all modules in the system, including any drivers,
+    services, middleware, and applications.
+
+  Precondition:
+    None.
+
+  Parameters:
+    data            - Pointer to the data structure containing any data
+                      necessary to initialize the module. This pointer may
+                      be null if no data is required and default initialization
+                      is to be used.
+
+  Returns:
+    None.
+
+  Example:
+    <code>
     SYS_Initialize ( NULL );
-    printf ("\n\r -------------------------------------------------------------");
-    printf ("\n\r                           WDT DEMO                           ");
-    printf ("\n\r -------------------------------------------------------------");
-    printf ("\n\r Press switch to emulate deadlock "); 
-    SYSTICK_TimerStart();
-    PIO_PinInterruptCallbackRegister(SWITCH_PIN, &switch_handler, (uintptr_t) NULL );
-    PIO_PinInterruptEnable(SWITCH_PIN);
-    switch_pressed = false;
+
     while ( true )
     {
-        if(switch_pressed == false)
-        {
-            if(SYSTICK_TimerPeriodHasExpired())
-            {
-                LED_Toggle();
-                WDT_Clear();
-            }
-        }
-        else
-        {   
-            printf ("\n\r Emulating deadlock................ ");
-            printf ("\n\r WDT should reset device in 4 seconds ");           
-            while(1);
-        }
+        SYS_Tasks ( );
     }
+    </code>
 
-    /* Execution should not come here during normal operation */
+  Remarks:
+    This function will only be called once, after system reset.
+*/
 
-    return ( EXIT_FAILURE );
+void SYS_Initialize( void *data );
+
+/* Nullify SYS_Tasks() if only PLIBs are used. */
+#define     SYS_Tasks()
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: extern declarations
+// *****************************************************************************
+// *****************************************************************************
+
+
+
+
+//DOM-IGNORE-BEGIN
+#ifdef __cplusplus
 }
+#endif
+//DOM-IGNORE-END
 
-
+#endif /* DEFINITIONS_H */
 /*******************************************************************************
  End of File
 */
+
