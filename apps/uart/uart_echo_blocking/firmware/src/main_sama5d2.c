3ba1930fadf9c5ac155cd7b979d1b2a17f47120f
Dan Sneddon <dan.sneddon@microchip.com>
2018-10-17 16:13:57 -0700

    [uart_echo_blocking] Add UART demo app
    MH3-1288

---
+++ apps/uart/uart_echo_blocking/firmware/src/main_sama5d2.c
@@ -0,0 +1,127 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+
+#define RX_BUFFER_SIZE 256
+#define LED_On()                        LED_Clear()
+#define LED_Off()                       LED_Set()
+
+char messageStart[] = "**** UART Line Echo Demo ****\r\n\
+**** Demo uses blocking model of UART PLIB. ****\r\n\
+**** Enter a line of characters, press ENTER key and observe it echo back. ****\
+\r\n**** LED toggles on each time the line is echoed ****\r\n";
+char newline[] = "\r\n";
+char errorMessage[] = "\r\n**** UART error has occurred ****\r\n";
+char receiveBuffer[RX_BUFFER_SIZE];
+uint8_t rxCounter = 0;
+char data = 0;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+//
+static void echoBuffer(void)
+{
+    UART1_Write(receiveBuffer,rxCounter);
+    UART1_Write(newline,sizeof(newline));
+    rxCounter = 0;
+}
+
+int main ( void )
+{
+
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+    LED_Off();
+
+    /* Send start message */
+    UART1_Write(&messageStart, sizeof(messageStart));
+
+    while ( true )
+    {
+        /* Check if there is a received character */
+        if(UART1_ReceiverIsReady() == true)
+        {
+            if(UART1_ErrorGet() == UART_ERROR_NONE)
+            {
+                UART1_Read(&data, 1);
+
+                if((data == '\n') || (data == '\r'))
+                {
+                    echoBuffer();
+                    LED_Toggle();
+                }
+                else
+                {
+                    receiveBuffer[rxCounter++] = data;
+                    if(rxCounter == RX_BUFFER_SIZE)
+                      echoBuffer();
+                }
+            }
+            else
+            {
+                UART1_Write(errorMessage,sizeof(errorMessage));
+            }
+        }
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+
