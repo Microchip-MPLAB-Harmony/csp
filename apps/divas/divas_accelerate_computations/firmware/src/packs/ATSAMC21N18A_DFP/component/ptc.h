3c25b322114cc255bd4b52230b3dc8841283f8ed
Mohan RP <mohan.kumar@microchip.com>
2018-10-25 10:48:46 +0530

    [DIVAS]
    Generated divas_accelerate_computations Application
    
    MH3-1394

---
+++ apps/divas/divas_accelerate_computations/firmware/src/packs/ATSAMC21N18A_DFP/component/ptc.h
@@ -0,0 +1,42 @@
+/**
+ * \brief Header file for SAMC/SAMC21 PTC
+ *
+ * Copyright (c) 2017-2018 Microchip Technology Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/* file generated from device description version 2018-06-27T08:27:01Z */
+#ifndef SAMC_SAMC21_PTC_MODULE_H
+#define SAMC_SAMC21_PTC_MODULE_H
+
+/** \addtogroup SAMC_SAMC21 Peripheral Touch Controller
+ *  @{
+ */
+/* ========================================================================== */
+/**  SOFTWARE API DEFINITION FOR SAMC21_PTC                                   */
+/* ========================================================================== */
+
+/** \brief PTC register offsets definitions */
+
+/** \brief PTC register API structure */
+typedef struct
+{
+} ptc_registers_t;
+/** @}  end of Peripheral Touch Controller */
+
+#endif /* SAMC_SAMC21_PTC_MODULE_H */
+
