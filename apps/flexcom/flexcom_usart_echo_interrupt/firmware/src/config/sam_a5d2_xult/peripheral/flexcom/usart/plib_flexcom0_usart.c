9ffc10d963f0251d98d97c15eadd3027bfb1bdef
Jignesh <http://support.microchip.com>
2018-11-07 17:32:42 +0530

    [FLEXCOM] Add flexcom_usart_echo_interrupt demo MH3-1275

---
+++ apps/flexcom/flexcom_usart_echo_interrupt/firmware/src/config/sam_a5d2_xult/peripheral/flexcom/usart/plib_flexcom0_usart.c
@@ -0,0 +1,384 @@
+/*******************************************************************************
+  FLEXCOM0 USART PLIB
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    plib_flexcom0_usart.c
+
+  Summary:
+    FLEXCOM0 USART PLIB Implementation File
+
+  Description
+    This file defines the interface to the FLEXCOM0 USART peripheral library. This
+    library provides access to and control of the associated peripheral
+    instance.
+
+  Remarks:
+    None.
+*******************************************************************************/
+
+/*******************************************************************************
+Copyright (c) 2018 released Microchip Technology Inc.  All rights reserved.
+
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED AS IS  WITHOUT  WARRANTY  OF  ANY  KIND,
+EITHER EXPRESS  OR  IMPLIED,  INCLUDING  WITHOUT  LIMITATION,  ANY  WARRANTY  OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A  PARTICULAR  PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR  ITS  LICENSORS  BE  LIABLE  OR  OBLIGATED  UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION,  BREACH  OF  WARRANTY,  OR
+OTHER LEGAL  EQUITABLE  THEORY  ANY  DIRECT  OR  INDIRECT  DAMAGES  OR  EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY  INCIDENTAL,  SPECIAL,  INDIRECT,  PUNITIVE  OR
+CONSEQUENTIAL DAMAGES, LOST  PROFITS  OR  LOST  DATA,  COST  OF  PROCUREMENT  OF
+SUBSTITUTE  GOODS,  TECHNOLOGY,  SERVICES,  OR  ANY  CLAIMS  BY  THIRD   PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE  THEREOF),  OR  OTHER  SIMILAR  COSTS.
+*******************************************************************************/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+/* This section lists the other files that are included in this file.
+*/
+#include "plib_flexcom0_usart.h"
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: FLEXCOM0 USART Implementation
+// *****************************************************************************
+// *****************************************************************************
+
+FLEXCOM_USART_OBJECT flexcom0UsartObj;
+
+void static FLEXCOM0_USART_ISR_RX_Handler( void )
+{
+    if(flexcom0UsartObj.rxBusyStatus == true)
+    {
+        while((FLEX_US_CSR_RXRDY_Msk == (FLEXCOM0_REGS->FLEX_US_CSR & FLEX_US_CSR_RXRDY_Msk)) && (flexcom0UsartObj.rxSize > flexcom0UsartObj.rxProcessedSize) )
+        {
+            flexcom0UsartObj.rxBuffer[flexcom0UsartObj.rxProcessedSize++] = (FLEXCOM0_REGS->FLEX_US_RHR & FLEX_US_RHR_RXCHR_Msk);
+        }
+
+        /* Check if the buffer is done */
+        if(flexcom0UsartObj.rxProcessedSize >= flexcom0UsartObj.rxSize)
+        {
+            flexcom0UsartObj.rxBusyStatus = false;
+            flexcom0UsartObj.rxSize = 0;
+            flexcom0UsartObj.rxProcessedSize = 0;
+
+            /* Disable Read, Overrun, Parity and Framing error interrupts */
+            FLEXCOM0_REGS->FLEX_US_IDR = (FLEX_US_IDR_RXRDY_Msk | FLEX_US_IDR_FRAME_Msk | FLEX_US_IDR_PARE_Msk | FLEX_US_IDR_OVRE_Msk);
+
+            if(flexcom0UsartObj.rxCallback != NULL)
+            {
+                flexcom0UsartObj.rxCallback(flexcom0UsartObj.rxContext);
+            }
+        }
+    }
+    else
+    {
+        /* Nothing to process */
+        ;
+    }
+
+    return;
+}
+
+void static FLEXCOM0_USART_ISR_TX_Handler( void )
+{
+    if(flexcom0UsartObj.txBusyStatus == true)
+    {
+        while((FLEX_US_CSR_TXEMPTY_Msk == (FLEXCOM0_REGS->FLEX_US_CSR & FLEX_US_CSR_TXEMPTY_Msk)) && (flexcom0UsartObj.txSize > flexcom0UsartObj.txProcessedSize) )
+        {
+            FLEXCOM0_REGS->FLEX_US_THR = flexcom0UsartObj.txBuffer[flexcom0UsartObj.txProcessedSize++];
+        }
+
+        /* Check if the buffer is done */
+        if(flexcom0UsartObj.txProcessedSize >= flexcom0UsartObj.txSize)
+        {
+            flexcom0UsartObj.txBusyStatus = false;
+            flexcom0UsartObj.txSize = 0;
+            flexcom0UsartObj.txProcessedSize = 0;
+            FLEXCOM0_REGS->FLEX_US_IDR = FLEX_US_IDR_TXEMPTY_Msk;
+
+            if(flexcom0UsartObj.txCallback != NULL)
+            {
+                flexcom0UsartObj.txCallback(flexcom0UsartObj.txContext);
+            }
+        }
+    }
+    else
+    {
+        /* Nothing to process */
+        ;
+    }
+
+    return;
+}
+
+void FLEXCOM0_InterruptHandler( void )
+{
+    /* Channel status */
+    uint32_t channelStatus = FLEXCOM0_REGS->FLEX_US_CSR;
+
+    /* Error status */
+    uint32_t errorStatus = (channelStatus & (FLEX_US_CSR_OVRE_Msk | FLEX_US_CSR_FRAME_Msk | FLEX_US_CSR_PARE_Msk));
+
+    if(errorStatus != 0)
+    {
+        /* Client must call FLEXCOM0_USART_ErrorGet() function to clear the errors */
+
+        /* USART errors are normally associated with the receiver, hence calling
+         * receiver context */
+        if( flexcom0UsartObj.rxCallback != NULL )
+        {
+            flexcom0UsartObj.rxCallback(flexcom0UsartObj.rxContext);
+        }
+
+        flexcom0UsartObj.rxBusyStatus = false;
+        flexcom0UsartObj.rxSize = 0;
+        flexcom0UsartObj.rxProcessedSize = 0;
+
+        /* Disable Read, Overrun, Parity and Framing error interrupts */
+        FLEXCOM0_REGS->FLEX_US_IDR = (FLEX_US_IDR_RXRDY_Msk | FLEX_US_IDR_FRAME_Msk | FLEX_US_IDR_PARE_Msk | FLEX_US_IDR_OVRE_Msk);
+    }
+
+    /* Receiver status */
+    if(FLEX_US_CSR_RXRDY_Msk == (channelStatus & FLEX_US_CSR_RXRDY_Msk))
+    {
+        FLEXCOM0_USART_ISR_RX_Handler();
+    }
+
+    /* Transmitter status */
+    if(FLEX_US_CSR_TXEMPTY_Msk == (channelStatus & FLEX_US_CSR_TXEMPTY_Msk))
+    {
+        FLEXCOM0_USART_ISR_TX_Handler();
+    }
+
+    return;
+}
+
+
+void static FLEXCOM0_USART_ErrorClear( void )
+{
+    uint8_t dummyData = 0u;
+
+    FLEXCOM0_REGS->FLEX_US_CR = FLEX_US_CR_RSTSTA_Msk;
+
+    /* Flush existing error bytes from the RX FIFO */
+    while( FLEX_US_CSR_RXRDY_Msk == (FLEXCOM0_REGS->FLEX_US_CSR& FLEX_US_CSR_RXRDY_Msk) )
+    {
+        dummyData = (FLEXCOM0_REGS->FLEX_US_RHR& FLEX_US_RHR_RXCHR_Msk);
+    }
+
+    /* Ignore the warning */
+    (void)dummyData;
+
+    return;
+}
+
+void FLEXCOM0_USART_Initialize( void )
+{
+    /* Set FLEXCOM USART operating mode */
+    FLEXCOM0_REGS->FLEX_MR = FLEX_MR_OPMODE_USART;
+
+    /* Reset FLEXCOM0 USART */
+    FLEXCOM0_REGS->FLEX_US_CR = (FLEX_US_CR_RSTRX_Msk | FLEX_US_CR_RSTTX_Msk | FLEX_US_CR_RSTSTA_Msk);
+
+    /* Enable FLEXCOM0 USART */
+    FLEXCOM0_REGS->FLEX_US_CR = (FLEX_US_CR_TXEN_Msk | FLEX_US_CR_RXEN_Msk);
+
+    /* Configure FLEXCOM0 USART mode */
+    FLEXCOM0_REGS->FLEX_US_MR = ((FLEX_US_MR_USCLKS_MCK) | FLEX_US_MR_CHRL_8_BIT | FLEX_US_MR_PAR_NO | FLEX_US_MR_NBSTOP_1_BIT | (0 << FLEX_US_MR_OVER_Pos));
+
+    /* Configure FLEXCOM0 USART Baud Rate */
+    FLEXCOM0_REGS->FLEX_US_BRGR = FLEX_US_BRGR_CD(45);
+
+    /* Initialize instance object */
+    flexcom0UsartObj.rxBuffer = NULL;
+    flexcom0UsartObj.rxSize = 0;
+    flexcom0UsartObj.rxProcessedSize = 0;
+    flexcom0UsartObj.rxBusyStatus = false;
+    flexcom0UsartObj.rxCallback = NULL;
+    flexcom0UsartObj.txBuffer = NULL;
+    flexcom0UsartObj.txSize = 0;
+    flexcom0UsartObj.txProcessedSize = 0;
+    flexcom0UsartObj.txBusyStatus = false;
+    flexcom0UsartObj.txCallback = NULL;
+
+    return;
+}
+
+FLEXCOM_USART_ERROR FLEXCOM0_USART_ErrorGet( void )
+{
+    FLEXCOM_USART_ERROR errors = FLEXCOM_USART_ERROR_NONE;
+    uint32_t status = FLEXCOM0_REGS->FLEX_US_CSR;
+
+    /* Collect all errors */
+    if(status & FLEX_US_CSR_OVRE_Msk)
+    {
+        errors = FLEXCOM_USART_ERROR_OVERRUN;
+    }
+    if(status & FLEX_US_CSR_PARE_Msk)
+    {
+        errors |= FLEXCOM_USART_ERROR_PARITY;
+    }
+    if(status & FLEX_US_CSR_FRAME_Msk)
+    {
+        errors |= FLEXCOM_USART_ERROR_FRAMING;
+    }
+
+    if(errors != FLEXCOM_USART_ERROR_NONE)
+    {
+        FLEXCOM0_USART_ErrorClear();
+    }
+
+    /* All errors are cleared, but send the previous error state */
+    return errors;
+}
+
+bool FLEXCOM0_USART_SerialSetup( FLEXCOM_USART_SERIAL_SETUP *setup, uint32_t srcClkFreq )
+{
+    uint32_t baud = 0;
+    uint32_t brgVal = 0;
+    uint32_t overSampVal = 0;
+    uint32_t usartMode;
+    bool status = false;
+
+    if((flexcom0UsartObj.rxBusyStatus == true) || (flexcom0UsartObj.txBusyStatus == true))
+    {
+        /* Transaction is in progress, so return without updating settings */
+        return false;
+    }
+
+    if (setup != NULL)
+    {
+        baud = setup->baudRate;
+        if(srcClkFreq == 0)
+        {
+            srcClkFreq = FLEXCOM0_USART_FrequencyGet();
+        }
+
+        /* Calculate BRG value */
+        if (srcClkFreq >= (16 * baud))
+        {
+            brgVal = (srcClkFreq / (16 * baud));
+        }
+        else
+        {
+            brgVal = (srcClkFreq / (8 * baud));
+            overSampVal = (1 << FLEX_US_MR_OVER_Pos) & FLEX_US_MR_OVER_Msk;
+        }
+
+        /* Configure FLEXCOM0 USART mode */
+        usartMode = FLEXCOM0_REGS->FLEX_US_MR;
+        usartMode &= ~(FLEX_US_MR_CHRL_Msk | FLEX_US_MR_MODE9_Msk | FLEX_US_MR_PAR_Msk | FLEX_US_MR_NBSTOP_Msk | FLEX_US_MR_OVER_Msk);
+        FLEXCOM0_REGS->FLEX_US_MR = usartMode | ((uint32_t)setup->dataWidth | (uint32_t)setup->parity | (uint32_t)setup->stopBits | overSampVal);
+
+        /* Configure FLEXCOM0 USART Baud Rate */
+        FLEXCOM0_REGS->FLEX_US_BRGR = FLEX_US_BRGR_CD(brgVal);
+        status = true;
+    }
+
+    return status;
+}
+
+bool FLEXCOM0_USART_Read( void *buffer, const size_t size )
+{
+    bool status = false;
+    uint8_t * lBuffer = (uint8_t *)buffer;
+
+    if(NULL != lBuffer)
+    {
+        /* Clear errors before submitting the request.
+         * ErrorGet clears errors internally. */
+        FLEXCOM0_USART_ErrorGet();
+
+        /* Check if receive request is in progress */
+        if(flexcom0UsartObj.rxBusyStatus == false)
+        {
+            flexcom0UsartObj.rxBuffer = lBuffer;
+            flexcom0UsartObj.rxSize = size;
+            flexcom0UsartObj.rxProcessedSize = 0;
+            flexcom0UsartObj.rxBusyStatus = true;
+            status = true;
+
+            /* Enable Read, Overrun, Parity and Framing error interrupts */
+            FLEXCOM0_REGS->FLEX_US_IER = (FLEX_US_IER_RXRDY_Msk | FLEX_US_IER_FRAME_Msk | FLEX_US_IER_PARE_Msk | FLEX_US_IER_OVRE_Msk);
+        }
+    }
+
+    return status;
+}
+
+bool FLEXCOM0_USART_Write( void *buffer, const size_t size )
+{
+    bool status = false;
+    uint8_t * lBuffer = (uint8_t *)buffer;
+
+    if(NULL != lBuffer)
+    {
+        /* Check if transmit request is in progress */
+        if(flexcom0UsartObj.txBusyStatus == false)
+        {
+            flexcom0UsartObj.txBuffer = lBuffer;
+            flexcom0UsartObj.txSize = size;
+            flexcom0UsartObj.txProcessedSize = 0;
+            flexcom0UsartObj.txBusyStatus = true;
+            status = true;
+
+            /* Initiate the transfer by sending first byte */
+            if(FLEX_US_CSR_TXEMPTY_Msk == (FLEXCOM0_REGS->FLEX_US_CSR& FLEX_US_CSR_TXEMPTY_Msk))
+            {
+                FLEXCOM0_REGS->FLEX_US_THR = (FLEX_US_THR_TXCHR(*lBuffer) & FLEX_US_THR_TXCHR_Msk);
+                flexcom0UsartObj.txProcessedSize++;
+            }
+
+            FLEXCOM0_REGS->FLEX_US_IER = FLEX_US_IER_TXEMPTY_Msk;
+        }
+    }
+
+    return status;
+}
+
+void FLEXCOM0_USART_WriteCallbackRegister( FLEXCOM_USART_CALLBACK callback, uintptr_t context )
+{
+    flexcom0UsartObj.txCallback = callback;
+    flexcom0UsartObj.txContext = context;
+}
+
+void FLEXCOM0_USART_ReadCallbackRegister( FLEXCOM_USART_CALLBACK callback, uintptr_t context )
+{
+    flexcom0UsartObj.rxCallback = callback;
+    flexcom0UsartObj.rxContext = context;
+}
+
+bool FLEXCOM0_USART_WriteIsBusy( void )
+{
+    return flexcom0UsartObj.txBusyStatus;
+}
+
+bool FLEXCOM0_USART_ReadIsBusy( void )
+{
+    return flexcom0UsartObj.rxBusyStatus;
+}
+
+size_t FLEXCOM0_USART_WriteCountGet( void )
+{
+    return flexcom0UsartObj.txProcessedSize;
+}
+
+size_t FLEXCOM0_USART_ReadCountGet( void )
+{
+    return flexcom0UsartObj.rxProcessedSize;
+}
+
---
+++ apps/flexcom/flexcom_usart_echo_interrupt/firmware/src/config/sam_a5d2_xult/peripheral/flexcom/usart/plib_flexcom_usart_local.h
@@ -0,0 +1,163 @@
+/*******************************************************************************
+  FLEXCOM USART PLIB
+
+  Company
+    Microchip Technology Inc.
+
+  File Name
+    plib_flexcom_usart_local.h
+
+  Summary
+    Data Type definition of the USART Peripheral Interface Plib.
+
+  Description
+    This file defines the Data Types for the USART Plib.
+
+  Remarks:
+    None.
+*******************************************************************************/
+
+/*******************************************************************************
+Copyright (c) 2018 released Microchip Technology Inc. All rights reserved.
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+*******************************************************************************/
+
+#ifndef PLIB_FLEXCOM_USART_LOCAL_H // Guards against multiple inclusion
+#define PLIB_FLEXCOM_USART_LOCAL_H
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+/* This section lists the other files that are included in this file.
+*/
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus // Provide C++ Compatibility
+
+    extern "C" {
+
+#endif
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Data Types
+// *****************************************************************************
+// *****************************************************************************
+// *****************************************************************************
+
+// *****************************************************************************
+
+/* FLEXCOM USART Errors */
+typedef enum
+{
+    FLEXCOM_USART_ERROR_NONE = 0,
+    FLEXCOM_USART_ERROR_OVERRUN = FLEX_US_CSR_OVRE_Msk,
+    FLEXCOM_USART_ERROR_PARITY = FLEX_US_CSR_PARE_Msk,
+    FLEXCOM_USART_ERROR_FRAMING = FLEX_US_CSR_FRAME_Msk,
+
+    /* Force the compiler to reserve 32-bit memory for each enum */
+    FLEXCOM_USART_ERROR_INVALID = 0xFFFFFFFF
+
+} FLEXCOM_USART_ERROR;
+
+/* FLEXCOM USART Data Width */
+typedef enum
+{
+    FLEXCOM_USART_DATA_5_BIT = FLEX_US_MR_CHRL_5_BIT,
+    FLEXCOM_USART_DATA_6_BIT = FLEX_US_MR_CHRL_6_BIT,
+    FLEXCOM_USART_DATA_7_BIT = FLEX_US_MR_CHRL_7_BIT,
+    FLEXCOM_USART_DATA_8_BIT = FLEX_US_MR_CHRL_8_BIT,
+    FLEXCOM_USART_DATA_9_BIT = FLEX_US_MR_MODE9_Msk,
+
+    /* Force the compiler to reserve 32-bit memory for each enum */
+    FLEXCOM_USART_DATA_INVALID = 0xFFFFFFFF
+
+} FLEXCOM_USART_DATA;
+
+/* FLEXCOM USART Parity */
+typedef enum
+{
+    FLEXCOM_USART_PARITY_NONE = FLEX_US_MR_PAR_NO,
+    FLEXCOM_USART_PARITY_ODD = FLEX_US_MR_PAR_ODD,
+    FLEXCOM_USART_PARITY_EVEN = FLEX_US_MR_PAR_EVEN,
+    FLEXCOM_USART_PARITY_MARK = FLEX_US_MR_PAR_MARK,
+    FLEXCOM_USART_PARITY_SPACE = FLEX_US_MR_PAR_SPACE,
+    FLEXCOM_USART_PARITY_MULTIDROP = FLEX_US_MR_PAR_MULTIDROP,
+
+    /* Force the compiler to reserve 32-bit memory for each enum */
+    FLEXCOM_USART_PARITY_INVALID = 0xFFFFFFFF
+
+} FLEXCOM_USART_PARITY;
+
+/* FLEXCOM USART Stop bits */
+typedef enum
+{
+    FLEXCOM_USART_STOP_1_BIT = FLEX_US_MR_NBSTOP_1_BIT,
+    FLEXCOM_USART_STOP_1_5_BIT = FLEX_US_MR_NBSTOP_1_5_BIT,
+    FLEXCOM_USART_STOP_2_BIT = FLEX_US_MR_NBSTOP_2_BIT,
+
+    /* Force the compiler to reserve 32-bit memory for each enum */
+    FLEXCOM_USART_STOP_INVALID = 0xFFFFFFFF
+
+} FLEXCOM_USART_STOP;
+
+/* FLEXCOM USART Serial Setup */
+typedef struct
+{
+    uint32_t baudRate;
+    FLEXCOM_USART_DATA dataWidth;
+    FLEXCOM_USART_PARITY parity;
+    FLEXCOM_USART_STOP stopBits;
+
+} FLEXCOM_USART_SERIAL_SETUP;
+
+/* Callback Function Pointer */
+typedef void (*FLEXCOM_USART_CALLBACK)( uintptr_t context );
+
+/* FLEXCOM USART Object */
+typedef struct
+{
+    uint8_t *               txBuffer;
+    size_t                  txSize;
+    size_t                  txProcessedSize;
+    FLEXCOM_USART_CALLBACK  txCallback;
+    uintptr_t               txContext;
+    bool                    txBusyStatus;
+
+    uint8_t *               rxBuffer;
+    size_t                  rxSize;
+    size_t                  rxProcessedSize;
+    FLEXCOM_USART_CALLBACK  rxCallback;
+    uintptr_t               rxContext;
+    bool                    rxBusyStatus;
+
+} FLEXCOM_USART_OBJECT ;
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+    }
+#endif
+// DOM-IGNORE-END
+
+#endif //PLIB_FLEXCOM_USART_LOCAL_H
