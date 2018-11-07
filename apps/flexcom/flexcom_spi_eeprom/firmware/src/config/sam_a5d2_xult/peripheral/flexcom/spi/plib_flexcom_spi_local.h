7e635e47be9644950975ff7d9356caba7c636a11
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2018-11-07 17:36:31 +0530

    [FLEXCOM] Add flexcom_spi_eeprom demo MH3-1275

---
+++ apps/flexcom/flexcom_spi_eeprom/firmware/src/config/sam_a5d2_xult/peripheral/flexcom/spi/plib_flexcom_spi_local.h
@@ -0,0 +1,149 @@
+/*******************************************************************************
+  FLEXCOM SPI PLIB
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    plib_flexcom_spi_local.h
+
+  Summary:
+   FLEXCOM SPI PLIB Common Header File.
+
+  Description:
+    This file has prototype of all the interfaces which are common for all the
+    FLEXCOM SPI peripherals.
+
+  Remarks:
+    None.
+
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+Copyright (c) 2018 released Microchip Technology Inc. All rights reserved.
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+#ifndef PLIB_FLEXCOM_SPI_LOCAL_H  // Guards against multiple inclusion
+#define PLIB_FLEXCOM_SPI_LOCAL_H
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+/* This section lists the other files that are included in this file.
+*/
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus // Provide C++ Compatibility
+
+extern "C" {
+
+#endif
+
+// DOM-IGNORE-END
+/****************************** ${FLEXCOM_INSTANCE_NAME} SPI Interface *********************************/
+
+typedef enum
+{
+    FLEXCOM_SPI_CLOCK_PHASE_TRAILING_EDGE = 0 << FLEX_SPI_CSR_NCPHA_Pos,
+    FLEXCOM_SPI_CLOCK_PHASE_LEADING_EDGE = 1 << FLEX_SPI_CSR_NCPHA_Pos,
+
+    /* Force the compiler to reserve 32-bit space for each enum value */
+    FLEXCOM_SPI_CLOCK_PHASE_INVALID = 0xFFFFFFFF
+
+}FLEXCOM_SPI_CLOCK_PHASE;
+
+typedef enum
+{
+    FLEXCOM_SPI_CLOCK_POLARITY_IDLE_LOW = 0 << FLEX_SPI_CSR_CPOL_Pos,
+    FLEXCOM_SPI_CLOCK_POLARITY_IDLE_HIGH = 1 << FLEX_SPI_CSR_CPOL_Pos,
+
+    /* Force the compiler to reserve 32-bit space for each enum value */
+    FLEXCOM_SPI_CLOCK_POLARITY_INVALID = 0xFFFFFFFF
+
+}FLEXCOM_SPI_CLOCK_POLARITY;
+
+typedef enum
+{
+    FLEXCOM_SPI_DATA_BITS_8 = FLEX_SPI_CSR_BITS_8_BIT_Val << FLEX_SPI_CSR_BITS_Pos,
+    FLEXCOM_SPI_DATA_BITS_9 = FLEX_SPI_CSR_BITS_9_BIT_Val << FLEX_SPI_CSR_BITS_Pos,
+    FLEXCOM_SPI_DATA_BITS_10 = FLEX_SPI_CSR_BITS_10_BIT_Val << FLEX_SPI_CSR_BITS_Pos,
+    FLEXCOM_SPI_DATA_BITS_11 = FLEX_SPI_CSR_BITS_11_BIT_Val << FLEX_SPI_CSR_BITS_Pos,
+    FLEXCOM_SPI_DATA_BITS_12 = FLEX_SPI_CSR_BITS_12_BIT_Val << FLEX_SPI_CSR_BITS_Pos,
+    FLEXCOM_SPI_DATA_BITS_13 = FLEX_SPI_CSR_BITS_13_BIT_Val << FLEX_SPI_CSR_BITS_Pos,
+    FLEXCOM_SPI_DATA_BITS_14 = FLEX_SPI_CSR_BITS_14_BIT_Val << FLEX_SPI_CSR_BITS_Pos,
+    FLEXCOM_SPI_DATA_BITS_15 = FLEX_SPI_CSR_BITS_15_BIT_Val << FLEX_SPI_CSR_BITS_Pos,
+    FLEXCOM_SPI_DATA_BITS_16 = FLEX_SPI_CSR_BITS_16_BIT_Val << FLEX_SPI_CSR_BITS_Pos,
+
+    /* Force the compiler to reserve 32-bit space for each enum value */
+    FLEXCOM_SPI_DATA_BITS_INVALID = 0xFFFFFFFF
+
+}FLEXCOM_SPI_DATA_BITS;
+
+typedef struct
+{     
+    uint32_t    clockFrequency;
+    FLEXCOM_SPI_CLOCK_PHASE clockPhase;
+    FLEXCOM_SPI_CLOCK_POLARITY clockPolarity;
+    FLEXCOM_SPI_DATA_BITS   dataBits;
+
+}FLEXCOM_SPI_TRANSFER_SETUP;
+
+typedef  void (*FLEXCOM_SPI_CALLBACK) (uintptr_t context);
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Local: **** Do Not Use ****
+// *****************************************************************************
+// *****************************************************************************
+
+typedef struct
+{
+    void*                   txBuffer;    
+    void*                   rxBuffer;
+    size_t                  txSize;
+    size_t                  rxSize;
+    size_t                  dummySize;
+    size_t                  rxCount;
+    size_t                  txCount;
+    bool                    transferIsBusy;
+    FLEXCOM_SPI_CALLBACK    callback;
+    uintptr_t               context;
+    uint32_t                status;
+
+} FLEXCOM_SPI_OBJECT ;
+
+#ifdef __cplusplus // Provide C++ Compatibility
+}
+#endif
+
+#endif /* PLIB_FLEXCOM_SPI_LOCAL_H */
+
+/*******************************************************************************
+ End of File
+*/
---
+++ apps/flexcom/flexcom_spi_eeprom/firmware/src/config/sam_a5d2_xult/peripheral/pio/plib_pio.c
@@ -0,0 +1,269 @@
+/*******************************************************************************
+  SYS PORTS Static Functions for PORTS System Service
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    plib_pio.c
+
+  Summary:
+    PIO function implementations for the PIO PLIB.
+
+  Description:
+    The PIO PLIB provides a simple interface to manage peripheral
+    input-output controller.
+
+*******************************************************************************/
+
+//DOM-IGNORE-BEGIN
+/*******************************************************************************
+Copyright (c) 2018 released Microchip Technology Inc.  All rights reserved.
+
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED AS IS WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+*******************************************************************************/
+//DOM-IGNORE-END
+
+#include "plib_pio.h"
+
+
+
+
+
+/******************************************************************************
+  Function:
+    PIO_Initialize ( void )
+
+  Summary:
+    Initialize the PIO library.
+
+  Remarks:
+    See plib_pio.h for more details.
+*/
+void PIO_Initialize ( void )
+{
+ /* Port B Pin 5 configuration */
+	PIOB_REGS->PIO_MSKR = 0x20;
+	PIOB_REGS->PIO_CFGR |= 0x100;
+	
+ /* Port B Pin 9 configuration */
+	PIOB_REGS->PIO_MSKR = 0x200;
+	PIOB_REGS->PIO_CFGR |= 0x100;
+	
+ /* Port B Latch configuration */
+	PIOB_REGS->PIO_SODR = 0x220;
+	
+ /* Port D Peripheral function C configuration */
+	PIOD_REGS->PIO_MSKR = 0x1c000000;
+	PIOD_REGS->PIO_CFGR = 0x3;
+	
+ /* Port D Pin 13 configuration */
+	PIOD_REGS->PIO_MSKR = 0x2000;
+	PIOD_REGS->PIO_CFGR |= 0x100;
+	
+ /* Port D Pin 29 configuration */
+	PIOD_REGS->PIO_MSKR = 0x20000000;
+	PIOD_REGS->PIO_CFGR |= 0x100;
+	
+ /* Port D Latch configuration */
+	PIOD_REGS->PIO_SODR = 0x20002000;
+	
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: PIO APIs which operates on multiple pins of a port
+// *****************************************************************************
+// *****************************************************************************
+
+// *****************************************************************************
+/* Function:
+    uint32_t PIO_PortRead ( PIO_PORT port )
+
+  Summary:
+    Read all the I/O lines of the selected port.
+
+  Description:
+    This function reads the live data values on all the I/O lines of the
+    selected port.  Bit values returned in each position indicate corresponding
+    pin levels.
+    1 = Pin is high.
+    0 = Pin is low.
+
+    This function reads the value regardless of pin configuration, whether it is
+    set as as an input, driven by the PIO Controller, or driven by a peripheral.
+
+  Remarks:
+    If the port has less than 32-bits, unimplemented pins will read as
+    low (0).
+    Implemented pins are Right aligned in the 32-bit return value.
+*/
+uint32_t PIO_PortRead(PIO_PORT port)
+{
+    return ((pio_registers_t*)port)->PIO_PDSR;
+}
+
+// *****************************************************************************
+/* Function:
+    void PIO_PortWrite (PIO_PORT port, uint32_t mask, uint32_t value);
+
+  Summary:
+    Write the value on the masked I/O lines of the selected port.
+
+  Remarks:
+    See plib_pio.h for more details.
+*/
+void PIO_PortWrite(PIO_PORT port, uint32_t mask, uint32_t value)
+{
+    ((pio_registers_t*)port)->PIO_MSKR = mask;
+    ((pio_registers_t*)port)->PIO_ODSR = value;
+}
+
+// *****************************************************************************
+/* Function:
+    uint32_t PIO_PortReadLatch ( PIO_PORT port )
+
+  Summary:
+    Read the latched value on all the I/O lines of the selected port.
+
+  Remarks:
+    See plib_pio.h for more details.
+*/
+uint32_t PIO_PortReadLatch(PIO_PORT port)
+{
+    return ((pio_registers_t*)port)->PIO_ODSR;
+}
+
+// *****************************************************************************
+/* Function:
+    void PIO_PortSet ( PIO_PORT port, uint32_t mask )
+
+  Summary:
+    Set the selected IO pins of a port.
+
+  Remarks:
+    See plib_pio.h for more details.
+*/
+void PIO_PortSet(PIO_PORT port, uint32_t mask)
+{
+    ((pio_registers_t*)port)->PIO_SODR = mask;
+}
+
+// *****************************************************************************
+/* Function:
+    void PIO_PortClear ( PIO_PORT port, uint32_t mask )
+
+  Summary:
+    Clear the selected IO pins of a port.
+
+  Remarks:
+    See plib_pio.h for more details.
+*/
+void PIO_PortClear(PIO_PORT port, uint32_t mask)
+{
+    ((pio_registers_t*)port)->PIO_CODR = mask;
+}
+
+// *****************************************************************************
+/* Function:
+    void PIO_PortToggle ( PIO_PORT port, uint32_t mask )
+
+  Summary:
+    Toggles the selected IO pins of a port.
+
+  Remarks:
+    See plib_pio.h for more details.
+*/
+void PIO_PortToggle(PIO_PORT port, uint32_t mask)
+{
+    /* Write into Clr and Set registers */
+    ((pio_registers_t*)port)->PIO_MSKR = mask;
+    ((pio_registers_t*)port)->PIO_ODSR ^= mask;
+}
+
+// *****************************************************************************
+/* Function:
+    void PIO_PortInputEnable ( PIO_PORT port, uint32_t mask )
+
+  Summary:
+    Enables selected IO pins of a port as input.
+
+  Remarks:
+    See plib_pio.h for more details.
+*/
+void PIO_PortInputEnable(PIO_PORT port, uint32_t mask)
+{
+    ((pio_registers_t*)port)->PIO_MSKR = mask;
+    ((pio_registers_t*)port)->PIO_CFGR &= ~(1 << PIO_CFGR_DIR_Pos);	
+}
+
+// *****************************************************************************
+/* Function:
+    void PIO_PortOutputEnable ( PIO_PORT port, uint32_t mask )
+
+  Summary:
+    Enables selected IO pins of a port as output(s).
+
+  Remarks:
+    See plib_pio.h for more details.
+*/
+void PIO_PortOutputEnable(PIO_PORT port, uint32_t mask)
+{
+    ((pio_registers_t*)port)->PIO_MSKR = mask;
+    ((pio_registers_t*)port)->PIO_CFGR |= (1 << PIO_CFGR_DIR_Pos);
+}
+
+// *****************************************************************************
+/* Function:
+    void PIO_PortInterruptEnable(PIO_PORT port, uint32_t mask)
+
+  Summary:
+    Enables IO interrupt on selected IO pins of a port.
+
+  Remarks:
+    See plib_pio.h for more details.
+*/
+void PIO_PortInterruptEnable(PIO_PORT port, uint32_t mask)
+{
+    ((pio_registers_t*)port)->PIO_IER = mask;
+}
+
+// *****************************************************************************
+/* Function:
+    void PIO_PortInterruptDisable(PIO_PORT port, uint32_t mask)
+
+  Summary:
+    Disables IO interrupt on selected IO pins of a port.
+
+  Remarks:
+    See plib_pio.h for more details.
+*/
+void PIO_PortInterruptDisable(PIO_PORT port, uint32_t mask)
+{
+    ((pio_registers_t*)port)->PIO_IDR = mask;
+}
+
+
+
+
+
+/*******************************************************************************
+ End of File
+*/
