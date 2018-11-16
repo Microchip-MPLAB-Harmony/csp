c22a164b3f61c9ddbe01d11b240189c211621da3
Bhargav <http://support.microchip.com>
2018-11-16 13:21:37 +0530

    [arch] Add fuse configuration support for SAMD2x
    MH3-2114

--- arch/arm/config/devices_cortex_m0/SAM_D20.py
+++ arch/arm/config/devices_cortex_m0/SAM_D20.py
@@ -38,6 +38,184 @@ deviceSecurity.addKey("CLEAR", "0", "Disable (Code Protection Disabled)" )
 deviceSecurity.addKey("SET", "1", "Enable (Code Protection Enabled)")
 deviceSecurity.setSelectedKey("CLEAR",1)
 
+# NVMCTRL fuse configuration
+nvmctrlFuseMenu = coreComponent.createMenuSymbol("DEVICE_NVMCTRL_FUSE_CONFIG", devCfgMenu)
+nvmctrlFuseMenu.setLabel("NVMCTRL Fuse Configuration")
+
+nvmctrlBootloaderSize = coreComponent.createKeyValueSetSymbol("DEVICE_NVMCTRL_BOOTPROT", nvmctrlFuseMenu)
+nvmctrlBootloaderSize.setLabel("Bootloader Size")
+
+nvmctrlBootloaderSize_Node = ATDF.getNode("/avr-tools-device-file/modules/module@[name=\"FUSES\"]/value-group@[name=\"USER_FUSES_NVMCTRL__BOOTPROT\"]")
+nvmctrlBootloaderSize_Values = []
+nvmctrlBootloaderSize_Values = nvmctrlBootloaderSize_Node.getChildren()
+
+for index in range(len(nvmctrlBootloaderSize_Values)):
+    nvmctrlBootloaderSize_Key_Name = nvmctrlBootloaderSize_Values[index].getAttribute("name")
+    nvmctrlBootloaderSize_Key_Description = nvmctrlBootloaderSize_Values[index].getAttribute("caption")
+    nvmctrlBootloaderSize_Key_Value = nvmctrlBootloaderSize_Values[index].getAttribute("value")
+
+    if nvmctrlBootloaderSize_Key_Name[-1:] == "K":
+        nvmctrlBootloaderSize_Key_Name = str(int(nvmctrlBootloaderSize_Key_Name[:-1]) * 1024)
+
+    nvmctrlBootloaderSize.addKey(nvmctrlBootloaderSize_Key_Name, nvmctrlBootloaderSize_Key_Value, nvmctrlBootloaderSize_Key_Description)
+
+nvmctrlBootloaderSize.setDefaultValue(0)
+nvmctrlBootloaderSize.setOutputMode("Key")
+nvmctrlBootloaderSize.setDisplayMode("Description")
+
+nvmctrlEEPROMSize = coreComponent.createKeyValueSetSymbol("DEVICE_NVMCTRL_EEPROM_SIZE", nvmctrlFuseMenu)
+nvmctrlEEPROMSize.setLabel("EEPROM Size")
+
+nvmctrlEEPROMSize_Node = ATDF.getNode("/avr-tools-device-file/modules/module@[name=\"FUSES\"]/value-group@[name=\"USER_FUSES_NVMCTRL__EEPROM_SIZE\"]")
+nvmctrlEEPROMSize_Values = []
+nvmctrlEEPROMSize_Values = nvmctrlEEPROMSize_Node.getChildren()
+
+for index in range(len(nvmctrlEEPROMSize_Values)):
+    nvmctrlEEPROMSize_Key_Name = nvmctrlEEPROMSize_Values[index].getAttribute("name")
+    nvmctrlEEPROMSize_Key_Description = nvmctrlEEPROMSize_Values[index].getAttribute("caption")
+    nvmctrlEEPROMSize_Key_Value = nvmctrlEEPROMSize_Values[index].getAttribute("value")
+
+    if nvmctrlEEPROMSize_Key_Name[-1:] == "K":
+        nvmctrlEEPROMSize_Key_Name = str(int(nvmctrlEEPROMSize_Key_Name[:-1]) * 1024)
+
+    nvmctrlEEPROMSize.addKey(nvmctrlEEPROMSize_Key_Name, nvmctrlEEPROMSize_Key_Value, nvmctrlEEPROMSize_Key_Description)
+
+nvmctrlEEPROMSize.setDefaultValue(0)
+nvmctrlEEPROMSize.setOutputMode("Key")
+nvmctrlEEPROMSize.setDisplayMode("Description")
+
+nvmctrlRegionLock = coreComponent.createHexSymbol("DEVICE_NVMCTRL_REGION_LOCK", nvmctrlFuseMenu)
+nvmctrlRegionLock.setLabel("NVM Region Lock Bits")
+nvmctrlRegionLock.setDefaultValue(0xFFFF)
+nvmctrlRegionLock.setMax(0xFFFF)
+nvmctrlRegionLock.setMin(0x0)
+
+# BOD33 fuse configuration
+bod33FuseMenu = coreComponent.createMenuSymbol("DEVICE_BOD33_FUSE_CONFIG", devCfgMenu)
+bod33FuseMenu.setLabel("BOD33 Fuse Configuration")
+
+bod33Enable = coreComponent.createKeyValueSetSymbol("DEVICE_BOD33_EN", bod33FuseMenu)
+bod33Enable.setLabel("BOD33 Enable")
+bod33Enable.setOutputMode("Key")
+bod33Enable.setDisplayMode("Description")
+bod33Enable.addKey("DISABLED", "0", "BOD33 is disabled")
+bod33Enable.addKey("ENABLED", "1", "BOD33 is enabled")
+bod33Enable.setSelectedKey("ENABLED", 1)
+
+bod33Action = coreComponent.createKeyValueSetSymbol("DEVICE_BOD33_ACTION", bod33FuseMenu)
+bod33Action.setLabel("BOD33 Action")
+
+bod33Action_Node = ATDF.getNode("/avr-tools-device-file/modules/module@[name=\"FUSES\"]/value-group@[name=\"SUPC_BODVDD__ACTION\"]")
+bod33Action_Values = []
+bod33Action_Values = bod33Action_Node.getChildren()
+
+for index in range(len(bod33Action_Values)):
+    bod33Action_Key_Name = bod33Action_Values[index].getAttribute("name")
+    bod33Action_Key_Description = bod33Action_Values[index].getAttribute("caption")
+    bod33Action_Key_Value = bod33Action_Values[index].getAttribute("value")
+    bod33Action.addKey(bod33Action_Key_Name, bod33Action_Key_Value, bod33Action_Key_Description)
+
+bod33Action.setSelectedKey("RESET", 1)
+bod33Action.setOutputMode("Key")
+bod33Action.setDisplayMode("Description")
+
+bod33Hysterisis = coreComponent.createKeyValueSetSymbol("DEVICE_BOD33_HYST", bod33FuseMenu)
+bod33Hysterisis.setLabel("BOD33 Hysterisis")
+bod33Hysterisis.setOutputMode("Key")
+bod33Hysterisis.setDisplayMode("Description")
+bod33Hysterisis.addKey("DISABLED", "0", "No hysterisis")
+bod33Hysterisis.addKey("ENABLED", "1", "Hysterisis enabled")
+bod33Hysterisis.setSelectedKey("ENABLED", 1)
+
+bod33UserLevel = coreComponent.createHexSymbol("DEVICE_BOD33USERLEVEL", bod33FuseMenu)
+bod33UserLevel.setLabel("BOD33 User Level")
+bod33UserLevel.setDefaultValue(0x7)
+bod33UserLevel.setMax(0x3F)
+bod33UserLevel.setMin(0x0)
+
+# WDT fuse configuration
+wdtFuseMenu = coreComponent.createMenuSymbol("DEVICE_WDT_FUSE_CONFIG", devCfgMenu)
+wdtFuseMenu.setLabel("WDT Fuse Configuration")
+
+wdtEnable = coreComponent.createKeyValueSetSymbol("DEVICE_WDT_ENABLE", wdtFuseMenu)
+wdtEnable.setLabel("WDT Enable")
+wdtEnable.setOutputMode("Key")
+wdtEnable.setDisplayMode("Description")
+wdtEnable.addKey("DISABLED", "0", "WDT is disabled")
+wdtEnable.addKey("ENABLED", "1", "WDT is enabled")
+wdtEnable.setSelectedKey("DISABLED", 1)
+
+wdtAlwaysOn = coreComponent.createKeyValueSetSymbol("DEVICE_WDT_ALWAYSON", wdtFuseMenu)
+wdtAlwaysOn.setLabel("WDT Always On")
+wdtAlwaysOn.setOutputMode("Key")
+wdtAlwaysOn.setDisplayMode("Description")
+wdtAlwaysOn.addKey("DISABLED", "0", "WDT is disabled")
+wdtAlwaysOn.addKey("ENABLED", "1", "WDT is enabled and disabled through ENABLE bit")
+wdtAlwaysOn.setSelectedKey("DISABLED", 1)
+
+wdtPeriod = coreComponent.createKeyValueSetSymbol("DEVICE_WDT_PER", wdtFuseMenu)
+wdtPeriod.setLabel("WDT Period")
+
+wdtPeriod_Node = ATDF.getNode("/avr-tools-device-file/modules/module@[name=\"FUSES\"]/value-group@[name=\"WDT_CONFIG__PER\"]")
+wdtPeriod_Values = []
+wdtPeriod_Values = wdtPeriod_Node.getChildren()
+
+for index in range(len(wdtPeriod_Values)):
+    wdtPeriod_Key_Name = wdtPeriod_Values[index].getAttribute("name")
+
+    if wdtPeriod_Key_Name[-1:] == "K":
+        wdtPeriod_Key_Name = str(int(wdtPeriod_Key_Name[:-1]) * 1024)
+
+    wdtPeriod_Key_Description = wdtPeriod_Values[index].getAttribute("caption")
+    wdtPeriod_Key_Value = wdtPeriod_Values[index].getAttribute("value")
+    wdtPeriod.addKey(wdtPeriod_Key_Name, wdtPeriod_Key_Value, wdtPeriod_Key_Description)
+
+wdtPeriod.setDefaultValue(11)
+wdtPeriod.setOutputMode("Key")
+wdtPeriod.setDisplayMode("Description")
+
+wdtWindowModeEnable = coreComponent.createKeyValueSetSymbol("DEVICE_WDT_WEN", wdtFuseMenu)
+wdtWindowModeEnable.setLabel("WDT Window Mode Enable")
+wdtWindowModeEnable.setOutputMode("Key")
+wdtWindowModeEnable.setDisplayMode("Description")
+wdtWindowModeEnable.addKey("DISABLED", "0", "WDT window mode is disabled")
+wdtWindowModeEnable.addKey("ENABLED", "1", "WDT window mode is enabled")
+wdtWindowModeEnable.setSelectedKey("DISABLED", 1)
+
+wdtWindow_0 = coreComponent.createKeyValueSetSymbol("DEVICE_WDT_WINDOW_0", wdtFuseMenu)
+wdtWindow_0.setLabel("WDT Window bit 0")
+wdtWindow_0.addKey("CLEAR", "0", "CLEAR")
+wdtWindow_0.addKey("SET", "1", "SET")
+wdtWindow_0.setSelectedKey("SET", 1)
+wdtWindow_0.setOutputMode("Key")
+wdtWindow_0.setDisplayMode("Description")
+
+wdtWindow_1 = coreComponent.createHexSymbol("DEVICE_WDT_WINDOW_1", wdtFuseMenu)
+wdtWindow_1.setLabel("WDT Window bits 3:1")
+wdtWindow_1.setDefaultValue(0x4)
+wdtWindow_1.setMax(0x7)
+wdtWindow_1.setMin(0x0)
+
+wdtEarlyWarningOffset = coreComponent.createKeyValueSetSymbol("DEVICE_WDT_EWOFFSET", wdtFuseMenu)
+wdtEarlyWarningOffset.setLabel("WDT Early Warning Offset")
+
+wdtEarlyWarningOffset_Node = ATDF.getNode("/avr-tools-device-file/modules/module@[name=\"FUSES\"]/value-group@[name=\"WDT_EWCTRL__EWOFFSET\"]")
+wdtEarlyWarningOffset_Values = []
+wdtEarlyWarningOffset_Values = wdtEarlyWarningOffset_Node.getChildren()
+
+for index in range(len(wdtEarlyWarningOffset_Values)):
+    wdtEarlyWarningOffset_Key_Name = wdtEarlyWarningOffset_Values[index].getAttribute("name")
+
+    if wdtEarlyWarningOffset_Key_Name[-1:] == "K":
+        wdtEarlyWarningOffset_Key_Name = str(int(wdtEarlyWarningOffset_Key_Name[:-1]) * 1024)
+
+    wdtEarlyWarningOffset_Key_Description = wdtEarlyWarningOffset_Values[index].getAttribute("caption")
+    wdtEarlyWarningOffset_Key_Value = wdtEarlyWarningOffset_Values[index].getAttribute("value")
+    wdtEarlyWarningOffset.addKey(wdtEarlyWarningOffset_Key_Name, wdtEarlyWarningOffset_Key_Value, wdtEarlyWarningOffset_Key_Description)
+
+wdtEarlyWarningOffset.setDefaultValue(11)
+wdtEarlyWarningOffset.setOutputMode("Key")
+wdtEarlyWarningOffset.setDisplayMode("Description")
 
 coreArch = coreComponent.createStringSymbol("CoreArchitecture", devCfgMenu)
 coreArch.setLabel("Core Architecture")
@@ -123,4 +301,10 @@ armLibCSourceFile.setDestPath("")
 armLibCSourceFile.setProjectPath("config/" + configName + "/")
 armLibCSourceFile.setType("SOURCE")
 armLibCSourceFile.setDependencies(genSysSourceFile, ["CoreSysCallsFile", "CoreSysFiles"])
+
+devconSystemInitFile = coreComponent.createFileSymbol("DEVICE_CONFIG_SYSTEM_INIT", None)
+devconSystemInitFile.setType("STRING")
+devconSystemInitFile.setOutputName("core.LIST_SYSTEM_INIT_C_CONFIG_BITS_INITIALIZATION")
+devconSystemInitFile.setSourcePath("arm/templates/SAM_D20_D21.c.ftl")
+devconSystemInitFile.setMarkup(True)
 
