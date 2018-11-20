41453b5187e8fc9ba44164e6b7e1a238de6efbe9
Amit Raddi <amit.raddi@microchip.com>
2018-11-20 14:17:43 +0530

    [DSU] Generated dsu_crc32_generate application for SAM C2X/D2X Devices
    MH3-2442 MH3-1999
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

---
+++ apps/dsu/dsu_crc32_generate/firmware/src/config/sam_c21n_xpro/peripheral/pac/plib_pac.c
@@ -0,0 +1,78 @@
+/*******************************************************************************
+  Peripheral Access Controller (PAC) PLIB
+
+  Company
+    Microchip Technology Inc.
+
+  File Name
+    plib_pac.c
+
+  Summary
+    Source for PAC peripheral library interface Implementation.
+
+  Description
+    This file defines the interface to the PAC peripheral library. This
+    library provides access to and control of the associated peripheral
+    instance.
+
+  Remarks:
+    None.
+
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include "plib_pac.h"
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Global Data
+// *****************************************************************************
+// *****************************************************************************
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: PAC Interface Implementations
+// *****************************************************************************
+// *****************************************************************************
+
+void PAC_Initialize( void )
+{
+}
+
+void PAC_PeripheralProtectSetup( PAC_PERIPHERAL peripheral, PAC_PROTECTION operation )
+{
+    /* Set Peripheral Access Control */
+    PAC_REGS->PAC_WRCTRL = PAC_WRCTRL_PERID(peripheral) | PAC_WRCTRL_KEY(operation);
+}
+
---
+++ apps/dsu/dsu_crc32_generate/firmware/src/config/sam_d20_xpro/peripheral/pac/plib_pac.h
@@ -0,0 +1,199 @@
+/*******************************************************************************
+  Peripheral Access Controller(PAC) PLIB
+
+  Company
+    Microchip Technology Inc.
+
+  File Name
+    plib_pac.h
+
+  Summary
+    PAC PLIB Header File.
+
+  Description
+    This file defines the interface to the PAC peripheral library. This
+    library provides access to and control of the associated peripheral
+    instance.
+
+  Remarks:
+    None.
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+#ifndef PLIB_PAC_H  // Guards against multiple inclusion
+#define PLIB_PAC_H
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stdbool.h>
+#include <stddef.h>
+#include "device.h"
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus // Provide C++ Compatibility
+
+    extern "C" {
+
+#endif
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Data Types
+// *****************************************************************************
+// *****************************************************************************
+
+typedef enum
+{
+    /* Clear the peripheral write control protection */
+    PAC_PROTECTION_CLEAR,
+
+    /* Set the peripheral write control protection */
+    PAC_PROTECTION_SET,
+
+} PAC_PROTECTION;
+
+typedef enum
+{
+    /* Instance Id for Peripheral AC */
+    PAC_PERIPHERAL_AC = ID_AC,
+
+    /* Instance Id for Peripheral ADC */
+    PAC_PERIPHERAL_ADC = ID_ADC,
+
+    /* Instance Id for Peripheral DAC */
+    PAC_PERIPHERAL_DAC = ID_DAC,
+
+    /* Instance Id for Peripheral DSU */
+    PAC_PERIPHERAL_DSU = ID_DSU,
+
+    /* Instance Id for Peripheral EIC */
+    PAC_PERIPHERAL_EIC = ID_EIC,
+
+    /* Instance Id for Peripheral EVSYS */
+    PAC_PERIPHERAL_EVSYS = ID_EVSYS,
+
+    /* Instance Id for Peripheral GCLK */
+    PAC_PERIPHERAL_GCLK = ID_GCLK,
+
+    /* Instance Id for Peripheral NVMCTRL */
+    PAC_PERIPHERAL_NVMCTRL = ID_NVMCTRL,
+
+    /* Instance Id for Peripheral PAC0 */
+    PAC_PERIPHERAL_PAC0 = ID_PAC0,
+
+    /* Instance Id for Peripheral PAC1 */
+    PAC_PERIPHERAL_PAC1 = ID_PAC1,
+
+    /* Instance Id for Peripheral PAC2 */
+    PAC_PERIPHERAL_PAC2 = ID_PAC2,
+
+    /* Instance Id for Peripheral PM */
+    PAC_PERIPHERAL_PM = ID_PM,
+
+    /* Instance Id for Peripheral PORT */
+    PAC_PERIPHERAL_PORT = ID_PORT,
+
+    /* Instance Id for Peripheral PTC */
+    PAC_PERIPHERAL_PTC = ID_PTC,
+
+    /* Instance Id for Peripheral RTC */
+    PAC_PERIPHERAL_RTC = ID_RTC,
+
+    /* Instance Id for Peripheral SERCOM0 */
+    PAC_PERIPHERAL_SERCOM0 = ID_SERCOM0,
+
+    /* Instance Id for Peripheral SERCOM1 */
+    PAC_PERIPHERAL_SERCOM1 = ID_SERCOM1,
+
+    /* Instance Id for Peripheral SERCOM2 */
+    PAC_PERIPHERAL_SERCOM2 = ID_SERCOM2,
+
+    /* Instance Id for Peripheral SERCOM3 */
+    PAC_PERIPHERAL_SERCOM3 = ID_SERCOM3,
+
+    /* Instance Id for Peripheral SERCOM4 */
+    PAC_PERIPHERAL_SERCOM4 = ID_SERCOM4,
+
+    /* Instance Id for Peripheral SERCOM5 */
+    PAC_PERIPHERAL_SERCOM5 = ID_SERCOM5,
+
+    /* Instance Id for Peripheral SYSCTRL */
+    PAC_PERIPHERAL_SYSCTRL = ID_SYSCTRL,
+
+    /* Instance Id for Peripheral TC0 */
+    PAC_PERIPHERAL_TC0 = ID_TC0,
+
+    /* Instance Id for Peripheral TC1 */
+    PAC_PERIPHERAL_TC1 = ID_TC1,
+
+    /* Instance Id for Peripheral TC2 */
+    PAC_PERIPHERAL_TC2 = ID_TC2,
+
+    /* Instance Id for Peripheral TC3 */
+    PAC_PERIPHERAL_TC3 = ID_TC3,
+
+    /* Instance Id for Peripheral TC4 */
+    PAC_PERIPHERAL_TC4 = ID_TC4,
+
+    /* Instance Id for Peripheral TC5 */
+    PAC_PERIPHERAL_TC5 = ID_TC5,
+
+    /* Instance Id for Peripheral TC6 */
+    PAC_PERIPHERAL_TC6 = ID_TC6,
+
+    /* Instance Id for Peripheral TC7 */
+    PAC_PERIPHERAL_TC7 = ID_TC7,
+
+    /* Instance Id for Peripheral WDT */
+    PAC_PERIPHERAL_WDT = ID_WDT,
+
+    PAC_PERIPHERAL_NONE = -1
+
+} PAC_PERIPHERAL;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Interface Routines
+// *****************************************************************************
+// *****************************************************************************
+
+void PAC_PeripheralProtectSetup( PAC_PERIPHERAL peripheral, PAC_PROTECTION operation );
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus // Provide C++ Compatibility
+
+    }
+
+#endif
+// DOM-IGNORE-END
+
+#endif /* PLIB_PAC_H */
