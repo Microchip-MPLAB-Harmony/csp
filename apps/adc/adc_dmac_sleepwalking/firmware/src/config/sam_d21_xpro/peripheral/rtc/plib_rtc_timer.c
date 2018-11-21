41a8f4d7be8a714f3be29c96354ac361951f267f
Jain <http://support.microchip.com>
2018-11-21 08:56:33 +0530

    [ADC] Regenerated ADC DMAC Sleepwalking for SAM D21 Xpro MH3-1992

---
+++ apps/adc/adc_dmac_sleepwalking/firmware/src/config/sam_d21_xpro/peripheral/rtc/plib_rtc_timer.c
@@ -0,0 +1,174 @@
+/*******************************************************************************
+  Real Time Counter (RTC) PLIB
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    plib_rtc_timer.c
+
+  Summary:
+    RTC PLIB Implementation file
+
+  Description:
+    This file defines the interface to the RTC peripheral library. This
+    library provides access to and control of the associated peripheral
+    instance in timer mode.
+
+*******************************************************************************/
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+#include "plib_rtc.h"
+#include "device.h"
+#include <stdlib.h>
+
+
+void RTC_Initialize(void)
+{
+    RTC_REGS->MODE0.RTC_CTRL = RTC_MODE0_CTRL_SWRST_Msk;
+    
+    while((RTC_REGS->MODE0.RTC_STATUS & RTC_STATUS_SYNCBUSY_Msk) == RTC_STATUS_SYNCBUSY_Msk)
+    {
+        /* Wait for Synchronization after Software Reset */
+    }
+    
+    RTC_REGS->MODE0.RTC_READREQ |= RTC_READREQ_RCONT_Msk;
+
+    RTC_REGS->MODE0.RTC_CTRL = RTC_MODE0_CTRL_MODE(0) | RTC_MODE0_CTRL_PRESCALER(0x0) |RTC_MODE0_CTRL_MATCHCLR_Msk;
+
+
+    RTC_REGS->MODE0.RTC_COMP = 0x200;
+RTC_REGS->MODE0.RTC_EVCTRL = 0x100;
+}
+
+
+bool RTC_PeriodicIntervalHasCompleted (RTC_PERIODIC_INT_MASK period)
+{
+   bool periodIntervalComplete = false;
+   if( (RTC_REGS->MODE0.RTC_INTFLAG & period) == period)
+   {
+       periodIntervalComplete = true;
+
+       /* Clear Periodic Interval Interrupt */
+       RTC_REGS->MODE0.RTC_INTFLAG = period;
+   }
+
+    return periodIntervalComplete;
+}
+
+
+bool RTC_Timer32CompareHasMatched(void)
+{
+   bool status = false;
+
+   if((RTC_REGS->MODE0.RTC_INTFLAG & RTC_MODE0_INTFLAG_CMP0_Msk) == RTC_MODE0_INTFLAG_CMP0_Msk)
+   {
+       status = true;
+       RTC_REGS->MODE0.RTC_INTFLAG = RTC_MODE0_INTFLAG_CMP0_Msk;
+   }
+
+   return status;
+}
+
+bool RTC_Timer32CounterHasOverflowed ( void )
+{
+   bool status = false;
+
+   if((RTC_REGS->MODE0.RTC_INTFLAG & RTC_MODE0_INTFLAG_OVF_Msk) == RTC_MODE0_INTFLAG_OVF_Msk)
+   {
+       status = true;
+       RTC_REGS->MODE0.RTC_INTFLAG = RTC_MODE0_INTFLAG_OVF_Msk;
+   }
+
+   return status;
+}
+
+
+void RTC_Timer32Start ( void )
+{
+    RTC_REGS->MODE0.RTC_CTRL |= RTC_MODE0_CTRL_ENABLE_Msk;
+        
+    while((RTC_REGS->MODE0.RTC_STATUS & RTC_STATUS_SYNCBUSY_Msk) == RTC_STATUS_SYNCBUSY_Msk)
+    {
+        /* Wait for synchronization after Enabling RTC */
+    }
+}
+
+
+void RTC_Timer32Stop ( void )
+{
+    RTC_REGS->MODE0.RTC_CTRL &= ~(RTC_MODE0_CTRL_ENABLE_Msk);
+
+    while((RTC_REGS->MODE0.RTC_STATUS & RTC_STATUS_SYNCBUSY_Msk) == RTC_STATUS_SYNCBUSY_Msk)
+    {
+        /* Wait for Synchronization after Disabling RTC */
+    }
+}
+
+void RTC_Timer32CounterSet ( uint32_t count )
+{
+    RTC_REGS->MODE0.RTC_COUNT = count;
+
+    while((RTC_REGS->MODE0.RTC_STATUS & RTC_STATUS_SYNCBUSY_Msk) == RTC_STATUS_SYNCBUSY_Msk)
+    {
+        /* Wait for Synchronization after writing value to Count Register */
+    }
+}
+
+
+void RTC_Timer32CompareSet ( uint32_t compareValue )
+{
+    RTC_REGS->MODE0.RTC_COMP = compareValue;
+
+    while((RTC_REGS->MODE0.RTC_STATUS & RTC_STATUS_SYNCBUSY_Msk) == RTC_STATUS_SYNCBUSY_Msk)
+    {
+        /* Wait for Synchronization after writing Compare Value */
+    }
+}
+
+uint32_t RTC_Timer32CounterGet ( void )
+{
+    while((RTC_REGS->MODE0.RTC_STATUS & RTC_STATUS_SYNCBUSY_Msk) == RTC_STATUS_SYNCBUSY_Msk)
+    {
+        /* Wait for Synchronization before reading value from Count Register */
+    }
+        
+    return(RTC_REGS->MODE0.RTC_COUNT);
+}
+
+uint32_t RTC_Timer32PeriodGet ( void )
+{
+    /* Get 32Bit Compare Value */
+    return (RTC_MODE0_COUNT_COUNT_Msk);
+}
+
+uint32_t RTC_Timer32FrequencyGet ( void )
+{
+    /* Return Frequency of RTC Clock */
+    return RTC_COUNTER_CLOCK_FREQUENCY;
+}
+
+
