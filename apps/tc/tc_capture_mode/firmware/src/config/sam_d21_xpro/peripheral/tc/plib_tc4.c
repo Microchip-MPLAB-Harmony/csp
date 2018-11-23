725e48070ddd1f233890d10a9385936f4565b844
Prachi Phadke <prachi.phadke@microchip.com>
2018-11-23 16:04:05 +0530

    [TC] Added capture mode application for SAMD2x
    MH3-2011

---
+++ apps/tc/tc_capture_mode/firmware/src/config/sam_d21_xpro/peripheral/tc/plib_tc4.c
@@ -0,0 +1,169 @@
+/*******************************************************************************
+  Timer/Counter(TC4) PLIB
+
+  Company
+    Microchip Technology Inc.
+
+  File Name
+    plib_TC4.c
+
+  Summary
+    TC4 PLIB Implementation File.
+
+  Description
+    This file defines the interface to the TC peripheral library. This
+    library provides access to and control of the associated peripheral
+    instance.
+
+  Remarks:
+    None.
+
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+/* This section lists the other files that are included in this file.
+*/
+
+#include "plib_tc4.h"
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Global Data
+// *****************************************************************************
+// *****************************************************************************
+
+
+TC_CAPTURE_CALLBACK_OBJ TC4_CallbackObject;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: TC4 Implementation
+// *****************************************************************************
+// *****************************************************************************
+
+void TC4_CaptureInitialize( void )
+{
+    /* Reset TC */
+    TC4_REGS->COUNT16.TC_CTRLA = TC_CTRLA_SWRST_Msk;
+
+    while((TC4_REGS->COUNT16.TC_STATUS & TC_STATUS_SYNCBUSY_Msk))
+    {
+        /* Wait for Write Synchronization */
+    }
+
+    /* Configure counter mode, prescaler, standby */
+    TC4_REGS->COUNT16.TC_CTRLA = TC_CTRLA_MODE_COUNT16 | TC_CTRLA_PRESCALER_DIV1 ;
+
+    TC4_REGS->COUNT16.TC_CTRLC = TC_CTRLC_CPTEN0_Msk | TC_CTRLC_CPTEN1_Msk;
+    
+    TC4_REGS->COUNT16.TC_EVCTRL = TC_EVCTRL_EVACT_PPW | TC_EVCTRL_TCEI_Msk;
+
+    /* Clear all interrupt flags */
+    TC4_REGS->COUNT16.TC_INTFLAG = TC_INTFLAG_Msk;
+
+    /* Enable Interrupt */
+    TC4_REGS->COUNT16.TC_INTENSET = TC_INTENSET_MC0_Msk;
+    TC4_CallbackObject.callback = NULL;
+}
+
+
+void TC4_CaptureStart( void )
+{
+    /* Enable TC */
+    TC4_REGS->COUNT16.TC_CTRLA |= TC_CTRLA_ENABLE_Msk;
+
+    while((TC4_REGS->COUNT16.TC_STATUS & TC_STATUS_SYNCBUSY_Msk))
+    {
+        /* Wait for Write Synchronization */
+    }
+}
+
+void TC4_CaptureStop( void )
+{
+    /* Disable TC */
+    TC4_REGS->COUNT16.TC_CTRLA &= ~TC_CTRLA_ENABLE_Msk;
+
+    while((TC4_REGS->COUNT16.TC_STATUS & TC_STATUS_SYNCBUSY_Msk))
+    {
+        /* Wait for Write Synchronization */
+    }
+}
+
+uint32_t TC4_CaptureFrequencyGet( void )
+{
+    return (uint32_t)(47972352UL);
+}
+
+
+uint16_t TC4_Capture16bitChannel0Get( void )
+{
+    /* Write command to force CC register read synchronization */
+    TC4_REGS->COUNT16.TC_READREQ = TC_READREQ_RREQ_Msk | TC_COUNT16_CC_REG_OFST;
+
+    while((TC4_REGS->COUNT16.TC_STATUS & TC_STATUS_SYNCBUSY_Msk))
+    {
+        /* Wait for Write Synchronization */
+    }
+    return (uint16_t)TC4_REGS->COUNT16.TC_CC[0];
+}
+
+uint16_t TC4_Capture16bitChannel1Get( void )
+{
+    /* Write command to force CC register read synchronization */
+    TC4_REGS->COUNT16.TC_READREQ = TC_READREQ_RREQ_Msk | TC_COUNT16_CC_REG_OFST;
+
+    while((TC4_REGS->COUNT16.TC_STATUS & TC_STATUS_SYNCBUSY_Msk))
+    {
+        /* Wait for Write Synchronization */
+    }
+    return (uint16_t)TC4_REGS->COUNT16.TC_CC[1];
+}
+
+void TC4_CaptureCallbackRegister( TC_CAPTURE_CALLBACK callback, uintptr_t context )
+{
+    TC4_CallbackObject.callback = callback;
+    TC4_CallbackObject.context = context;
+}
+
+void TC4_CaptureInterruptHandler( void )
+{
+    TC_CAPTURE_STATUS status;
+    status = TC4_REGS->COUNT16.TC_INTFLAG;
+    /* Clear all interrupts */
+    TC4_REGS->COUNT16.TC_INTFLAG = TC_INTFLAG_Msk;
+
+    if(TC4_CallbackObject.callback != NULL)
+    {
+        TC4_CallbackObject.callback(status, TC4_CallbackObject.context);
+    }
+}
+
