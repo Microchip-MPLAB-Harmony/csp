04157517f61a503ea176295b7900e79b68660400
Bhargav Mehta <bhargav.mehta@microchip.com>
2018-11-28 14:14:29 +0530

    [SERCOM] Regenerated and tested usart_echo_interrupt_sam_c21n_xpro
    MH3-3279

--- apps/sercom/usart/usart_echo_interrupt/firmware/src/config/sam_c21n_xpro/peripheral/nvic/plib_nvic.c
+++ apps/sercom/usart/usart_echo_interrupt/firmware/src/config/sam_c21n_xpro/peripheral/nvic/plib_nvic.c
@@ -56,7 +56,7 @@ void NVIC_Initialize( void )
     __enable_irq();
 
     /* Enable the interrupt sources and configure the priorities as configured
-     * from within the "Interrupt Manager" of MCC. */
+     * from within the "Interrupt Manager" of MHC. */
     NVIC_SetPriority(SERCOM4_IRQn, 3);
     NVIC_EnableIRQ(SERCOM4_IRQn);
 
