fe9b6dd838afeca3f31c99017e8ded4d407f49fc
Prachi Phadke <prachi.phadke@microchip.com>
2018-11-28 15:53:49 +0530

    [TC] Regenerated and tested tc_compare_mode_sam_e70_xult
    MH3-3337

--- apps/tc/tc_compare_mode/firmware/src/config/sam_e70_xult/definitions.h
+++ apps/tc/tc_compare_mode/firmware/src/config/sam_e70_xult/definitions.h
@@ -51,8 +51,8 @@
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/nvic/plib_nvic.h"
-#include "peripheral/tc/plib_TC0.h"
-#include "peripheral/tc/plib_TC3.h"
+#include "peripheral/tc/plib_tc0.h"
+#include "peripheral/tc/plib_tc3.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
