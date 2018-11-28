b70d5ad47e2f76c4f6abe6709b2d24eeb16ec703
Rajat Batra <rajat.batra@microchip.com>
2018-11-28 20:29:58 +0530

    [SUPC] Regenerated demo for supc_wakeup_rtc_sam_v71_xult MH3-3303

--- apps/supc/supc_wakeup_rtc/firmware/src/config/sam_v71_xult/initialization.c
+++ apps/supc/supc_wakeup_rtc/firmware/src/config/sam_v71_xult/initialization.c
@@ -94,15 +94,16 @@ void SYS_Initialize ( void* data )
     CLK_Initialize();
 	PIO_Initialize();
 
+
     NVIC_Initialize();
 	SYSTICK_TimerInitialize();
 	RSWDT_REGS->RSWDT_MR = RSWDT_MR_WDDIS_Msk;	// Disable RSWDT 
 
 	WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk; 		// Disable WDT 
 
-	RTC_Initialize();
-
 	SUPC_Initialize();
+
+	RTC_Initialize();
 
 	USART1_Initialize();
 
