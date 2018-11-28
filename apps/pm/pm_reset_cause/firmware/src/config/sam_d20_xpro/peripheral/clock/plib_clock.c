eca253f9787e9fc9f41cf5bff9f5ab6e0fa71be2
Rajat <http://support.microchip.com>
2018-11-28 16:30:27 +0530

    [PM] Regenerated demo for pm_reset_cause_sam_d20_xpro MH3-3093

--- apps/pm/pm_reset_cause/firmware/src/config/sam_d20_xpro/peripheral/clock/plib_clock.c
+++ apps/pm/pm_reset_cause/firmware/src/config/sam_d20_xpro/peripheral/clock/plib_clock.c
@@ -77,21 +77,24 @@ static void DFLL_Initialize(void)
         /* Waiting for the Ready state */
     }
 
-
     /*Load Calibration Value*/
     uint8_t calibCoarse = (uint8_t)(((*(uint32_t*)0x806024) >> 26 ) & 0x3f);
+    calibCoarse = (((calibCoarse) == 0x3F) ? 0x1F : (calibCoarse));
     uint16_t calibFine = (uint16_t)(((*(uint32_t*)0x806028)) & 0x3ff);
 
     SYSCTRL_REGS->SYSCTRL_DFLLVAL = SYSCTRL_DFLLVAL_COARSE(calibCoarse) | SYSCTRL_DFLLVAL_FINE(calibFine);
+    GCLK_REGS->GCLK_CLKCTRL = GCLK_CLKCTRL_GEN(0x1)  | GCLK_CLKCTRL_CLKEN_Msk | GCLK_CLKCTRL_ID(0);
 
+    SYSCTRL_REGS->SYSCTRL_DFLLMUL = SYSCTRL_DFLLMUL_MUL(1464) | SYSCTRL_DFLLMUL_FSTEP(8) | SYSCTRL_DFLLMUL_CSTEP(8);
 
     /* Configure DFLL    */
-    SYSCTRL_REGS->SYSCTRL_DFLLCTRL = SYSCTRL_DFLLCTRL_ENABLE_Msk ;
+    SYSCTRL_REGS->SYSCTRL_DFLLCTRL = SYSCTRL_DFLLCTRL_ENABLE_Msk | SYSCTRL_DFLLCTRL_MODE_Msk ;
 
-    while((SYSCTRL_REGS->SYSCTRL_PCLKSR & SYSCTRL_PCLKSR_DFLLRDY_Msk) != SYSCTRL_PCLKSR_DFLLRDY_Msk)
+    while((SYSCTRL_REGS->SYSCTRL_PCLKSR & SYSCTRL_PCLKSR_DFLLLCKF_Msk) != SYSCTRL_PCLKSR_DFLLLCKF_Msk)
     {
-        /* Waiting for the Ready state */
+        /* Waiting for DFLL to fully lock to meet clock accuracy */
     }
+
 }
 
 
@@ -108,9 +111,8 @@ static void GCLK0_Initialize(void)
 
 static void GCLK1_Initialize(void)
 {
-    GCLK_REGS->GCLK_GENCTRL = GCLK_GENCTRL_SRC(7) | GCLK_GENCTRL_GENEN_Msk | GCLK_GENCTRL_ID(1);
+    GCLK_REGS->GCLK_GENCTRL = GCLK_GENCTRL_SRC(5) | GCLK_GENCTRL_GENEN_Msk | GCLK_GENCTRL_ID(1);
 
-    GCLK_REGS->GCLK_GENDIV = GCLK_GENDIV_DIV(48000) | GCLK_GENDIV_ID(1);
     while((GCLK_REGS->GCLK_STATUS & GCLK_STATUS_SYNCBUSY_Msk) == GCLK_STATUS_SYNCBUSY_Msk)
     {
         /* wait for the Generator 1 synchronization */
@@ -125,9 +127,9 @@ void CLOCK_Initialize (void)
     /* Function to Initialize the Oscillators */
     SYSCTRL_Initialize();
 
+    GCLK1_Initialize();
     DFLL_Initialize();
     GCLK0_Initialize();
-    GCLK1_Initialize();
 
 
 
