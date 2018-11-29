192e6c1baf2cd871a3adc0dc237f2fde36a65dd1
Prachi <http://support.microchip.com>
2018-11-29 12:22:23 +0530

    [AFEC] Regenerated and tested afec_polled_mode_sam_e70_xult
    MH3-3001

--- apps/afec/afec_polled_mode/firmware/src/config/sam_e70_xult/initialization.c
+++ apps/afec/afec_polled_mode/firmware/src/config/sam_e70_xult/initialization.c
@@ -94,13 +94,14 @@ void SYS_Initialize ( void* data )
     CLK_Initialize();
 	PIO_Initialize();
 
+
+    AFEC1_Initialize();
+
     NVIC_Initialize();
 	SYSTICK_TimerInitialize();
 	RSWDT_REGS->RSWDT_MR = RSWDT_MR_WDDIS_Msk;	// Disable RSWDT 
 
 	WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk; 		// Disable WDT 
-
-    AFEC1_Initialize();
 
 	DACC_Initialize();
 
