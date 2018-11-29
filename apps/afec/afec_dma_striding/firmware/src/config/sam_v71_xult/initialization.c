f0ad745d461aa0f3170485a59aba29e60d1b859e
Prachi <http://support.microchip.com>
2018-11-29 12:11:16 +0530

    [AFEC] Regenerated and tested afec_dma_striding_sam_v71_xult
    MH3-2999

--- apps/afec/afec_dma_striding/firmware/src/config/sam_v71_xult/initialization.c
+++ apps/afec/afec_dma_striding/firmware/src/config/sam_v71_xult/initialization.c
@@ -94,14 +94,15 @@ void SYS_Initialize ( void* data )
     CLK_Initialize();
 	PIO_Initialize();
 
+
+    AFEC1_Initialize();
+
     NVIC_Initialize();
     XDMAC_Initialize();
 
 	RSWDT_REGS->RSWDT_MR = RSWDT_MR_WDDIS_Msk;	// Disable RSWDT 
 
 	WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk; 		// Disable WDT 
-
-    AFEC1_Initialize();
 
 	DACC_Initialize();
 
