5d110582110b6305fa7b1e56bc5b81314dc1b5a2
Rajat <http://support.microchip.com>
2018-12-03 11:38:13 +0530

    [RTC] Regenerated Demo for rtc_alarm_sam_c21n_xpro MH3-3215

--- apps/rtc/rtc_alarm/firmware/src/config/sam_c21n_xpro/peripheral/port/plib_port.c
+++ apps/rtc/rtc_alarm/firmware/src/config/sam_c21n_xpro/peripheral/port/plib_port.c
@@ -80,6 +80,7 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[1].PORT_PMUX[5] = 0x33;
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x20;
+   PORT_REGS->GROUP[2].PORT_OUT = 0x20;
 
 }
 
