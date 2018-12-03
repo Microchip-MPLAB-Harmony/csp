14e3e2999635fca13ba589a5f7eade0f73bb9f0a
Rajat Batra <rajat.batra@microchip.com>
2018-12-03 11:38:13 +0530

    [RTC] Regenerated Demo for rtc_alarm_sam_c21n_xpro MH3-3215

--- apps/rtc/rtc_alarm/firmware/src/config/sam_c21n_xpro/peripheral/rtc/plib_rtc_clock.c
+++ apps/rtc/rtc_alarm/firmware/src/config/sam_c21n_xpro/peripheral/rtc/plib_rtc_clock.c
@@ -86,7 +86,7 @@ void RTC_Initialize(void)
     {
         /* Wait for Synchronization after Enabling RTC */
     }
-    
+
 }
 
 
@@ -138,7 +138,7 @@ bool RTC_RTCCAlarmSet (struct tm * alarmTime, RTC_ALARM_MASK mask)
      * Set YEAR(according to Reference Year), MONTH and DAY
      * Set Hour, Minute and second
      */
-    RTC_REGS->MODE2.RTC_Mode2Alarm = ((TM_STRUCT_REFERENCE_YEAR + alarmTime->tm_year) - REFERENCE_YEAR) << RTC_MODE2_CLOCK_YEAR_Pos |
+    RTC_REGS->MODE2.RTC_ALARM = ((TM_STRUCT_REFERENCE_YEAR + alarmTime->tm_year) - REFERENCE_YEAR) << RTC_MODE2_CLOCK_YEAR_Pos |
                     (ADJUST_MONTH(alarmTime->tm_mon) << RTC_MODE2_CLOCK_MONTH_Pos) |
                     (alarmTime->tm_mday << RTC_MODE2_CLOCK_DAY_Pos) |
                     (alarmTime->tm_hour << RTC_MODE2_CLOCK_HOUR_Pos) |
@@ -150,15 +150,15 @@ bool RTC_RTCCAlarmSet (struct tm * alarmTime, RTC_ALARM_MASK mask)
         /* Synchronization after writing to ALARM register */
     }
 
-    RTC_REGS->MODE2.RTC_Mode2AlarmMask = mask;
+    RTC_REGS->MODE2.RTC_MASK = mask;
 
     while((RTC_REGS->MODE2.RTC_SYNCBUSY & RTC_MODE2_SYNCBUSY_MASK0_Msk) == RTC_MODE2_SYNCBUSY_MASK0_Msk)
     {
         /* Synchronization after writing value to MASK Register */
     }
-        
+
     RTC_REGS->MODE2.RTC_INTENSET = RTC_MODE2_INTENSET_ALARM0_Msk;
-     
+
     return true;
 }
 
