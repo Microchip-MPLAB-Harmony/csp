1d7e56d8f8619e0120da9f51ba5219e9fcd05714
Rajat <http://support.microchip.com>
2018-12-21 12:21:23 +0530

    [PIO] Fixing Pin manager for A5D2 MH3-4163

--- arch/arm/config/devices_cortex_a5/SAMA5D2.py
+++ arch/arm/config/devices_cortex_a5/SAMA5D2.py
@@ -32,7 +32,7 @@ execfile(Variables.get("__CORE_DIR") + "/../peripheral/clk_sam_a5d2/config/clk.p
 
 # load device specific pin manager information
 execfile(Variables.get("__CORE_DIR") + "/../peripheral/pio_11264/config/pio.py")
-coreComponent.addPlugin("../peripheral/pio_11264/plugin/PIC32CZDApinmanager.jar")
+coreComponent.addPlugin("../peripheral/pio_11264/plugin/SAME70pinmanager.jar")
 
 # load AIC
 execfile(Variables.get("__CORE_DIR") + "/../peripheral/aic_11051/config/aic.py")
