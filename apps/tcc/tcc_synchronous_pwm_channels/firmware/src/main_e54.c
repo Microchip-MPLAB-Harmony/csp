27f74d18d0e1f243b8e2e1c5d8955c4d7f48a78b
Mohan <http://support.microchip.com>
2019-01-02 12:38:03 +0530

    [TCC] Generated tcc_synchronous_pwm_channels application for SAME5x
    MH3-1857

---
+++ apps/tcc/tcc_synchronous_pwm_channels/firmware/src/main_e54.c
@@ -0,0 +1,130 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+
+/***************************************
+ * Check PWM outputs on pins
+ * Channel 0 PWMH - PC04
+ * Channel 0 PWML - PD11
+ * Channel 1 PWMH - PD08
+ * Channel 1 PWML - PB17
+ * Channel 2 PWMH - PB14
+ * Channel 2 PWML - PC22
+***************************************/
+
+/* Duty cycle increment value */
+#define DUTY_INCREMENT (10U)
+
+/* Save PWM period */
+uint32_t period;
+
+/* This function is called after TCC period event */
+void TCC_PeriodEventHandler(uint32_t status, uintptr_t context)
+{
+    /* duty cycle values */
+    static uint32_t duty0 = 0U;
+    static uint32_t duty1 = 800U;
+    static uint32_t duty2 = 1600U;
+
+    TCC0_PWM24bitDutySet(TCC0_CHANNEL0, duty0);
+    TCC0_PWM24bitDutySet(TCC0_CHANNEL1, duty1);
+    TCC0_PWM24bitDutySet(TCC0_CHANNEL2, duty2);
+    
+    /* Increment duty cycle values */
+    duty0 += DUTY_INCREMENT;
+    duty1 += DUTY_INCREMENT;
+    duty2 += DUTY_INCREMENT;
+    
+    if (duty0 > period)
+        duty0 = 0U;
+    if (duty1 > period)
+        duty1 = 0U;
+    if (duty2 > period)
+        duty2 = 0U;
+
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+    
+        /* Register callback function for period event */
+    TCC0_PWMCallbackRegister(TCC_PeriodEventHandler, (uintptr_t)NULL);
+    
+    /* Read the period */
+    period = TCC0_PWM24bitPeriodGet();
+    
+    /* Start PWM*/
+    TCC0_PWMStart();
+
+    while ( true )
+    {
+        /* Maintain state machines of all polled MPLAB Harmony modules. */
+        SYS_Tasks ( );
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+/*******************************************************************************
+ End of File
+*/
+
