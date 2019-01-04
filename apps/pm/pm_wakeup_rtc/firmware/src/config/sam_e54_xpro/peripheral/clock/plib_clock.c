1f2eb1106841262bd452968633bf243cb6f3e3a0
Mohan RP <mohan.kumar@microchip.com>
2019-01-04 15:04:14 +0530

    [PM] Generated pm_wakeup_rtc Application for SAME5x
    MH3-4097

---
+++ apps/pm/pm_wakeup_rtc/firmware/src/config/sam_e54_xpro/peripheral/clock/plib_clock.c
@@ -0,0 +1,184 @@
+/*******************************************************************************
+ CLOCK PLIB
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    plib_clock.c
+
+  Summary:
+    CLOCK PLIB Implementation File.
+
+  Description:
+    None
+
+*******************************************************************************/
+
+/*******************************************************************************
+Copyright (c) 2017 released Microchip Technology Inc.  All rights reserved.
+
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED AS IS  WITHOUT  WARRANTY  OF  ANY  KIND,
+EITHER EXPRESS  OR  IMPLIED,  INCLUDING  WITHOUT  LIMITATION,  ANY  WARRANTY  OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A  PARTICULAR  PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR  ITS  LICENSORS  BE  LIABLE  OR  OBLIGATED  UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION,  BREACH  OF  WARRANTY,  OR
+OTHER LEGAL  EQUITABLE  THEORY  ANY  DIRECT  OR  INDIRECT  DAMAGES  OR  EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY  INCIDENTAL,  SPECIAL,  INDIRECT,  PUNITIVE  OR
+CONSEQUENTIAL DAMAGES, LOST  PROFITS  OR  LOST  DATA,  COST  OF  PROCUREMENT  OF
+SUBSTITUTE  GOODS,  TECHNOLOGY,  SERVICES,  OR  ANY  CLAIMS  BY  THIRD   PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE  THEREOF),  OR  OTHER  SIMILAR  COSTS.
+*******************************************************************************/
+
+#include "plib_clock.h"
+#include "device.h"
+
+static void OSCCTRL_Initialize(void)
+{
+
+    /****************** XOSC1 Initialization   ********************************/
+
+    /* Configure External Oscillator */
+    OSCCTRL_REGS->OSCCTRL_XOSCCTRL[1] = OSCCTRL_XOSCCTRL_STARTUP(10) | OSCCTRL_XOSCCTRL_IMULT(4) | OSCCTRL_XOSCCTRL_IPTAT(3) | OSCCTRL_XOSCCTRL_XTALEN_Msk | OSCCTRL_XOSCCTRL_ENABLE_Msk;
+
+    while((OSCCTRL_REGS->OSCCTRL_STATUS & OSCCTRL_STATUS_XOSCRDY1_Msk) != OSCCTRL_STATUS_XOSCRDY1_Msk)
+    {
+        /* Waiting for the XOSC Ready state */
+    }
+}
+
+static void OSC32KCTRL_Initialize(void)
+{
+    /****************** XOSC32K initialization  ******************************/
+
+    /* Configure 32K External Oscillator */
+    OSC32KCTRL_REGS->OSC32KCTRL_XOSC32K = OSC32KCTRL_XOSC32K_STARTUP(0) | OSC32KCTRL_XOSC32K_ENABLE_Msk | OSC32KCTRL_XOSC32K_CGM(1) | OSC32KCTRL_XOSC32K_EN32K_Msk | OSC32KCTRL_XOSC32K_XTALEN_Msk;
+
+
+    while(!((OSC32KCTRL_REGS->OSC32KCTRL_STATUS & OSC32KCTRL_STATUS_XOSC32KRDY_Msk) == OSC32KCTRL_STATUS_XOSC32KRDY_Msk))
+    {
+        /* Waiting for the XOSC32K Ready state */
+    }
+
+	OSC32KCTRL_REGS->OSC32KCTRL_RTCCTRL = OSC32KCTRL_RTCCTRL_RTCSEL(0);
+}
+
+
+
+static void DFLL_Initialize(void)
+{
+    /****************** DFLL Initialization  *********************************/
+    GCLK_REGS->GCLK_PCHCTRL[0] = GCLK_PCHCTRL_GEN(0x2)  | GCLK_PCHCTRL_CHEN_Msk;
+	while ((GCLK_REGS->GCLK_PCHCTRL[0] & GCLK_PCHCTRL_CHEN_Msk) != GCLK_PCHCTRL_CHEN_Msk)
+    {
+        /* Wait for synchronization */
+    }
+
+    OSCCTRL_REGS->OSCCTRL_DFLLMUL = OSCCTRL_DFLLMUL_MUL(1464) | OSCCTRL_DFLLMUL_FSTEP(1) | OSCCTRL_DFLLMUL_CSTEP(1);
+    while((OSCCTRL_REGS->OSCCTRL_DFLLSYNC & OSCCTRL_DFLLSYNC_DFLLMUL_Msk) == OSCCTRL_DFLLSYNC_DFLLMUL_Msk )
+    {
+        /* Waiting for the DPLL enable synchronization */
+    }
+
+    /* Configure DFLL    */
+    OSCCTRL_REGS->OSCCTRL_DFLLCTRLB = OSCCTRL_DFLLCTRLB_MODE_Msk ;
+
+    while((OSCCTRL_REGS->OSCCTRL_DFLLSYNC & OSCCTRL_DFLLSYNC_DFLLCTRLB_Msk) == OSCCTRL_DFLLSYNC_DFLLCTRLB_Msk )
+    {
+        /* Waiting for the DPLL enable synchronization */
+    }
+
+    OSCCTRL_REGS->OSCCTRL_DFLLCTRLA = OSCCTRL_DFLLCTRLA_ENABLE_Msk ;
+
+    while((OSCCTRL_REGS->OSCCTRL_DFLLSYNC & OSCCTRL_DFLLSYNC_ENABLE_Msk) == OSCCTRL_DFLLSYNC_ENABLE_Msk )
+    {
+        /* Waiting for the DPLL enable synchronization */
+    }
+
+    while((OSCCTRL_REGS->OSCCTRL_STATUS & OSCCTRL_STATUS_DFLLRDY_Msk) != OSCCTRL_STATUS_DFLLRDY_Msk)
+    {
+        /* Waiting for the XOSC Ready state */
+    }
+}
+
+
+static void GCLK0_Initialize(void)
+{
+    GCLK_REGS->GCLK_GENCTRL[0] = GCLK_GENCTRL_DIV(1) | GCLK_GENCTRL_SRC(6) | GCLK_GENCTRL_GENEN_Msk;
+
+    while((GCLK_REGS->GCLK_SYNCBUSY & GCLK_SYNCBUSY_GENCTRL0_Msk) == GCLK_SYNCBUSY_GENCTRL0_Msk)
+    {
+        /* wait for the Generator 0 synchronization */
+    }
+}
+
+static void GCLK1_Initialize(void)
+{
+    GCLK_REGS->GCLK_GENCTRL[1] = GCLK_GENCTRL_DIV(0) | GCLK_GENCTRL_SRC(1) | GCLK_GENCTRL_GENEN_Msk;
+
+    while((GCLK_REGS->GCLK_SYNCBUSY & GCLK_SYNCBUSY_GENCTRL1_Msk) == GCLK_SYNCBUSY_GENCTRL1_Msk)
+    {
+        /* wait for the Generator 1 synchronization */
+    }
+}
+
+static void GCLK2_Initialize(void)
+{
+    GCLK_REGS->GCLK_GENCTRL[2] = GCLK_GENCTRL_DIV(0) | GCLK_GENCTRL_SRC(5) | GCLK_GENCTRL_GENEN_Msk;
+
+    while((GCLK_REGS->GCLK_SYNCBUSY & GCLK_SYNCBUSY_GENCTRL2_Msk) == GCLK_SYNCBUSY_GENCTRL2_Msk)
+    {
+        /* wait for the Generator 2 synchronization */
+    }
+}
+
+void CLOCK_Initialize (void)
+{
+    /* NVM Wait States */
+    NVMCTRL_REGS->NVMCTRL_CTRLA |= NVMCTRL_CTRLA_RWS(1);
+
+    /* Function to Initialize the Oscillators */
+    OSCCTRL_Initialize();
+
+    /* Function to Initialize the 32KHz Oscillators */
+    OSC32KCTRL_Initialize();
+
+    GCLK1_Initialize();
+    GCLK2_Initialize();
+    DFLL_Initialize();
+    GCLK0_Initialize();
+
+    /* selection of the CPU clock Division */
+    MCLK_REGS->MCLK_CPUDIV = MCLK_CPUDIV_DIV(0x01);
+
+    while((MCLK_REGS->MCLK_INTFLAG & MCLK_INTFLAG_CKRDY_Msk) != MCLK_INTFLAG_CKRDY_Msk)
+    {
+        /* Wait for the Main Clock to be Ready */
+    }
+
+	/* Selection of the Generator and write Lock for SERCOM2_CORE */
+    GCLK_REGS->GCLK_PCHCTRL[23] = GCLK_PCHCTRL_GEN(0x1)  | GCLK_PCHCTRL_CHEN_Msk;
+
+    while ((GCLK_REGS->GCLK_PCHCTRL[23] & GCLK_PCHCTRL_CHEN_Msk) != GCLK_PCHCTRL_CHEN_Msk)
+    {
+        /* Wait for synchronization */
+    }
+
+    /* Configure the AHB Bridge Clocks */
+    MCLK_REGS->MCLK_AHBMASK = 0xffffff;
+
+    /* Configure the APBA Bridge Clocks */
+    MCLK_REGS->MCLK_APBAMASK = 0x7ff;
+
+    /* Configure the APBB Bridge Clocks */
+    MCLK_REGS->MCLK_APBBMASK = 0x18256;
+
+}
---
+++ apps/pm/pm_wakeup_rtc/firmware/src/main_e54.c
@@ -0,0 +1,192 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+#define LED_ON LED_Clear
+#define LED_OFF LED_Set
+
+enum
+{
+  IDLE_SLEEP_MODE = 'a',
+  STANDBY_SLEEP_MODE = 'b',
+  HIBERNATE_SLEEP_MODE = 'c',
+  OFF_SLEEP_MODE ='d',
+}SLEEP_MODES;
+
+uint8_t cmd = 0;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Callback Functions
+// *****************************************************************************
+// *****************************************************************************
+void timeout (uintptr_t context)
+{
+    LED_Toggle();    
+}
+
+void configure_alarm()
+{
+    RTC_Timer32Stop();
+    RTC_Timer32CounterSet(0);
+    RTC_Timer32Start();
+}
+
+void display_menu (void)
+{
+    printf("\n\n\n\rSelect the low power mode to enter");
+    printf("\n\ra) Idle Sleep Mode");
+    printf("\n\rb) Standby Sleep Mode"); 
+    printf("\n\rc) Hibernate Sleep Mode");
+    printf("\n\rd) Off Sleep Mode");
+    printf("\n\rEnter your choice");    
+    scanf("%c", &cmd);
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    RSTC_RESET_CAUSE reset_cause;
+    RSTC_BKUPEXIT_CAUSE reset_cause_bkup;
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+    SUPC_SelectVoltageRegulator(SUPC_VREGSEL_BUCK);
+
+    reset_cause_bkup = RSTC_BackupExitCauseGet();
+    reset_cause = RSTC_ResetCauseGet();
+   
+    
+    printf("\n\n\r----------------------------------------------");
+    printf("\n\r             Low power demo using RTC           ");
+    printf("\n\r----------------------------------------------"); 
+          
+    if(reset_cause_bkup == RSTC_BKUPEXIT_HIB_Msk)
+        printf("\n\n\rDevice exited from Hibernate mode\n");
+    else if(reset_cause == RSTC_RCAUSE_POR_Msk)
+        printf("\n\n\rDevice exited from OFF mode\n");
+
+    SYSTICK_TimerCallbackSet(&timeout, (uintptr_t) NULL);
+    SYSTICK_TimerStart();
+    RTC_Timer32InterruptEnable(RTC_TIMER32_INT_MASK_CMP0);
+    display_menu();
+    while(1)
+    {
+        switch(cmd)
+        {
+            case IDLE_SLEEP_MODE:
+            {
+                printf("\n\n\rConfiguring RTC Compare Match for wake up.......");
+                configure_alarm();
+                SYSTICK_TimerStop();
+                printf("\n\rEntering Idle Mode");
+                LED_OFF();
+                PM_IdleModeEnter();
+                printf("\n\rRTC Compare Match triggered waking up device from Idle mode");
+                SYSTICK_TimerStart();
+                display_menu();
+                break;
+            }
+            case STANDBY_SLEEP_MODE:
+            {
+                printf("\n\n\rConfiguring RTC Compare Match for wake up.......");
+                configure_alarm();
+                SYSTICK_TimerStop();
+                printf("\n\rEntering Standby Mode");
+                LED_OFF();
+                PM_StandbyModeEnter();
+                printf("\n\rRTC Compare Match triggered waking up device from Standby mode");
+                SYSTICK_TimerStart();
+                display_menu();
+                break;
+            }
+            case HIBERNATE_SLEEP_MODE:
+            {
+                printf("\n\n\rConfiguring RTC Compare Match for wake up.......");
+                configure_alarm();
+                SYSTICK_TimerStop();
+                printf("\n\rEntering Hibernate Mode   ");
+                LED_OFF();
+                PM_HibernateModeEnter();
+                break;
+            }
+            case OFF_SLEEP_MODE:
+            {
+                printf("\n\n\rConfiguring RTC Compare Match for wake up.......");
+                configure_alarm();
+                SYSTICK_TimerStop();
+                printf("\n\rEntering OFF Mode");
+                printf("\n\rPress Reset button to wakeup the device   ");   
+                LED_OFF();
+                PM_OffModeEnter();
+                break;
+            }
+            default:
+            {
+                printf("\n\rInvalid choice");
+                display_menu();
+                break;
+            }
+        } 
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+/*******************************************************************************
+ End of File
+*/
+
