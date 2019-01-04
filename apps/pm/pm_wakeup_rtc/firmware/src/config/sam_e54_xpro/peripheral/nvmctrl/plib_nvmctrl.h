98d3ff8d4b0d36e670edc065c4de4b0834b9140c
Mohan <http://support.microchip.com>
2019-01-04 15:04:14 +0530

    [PM] Generated pm_wakeup_rtc Application for SAME5x
    MH3-4097

---
+++ apps/pm/pm_wakeup_rtc/firmware/src/config/sam_e54_xpro/peripheral/nvmctrl/plib_nvmctrl.h
@@ -0,0 +1,164 @@
+/*******************************************************************************
+  Non-Volatile Memory Controller(NVMCTRL) PLIB.
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    plib_nvmctrl.h
+
+  Summary:
+    Interface definition of NVMCTRL Plib.
+
+  Description:
+    This file defines the interface for the NVMCTRL Plib.
+    It allows user to Program, Erase and lock the on-chip Non Volatile Flash
+    Memory.
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+#ifndef PLIB_NVMCTRL_H
+#define PLIB_NVMCTRL_H
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include "device.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus // Provide C++ Compatibility
+ extern "C" {
+#endif
+
+// DOM-IGNORE-END
+
+#define NVMCTRL_FLASH_START_ADDRESS        (0U)
+#define NVMCTRL_FLASH_SIZE                 (1048576U)
+#define NVMCTRL_FLASH_PAGESIZE             (512U)
+#define NVMCTRL_FLASH_ROWSIZE              (8192U)
+
+
+typedef void (*NVMCTRL_CALLBACK)(uint8_t int_flags, uintptr_t context);
+
+typedef struct
+{
+    NVMCTRL_CALLBACK callback_fn;
+    uintptr_t context;
+    uint8_t int_flags;
+}NVMCTRL_CALLBACK_OBJECT;
+
+typedef enum
+{
+    /* No error */
+    NVMCTRL_ERROR_NONE = 0x0,
+
+    /* NVMCTRL invalid commands and/or bad keywords error */
+    NVMCTRL_ERROR_PROG = 0x4,
+
+    /* NVMCTRL lock error */
+    NVMCTRL_ERROR_LOCK = 0x8,
+
+    /* NVMCTRL programming or erase error */
+    NVMCTRL_ERROR_NVM = 0x10,
+
+} NVMCTRL_ERROR;
+
+/* NVM supports four write modes */
+typedef enum
+{
+    NVMCTRL_WMODE_MAN = NVMCTRL_CTRLA_WMODE_MAN,
+    NVMCTRL_WMODE_ADW = NVMCTRL_CTRLA_WMODE_ADW,
+    NVMCTRL_WMODE_AQW = NVMCTRL_CTRLA_WMODE_AQW,
+    NVMCTRL_WMODE_AP = NVMCTRL_CTRLA_WMODE_AP,
+} NVMCTRL_WRITEMODE;
+
+/* Interrupt sources for the main flash */
+typedef enum
+{
+    NVMCTRL_INT_DONE = NVMCTRL_INTENCLR_DONE_Msk,
+    NVMCTRL_INT_ADDRE = NVMCTRL_INTENCLR_ADDRE_Msk,
+    NVMCTRL_INT_PROGE = NVMCTRL_INTENCLR_PROGE_Msk,
+    NVMCTRL_INT_LOCKE = NVMCTRL_INTENCLR_LOCKE_Msk,
+    NVMCTRL_INT_ECCSE = NVMCTRL_INTENCLR_ECCSE_Msk,
+    NVMCTRL_INT_ECCDE = NVMCTRL_INTENCLR_ECCDE_Msk,
+    NVMCTRL_INT_NVME = NVMCTRL_INTENCLR_NVME_Msk,
+    NVMCTRL_INT_SUSP = NVMCTRL_INTENCLR_SUSP_Msk
+} NVMCTRL_INTERRUPT0_SOURCE;
+
+
+void NVMCTRL_Initialize(void);
+
+void NVMCTRL_Read( uint32_t *data, uint32_t length, uint32_t address );
+
+void NVMCTRL_SetWriteMode(NVMCTRL_WRITEMODE mode);
+
+uint8_t NVMCTRL_QuadWordWrite(uint32_t *data, const uint32_t address);
+
+uint8_t NVMCTRL_DoubleWordWrite(uint32_t *data, const uint32_t address);
+
+void NVMCTRL_PageWrite( uint32_t* data, uint32_t address );
+
+void NVMCTRL_BlockErase( uint32_t address );
+
+uint16_t NVMCTRL_ErrorGet( void );
+
+uint16_t NVMCTRL_StatusGet( void );
+
+bool NVMCTRL_IsBusy( void );
+
+void NVMCTRL_RegionLock (uint32_t address);
+
+void NVMCTRL_RegionUnlock (uint32_t address);
+
+bool NVMCTRLSmartEEPROM_IsBusy(void);
+
+uint16_t NVMCTRL_SmartEepromStatusGet( void );
+
+bool NVMCTRLSmartEEPROM_IsActiveSectorFull(void);
+
+void NVMCTRL_SmartEepromSectorReallocate(void);
+
+void NVMCTRL_SmartEepromFlushPageBuffer(void);
+
+void NVMCTRL_BankSwap(void);
+
+void NVMCTRL_MainCallbackRegister( NVMCTRL_CALLBACK callback, uintptr_t context );
+void NVMCTRL_EnableMainFlashInterruptSource(NVMCTRL_INTERRUPT0_SOURCE int_source);
+void NVMCTRL_DisableMainFlashInterruptSource(NVMCTRL_INTERRUPT0_SOURCE int_source);
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus // Provide C++ Compatibility
+}
+#endif
+// DOM-IGNORE-END
+#endif // PLIB_NVMCTRL_H
---
+++ apps/pm/pm_wakeup_rtc/firmware/src/config/sam_e54_xpro/peripheral/pm/plib_pm.c
@@ -0,0 +1,129 @@
+/*******************************************************************************
+  Power Manager(PM) PLIB
+
+  Company
+    Microchip Technology Inc.
+
+  File Name
+    plib_pm.c
+
+  Summary
+    PM PLIB Implementation File.
+
+  Description
+    This file defines the interface to the PM peripheral library. This
+    library provides access to and control of the associated peripheral
+    instance.
+
+  Remarks:
+    None.
+
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+/* This section lists the other files that are included in this file.
+*/
+
+#include "device.h"
+#include "plib_pm.h"
+void PM_Initialize( void )
+{
+    /* Configure PM */
+    PM_REGS->PM_STDBYCFG = PM_STDBYCFG_RAMCFG(0)| PM_STDBYCFG_FASTWKUP(1);
+    PM_REGS->PM_HIBCFG = PM_HIBCFG_RAMCFG(2)| PM_HIBCFG_BRAMCFG(2);
+    PM_REGS->PM_BKUPCFG = PM_BKUPCFG_BRAMCFG(2);
+}
+
+void PM_IdleModeEnter( void )
+{
+    /* Configure Idle Sleep */
+    PM_REGS->PM_SLEEPCFG = PM_SLEEPCFG_SLEEPMODE_IDLE_Val;
+    /* Wait for interrupt instruction execution */
+    __WFI();
+}
+
+void PM_StandbyModeEnter( void )
+{
+    /* Configure Standby Sleep */
+    PM_REGS->PM_SLEEPCFG = PM_SLEEPCFG_SLEEPMODE_STANDBY_Val;
+    /* Wait till the voltage regulator low power mode is ready */
+    while(!PM_REGS->PM_INTFLAG & PM_INTFLAG_SLEEPRDY_Msk);
+    /* Wait for interrupt instruction execution */
+    __WFI();
+}
+
+void PM_HibernateModeEnter( void )
+{
+    /* Configure Hibernate Sleep */
+    PM_REGS->PM_SLEEPCFG = PM_SLEEPCFG_SLEEPMODE_HIBERNATE_Val;
+    /* Wait till the voltage regulator low power mode is ready */
+    while(!PM_REGS->PM_INTFLAG & PM_INTFLAG_SLEEPRDY_Msk);
+    /* Wait for interrupt instruction execution */
+    __WFI();
+}
+
+void PM_BackupModeEnter( void )
+{
+    /* Configure Backup Sleep */
+    PM_REGS->PM_SLEEPCFG = PM_SLEEPCFG_SLEEPMODE_BACKUP_Val;
+    /* Wait till the voltage regulator low power mode is ready */
+    while(!PM_REGS->PM_INTFLAG & PM_INTFLAG_SLEEPRDY_Msk);
+    /* Wait for interrupt instruction execution */
+    __WFI();
+}
+
+void PM_OffModeEnter( void )
+{
+    /* Configure Off Sleep */
+    PM_REGS->PM_SLEEPCFG = PM_SLEEPCFG_SLEEPMODE_OFF_Val;
+    /* Wait for interrupt instruction execution */
+    __WFI();
+}
+
+/* ********Important Note********
+ * Refer the bit description of PM->CTRLA.IORET
+ * in datasheet before using this function.
+ * When IORET is enabled, SWD access to the device will not be
+ * available after waking up from Hibernate/Backup sleep until
+ * the bit is cleared by the application.
+ */
+void PM_IO_RetentionSet( void )
+{
+    PM_REGS->PM_CTRLA |= PM_CTRLA_IORET_Msk;
+}
+
+void PM_IO_RetentionClear( void )
+{
+    PM_REGS->PM_CTRLA &= (~PM_CTRLA_IORET_Msk);
+}
+
+
