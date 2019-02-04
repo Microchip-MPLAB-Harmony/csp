66da0f5ffb992f5bb332c7302f89cab037240c89
Jain <http://support.microchip.com>
2019-02-04 11:15:07 +0530

    [RNG] Generate ‘rng_random_number’ demo for PIC32MZ EF MH3-5254

---
+++ apps/rng/rng_random_number/firmware/src/main.c
@@ -0,0 +1,108 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+ 
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+#include <string.h>
+#include <stdio.h>
+
+uint32_t rand_num[2];
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+
+    printf("\r\nRNG demo. Press SW1 to generate random number\r\n");
+    RNG_TrngEnable();
+
+    while ( true )
+    {
+        /* Maintain state machines of all polled MPLAB Harmony modules. */
+        SYS_Tasks ( );
+        if(!(SW1_Get()))
+        {
+            LED1_Set();
+            RNG_WaitForTrngCnt();
+            rand_num[0] = RNG_Seed1Get();
+            rand_num[1] = RNG_Seed2Get();
+            printf("\r\n\r\nSeed available in TRNG 0x%x%x", rand_num[0], rand_num[1]);
+
+            /* Prepare PRNG to get seed from TRNG */
+            RNG_LoadSet();
+            RNG_PrngEnable();
+            CORETIMER_DelayUs(100);
+            rand_num[0] = RNG_NumGen1Get();
+            rand_num[1] = RNG_NumGen2Get();
+            RNG_PrngDisable();
+            printf("\r\nGenerated 64-bit Pseudo-Random Number 0x%x%x\r\n", rand_num[0], rand_num[1]);
+            /* Wait till SW0 is released */
+            while(!(SW1_Get()));
+            LED1_Clear();
+        }
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+
