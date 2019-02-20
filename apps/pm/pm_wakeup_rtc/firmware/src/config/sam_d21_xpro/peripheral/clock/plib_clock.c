2c46849d6c30ebb9d4366983dfd29597f14ebf57
Rajat <http://support.microchip.com>
2019-02-21 04:32:50 +0530

    [PM] Regenerated demo for pm_wakeup_rtc_sam_d21_xpro MH3-6026

--- apps/pm/pm_wakeup_rtc/firmware/src/config/sam_d21_xpro/peripheral/clock/plib_clock.c
+++ apps/pm/pm_wakeup_rtc/firmware/src/config/sam_d21_xpro/peripheral/clock/plib_clock.c
@@ -150,7 +150,7 @@ void CLOCK_Initialize (void)
     GCLK_REGS->GCLK_CLKCTRL = GCLK_CLKCTRL_ID(4) | GCLK_CLKCTRL_GEN(0x1)  | GCLK_CLKCTRL_CLKEN_Msk;
     /* Selection of the Generator and write Lock for SERCOM3_CORE */
     GCLK_REGS->GCLK_CLKCTRL = GCLK_CLKCTRL_ID(23) | GCLK_CLKCTRL_GEN(0x0)  | GCLK_CLKCTRL_CLKEN_Msk;
-
+    
     /* Configure the APBC Bridge Clocks */
     PM_REGS->PM_APBCMASK = 0x10020;
 
