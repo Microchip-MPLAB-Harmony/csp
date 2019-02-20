b2dd193073d44a43c216c7a24189a233ff52dc66
Rajat <http://support.microchip.com>
2019-02-21 04:28:50 +0530

    [NVMCTRL] Regenerated demo for nvmctrl_smarteeprom_sam_e54_xpro MH3-5996

--- apps/nvmctrl/nvmctrl_smarteeprom/firmware/src/packs/ATSAME54P20A_DFP/component/pdec.h
+++ apps/nvmctrl/nvmctrl_smarteeprom/firmware/src/packs/ATSAME54P20A_DFP/component/pdec.h
@@ -20,7 +20,7 @@
  *
  */
 
-/* file generated from device description version 2019-01-17T21:53:39Z */
+/* file generated from device description version 2019-02-04T09:12:27Z */
 #ifndef _SAME54_PDEC_COMPONENT_H_
 #define _SAME54_PDEC_COMPONENT_H_
 
