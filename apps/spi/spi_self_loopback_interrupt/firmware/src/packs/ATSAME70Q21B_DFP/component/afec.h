2005754e56d1f4b58a60a028bfe11fcb72ce73e0
Santosh <http://support.microchip.com>
2019-02-21 13:04:40 +0530

    [SPI] Regenerated demo for spi_self_loopback_interrupt_sam_e70_xult MH3-6261

--- apps/spi/spi_self_loopback_interrupt/firmware/src/packs/ATSAME70Q21B_DFP/component/afec.h
+++ apps/spi/spi_self_loopback_interrupt/firmware/src/packs/ATSAME70Q21B_DFP/component/afec.h
@@ -1,7 +1,7 @@
 /**
  * \brief Component description for AFEC
  *
- * © 2018 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2019 Microchip Technology Inc. and its subsidiaries.
  *
  * Subject to your compliance with these terms, you may use Microchip software and any derivatives
  * exclusively with Microchip products. It is your responsibility to comply with third party license
@@ -20,7 +20,7 @@
  *
  */
 
-/* file generated from device description version 2018-09-19T14:04:45Z */
+/* file generated from device description version 2019-01-18T21:19:59Z */
 #ifndef _SAME70_AFEC_COMPONENT_H_
 #define _SAME70_AFEC_COMPONENT_H_
 
