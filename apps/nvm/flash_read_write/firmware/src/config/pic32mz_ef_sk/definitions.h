1e5fa00293bc3b75f9972a4e32dcb74dad8f23e7
Bhargav <http://support.microchip.com>
2019-02-21 11:30:13 +0530

    [NVM] Regenerated demo for flash_read_write_pic32mz_ef_sk
    MH3-5984

--- apps/nvm/flash_read_write/firmware/src/config/pic32mz_ef_sk/definitions.h
+++ apps/nvm/flash_read_write/firmware/src/config/pic32mz_ef_sk/definitions.h
@@ -51,7 +51,6 @@
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/gpio/plib_gpio.h"
 #include "peripheral/evic/plib_evic.h"
-#include "peripheral/wdt/plib_wdt.h"
 #include "peripheral/nvm/plib_nvm.h"
 
 // DOM-IGNORE-BEGIN
--- apps/nvm/flash_read_write/firmware/src/config/pic32mz_ef_sk/peripheral/evic/plib_evic.h
+++ apps/nvm/flash_read_write/firmware/src/config/pic32mz_ef_sk/peripheral/evic/plib_evic.h
@@ -51,6 +51,7 @@
 #include <device.h>
 #include <stddef.h>
 #include <stdbool.h>
+#include <device.h>
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
