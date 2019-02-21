9255f484354ac233f4851a6be26ad4dd32c28830
Jain Joseph <jain.joseph@microchip.com>
2019-02-21 12:08:03 +0530

    [AC] Regenerated demo for ac_sleepwalk_singleshot_sam_d20_xpro MH3-5713
    Signed-off-by: Jain Joseph <jain.joseph@microchip.com>

--- apps/ac/ac_sleepwalk_singleshot/firmware/src/config/sam_d20_xpro/peripheral/clock/plib_clock.c
+++ apps/ac/ac_sleepwalk_singleshot/firmware/src/config/sam_d20_xpro/peripheral/clock/plib_clock.c
@@ -108,10 +108,11 @@ void CLOCK_Initialize (void)
     /* Configure the AHB Bridge Clocks */
     PM_REGS->PM_AHBMASK = 0x1f;
 
+
     /* Configure the APBB Bridge Clocks */
     PM_REGS->PM_APBBMASK = 0x1f;
 
+
     /* Configure the APBC Bridge Clocks */
     PM_REGS->PM_APBCMASK = 0x20022;
-
 }
