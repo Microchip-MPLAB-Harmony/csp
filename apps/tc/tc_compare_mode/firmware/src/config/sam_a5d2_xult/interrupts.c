a3a25d910b4e109ee4b11a2cdc85f9a32e48483d
Syam Nair <syam.nair@microchip.com>
2019-02-21 17:48:56 +0530

    [TC] Regenerated demo for tc_compare_mode
    MH3-7042

--- apps/tc/tc_compare_mode/firmware/src/config/sam_a5d2_xult/interrupts.c
+++ apps/tc/tc_compare_mode/firmware/src/config/sam_a5d2_xult/interrupts.c
@@ -69,7 +69,6 @@ void DefaultInterruptHandlerForSpurious( void )
     ++spuriousEventCount;
 }
 
-
 /*******************************************************************************
  End of File
 */
