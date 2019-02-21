400db3f6eba4085ce9441dcc5050d92f519d0d9b
Bhargav <http://support.microchip.com>
2019-02-21 11:36:26 +0530

    [SQI] Regenerated demo for sqi_read_write_pic32mz_ef_sk
    MH3-6267

--- apps/sqi/sqi_read_write/firmware/src/config/pic32mz_ef_sk/initialization.c
+++ apps/sqi/sqi_read_write/firmware/src/config/pic32mz_ef_sk/initialization.c
@@ -159,9 +159,12 @@ void SYS_Initialize ( void* data )
   
     CLK_Initialize();
 	GPIO_Initialize();
+    /* Configure Prefetch, Wait States and ECC */
+    PRECONbits.PREFEN = 3;
+    PRECONbits.PFMWS = 2;
+    CFGCONbits.ECCCON = 3;
 
 
-    WDT_Initialize();
 
     SQI1_Initialize();
 
