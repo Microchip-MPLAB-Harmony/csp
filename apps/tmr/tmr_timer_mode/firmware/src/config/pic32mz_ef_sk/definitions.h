420a907f0284a21d8b8b3cdf5f6eca474a54f977
Bhargav Mehta <bhargav.mehta@microchip.com>
2019-02-21 11:37:10 +0530

    [TMR] Regenerated demo for tmr_timer_mode_pic32mz_ef_sk
    MH3-6339
    
    Signed-off-by: Bhargav Mehta <bhargav.mehta@microchip.com>

--- apps/tmr/tmr_timer_mode/firmware/src/config/pic32mz_ef_sk/definitions.h
+++ apps/tmr/tmr_timer_mode/firmware/src/config/pic32mz_ef_sk/definitions.h
@@ -52,7 +52,6 @@
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/gpio/plib_gpio.h"
 #include "peripheral/evic/plib_evic.h"
-#include "peripheral/wdt/plib_wdt.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
