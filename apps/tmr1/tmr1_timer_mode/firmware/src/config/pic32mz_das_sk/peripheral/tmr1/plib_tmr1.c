a2fa8c15cc0eb7e007bc9aa13c2487307be5ff86
Santosh <http://support.microchip.com>
2019-02-21 13:07:10 +0530

    [TMR1] Regenerated demo for tmr1_timer_mode_pic32mz_das_sk MH3-6341

--- apps/tmr1/tmr1_timer_mode/firmware/src/config/pic32mz_das_sk/peripheral/tmr1/plib_tmr1.c
+++ apps/tmr1/tmr1_timer_mode/firmware/src/config/pic32mz_das_sk/peripheral/tmr1/plib_tmr1.c
@@ -73,7 +73,7 @@ void TMR1_Initialize(void)
     TMR1 = 0x0;
 
     /*Set period */
-    PR1 = 58593;
+    PR1 = 39062;
 
     /* Setup TMR1 Interrupt */
     TMR1_InterruptEnable();  /* Enable interrupt on the way out */
