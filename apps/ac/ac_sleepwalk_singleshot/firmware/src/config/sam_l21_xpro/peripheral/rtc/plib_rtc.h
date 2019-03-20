cccb27d770a1eeb4e47c5eef887c2f557dc43ba9
Yuvaraja M <yuvaraja.m@microchip.com>
2019-03-20 16:12:50 +0530

    [AC] - Added AC Plib Demo for SAML21 MH3-4220 MH3-4220

---
+++ apps/ac/ac_sleepwalk_singleshot/firmware/src/config/sam_l21_xpro/peripheral/rtc/plib_rtc.h
@@ -0,0 +1,92 @@
+/*******************************************************************************
+  Real Time Counter (RTC) PLIB
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    plib_rtc.h
+
+  Summary:
+    RTC PLIB Header file
+
+  Description:
+    This file defines the interface to the RTC peripheral library. This
+    library provides access to and control of the associated peripheral
+    instance.
+
+*******************************************************************************/
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+#ifndef PLIB_RTC_H
+#define PLIB_RTC_H
+
+#include "device.h"
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus // Provide C++ Compatibility
+extern "C" {
+#endif
+// DOM-IGNORE-END
+
+/* Frequency of Counter Clock for RTC */
+#define RTC_COUNTER_CLOCK_FREQUENCY        (1024 / (1 << (0x1 - 1)))
+
+typedef enum
+{
+    RTC_PER0_MASK = 0x0001,
+    RTC_PER1_MASK = 0x0002,
+    RTC_PER2_MASK = 0x0004,
+    RTC_PER3_MASK = 0x0008,
+    RTC_PER4_MASK = 0x0010,
+    RTC_PER5_MASK = 0x0020,
+    RTC_PER6_MASK = 0x0040,
+    RTC_PER7_MASK = 0x0080
+} RTC_PERIODIC_INT_MASK;
+
+
+void RTC_Initialize(void);
+bool RTC_PeriodicIntervalHasCompleted ( RTC_PERIODIC_INT_MASK period );
+bool RTC_Timer32CounterHasOverflowed ( void );
+bool RTC_Timer32Compare0HasMatched( void );
+void RTC_Timer32Start ( void );
+void RTC_Timer32Stop ( void );
+void RTC_Timer32CounterSet ( uint32_t count );
+uint32_t RTC_Timer32CounterGet ( void );
+uint32_t RTC_Timer32FrequencyGet ( void );
+void RTC_Timer32CompareSet ( uint32_t compareValue );
+uint32_t RTC_Timer32PeriodGet ( void );
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+}
+#endif
+// DOM-IGNORE-END
+
+#endif /* PLIB_RTC_H */
---
+++ apps/ac/ac_sleepwalk_singleshot/firmware/src/config/sam_l21_xpro/peripheral/rtc/plib_rtc_timer.c
@@ -0,0 +1,168 @@
+/*******************************************************************************
+  Real Time Counter (RTC) PLIB
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    plib_rtc_timer.c
+
+  Summary:
+    RTC PLIB Implementation file
+
+  Description:
+    This file defines the interface to the RTC peripheral library. This
+    library provides access to and control of the associated peripheral
+    instance in timer mode.
+
+*******************************************************************************/
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+#include "plib_rtc.h"
+#include <stdlib.h>
+
+
+void RTC_Initialize(void)
+{
+    RTC_REGS->MODE0.RTC_CTRLA = RTC_MODE0_CTRLA_SWRST_Msk;
+
+    while((RTC_REGS->MODE0.RTC_SYNCBUSY & RTC_MODE0_SYNCBUSY_SWRST_Msk) == RTC_MODE0_SYNCBUSY_SWRST_Msk)
+    {
+        /* Wait for Synchronization after Software Reset */
+    }
+
+
+    RTC_REGS->MODE0.RTC_CTRLA = RTC_MODE0_CTRLA_MODE(0) | RTC_MODE0_CTRLA_PRESCALER(0x1) | RTC_MODE0_CTRLA_COUNTSYNC_Msk |RTC_MODE0_CTRLA_MATCHCLR_Msk ;
+
+   RTC_REGS->MODE0.RTC_COMP = 0x200;
+
+    RTC_REGS->MODE0.RTC_EVCTRL = 0x100;
+}
+
+
+bool RTC_PeriodicIntervalHasCompleted (RTC_PERIODIC_INT_MASK period)
+{
+   bool periodIntervalComplete = false;
+   if( (RTC_REGS->MODE0.RTC_INTFLAG & period) == period)
+   {
+       periodIntervalComplete = true;
+
+       /* Clear Periodic Interval Interrupt */
+       RTC_REGS->MODE0.RTC_INTFLAG = period;
+   }
+
+    return periodIntervalComplete;
+}
+
+bool RTC_Timer32CompareHasMatched(void)
+{
+   bool status = false;
+
+   if((RTC_REGS->MODE0.RTC_INTFLAG & RTC_MODE0_INTFLAG_CMP0_Msk) == RTC_MODE0_INTFLAG_CMP0_Msk)
+   {
+       status = true;
+       RTC_REGS->MODE0.RTC_INTFLAG = RTC_MODE0_INTFLAG_CMP0_Msk;
+   }
+
+   return status;
+}
+bool RTC_Timer32CounterHasOverflowed ( void )
+{
+   bool status = false;
+
+   if((RTC_REGS->MODE0.RTC_INTFLAG & RTC_MODE0_INTFLAG_OVF_Msk) == RTC_MODE0_INTFLAG_OVF_Msk)
+   {
+       status = true;
+       RTC_REGS->MODE0.RTC_INTFLAG = RTC_MODE0_INTFLAG_OVF_Msk;
+   }
+
+   return status;
+}
+
+
+void RTC_Timer32Start ( void )
+{
+    RTC_REGS->MODE0.RTC_CTRLA |= RTC_MODE0_CTRLA_ENABLE_Msk;
+
+    while((RTC_REGS->MODE0.RTC_SYNCBUSY & RTC_MODE0_SYNCBUSY_ENABLE_Msk) == RTC_MODE0_SYNCBUSY_ENABLE_Msk)
+    {
+        /* Wait for synchronization after Enabling RTC */
+    }
+}
+
+
+void RTC_Timer32Stop ( void )
+{
+    RTC_REGS->MODE0.RTC_CTRLA &= ~(RTC_MODE0_CTRLA_ENABLE_Msk);
+
+    while((RTC_REGS->MODE0.RTC_SYNCBUSY & RTC_MODE0_SYNCBUSY_ENABLE_Msk) == RTC_MODE0_SYNCBUSY_ENABLE_Msk)
+    {
+        /* Wait for Synchronization after Disabling RTC */
+    }
+}
+
+void RTC_Timer32CounterSet ( uint32_t count )
+{
+    RTC_REGS->MODE0.RTC_COUNT = count;
+
+    while((RTC_REGS->MODE0.RTC_SYNCBUSY & RTC_MODE0_SYNCBUSY_COUNT_Msk) == RTC_MODE0_SYNCBUSY_COUNT_Msk)
+    {
+        /* Wait for Synchronization after writing value to Count Register */
+    }
+}
+
+void RTC_Timer32CompareSet ( uint32_t compareValue )
+{
+    RTC_REGS->MODE0.RTC_COMP = compareValue;
+
+    while((RTC_REGS->MODE0.RTC_SYNCBUSY & RTC_MODE0_SYNCBUSY_COMP0_Msk) == RTC_MODE0_SYNCBUSY_COMP0_Msk)
+    {
+        /* Wait for Synchronization after writing Compare Value */
+    }
+}
+uint32_t RTC_Timer32CounterGet ( void )
+{
+    while((RTC_REGS->MODE0.RTC_SYNCBUSY & RTC_MODE0_SYNCBUSY_COUNT_Msk) == RTC_MODE0_SYNCBUSY_COUNT_Msk)
+    {
+        /* Wait for Synchronization before reading value from Count Register */
+    }
+
+    return(RTC_REGS->MODE0.RTC_COUNT + 4);
+}
+
+uint32_t RTC_Timer32PeriodGet ( void )
+{
+    /* Get 32Bit Compare Value */
+    return (RTC_MODE0_COUNT_COUNT_Msk);
+}
+
+uint32_t RTC_Timer32FrequencyGet ( void )
+{
+    /* Return Frequency of RTC Clock */
+    return RTC_COUNTER_CLOCK_FREQUENCY;
+}
+
+
