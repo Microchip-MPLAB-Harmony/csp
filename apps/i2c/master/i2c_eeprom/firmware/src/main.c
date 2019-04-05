75724ccb894176c185c679ca0e0c25a7660c01da
Santosh Tangudu <santosh.tangudu@microchip.com>
2019-04-05 12:42:07 +0530

    [I2C] Created and generated PIC32MX_470 I2C PLIB i2c_eeprom demo
    MH3-8254

---
+++ apps/i2c/master/i2c_eeprom/firmware/src/main.c
@@ -0,0 +1,249 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+#include <string.h>
+#define LED_ON()                       LED1_Set()
+#define LED_OFF()                      LED1_Clear()
+
+#define APP_AT24MAC_DEVICE_ADDR             0x0054
+#define APP_AT24MAC_MEMORY_ADDR             0x00
+#define APP_AT24MAC_MEMORY_ADDR1            0x00
+#define APP_TRANSMIT_DATA_LENGTH            6
+#define APP_ACK_DATA_LENGTH                 1
+#define APP_RECEIVE_DUMMY_WRITE_LENGTH      2
+#define APP_RECEIVE_DATA_LENGTH             4
+
+static uint8_t testTxData[APP_TRANSMIT_DATA_LENGTH] =
+{
+    APP_AT24MAC_MEMORY_ADDR,APP_AT24MAC_MEMORY_ADDR1,
+    'M','C','H','P',
+};
+
+static uint8_t  testRxData[APP_RECEIVE_DATA_LENGTH];
+
+typedef enum
+{
+    APP_STATE_EEPROM_STATUS_VERIFY,
+    APP_STATE_EEPROM_WRITE,
+    APP_STATE_EEPROM_WAIT_WRITE_COMPLETE,
+    APP_STATE_EEPROM_CHECK_INTERNAL_WRITE_STATUS,
+    APP_STATE_EEPROM_READ,
+    APP_STATE_EEPROM_WAIT_READ_COMPLETE,
+    APP_STATE_VERIFY,
+    APP_STATE_IDLE,
+    APP_STATE_XFER_SUCCESSFUL,
+    APP_STATE_XFER_ERROR
+
+} APP_STATES;
+
+typedef enum
+{
+    APP_TRANSFER_STATUS_IN_PROGRESS,
+    APP_TRANSFER_STATUS_SUCCESS,
+    APP_TRANSFER_STATUS_ERROR,
+    APP_TRANSFER_STATUS_IDLE,
+
+} APP_TRANSFER_STATUS;
+
+void APP_I2CCallback(uintptr_t context )
+{
+    APP_TRANSFER_STATUS* transferStatus = (APP_TRANSFER_STATUS*)context;
+
+    if(I2C1_ErrorGet() == I2C_ERROR_NONE)
+    {
+        if (transferStatus)
+        {
+            *transferStatus = APP_TRANSFER_STATUS_SUCCESS;
+        }
+    }
+    else
+    {
+        if (transferStatus)
+        {
+            *transferStatus = APP_TRANSFER_STATUS_ERROR;
+        }
+    }
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    APP_STATES state = APP_STATE_EEPROM_STATUS_VERIFY;
+    volatile APP_TRANSFER_STATUS transferStatus = APP_TRANSFER_STATUS_ERROR;
+    uint8_t ackData = 0;
+
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+
+    while(1)
+    {
+        /* Check the application's current state. */
+        switch (state)
+        {
+            case APP_STATE_EEPROM_STATUS_VERIFY:
+
+                /* Register the TWIHS Callback with transfer status as context */
+                I2C1_CallbackRegister( APP_I2CCallback, (uintptr_t)&transferStatus );
+
+               /* Verify if EEPROM is ready to accept new requests */
+                transferStatus = APP_TRANSFER_STATUS_IN_PROGRESS;
+                I2C1_Write(APP_AT24MAC_DEVICE_ADDR, &ackData, APP_ACK_DATA_LENGTH);
+
+                state = APP_STATE_EEPROM_WRITE;
+                break;
+
+            case APP_STATE_EEPROM_WRITE:
+
+                if (transferStatus == APP_TRANSFER_STATUS_SUCCESS)
+                {
+                    /* Write data to EEPROM */
+                    transferStatus = APP_TRANSFER_STATUS_IN_PROGRESS;
+                    I2C1_Write(APP_AT24MAC_DEVICE_ADDR, &testTxData[0], APP_TRANSMIT_DATA_LENGTH);
+                    state = APP_STATE_EEPROM_WAIT_WRITE_COMPLETE;
+                }
+                else if (transferStatus == APP_TRANSFER_STATUS_ERROR)
+                {
+                    /* EEPROM is not ready to accept new requests */
+                    state = APP_STATE_XFER_ERROR;
+                }
+                break;
+
+            case APP_STATE_EEPROM_WAIT_WRITE_COMPLETE:
+
+                if (transferStatus == APP_TRANSFER_STATUS_SUCCESS)
+                {
+                    /* Read the status of internal write cycle */
+                    transferStatus = APP_TRANSFER_STATUS_IN_PROGRESS;
+                    I2C1_Write(APP_AT24MAC_DEVICE_ADDR, &ackData, APP_ACK_DATA_LENGTH);
+                    state = APP_STATE_EEPROM_CHECK_INTERNAL_WRITE_STATUS;
+                }
+                else if (transferStatus == APP_TRANSFER_STATUS_ERROR)
+                {
+                    state = APP_STATE_XFER_ERROR;
+                }
+                break;
+
+             case APP_STATE_EEPROM_CHECK_INTERNAL_WRITE_STATUS:
+
+                if (transferStatus == APP_TRANSFER_STATUS_SUCCESS)
+                {
+                    state = APP_STATE_EEPROM_READ;
+                }
+                else if (transferStatus == APP_TRANSFER_STATUS_ERROR)
+                {
+                    /* EEPROM's internal write cycle is not complete. Keep checking. */
+                    transferStatus = APP_TRANSFER_STATUS_IN_PROGRESS;
+                    I2C1_Write(APP_AT24MAC_DEVICE_ADDR, &ackData, APP_ACK_DATA_LENGTH);
+                }
+                break;
+
+            case APP_STATE_EEPROM_READ:
+
+                transferStatus = APP_TRANSFER_STATUS_IN_PROGRESS;
+                /* Read the data from the page written earlier */
+                I2C1_WriteRead(APP_AT24MAC_DEVICE_ADDR, &testTxData[0], APP_RECEIVE_DUMMY_WRITE_LENGTH,  &testRxData[0], APP_RECEIVE_DATA_LENGTH);
+
+                state = APP_STATE_EEPROM_WAIT_READ_COMPLETE;
+
+                break;
+
+            case APP_STATE_EEPROM_WAIT_READ_COMPLETE:
+
+                if (transferStatus == APP_TRANSFER_STATUS_SUCCESS)
+                {
+                    state = APP_STATE_VERIFY;
+                }
+                else if (transferStatus == APP_TRANSFER_STATUS_ERROR)
+                {
+                    state = APP_STATE_XFER_ERROR;
+                }
+                break;
+
+            case APP_STATE_VERIFY:
+
+                if (memcmp(&testTxData[2], &testRxData[0], APP_RECEIVE_DATA_LENGTH != 0))
+                {
+                    /* It means received data is not same as transmitted data */
+                    state = APP_STATE_XFER_ERROR;
+                }
+                else
+                {
+                    /* It means received data is same as transmitted data */
+                    state = APP_STATE_XFER_SUCCESSFUL;
+                }
+                break;
+
+            case APP_STATE_XFER_SUCCESSFUL:
+            {
+                LED_ON();
+                break;
+            }
+            case APP_STATE_XFER_ERROR:
+            {
+                LED_OFF();
+                break;
+            }
+            default:
+                break;
+        }
+    }
+}
+/*******************************************************************************
+ End of File
+*/
+
