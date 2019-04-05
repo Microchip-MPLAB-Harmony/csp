b9346c50f7b52d3dd9de2455e24ee52a2f5824c9
Dan <http://support.microchip.com>
2019-04-05 13:09:23 -0700

    [qspi_read_write] Add SAM_9X60_EK configuration
    MH3-7473

---
+++ apps/qspi/qspi_read_write/firmware/src/app_sam_9x60_ek.h
@@ -0,0 +1,333 @@
+/*******************************************************************************
+  Application Header File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    app.h
+
+  Summary:
+    This header file provides prototypes and definitions for the application.
+
+  Description:
+    This header file provides function prototypes and data type definitions for
+    the application.  Some of these are required by the system (such as the
+    "APP_Initialize" and "APP_Tasks" prototypes) and some of them are only used
+    internally by the application (such as the "APP_STATES" definition).  Both
+    are defined here for convenience.
+*******************************************************************************/
+
+//DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+//DOM-IGNORE-END
+
+#ifndef APP_H
+#define APP_H
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stdlib.h>
+#include <string.h>
+#include "definitions.h"
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+extern "C" {
+
+#endif
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Type Definitions
+// *****************************************************************************
+// *****************************************************************************
+
+#define PAGE_SIZE                  (256U)
+#define SECTOR_SIZE                (4096U)
+
+/* Erase, Write and Read 80KBytes of memory */
+#define SECTORS_TO_EWR             (20U)
+
+#define BUFFER_SIZE                (SECTOR_SIZE * SECTORS_TO_EWR)
+
+#define MEM_START_ADDRESS          (0x0U)
+
+#define SST26VF032B_JEDEC_ID       (0x004326BFUL)
+
+#define LED_ON                     LED_Set
+#define LED_OFF                    LED_Clear
+#define LED_TOGGLE                 LED_Toggle
+
+// *****************************************************************************
+/* Application states
+
+  Summary:
+    Application states enumeration
+
+  Description:
+    This enumeration defines the valid application states.  These states
+    determine the behavior of the application at various times.
+*/
+
+typedef enum
+{
+    /* The app mounts the disk */
+    APP_STATE_INIT = 0,
+
+    /* Reset Flash*/
+    APP_STATE_RESET_FLASH,
+
+    /* Enable Quad IO Mode*/
+    APP_STATE_ENABLE_QUAD_IO,
+
+    /* Unlock Flash*/
+    APP_STATE_UNLOCK_FLASH,
+
+    /* Read JEDEC ID*/
+    APP_STATE_READ_JEDEC_ID,
+
+    /* Erase Flash */
+    APP_STATE_ERASE_FLASH,
+
+    /* Erase Wait */
+    APP_STATE_ERASE_WAIT,
+
+    /* Write to Memory */
+    APP_STATE_WRITE_MEMORY,
+
+    /* Erase Wait */
+    APP_STATE_WRITE_WAIT,
+
+    /* Read From Memory */
+    APP_STATE_READ_MEMORY,
+
+    /* Verify Data Read */
+    APP_STATE_VERIFY_DATA,
+
+    /* The app idles */
+    APP_STATE_SUCCESS,
+
+    /* An app error has occurred */
+    APP_STATE_ERROR
+
+} APP_STATES;
+
+// *****************************************************************************
+/* Application Data
+
+  Summary:
+    Holds application data
+
+  Description:
+    This structure holds the application's data.
+
+  Remarks:
+    Application strings and buffers are be defined outside this structure.
+ */
+
+typedef struct
+{
+    /* Application's current state */
+    APP_STATES state;
+
+    /* Application transfer status */
+    volatile bool xfer_done;
+
+    /* Jedec-ID*/
+    uint32_t jedec_id;
+
+    /* Read Buffer */
+    uint8_t readBuffer[BUFFER_SIZE];
+
+    /* Write Buffer*/
+    uint8_t writeBuffer[BUFFER_SIZE];
+} APP_DATA;
+
+/* SST26 Command set
+
+  Summary:
+    Enumeration listing the SST26VF commands.
+
+  Description:
+    This enumeration defines the commands used to interact with the SST26VF
+    series of devices.
+
+  Remarks:
+    None
+*/
+
+typedef enum
+{
+    /* Reset enable command. */
+    SST26_CMD_FLASH_RESET_ENABLE = 0x66,
+
+    /* Command to reset the flash. */
+    SST26_CMD_FLASH_RESET        = 0x99,
+
+    /* Command to Enable QUAD IO */
+    SST26_CMD_ENABLE_QUAD_IO     = 0x38,
+
+    /* Command to Reset QUAD IO */
+    SST26_CMD_RESET_QUAD_IO      = 0xFF,
+
+    /* Command to read JEDEC-ID of the flash device. */
+    SST26_CMD_JEDEC_ID_READ      = 0x9F,
+
+    /* QUAD Command to read JEDEC-ID of the flash device. */
+    SST26_CMD_QUAD_JEDEC_ID_READ = 0xAF,
+
+    /* Command to perfrom High Speed Read */
+    SST26_CMD_HIGH_SPEED_READ    = 0x0B,
+
+    /* Write enable command. */
+    SST26_CMD_WRITE_ENABLE       = 0x06,
+
+    /* Page Program command. */
+    SST26_CMD_PAGE_PROGRAM       = 0x02,
+
+    /* Command to read the Flash status register. */
+    SST26_CMD_READ_STATUS_REG    = 0x05,
+
+    /* Command to perform sector erase */
+    SST26_CMD_SECTOR_ERASE       = 0x20,
+
+    /* Command to perform Bulk erase */
+    SST26_CMD_BULK_ERASE_64K     = 0xD8,
+
+    /* Command to perform Chip erase */
+    SST26_CMD_CHIP_ERASE         = 0xC7,
+
+    /* Command to unlock the flash device. */
+    SST26_CMD_UNPROTECT_GLOBAL   = 0x98
+
+} SST26_CMD;
+
+typedef enum
+{
+    /* Buffer is being processed */
+    APP_TRANSFER_BUSY,
+    /* APP Buffer transfer is successfully completed*/
+    APP_TRANSFER_COMPLETED,
+    /* APP Buffer transfer had error*/
+    APP_TRANSFER_ERROR_UNKNOWN,
+    /* APP Transfer has not been asked yet */
+    APP_TRANSFER_IDLE
+} APP_TRANSFER_STATUS;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Callback Routines
+// *****************************************************************************
+// *****************************************************************************
+/* These routines are called by drivers when certain events occur.
+*/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Initialization and State Machine Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/*******************************************************************************
+  Function:
+    void APP_Initialize ( void )
+
+  Summary:
+     Application initialization routine.
+
+  Description:
+    This function initializes the application.  It places the
+    application in its initial state and prepares it to run so that its
+    APP_Tasks function can be called.
+
+  Parameters:
+    None.
+
+  Returns:
+    None.
+
+  Example:
+    <code>
+    APP_Initialize();
+    </code>
+
+  Remarks:
+    This routine must be called from the main function.
+*/
+
+void APP_Initialize ( void );
+
+
+/*******************************************************************************
+  Function:
+    void APP_Tasks ( void )
+
+  Summary:
+    Application tasks function
+
+  Description:
+    This routine is the Application's tasks function.  It
+    defines the application's state machine and core logic.
+
+  Precondition:
+    The system and application initialization ("SYS_Initialize") should be
+    called before calling this.
+
+  Parameters:
+    None.
+
+  Returns:
+    None.
+
+  Example:
+    <code>
+    APP_Tasks();
+    </code>
+
+  Remarks:
+    This routine must be called from SYS_Tasks() routine.
+ */
+
+void APP_Tasks( void );
+
+
+#endif /* APP_H */
+
+//DOM-IGNORE-BEGIN
+#ifdef __cplusplus
+}
+#endif
+//DOM-IGNORE-END
+
+/*******************************************************************************
+ End of File
+ */
+
---
+++ apps/qspi/qspi_read_write/firmware/src/config/sam_9x60_ek/peripheral/pit64b/plib_pit64b.c
@@ -0,0 +1,135 @@
+/*******************************************************************************
+  Periodic Interval Timer (PIT64B) 
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    plib_pit64b.h
+
+  Summary:
+    Periodic Interval Timer (PIT64B) PLIB.
+
+  Description:
+    This file declares the interface for the Periodic Interval Timer (PIT64B).
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+#include "plib_pit64b.h"
+
+typedef struct
+{
+    PIT64B_CALLBACK callback;
+    uintptr_t context;
+    volatile uint32_t tickCounter;
+    bool running;
+} PIT64B_OBJECT;
+
+static PIT64B_OBJECT pit64b;
+
+void PIT64B_TimerInitialize(void)
+{
+    PIT64B_REGS->PIT64B_CR = PIT64B_CR_SWRST_Msk;
+    PIT64B_REGS->PIT64B_MR = PIT64B_MR_CONT(0) | PIT64B_MR_SGCLK(0) | PIT64B_MR_PRESCALER(0);
+    PIT64B_REGS->PIT64B_MSBPR = 0;
+    PIT64B_REGS->PIT64B_LSBPR = 0;
+    PIT64B_REGS->PIT64B_MR |= PIT64B_MR_SMOD(0);
+    pit64b.running = 0;
+}
+
+void PIT64B_TimerRestart(void)
+{
+    PIT64B_TimerInitialize();
+    PIT64B_TimerStart();
+    pit64b.running = 1;
+}
+
+void PIT64B_TimerStart(void)
+{
+    PIT64B_REGS->PIT64B_CR = PIT64B_CR_START_Msk;
+    pit64b.running = 1;
+}
+
+void PIT64B_TimerStop(void)
+{
+    PIT64B_TimerInitialize();
+    pit64b.running = 0;
+}
+
+void PIT64B_TimerPeriodSet(uint64_t period)
+{
+    PIT64B_REGS->PIT64B_MSBPR = (period & 0xFFFFFFFF00000000)>>32;
+    PIT64B_REGS->PIT64B_LSBPR = (period & 0xFFFFFFFF);
+}
+
+uint64_t PIT64B_TimerPeriodGet(void)
+{
+    uint64_t reg = PIT64B_REGS->PIT64B_MSBPR;
+    reg = reg<<32;
+    reg |= PIT64B_REGS->PIT64B_LSBPR;
+    return reg;
+}
+
+uint64_t PIT64B_TimerCounterGet(void)
+{
+    uint64_t reg = PIT64B_REGS->PIT64B_TMSBR;
+    reg = reg<<32;
+    reg |= PIT64B_REGS->PIT64B_TLSBR;
+    return reg;
+}
+
+uint32_t PIT64B_TimerFrequencyGet(void)
+{
+    return 200000000;
+}
+
+void PIT64B_DelayMs(uint32_t delay)
+{
+    uint32_t tickStart;
+    uint32_t delayTicks;
+
+    if (pit64b.running && ((PIT64B_REGS->PIT64B_IMR & PIT64B_IMR_PERIOD_Msk) == PIT64B_IMR_PERIOD_Msk)) {
+        tickStart=pit64b.tickCounter;
+        delayTicks=1000*delay/1000;
+
+        while((pit64b.tickCounter-tickStart) < delayTicks);
+    }
+}
+
+void PIT64B_TimerCallbackSet(PIT64B_CALLBACK callback, uintptr_t context)
+{
+    pit64b.callback = callback;
+    pit64b.context = context;
+}
+
+void PIT64B_InterruptHandler(void)
+{
+    uint32_t reg = PIT64B_REGS->PIT64B_ISR;
+    pit64b.tickCounter++;
+    if(pit64b.callback)
+        pit64b.callback(pit64b.context);
+}
