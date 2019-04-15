17baf46c7910ad3bc767864f1e41585c96f0f4da
Santosh <http://support.microchip.com>
2019-04-15 15:09:26 +0530

    [CMP] Created and generated PIC32MX_XLP CMP PLIB cmp_configurable_reference demo
    MH3-8672

---
+++ apps/cmp/cmp_configurable_reference/firmware/src/config/pic32mx_xlp_sk/peripheral/cvr/plib_cvr.h
@@ -0,0 +1,77 @@
+/*******************************************************************************
+  Comparator Voltage Reference (CVR) Peripheral Library Interface Header File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    plib_cvr.h
+
+  Summary:
+    CVR PLIB Header File
+
+  Description:
+    None
+
+*******************************************************************************/
+
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+
+#ifndef _PLIB_CVR_H
+#define _PLIB_CVR_H
+
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "device.h"
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+    extern "C" {
+#endif
+// DOM-IGNORE-END
+
+
+// *****************************************************************************
+// Section: Interface
+// *****************************************************************************
+// *****************************************************************************
+
+/*************************** CVR API ***************************/
+
+void CVR_Initialize (void);
+
+void CVR_Start (void);
+
+void CVR_Stop (void);
+
+void CVR_UpdateValue (uint8_t value);
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+    }
+#endif
+
+// DOM-IGNORE-END
+#endif // _PLIB_CVR_H
