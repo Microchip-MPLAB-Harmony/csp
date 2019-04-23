10f3ef7ed4ebcf373ccff109042e0501d1779baf
Syam <http://support.microchip.com>
2019-04-23 20:38:12 +0530

    [TC] Add Compare mode demo for SAM9x60
    MH3-7370

---
+++ apps/tc/tc_compare_mode/firmware/src/config/sam_9x60_ek/peripheral/tc/plib_tc0.c
@@ -0,0 +1,145 @@
+/*******************************************************************************
+  TC Peripheral Library Interface Source File
+
+  Company
+    Microchip Technology Inc.
+
+  File Name
+    plib_tc0.c
+
+  Summary
+    TC peripheral library source file.
+
+  Description
+    This file implements the interface to the TC peripheral library.  This
+    library provides access to and control of the associated peripheral
+    instance.
+
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+/*  This section lists the other files that are included in this file.
+*/
+#include "device.h"
+#include "plib_tc0.h"
+ 
+
+ 
+
+ 
+ 
+
+ 
+
+ 
+
+
+/* Initialize channel in compare mode */
+void TC0_CH1_CompareInitialize (void)
+{
+    /* clock selection and waveform selection */
+    TC0_REGS->TC_CHANNEL[1].TC_CMR = TC_CMR_TCCLKS_TIMER_CLOCK2 | TC_CMR_WAVEFORM_WAVSEL_UP_RC | TC_CMR_WAVE_Msk | \
+            TC_CMR_WAVEFORM_ACPA_CLEAR | TC_CMR_WAVEFORM_ACPC_SET | TC_CMR_WAVEFORM_AEEVT_CLEAR \
+           | TC_CMR_WAVEFORM_BCPB_SET | TC_CMR_WAVEFORM_BCPC_CLEAR | TC_CMR_WAVEFORM_BEEVT_CLEAR ;
+
+    /* external reset event configurations */
+    TC0_REGS->TC_CHANNEL[1].TC_CMR |= TC_CMR_WAVEFORM_ENETRG_Msk | TC_CMR_WAVEFORM_EEVT_XC0 | \
+                TC_CMR_WAVEFORM_EEVTEDG_NONE;
+
+    /* write period */
+    TC0_REGS->TC_CHANNEL[1].TC_RC = 250000U;
+
+    /* write compare values */
+    TC0_REGS->TC_CHANNEL[1].TC_RA = 187500U;
+    TC0_REGS->TC_CHANNEL[1].TC_RB = 3000U;
+
+}
+
+/* Start the compare mode */
+void TC0_CH1_CompareStart (void)
+{
+    TC0_REGS->TC_CHANNEL[1].TC_CCR = (TC_CCR_CLKEN_Msk | TC_CCR_SWTRG_Msk);
+}
+
+/* Stop the compare mode */
+void TC0_CH1_CompareStop (void)
+{
+    TC0_REGS->TC_CHANNEL[1].TC_CCR = (TC_CCR_CLKDIS_Msk);
+}
+
+uint32_t TC0_CH1_CompareFrequencyGet( void )
+{
+    return (uint32_t)(25000000UL);
+}
+
+/* Configure the period value */
+void TC0_CH1_ComparePeriodSet (uint32_t period)
+{
+    TC0_REGS->TC_CHANNEL[1].TC_RC = period;
+}
+
+/* Read the period value */
+uint32_t TC0_CH1_ComparePeriodGet (void)
+{
+    return TC0_REGS->TC_CHANNEL[1].TC_RC;
+}
+
+/* Set the compare A value */
+void TC0_CH1_CompareASet (uint32_t value)
+{
+    TC0_REGS->TC_CHANNEL[1].TC_RA = value;
+}
+
+/* Set the compare B value */
+void TC0_CH1_CompareBSet (uint32_t value)
+{
+    TC0_REGS->TC_CHANNEL[1].TC_RB = value;
+}
+
+TC_COMPARE_STATUS TC0_CH1_CompareStatusGet(void)
+{
+    return (TC_COMPARE_STATUS)(TC0_REGS->TC_CHANNEL[1].TC_SR & TC_COMPARE_STATUS_MSK);
+}
+ 
+
+ 
+ 
+
+ 
+
+ 
+ 
+/**
+ End of File
+*/
---
+++ apps/tc/tc_compare_mode/firmware/src/main_sam_9x60.c
@@ -0,0 +1,69 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+
+// *****************************************************************************
+// *****************************************************************************
+// This demo project generates one PWM outputs with a period of 10ms and
+// 75% duty cycle. PWM signal is output through PINS PA22 (TX pin on Mikrobus
+// connector. 
+// *****************************************************************************
+// *****************************************************************************
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+    
+    /* Disable the WILC3000 Wifi module to enable mikrobus flexcom output */
+    WILC_EN_Clear();
+    
+    TC0_CH1_CompareStart();
+
+    while ( true )
+    {
+        /* Maintain state machines of all polled MPLAB Harmony modules. */
+        SYS_Tasks ( );
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+
