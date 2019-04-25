971c4795258596f26731d9f9bc063653c11a1382
Dan <http://support.microchip.com>
2019-04-25 14:39:00 -0700

    [spi_self_loopback_interrupt] SAMA5D2 CSP 3.3 DFT
    MH3-11054

--- apps/spi/spi_self_loopback_interrupt/firmware/src/config/sam_a5d2_xult/interrupts.c
+++ apps/spi/spi_self_loopback_interrupt/firmware/src/config/sam_a5d2_xult/interrupts.c
@@ -160,7 +160,7 @@ void SYSC_SharedHandler( void )
 
 /* data for irq register initialization */
 IrqData irqData[] = {
-    { 34,  (uint32_t) AIC_REGS,    SPI1_InterruptHandler,      AIC_SMR_SRCTYPE_INT_LEVEL_SENSITIVE_Val,  AIC_SMR_PRIORITY_MINIMUM_Val   },
+    { 34,  (uint32_t) AIC_REGS,    SPI1_InterruptHandler,      AIC_SMR_SRCTYPE_INT_LEVEL_SENSITIVE_Val,  0x0 },
 };
 
 uint32_t irqDataEntryCount = sizeof( irqData ) / sizeof( irqData[ 0 ]);
