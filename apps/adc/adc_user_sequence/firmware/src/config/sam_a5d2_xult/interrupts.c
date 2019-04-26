2b108e23ae8f02d2c1753f4800f8802e7e83ae4c
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2019-04-26 12:43:03 +0530

    [ADC] DFT regenerated and tested adc_user_sequence demo
    MH3-10970

--- apps/adc/adc_user_sequence/firmware/src/config/sam_a5d2_xult/interrupts.c
+++ apps/adc/adc_user_sequence/firmware/src/config/sam_a5d2_xult/interrupts.c
@@ -161,8 +161,8 @@ void SYSC_SharedHandler( void )
 
 /* data for irq register initialization */
 IrqData irqData[] = {
-    { 3,   (uint32_t) AIC_REGS,    PIT_InterruptHandler,       AIC_SMR_SRCTYPE_INT_LEVEL_SENSITIVE_Val,  AIC_SMR_PRIORITY_MINIMUM_Val   },
-    { 40,  (uint32_t) AIC_REGS,    ADC_InterruptHandler,       AIC_SMR_SRCTYPE_INT_LEVEL_SENSITIVE_Val,  AIC_SMR_PRIORITY_MINIMUM_Val   },
+    { 3,   (uint32_t) AIC_REGS,    PIT_InterruptHandler,       AIC_SMR_SRCTYPE_INT_LEVEL_SENSITIVE_Val,  0x0 },
+    { 40,  (uint32_t) AIC_REGS,    ADC_InterruptHandler,       AIC_SMR_SRCTYPE_INT_LEVEL_SENSITIVE_Val,  0x0 },
 };
 
 uint32_t irqDataEntryCount = sizeof( irqData ) / sizeof( irqData[ 0 ]);
