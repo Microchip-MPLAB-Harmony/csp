6d2adee91299bbfaeac69bf11a493732accaf6a2
Bhargav <http://support.microchip.com>
2019-04-26 15:03:06 +0530

    [RSWDT] Regenerated demo for rswdt_rswdt_timeout_sam_v71_xult
    MH3-10376

--- apps/rswdt/rswdt_timeout/firmware/src/config/sam_v71_xult/definitions.h
+++ apps/rswdt/rswdt_timeout/firmware/src/config/sam_v71_xult/definitions.h
@@ -55,6 +55,7 @@
 #include "peripheral/systick/plib_systick.h"
 #include "peripheral/rswdt/plib_rswdt.h"
 #include "peripheral/wdt/plib_wdt.h"
+#include "peripheral/efc/plib_efc.h"
 #include "peripheral/usart/plib_usart1.h"
 
 // DOM-IGNORE-BEGIN
