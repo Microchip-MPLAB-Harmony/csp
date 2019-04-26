f1977e1079141516e43c52bcbe81667173a565fa
Santosh <http://support.microchip.com>
2019-04-26 15:20:14 +0530

    [CMP] Regenerated demo for cmp_cmp_configurable_reference_pic32mz_ef_sk MH3-9932

--- apps/cmp/cmp_configurable_reference/firmware/src/config/pic32mz_ef_sk/peripheral/cmp/plib_cmp.c
+++ apps/cmp/cmp_configurable_reference/firmware/src/config/pic32mz_ef_sk/peripheral/cmp/plib_cmp.c
@@ -15,8 +15,9 @@
 
 *******************************************************************************/
 
+// DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018-2019 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -37,65 +38,55 @@
 * ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
 * THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
 *******************************************************************************/
+// DOM-IGNORE-END
 
 #include "plib_cmp.h"
 
 // *****************************************************************************
-
 // *****************************************************************************
 // Section: CMP Implementation
 // *****************************************************************************
 // *****************************************************************************
 
-void CMP_Initialize (void)
+void CMP_Initialize(void)
 {
     /*  Setup CM1CON    */
-    /*  CCH     = 2     */
-    /*  CREF    = 1        */
-    /*  EVPOL   = 1       */
-    /*  CPOL    = false  */
-    /*  COE     = false   */
-
+    /*  CCH     = 2    */
+    /*  CREF    = 1    */
+    /*  EVPOL   = 1    */
+    /*  CPOL    = false    */
+    /*  COE     = false    */
     CM1CON = 0x52;
 
+    /* Enable Comparator 1 Interrupt */
     IEC4SET = _IEC4_CMP1IE_MASK;
 
     /*  Setup CM2CON    */
-    /*  CCH     = 0     */
-    /*  CREF    = 0        */
-    /*  EVPOL   = 0       */
-    /*  CPOL    = false  */
-    /*  COE     = false   */
-
+    /*  CCH     = 0    */
+    /*  CREF    = 0    */
+    /*  EVPOL   = 0    */
+    /*  CPOL    = false    */
+    /*  COE     = false    */
     CM2CON = 0x0;
-    
+
+    /* Clear Idle Control */
+    CMSTATCLR = _CMSTAT_SIDL_MASK;
 }
 
-void CMP_1_CompareEnable (void)
-{
-    CM1CONSET = _CM1CON_ON_MASK;
-}
-
-void CMP_1_CompareDisable (void)
-{
-    CM1CONCLR = _CM1CON_ON_MASK;
-}
-
-void CMP_2_CompareEnable (void)
-{
-    CM2CONSET = _CM2CON_ON_MASK;
-}
-
-void CMP_2_CompareDisable (void)
-{
-    CM2CONCLR = _CM2CON_ON_MASK;
-}
-
-bool CMP_StatusGet (CMP_STATUS_SOURCE ch_status)
+bool CMP_StatusGet(CMP_STATUS_SOURCE ch_status)
 {
     return ((CMSTAT & ch_status)?true:false);
 }
 
+void CMP_1_CompareEnable(void)
+{
+    CM1CONSET = _CM1CON_ON_MASK;
+}
+
+void CMP_1_CompareDisable(void)
+{
+    CM1CONCLR = _CM1CON_ON_MASK;
+}
 
 CMP_OBJECT cmp1Obj;
 
@@ -116,5 +107,32 @@ void COMPARATOR_1_InterruptHandler(void)
     }
 }
 
+void CMP_2_CompareEnable(void)
+{
+    CM2CONSET = _CM2CON_ON_MASK;
+}
 
+void CMP_2_CompareDisable(void)
+{
+    CM2CONCLR = _CM2CON_ON_MASK;
+}
+
+CMP_OBJECT cmp2Obj;
+
+void CMP_2_CallbackRegister(CMP_CALLBACK callback, uintptr_t context)
+{
+    cmp2Obj.callback = callback;
+
+    cmp2Obj.context = context;
+}
+
+void COMPARATOR_2_InterruptHandler(void)
+{
+    IFS4CLR = _IFS4_CMP2IF_MASK; //Clear IRQ flag
+
+    if(cmp2Obj.callback != NULL)
+    {
+        cmp2Obj.callback(cmp2Obj.context);
+    }
+}
 
