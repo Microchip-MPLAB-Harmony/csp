bf7e8f7914ac8f11fe51f716f4131408a41f3b56
Bhargav <http://support.microchip.com>
2019-04-26 15:18:02 +0530

    [RTC] Regenerated demo for rtc_rtc_alarm_sam_e70_xult
    MH3-10386

--- apps/rtc/rtc_alarm/firmware/src/config/sam_e70_xult/definitions.h
+++ apps/rtc/rtc_alarm/firmware/src/config/sam_e70_xult/definitions.h
@@ -52,6 +52,7 @@
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/nvic/plib_nvic.h"
+#include "peripheral/efc/plib_efc.h"
 #include "peripheral/rtc/plib_rtc.h"
 #include "peripheral/usart/plib_usart1.h"
 
