afb1ddb06f391e12db039eb89d8a40b0ee473e0a
Prachi Phadke <prachi.phadke@microchip.com>
2019-04-28 23:29:40 +0530

    [TC] Regenerated and tested tc_tc_timer_mode_sam_v71_xult
    MH3-10785

--- apps/tc/tc_timer_mode/firmware/src/config/sam_v71_xult/definitions.h
+++ apps/tc/tc_timer_mode/firmware/src/config/sam_v71_xult/definitions.h
@@ -51,6 +51,7 @@
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/nvic/plib_nvic.h"
+#include "peripheral/efc/plib_efc.h"
 #include "peripheral/tc/plib_tc0.h"
 
 // DOM-IGNORE-BEGIN
