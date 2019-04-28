816ef04b5af3f5e1083ec678b402c245b1eded51
Prachi Phadke <prachi.phadke@microchip.com>
2019-04-28 23:37:50 +0530

    [TC] Regenerated and tested tc_tc_capture_mode_sam_v71_xult
    MH3-10753

--- apps/tc/tc_capture_mode/firmware/src/config/sam_v71_xult/definitions.h
+++ apps/tc/tc_capture_mode/firmware/src/config/sam_v71_xult/definitions.h
@@ -54,6 +54,7 @@
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/systick/plib_systick.h"
 #include "peripheral/tc/plib_tc1.h"
+#include "peripheral/efc/plib_efc.h"
 #include "peripheral/tc/plib_tc0.h"
 #include "peripheral/usart/plib_usart1.h"
 
