3e6afae07cef6cd99f0c52b91f86b792ca0e0cbf
Syam <http://support.microchip.com>
2019-04-29 19:29:40 +0530

    [TC] Regenerated tc_timer_mode app for SAMA5D2
    MH3-11066

--- apps/tc/tc_timer_mode/firmware/src/config/sam_a5d2_xult/interrupts.c
+++ apps/tc/tc_timer_mode/firmware/src/config/sam_a5d2_xult/interrupts.c
@@ -160,7 +160,7 @@ void SYSC_SharedHandler( void )
 
 /* data for irq register initialization */
 IrqData irqData[] = {
-    { 35,  (uint32_t) AIC_REGS,    TC0_InterruptHandler,       AIC_SMR_SRCTYPE_INT_LEVEL_SENSITIVE_Val,  AIC_SMR_PRIORITY_MINIMUM_Val   },
+    { 35,  (uint32_t) AIC_REGS,    TC0_InterruptHandler,       AIC_SMR_SRCTYPE_INT_LEVEL_SENSITIVE_Val,  0x0 },
 };
 
 uint32_t irqDataEntryCount = sizeof( irqData ) / sizeof( irqData[ 0 ]);
