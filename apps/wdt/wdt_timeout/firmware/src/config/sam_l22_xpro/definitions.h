3687534e84ea29f5ec08fd2a13baddfa7b8ad037
Bhargav Mehta <bhargav.mehta@microchip.com>
2019-04-29 21:40:07 +0530

    [WDT] Regenerated demo for wdt_wdt_timeout_sam_l22_xpro
    MH3-10889

--- apps/wdt/wdt_timeout/firmware/src/config/sam_l22_xpro/definitions.h
+++ apps/wdt/wdt_timeout/firmware/src/config/sam_l22_xpro/definitions.h
@@ -49,7 +49,7 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include "peripheral/evsys/plib_evsys.h"
+#include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
