0bf0179af92bdf8f142e65410c3767d664608f8c
Bhargav <http://support.microchip.com>
2019-04-29 22:16:06 +0530

    [SERCOM] Regenerated demo for sercom_spi_spi_self_loopback_blocking_sam_l21_xpro
    MH3-10555

--- apps/sercom/spi/spi_self_loopback_blocking/firmware/src/config/sam_l21_xpro/interrupts.c
+++ apps/sercom/spi/spi_self_loopback_blocking/firmware/src/config/sam_l21_xpro/interrupts.c
@@ -86,7 +86,7 @@ void SERCOM1_Handler            ( void ) __attribute__((weak, alias("Dummy_Handl
 void SERCOM2_Handler            ( void ) __attribute__((weak, alias("Dummy_Handler")));
 void SERCOM3_Handler            ( void ) __attribute__((weak, alias("Dummy_Handler")));
 void SERCOM4_Handler            ( void ) __attribute__((weak, alias("Dummy_Handler")));
-void SERCOM5_Handler            ( void ) __attribute__((weak, alias("Dummy_Handler")));
+void SERCOM5_SPI_InterruptHandler ( void ) __attribute__((weak, alias("Dummy_Handler")));
 void TCC0_Handler               ( void ) __attribute__((weak, alias("Dummy_Handler")));
 void TCC1_Handler               ( void ) __attribute__((weak, alias("Dummy_Handler")));
 void TCC2_Handler               ( void ) __attribute__((weak, alias("Dummy_Handler")));
@@ -133,7 +133,7 @@ const DeviceVectors exception_table=
     .pfnSERCOM2_Handler            = ( void * ) SERCOM2_Handler,
     .pfnSERCOM3_Handler            = ( void * ) SERCOM3_Handler,
     .pfnSERCOM4_Handler            = ( void * ) SERCOM4_Handler,
-    .pfnSERCOM5_Handler            = ( void * ) SERCOM5_Handler,
+    .pfnSERCOM5_Handler            = ( void * ) SERCOM5_SPI_InterruptHandler,
     .pfnTCC0_Handler               = ( void * ) TCC0_Handler,
     .pfnTCC1_Handler               = ( void * ) TCC1_Handler,
     .pfnTCC2_Handler               = ( void * ) TCC2_Handler,
