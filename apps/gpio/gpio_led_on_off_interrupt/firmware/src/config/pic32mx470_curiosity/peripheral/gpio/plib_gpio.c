787a72c4abec6e0f2c23580b4ed96a665859cb4c
Santosh <http://support.microchip.com>
2019-04-30 10:24:00 +0530

    [GPIO] Regenerated demo for gpio_gpio_led_on_off_interrupt_pic32mx470_curiosity
    MH3-10143

--- apps/gpio/gpio_led_on_off_interrupt/firmware/src/config/pic32mx470_curiosity/peripheral/gpio/plib_gpio.c
+++ apps/gpio/gpio_led_on_off_interrupt/firmware/src/config/pic32mx470_curiosity/peripheral/gpio/plib_gpio.c
@@ -74,7 +74,7 @@ void GPIO_Initialize ( void )
     IEC1SET = _IEC1_CNDIE_MASK;
 
     /* PORTE Initialization */
-    LATE = 0x00; /* Initial Latch Value */
+    LATE = 0x0; /* Initial Latch Value */
     TRISECLR = 0x10; /* Direction Control */
     ANSELECLR = 0x10; /* Digital Mode Enable */
 
