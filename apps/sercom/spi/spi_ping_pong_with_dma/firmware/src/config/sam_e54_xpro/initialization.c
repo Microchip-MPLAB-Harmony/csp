a554355e3e225d14a5975458793d0bd585f5e412
Bhargav Mehta <bhargav.mehta@microchip.com>
2019-04-30 15:18:02 +0530

    [SERCOM] Regenerated demo for sercom_spi_spi_ping_pong_with_dma_sam_e54_xpro
    MH3-10541

--- apps/sercom/spi/spi_ping_pong_with_dma/firmware/src/config/sam_e54_xpro/initialization.c
+++ apps/sercom/spi/spi_ping_pong_with_dma/firmware/src/config/sam_e54_xpro/initialization.c
@@ -112,14 +112,13 @@
 
 void SYS_Initialize ( void* data )
 {
+    NVMCTRL_Initialize( );
+
   
     PORT_Initialize();
 
     CLOCK_Initialize();
 
-
-
-    EVSYS_Initialize();
 
     SERCOM6_SPI_Initialize();
 
