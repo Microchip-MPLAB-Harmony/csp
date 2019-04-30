ddc5d95667c650414207f33a4c58288955fb6e21
Prachi Phadke <prachi.phadke@microchip.com>
2019-04-30 12:26:59 +0530

    [ADC] Regenerated and tested adc_adc_window_sleepwalking_sam_d21_xpro
    MH3-9864

--- apps/adc/adc_window_sleepwalking/firmware/src/config/sam_d21_xpro/peripheral/adc/plib_adc.h
+++ apps/adc/adc_window_sleepwalking/firmware/src/config/sam_d21_xpro/peripheral/adc/plib_adc.h
@@ -104,6 +104,8 @@ bool ADC_ConversionStatusGet( void );
 
 void ADC_ComparisonWindowSet(uint16_t low_threshold, uint16_t high_threshold);
 
+void ADC_WindowModeSet(ADC_WINMODE mode);
+
 
 void ADC_CallbackRegister( ADC_CALLBACK callback, uintptr_t context );
 
