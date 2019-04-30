bc968ef787e3e7ecfe3a9a9e7897529400b6ec33
Jain <http://support.microchip.com>
2019-04-30 19:38:26 +0530

    [SUPC] Regenerated supc_wakeup_rtt_sam_v71_xult MH3-10647

--- apps/supc/supc_wakeup_rtt/firmware/src/config/sam_v71_xult/definitions.h
+++ apps/supc/supc_wakeup_rtt/firmware/src/config/sam_v71_xult/definitions.h
@@ -55,6 +55,7 @@
 #include "peripheral/systick/plib_systick.h"
 #include "peripheral/supc/plib_supc.h"
 #include "peripheral/rtt/plib_rtt.h"
+#include "peripheral/efc/plib_efc.h"
 #include "peripheral/usart/plib_usart1.h"
 
 // DOM-IGNORE-BEGIN
