fa429247c997a8cb7e0d1f921600cc8a20e47671
Bhargav Mehta <bhargav.mehta@microchip.com>
2019-05-01 22:54:58 +0530

    [WDT] Regenerated demo for wdt_wdt_timeout_sam_d21_xpro
    MH3-10881

--- apps/wdt/wdt_timeout/firmware/src/config/sam_d21_xpro/definitions.h
+++ apps/wdt/wdt_timeout/firmware/src/config/sam_d21_xpro/definitions.h
@@ -50,7 +50,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include "peripheral/sercom/usart/plib_sercom3_usart.h"
-#include "peripheral/evsys/plib_evsys.h"
+#include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
