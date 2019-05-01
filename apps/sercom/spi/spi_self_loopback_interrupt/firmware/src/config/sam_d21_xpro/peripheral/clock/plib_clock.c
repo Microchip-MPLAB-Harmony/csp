6fe942a06234319178b30e19a2c51dfc8dba9a8d
Bhargav Mehta <bhargav.mehta@microchip.com>
2019-05-01 22:11:27 +0530

    [SERCOM] Regenerated demo for sercom_spi_spi_self_loopback_interrupt_sam_d21_xpro
    MH3-10563

--- apps/sercom/spi/spi_self_loopback_interrupt/firmware/src/config/sam_d21_xpro/peripheral/clock/plib_clock.c
+++ apps/sercom/spi/spi_self_loopback_interrupt/firmware/src/config/sam_d21_xpro/peripheral/clock/plib_clock.c
@@ -43,24 +43,41 @@
 
 static void SYSCTRL_Initialize(void)
 {
-
-	/* Configure 8MHz Oscillator */
-    SYSCTRL_REGS->SYSCTRL_OSC8M = (SYSCTRL_REGS->SYSCTRL_OSC8M & (SYSCTRL_OSC8M_CALIB_Msk | SYSCTRL_OSC8M_FRANGE_Msk)) | SYSCTRL_OSC8M_ENABLE_Msk | SYSCTRL_OSC8M_PRESC(0x0) ;
-
-    while((SYSCTRL_REGS->SYSCTRL_PCLKSR & SYSCTRL_PCLKSR_OSC8MRDY_Msk) != SYSCTRL_PCLKSR_OSC8MRDY_Msk)
-    {
-        /* Waiting for the OSC8M Ready state */
-    }
-
     SYSCTRL_REGS->SYSCTRL_OSC32K = 0x0;
 }
 
 
+static void DFLL_Initialize(void)
+{
+    /****************** DFLL Initialization  *********************************/
+    SYSCTRL_REGS->SYSCTRL_DFLLCTRL &= ~SYSCTRL_DFLLCTRL_ONDEMAND_Msk;
+
+    while((SYSCTRL_REGS->SYSCTRL_PCLKSR & SYSCTRL_PCLKSR_DFLLRDY_Msk) != SYSCTRL_PCLKSR_DFLLRDY_Msk)
+    {
+        /* Waiting for the Ready state */
+    }
+
+    /*Load Calibration Value*/
+    uint8_t calibCoarse = (uint8_t)(((*(uint32_t*)0x806024) >> 26 ) & 0x3f);
+    calibCoarse = (((calibCoarse) == 0x3F) ? 0x1F : (calibCoarse));
+    uint16_t calibFine = (uint16_t)(((*(uint32_t*)0x806028)) & 0x3ff);
+
+    SYSCTRL_REGS->SYSCTRL_DFLLVAL = SYSCTRL_DFLLVAL_COARSE(calibCoarse) | SYSCTRL_DFLLVAL_FINE(calibFine);
+
+
+    /* Configure DFLL    */
+    SYSCTRL_REGS->SYSCTRL_DFLLCTRL = SYSCTRL_DFLLCTRL_ENABLE_Msk ;
+
+    while((SYSCTRL_REGS->SYSCTRL_PCLKSR & SYSCTRL_PCLKSR_DFLLRDY_Msk) != SYSCTRL_PCLKSR_DFLLRDY_Msk)
+    {
+        /* Waiting for DFLL to be ready */
+    }
+}
 
 
 static void GCLK0_Initialize(void)
 {
-    GCLK_REGS->GCLK_GENCTRL = GCLK_GENCTRL_SRC(6) | GCLK_GENCTRL_GENEN_Msk | GCLK_GENCTRL_ID(0);
+    GCLK_REGS->GCLK_GENCTRL = GCLK_GENCTRL_SRC(7) | GCLK_GENCTRL_GENEN_Msk | GCLK_GENCTRL_ID(0);
 
     while((GCLK_REGS->GCLK_STATUS & GCLK_STATUS_SYNCBUSY_Msk) == GCLK_STATUS_SYNCBUSY_Msk)
     {
@@ -70,12 +87,10 @@ static void GCLK0_Initialize(void)
 
 void CLOCK_Initialize (void)
 {
-    /* NVM Wait States */
-    NVMCTRL_REGS->NVMCTRL_CTRLB |= NVMCTRL_CTRLB_RWS(NVMCTRL_CTRLB_RWS_SINGLE_Val);
-
     /* Function to Initialize the Oscillators */
     SYSCTRL_Initialize();
 
+    DFLL_Initialize();
     GCLK0_Initialize();
 
 
@@ -86,4 +101,7 @@ void CLOCK_Initialize (void)
     /* Configure the APBC Bridge Clocks */
     PM_REGS->PM_APBCMASK = 0x10008;
 
+
+    /*Disable RC oscillator*/
+    SYSCTRL_REGS->SYSCTRL_OSC8M = 0x0;
 }
