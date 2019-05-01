00fb23ef0279633af2e595be3675efb89fd03bd4
Bhargav Mehta <bhargav.mehta@microchip.com>
2019-05-02 00:33:33 +0530

    [CORETIMER] Regenerated demo for coretimer_coretimer_periodic_timeout_pic32mk_gp_db
    MH3-9934

--- apps/coretimer/coretimer_periodic_timeout/firmware/src/config/pic32mk_gp_db/peripheral/gpio/plib_gpio.c
+++ apps/coretimer/coretimer_periodic_timeout/firmware/src/config/pic32mk_gp_db/peripheral/gpio/plib_gpio.c
@@ -70,6 +70,7 @@ void GPIO_Initialize ( void )
     /* PORTF Initialization */
 
     /* PORTG Initialization */
+    LATG = 0x0; /* Initial Latch Value */
     TRISGCLR = 0x1000; /* Direction Control */
 
 
