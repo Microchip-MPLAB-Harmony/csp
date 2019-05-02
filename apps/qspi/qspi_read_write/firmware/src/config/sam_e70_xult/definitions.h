acc2d1c6c76708eded2a0b6a1c5c4c4ed11ce7a3
Mohan <http://support.microchip.com>
2019-05-02 10:01:09 +0530

    [QSPI] DFT Regenerated and tested for qspi_qspi_read_write_sam_e70_xult
    MH3-10350

--- apps/qspi/qspi_read_write/firmware/src/config/sam_e70_xult/definitions.h
+++ apps/qspi/qspi_read_write/firmware/src/config/sam_e70_xult/definitions.h
@@ -54,6 +54,7 @@
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/mpu/plib_mpu.h"
 #include "peripheral/systick/plib_systick.h"
+#include "peripheral/efc/plib_efc.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
