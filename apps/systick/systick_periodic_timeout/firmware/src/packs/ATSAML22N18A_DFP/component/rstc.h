654b80fbea0b28dc9632812b119a985400c92eb3
Santosh <http://support.microchip.com>
2019-05-02 15:12:58 +0530

    [SYSTICK] Regenerated demo for systick_systick_periodic_timeout_sam_l22_xpro MH3-10663

--- apps/systick/systick_periodic_timeout/firmware/src/packs/ATSAML22N18A_DFP/component/rstc.h
+++ apps/systick/systick_periodic_timeout/firmware/src/packs/ATSAML22N18A_DFP/component/rstc.h
@@ -1,7 +1,7 @@
 /**
  * \brief Component description for RSTC
  *
- * © 2019 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2019 Microchip Technology Inc. and its subsidiaries.
  *
  * Subject to your compliance with these terms, you may use Microchip software and any derivatives
  * exclusively with Microchip products. It is your responsibility to comply with third party license
@@ -20,7 +20,7 @@
  *
  */
 
-/* file generated from device description version 2019-03-19T07:09:52Z */
+/* file generated from device description version 2019-04-17T13:08:53Z */
 #ifndef _SAML22_RSTC_COMPONENT_H_
 #define _SAML22_RSTC_COMPONENT_H_
 
@@ -29,44 +29,53 @@
 /* ************************************************************************** */
 
 /* -------- RSTC_RCAUSE : (RSTC Offset: 0x00) ( R/ 8) Reset Cause -------- */
-#define RSTC_RCAUSE_POR_Pos                   _U_(0)                                         /**< (RSTC_RCAUSE) Power On Reset Position */
-#define RSTC_RCAUSE_POR_Msk                   (_U_(0x1) << RSTC_RCAUSE_POR_Pos)              /**< (RSTC_RCAUSE) Power On Reset Mask */
-#define RSTC_RCAUSE_BODCORE_Pos               _U_(1)                                         /**< (RSTC_RCAUSE) Brown Out CORE Detector Reset Position */
-#define RSTC_RCAUSE_BODCORE_Msk               (_U_(0x1) << RSTC_RCAUSE_BODCORE_Pos)          /**< (RSTC_RCAUSE) Brown Out CORE Detector Reset Mask */
-#define RSTC_RCAUSE_BODVDD_Pos                _U_(2)                                         /**< (RSTC_RCAUSE) Brown Out VDD Detector Reset Position */
-#define RSTC_RCAUSE_BODVDD_Msk                (_U_(0x1) << RSTC_RCAUSE_BODVDD_Pos)           /**< (RSTC_RCAUSE) Brown Out VDD Detector Reset Mask */
-#define RSTC_RCAUSE_EXT_Pos                   _U_(4)                                         /**< (RSTC_RCAUSE) External Reset Position */
-#define RSTC_RCAUSE_EXT_Msk                   (_U_(0x1) << RSTC_RCAUSE_EXT_Pos)              /**< (RSTC_RCAUSE) External Reset Mask */
-#define RSTC_RCAUSE_WDT_Pos                   _U_(5)                                         /**< (RSTC_RCAUSE) Watchdog Reset Position */
-#define RSTC_RCAUSE_WDT_Msk                   (_U_(0x1) << RSTC_RCAUSE_WDT_Pos)              /**< (RSTC_RCAUSE) Watchdog Reset Mask */
-#define RSTC_RCAUSE_SYST_Pos                  _U_(6)                                         /**< (RSTC_RCAUSE) System Reset Request Position */
-#define RSTC_RCAUSE_SYST_Msk                  (_U_(0x1) << RSTC_RCAUSE_SYST_Pos)             /**< (RSTC_RCAUSE) System Reset Request Mask */
-#define RSTC_RCAUSE_BACKUP_Pos                _U_(7)                                         /**< (RSTC_RCAUSE) Backup Reset Position */
-#define RSTC_RCAUSE_BACKUP_Msk                (_U_(0x1) << RSTC_RCAUSE_BACKUP_Pos)           /**< (RSTC_RCAUSE) Backup Reset Mask */
-#define RSTC_RCAUSE_Msk                       _U_(0xF7)                                      /**< (RSTC_RCAUSE) Register Mask  */
+#define RSTC_RCAUSE_POR_Pos                   _U_(0)                                               /**< (RSTC_RCAUSE) Power On Reset Position */
+#define RSTC_RCAUSE_POR_Msk                   (_U_(0x1) << RSTC_RCAUSE_POR_Pos)                    /**< (RSTC_RCAUSE) Power On Reset Mask */
+#define RSTC_RCAUSE_POR(value)                (RSTC_RCAUSE_POR_Msk & ((value) << RSTC_RCAUSE_POR_Pos))
+#define RSTC_RCAUSE_BODCORE_Pos               _U_(1)                                               /**< (RSTC_RCAUSE) Brown Out CORE Detector Reset Position */
+#define RSTC_RCAUSE_BODCORE_Msk               (_U_(0x1) << RSTC_RCAUSE_BODCORE_Pos)                /**< (RSTC_RCAUSE) Brown Out CORE Detector Reset Mask */
+#define RSTC_RCAUSE_BODCORE(value)            (RSTC_RCAUSE_BODCORE_Msk & ((value) << RSTC_RCAUSE_BODCORE_Pos))
+#define RSTC_RCAUSE_BODVDD_Pos                _U_(2)                                               /**< (RSTC_RCAUSE) Brown Out VDD Detector Reset Position */
+#define RSTC_RCAUSE_BODVDD_Msk                (_U_(0x1) << RSTC_RCAUSE_BODVDD_Pos)                 /**< (RSTC_RCAUSE) Brown Out VDD Detector Reset Mask */
+#define RSTC_RCAUSE_BODVDD(value)             (RSTC_RCAUSE_BODVDD_Msk & ((value) << RSTC_RCAUSE_BODVDD_Pos))
+#define RSTC_RCAUSE_EXT_Pos                   _U_(4)                                               /**< (RSTC_RCAUSE) External Reset Position */
+#define RSTC_RCAUSE_EXT_Msk                   (_U_(0x1) << RSTC_RCAUSE_EXT_Pos)                    /**< (RSTC_RCAUSE) External Reset Mask */
+#define RSTC_RCAUSE_EXT(value)                (RSTC_RCAUSE_EXT_Msk & ((value) << RSTC_RCAUSE_EXT_Pos))
+#define RSTC_RCAUSE_WDT_Pos                   _U_(5)                                               /**< (RSTC_RCAUSE) Watchdog Reset Position */
+#define RSTC_RCAUSE_WDT_Msk                   (_U_(0x1) << RSTC_RCAUSE_WDT_Pos)                    /**< (RSTC_RCAUSE) Watchdog Reset Mask */
+#define RSTC_RCAUSE_WDT(value)                (RSTC_RCAUSE_WDT_Msk & ((value) << RSTC_RCAUSE_WDT_Pos))
+#define RSTC_RCAUSE_SYST_Pos                  _U_(6)                                               /**< (RSTC_RCAUSE) System Reset Request Position */
+#define RSTC_RCAUSE_SYST_Msk                  (_U_(0x1) << RSTC_RCAUSE_SYST_Pos)                   /**< (RSTC_RCAUSE) System Reset Request Mask */
+#define RSTC_RCAUSE_SYST(value)               (RSTC_RCAUSE_SYST_Msk & ((value) << RSTC_RCAUSE_SYST_Pos))
+#define RSTC_RCAUSE_BACKUP_Pos                _U_(7)                                               /**< (RSTC_RCAUSE) Backup Reset Position */
+#define RSTC_RCAUSE_BACKUP_Msk                (_U_(0x1) << RSTC_RCAUSE_BACKUP_Pos)                 /**< (RSTC_RCAUSE) Backup Reset Mask */
+#define RSTC_RCAUSE_BACKUP(value)             (RSTC_RCAUSE_BACKUP_Msk & ((value) << RSTC_RCAUSE_BACKUP_Pos))
+#define RSTC_RCAUSE_Msk                       _U_(0xF7)                                            /**< (RSTC_RCAUSE) Register Mask  */
 
 
 /* -------- RSTC_BKUPEXIT : (RSTC Offset: 0x02) ( R/ 8) Backup Exit Source -------- */
-#define RSTC_BKUPEXIT_RESETVALUE            _U_(0x00)                                     /**<  (RSTC_BKUPEXIT) Backup Exit Source  Reset Value */
+#define RSTC_BKUPEXIT_RESETVALUE              _U_(0x00)                                            /**<  (RSTC_BKUPEXIT) Backup Exit Source  Reset Value */
 
-#define RSTC_BKUPEXIT_RTC_Pos                 _U_(1)                                         /**< (RSTC_BKUPEXIT) Real Timer Counter Interrupt Position */
-#define RSTC_BKUPEXIT_RTC_Msk                 (_U_(0x1) << RSTC_BKUPEXIT_RTC_Pos)            /**< (RSTC_BKUPEXIT) Real Timer Counter Interrupt Mask */
-#define RSTC_BKUPEXIT_BBPS_Pos                _U_(2)                                         /**< (RSTC_BKUPEXIT) Battery Backup Power Switch Position */
-#define RSTC_BKUPEXIT_BBPS_Msk                (_U_(0x1) << RSTC_BKUPEXIT_BBPS_Pos)           /**< (RSTC_BKUPEXIT) Battery Backup Power Switch Mask */
-#define RSTC_BKUPEXIT_Msk                     _U_(0x06)                                      /**< (RSTC_BKUPEXIT) Register Mask  */
+#define RSTC_BKUPEXIT_RTC_Pos                 _U_(1)                                               /**< (RSTC_BKUPEXIT) Real Timer Counter Interrupt Position */
+#define RSTC_BKUPEXIT_RTC_Msk                 (_U_(0x1) << RSTC_BKUPEXIT_RTC_Pos)                  /**< (RSTC_BKUPEXIT) Real Timer Counter Interrupt Mask */
+#define RSTC_BKUPEXIT_RTC(value)              (RSTC_BKUPEXIT_RTC_Msk & ((value) << RSTC_BKUPEXIT_RTC_Pos))
+#define RSTC_BKUPEXIT_BBPS_Pos                _U_(2)                                               /**< (RSTC_BKUPEXIT) Battery Backup Power Switch Position */
+#define RSTC_BKUPEXIT_BBPS_Msk                (_U_(0x1) << RSTC_BKUPEXIT_BBPS_Pos)                 /**< (RSTC_BKUPEXIT) Battery Backup Power Switch Mask */
+#define RSTC_BKUPEXIT_BBPS(value)             (RSTC_BKUPEXIT_BBPS_Msk & ((value) << RSTC_BKUPEXIT_BBPS_Pos))
+#define RSTC_BKUPEXIT_Msk                     _U_(0x06)                                            /**< (RSTC_BKUPEXIT) Register Mask  */
 
 
 /** \brief RSTC register offsets definitions */
-#define RSTC_RCAUSE_REG_OFST           (0x00)         /**< (RSTC_RCAUSE) Reset Cause Offset */
-#define RSTC_BKUPEXIT_REG_OFST         (0x02)         /**< (RSTC_BKUPEXIT) Backup Exit Source Offset */
+#define RSTC_RCAUSE_REG_OFST           (0x00)              /**< (RSTC_RCAUSE) Reset Cause Offset */
+#define RSTC_BKUPEXIT_REG_OFST         (0x02)              /**< (RSTC_BKUPEXIT) Backup Exit Source Offset */
 
 #if !(defined(__ASSEMBLER__) || defined(__IAR_SYSTEMS_ASM__))
 /** \brief RSTC register API structure */
 typedef struct
 {  /* Reset Controller */
-  __I   uint8_t                        RSTC_RCAUSE;     /**< Offset: 0x00 (R/   8) Reset Cause */
+  __I   uint8_t                        RSTC_RCAUSE;        /**< Offset: 0x00 (R/   8) Reset Cause */
   __I   uint8_t                        Reserved1[0x01];
-  __I   uint8_t                        RSTC_BKUPEXIT;   /**< Offset: 0x02 (R/   8) Backup Exit Source */
+  __I   uint8_t                        RSTC_BKUPEXIT;      /**< Offset: 0x02 (R/   8) Backup Exit Source */
 } rstc_registers_t;
 
 
