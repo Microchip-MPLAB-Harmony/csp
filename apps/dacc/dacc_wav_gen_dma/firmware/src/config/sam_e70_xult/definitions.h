849831e51a7fbe7c0e7ba71678800ee6034dd1bb
Santosh Tangudu <santosh.tangudu@microchip.com>
2019-05-02 20:56:27 +0530

    [DACC] Regenerated demo for dacc_dacc_wav_gen_dma_sam_e70_xult MH3-9966

--- apps/dacc/dacc_wav_gen_dma/firmware/src/config/sam_e70_xult/definitions.h
+++ apps/dacc/dacc_wav_gen_dma/firmware/src/config/sam_e70_xult/definitions.h
@@ -53,6 +53,7 @@
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/xdmac/plib_xdmac.h"
 #include "peripheral/dacc/plib_dacc.h"
+#include "peripheral/efc/plib_efc.h"
 #include "peripheral/tc/plib_tc0.h"
 
 // DOM-IGNORE-BEGIN
