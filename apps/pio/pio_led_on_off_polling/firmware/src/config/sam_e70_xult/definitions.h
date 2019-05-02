bb5a26296ff4ad03ed1522b62951bda55c5ea93d
Santosh <http://support.microchip.com>
2019-05-02 17:51:14 +0530

    [PIO] Regenerated demo for pio_pio_led_on_off_polling_sam_e70_xult MH3-10300

--- apps/pio/pio_led_on_off_polling/firmware/src/config/sam_e70_xult/definitions.h
+++ apps/pio/pio_led_on_off_polling/firmware/src/config/sam_e70_xult/definitions.h
@@ -51,6 +51,7 @@
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/nvic/plib_nvic.h"
+#include "peripheral/efc/plib_efc.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
