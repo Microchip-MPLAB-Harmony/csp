5901a46cda4b9e6cac4b215083989e44bc66d3de
Mohan <http://support.microchip.com>
2019-05-02 11:08:56 +0530

    [SDRAMC] DFT Regenerated and tested for sdramc_sdramc_read_write_perf_sam_v71_xult
    MH3-10432

--- apps/sdramc/sdramc_read_write_perf/firmware/src/config/sam_v71_xult/peripheral/clk/plib_clk.c
+++ apps/sdramc/sdramc_read_write_perf/firmware/src/config/sam_v71_xult/peripheral/clk/plib_clk.c
@@ -21,7 +21,6 @@
 * THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
 *******************************************************************************/
 
-
 #include "device.h"
 #include "plib_clk.h"
 
@@ -55,7 +54,7 @@ Initialize Master clock (MCK)
 static void CLK_MasterClockInitialize(void)
 {
     /* Program PMC_MCKR.PRES and wait for PMC_SR.MCKRDY to be set   */
-    PMC_REGS->PMC_MCKR = (PMC_REGS->PMC_MCKR & ~PMC_MCKR_PRES_Msk) | PMC_MCKR_PRES_CLK_2;
+    PMC_REGS->PMC_MCKR = (PMC_REGS->PMC_MCKR & ~PMC_MCKR_PRES_Msk) | PMC_MCKR_PRES_CLK_1;
     while ((PMC_REGS->PMC_SR & PMC_SR_MCKRDY_Msk) != PMC_SR_MCKRDY_Msk);
 
     /* Program PMC_MCKR.MDIV and Wait for PMC_SR.MCKRDY to be set   */
@@ -80,8 +79,6 @@ Clock Initialize
 *********************************************************************************/
 void CLK_Initialize( void )
 {
-    /* Set Flash Wait States and  Enable Code Loop Optimization */
-    EFC_REGS->EEFC_FMR = EEFC_FMR_FWS(3) | EEFC_FMR_CLOE_Msk;
 
 
 
