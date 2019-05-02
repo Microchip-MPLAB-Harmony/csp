2d4d7a27ac689f6286e201c7c9cb62203790cdac
Santosh Tangudu <santosh.tangudu@microchip.com>
2019-05-02 09:48:12 +0530

    [NVMCTRL] Regenerated demo for nvmctrl_nvmctrl_smarteeprom_sam_e54_xpro MH3-10203

--- apps/nvmctrl/nvmctrl_smarteeprom/firmware/src/config/sam_e54_xpro/initialization.c
+++ apps/nvmctrl/nvmctrl_smarteeprom/firmware/src/config/sam_e54_xpro/initialization.c
@@ -112,14 +112,13 @@
 
 void SYS_Initialize ( void* data )
 {
+    NVMCTRL_Initialize( );
+
   
     PORT_Initialize();
 
     CLOCK_Initialize();
 
-
-
-    NVMCTRL_Initialize( );
 
     SERCOM2_USART_Initialize();
 
