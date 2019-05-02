c63f3d441bb6fa798137e45ae3aa2eb47b18a323
Mohan RP <mohan.kumar@microchip.com>
2019-05-02 09:24:49 +0530

    [FREQM] DFT Regenerated and tested for freqm_freqm_measure_clock_freq_sam_l22_xpro
    MH3-10139

--- apps/freqm/freqm_measure_clock_freq/firmware/src/config/sam_l22_xpro/peripheral/clock/plib_clock.c
+++ apps/freqm/freqm_measure_clock_freq/firmware/src/config/sam_l22_xpro/peripheral/clock/plib_clock.c
@@ -189,9 +189,6 @@ static void GCLK3_Initialize(void)
 
 void CLOCK_Initialize (void)
 {
-    /* NVM Wait States */
-    NVMCTRL_REGS->NVMCTRL_CTRLB |= NVMCTRL_CTRLB_RWS(1);
-
     /* Function to Initialize the Oscillators */
     OSCCTRL_Initialize();
 
