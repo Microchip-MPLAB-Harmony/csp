43a4bbff254a39897bbd503c041a1c3202bc3e3c
Mohan RP <mohan.kumar@microchip.com>
2019-05-02 11:31:04 +0530

    [SERCOM] DFT Regenerated and tested for sercom_i2c_master_i2c_eeprom_sam_d21_xpro
    MH3-10438

--- apps/sercom/i2c/master/i2c_eeprom/firmware/src/config/sam_d21_xpro/peripheral/port/plib_port.c
+++ apps/sercom/i2c/master/i2c_eeprom/firmware/src/config/sam_d21_xpro/peripheral/port/plib_port.c
@@ -78,6 +78,7 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_PMUX[4] = 0x33;
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0x40000000;
+   PORT_REGS->GROUP[1].PORT_OUT = 0x40000000;
 
 }
 
