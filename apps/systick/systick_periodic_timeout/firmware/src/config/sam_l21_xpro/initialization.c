61f8c45523f0e1126483f2daccdb1d0f8c2c4e90
Santosh <http://support.microchip.com>
2019-05-02 15:05:56 +0530

    [SYSTICK] Regenerated demo for systick_systick_periodic_timeout_sam_l21_xpro MH3-10661

--- apps/systick/systick_periodic_timeout/firmware/src/config/sam_l21_xpro/initialization.c
+++ apps/systick/systick_periodic_timeout/firmware/src/config/sam_l21_xpro/initialization.c
@@ -53,6 +53,20 @@
 // Section: Configuration Bits
 // ****************************************************************************
 // ****************************************************************************
+#pragma config NVMCTRL_BOOTPROT = 0x7
+#pragma config NVMCTRL_EEPROM_SIZE = 0x7
+#pragma config BOD33USERLEVEL = 0x6
+#pragma config BOD33_DIS = CLEAR
+#pragma config BOD33_ACTION = 0x0
+#pragma config WDT_ENABLE = CLEAR
+#pragma config WDT_ALWAYSON = CLEAR
+#pragma config WDT_PER = 0xB
+#pragma config WDT_WINDOW = 0xB
+#pragma config WDT_EWOFFSET = 0xB
+#pragma config WDT_WEN = CLEAR
+#pragma config BOD33_HYST = CLEAR
+#pragma config NVMCTRL_REGION_LOCKS = 0xffff
+
 
 
 
@@ -96,12 +110,13 @@
 
 void SYS_Initialize ( void* data )
 {
+    NVMCTRL_Initialize( );
+
   
     PORT_Initialize();
 
 
     CLOCK_Initialize();
-
 
 
     EVSYS_Initialize();
