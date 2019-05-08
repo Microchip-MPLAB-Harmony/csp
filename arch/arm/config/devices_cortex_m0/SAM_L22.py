e8a9d022dc1e024b1ef243a4e1262ada0e66679a
Bhargav <http://support.microchip.com>
2019-05-08 13:12:32 +0530

    [ARCH] Launch PM by-default for SAML22
    MH3-12030

--- arch/arm/config/devices_cortex_m0/SAM_L22.py
+++ arch/arm/config/devices_cortex_m0/SAM_L22.py
@@ -171,7 +171,7 @@ nvmWaitStates = { #VDD > 2.7
                     24000000 : 0,
                     32000000 : 1
                 }
-                
+
 periphNode = ATDF.getNode("/avr-tools-device-file/devices/device/peripherals/module@[name=\"NVMCTRL\"]")
 modules = periphNode.getChildren()
 components = []
@@ -207,6 +207,14 @@ execfile(Variables.get("__CORE_DIR") + "/../peripheral/wdt_u2251/config/wdt.py")
 
 # load PAC
 execfile(Variables.get("__CORE_DIR") + "/../peripheral/pac_u2120/config/pac.py")
+
+# Activate PM
+periphNode = ATDF.getNode("/avr-tools-device-file/devices/device/peripherals/module@[name=\"PM\"]")
+modules = periphNode.getChildren()
+components = []
+for pm_instance in range (0, len(modules)):
+    components.append(str(modules[pm_instance].getAttribute("name")).lower())
+Database.activateComponents(components)
 
 # Activate Event System
 periphNode = ATDF.getNode("/avr-tools-device-file/devices/device/peripherals/module@[name=\"EVSYS\"]")
