e4601c53575d3f28a3c5e7f90e7b1fbe4ffeed82
Prachi <http://support.microchip.com>
2019-05-08 16:43:43 +0530

    [TC] Created separate main file for SAML21 tc_capture_mode demo to get exact 1% duty cycle increment
    MH3-12108

---
+++ apps/tc/tc_capture_mode/firmware/src/main_l21.c
@@ -0,0 +1,136 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+
+/* Input waveform - compare value increment*/
+#define CMP_VAL_INCREMENT (600U)
+
+/* Calculated duty cycle of the input waveform in %*/
+uint32_t duty;
+/* Calculated frequency of the input waveform in Hz*/
+uint32_t frequency;
+
+volatile bool tc_buffer_ready = false;
+
+void capture_handler( TC_CAPTURE_STATUS status, uintptr_t context)
+{   
+    if ((status  & TC_CAPTURE_STAUTS_CAPTURE0_READY) == TC_CAPTURE_STAUTS_CAPTURE0_READY)
+    {
+        tc_buffer_ready = true;
+    }
+}
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    uint16_t period,on_time;
+    uint16_t cmp_period, cmp_val=0; 
+    
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+    
+    SYSTICK_TimerStart();
+    cmp_period = TC0_Compare16bitPeriodGet();
+    
+    printf("\n\r---------------------------------------------------------");
+    printf("\n\r                    TC Capture Demo                 ");
+    printf("\n\r---------------------------------------------------------\n\r");
+    
+    TC0_CompareStart();
+    TC1_CaptureStart();
+    
+    TC1_CaptureCallbackRegister(capture_handler, (uintptr_t)NULL);
+
+    while (true )
+    {
+        /* Change the duty cycle of the input waveform  */
+        cmp_val += CMP_VAL_INCREMENT;
+        if (cmp_val >= cmp_period)
+        {
+            cmp_val = CMP_VAL_INCREMENT;
+        }
+        TC0_Compare16bitSet(cmp_val);
+        
+        /* Wait for 1 second */ 
+        SYSTICK_DelayMs(500);
+        
+        /* Wait for capture event */
+        while(tc_buffer_ready != true);
+           
+        /* Read Captured values */ 
+        period = TC1_Capture16bitChannel0Get();
+        on_time = TC1_Capture16bitChannel1Get();
+
+        /* Compute Duty Cycle in percentage and Frequency in Hz */
+        duty = ((on_time) * 100U) / period;
+        frequency = (TC1_CaptureFrequencyGet() / period);
+        
+        /* Send the measured data to console for display  */
+        printf("Frequency: %d Hz \t Duty Cycle: %d %%",(int) frequency,(int) duty);
+        printf("\r\n");       
+        tc_buffer_ready = false;
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+
