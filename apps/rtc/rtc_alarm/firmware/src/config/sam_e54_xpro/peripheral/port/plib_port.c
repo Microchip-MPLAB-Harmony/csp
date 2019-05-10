128df153bd3adb0ba725a9964558c5b46bcd42b8
Santosh <http://support.microchip.com>
2019-05-10 10:39:22 +0530

    [RTC] LED latching state is changed to HIGH, so LED will glow-off by default, once 20secs over LED will glow-on
    MH3-12149

--- apps/rtc/rtc_alarm/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.c
+++ apps/rtc/rtc_alarm/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.c
@@ -80,6 +80,7 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[1].PORT_PMUX[12] = 0x33;
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x40000;
+   PORT_REGS->GROUP[2].PORT_OUT = 0x40000;
 
    /************************** GROUP 3 Initialization *************************/
 
