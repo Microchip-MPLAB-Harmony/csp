5f7ae17f629fe2f54c6c1378a65dcb150db8b1ce
Rajat <http://support.microchip.com>
2019-05-13 15:56:56 +0530

    [MEM2MEM] Added Mem2Mem plib MH3-7955

---
+++ peripheral/mem2mem_11211/config/mem2mem.py
@@ -0,0 +1,65 @@
+# coding: utf-8
+"""*****************************************************************************
+* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*****************************************************************************"""
+def instantiateComponent(mem2memComponent):
+    mem2memInstanceName = mem2memComponent.createStringSymbol("MEM2MEM_INSTANCE_NAME", None)
+    mem2memInstanceName.setVisible(False)
+    mem2memInstanceName.setDefaultValue(mem2memComponent.getID().upper())
+    Database.setSymbolValue("core", mem2memInstanceName.getValue()+"_CLOCK_ENABLE", True)
+    
+    interruptVector = mem2memInstanceName.getValue() + "_INTERRUPT_ENABLE"
+    interruptHandler = mem2memInstanceName.getValue() + "_INTERRUPT_HANDLER"
+    interruptHandlerLock = mem2memInstanceName.getValue() + "_INTERRUPT_HANDLER_LOCK"
+
+    Database.clearSymbolValue("core", interruptVector)
+    Database.setSymbolValue("core", interruptVector, True, 2)
+
+    Database.clearSymbolValue("core", interruptHandler)
+    Database.setSymbolValue("core", interruptHandler, mem2memInstanceName.getValue() + "_InterruptHandler", 2)
+
+    Database.clearSymbolValue("core", interruptHandlerLock)
+    Database.setSymbolValue("core", interruptHandlerLock, True, 2)
+
+    configName = Variables.get("__CONFIGURATION_NAME")
+
+    mem2memHeader1File = mem2memComponent.createFileSymbol("MEM2MEM_FILE_0", None)
+    mem2memHeader1File.setSourcePath("../peripheral/mem2mem_11211/templates/plib_mem2mem.h.ftl")
+    mem2memHeader1File.setMarkup(True)
+    mem2memHeader1File.setOutputName("plib_" + mem2memInstanceName.getValue().lower() + ".h")
+    mem2memHeader1File.setDestPath("/peripheral/mem2mem/")
+    mem2memHeader1File.setProjectPath("config/" + configName + "/peripheral/mem2mem/")
+    mem2memHeader1File.setType("HEADER")
+
+    mem2memSource1File = mem2memComponent.createFileSymbol("MEM2MEM_FILE_1", None)
+    mem2memSource1File.setSourcePath("../peripheral/mem2mem_11211/templates/plib_mem2mem.c.ftl")
+    mem2memSource1File.setMarkup(True)
+    mem2memSource1File.setOutputName("plib_" + mem2memInstanceName.getValue().lower() + ".c")
+    mem2memSource1File.setDestPath("/peripheral/mem2mem/")
+    mem2memSource1File.setProjectPath("config/" + configName + "/peripheral/mem2mem/")
+    mem2memSource1File.setType("SOURCE")
+
+    mem2memSystemDefFile = mem2memComponent.createFileSymbol("MEM2MEM_FILE_3", None)
+    mem2memSystemDefFile.setType("STRING")
+    mem2memSystemDefFile.setOutputName("core.LIST_SYSTEM_DEFINITIONS_H_INCLUDES")
+    mem2memSystemDefFile.setSourcePath("../peripheral/mem2mem_11211/templates/system/definitions.h.ftl")
+    mem2memSystemDefFile.setMarkup(True)
