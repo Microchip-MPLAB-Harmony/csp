435e9c3f20c2636aa1e8635f5dcbce60a3be22aa
Rajat <http://support.microchip.com>
2019-05-16 12:27:54 +0530

    [PM] Fixing LED Blink Rate MH3-12692

--- apps/pm/pm_wakeup_eic/firmware/src/config/sam_l21_xpro/peripheral/systick/plib_systick.c
+++ apps/pm/pm_wakeup_eic/firmware/src/config/sam_l21_xpro/peripheral/systick/plib_systick.c
@@ -47,7 +47,7 @@ void SYSTICK_TimerInitialize ( void )
 {
 	SysTick->CTRL = 0;
 	SysTick->VAL = 0;
-	SysTick->LOAD = 0xbb80 - 1;
+	SysTick->LOAD = 0x493e00 - 1;
 	SysTick->CTRL = SysTick_CTRL_TICKINT_Msk | SysTick_CTRL_CLKSOURCE_Msk;
 
 	systick.tickCounter = 0;
