fb1e4069a7386ca2bd2fdef5a1cc7f6cf16d2963
Dan <http://support.microchip.com>
2019-06-04 12:00:06 -0700

    [wdt_timeout] Add sam_a5d2_xult_xc32 configuration
    MH3-13120

---
+++ apps/wdt/wdt_timeout/firmware/src/config/sam_a5d2_xult_xc32/peripheral/wdt/plib_wdt.c
@@ -0,0 +1,57 @@
+/*******************************************************************************
+  WDT Peripheral Library
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    plib_wdt.c
+
+  Summary:
+    WDT Source File
+
+  Description:
+    None
+
+*******************************************************************************/
+
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+
+#include "device.h"
+#include "plib_wdt.h"
+
+
+void WDT_Initialize( void )
+{
+	WDT_REGS->WDT_MR = WDT_MR_WDD (4095) | WDT_MR_WDV(1024) \
+							 | WDT_MR_WDRSTEN_Msk;
+							
+}
+
+void WDT_Clear(void)
+{
+	WDT_REGS->WDT_CR = (WDT_CR_KEY_PASSWD | WDT_CR_WDRSTT_Msk);
+}
+
+
