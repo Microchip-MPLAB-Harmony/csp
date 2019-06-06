bdd20f676a9b2af2710e542873e22dcce672505c
Jignesh <http://support.microchip.com>
2019-06-06 16:09:24 +0530

    [MCAN] Added mcan_normal_operation_blocking demo for SAMA5D2 with XC32
    MH3-13373

--- apps/mcan/mcan_normal_operation_blocking/firmware/src/main_sama5d2.c
+++ apps/mcan/mcan_normal_operation_blocking/firmware/src/main_sama5d2.c
@@ -125,7 +125,7 @@ int main ( void )
                     {
                         /* Print message to UART1 */
                         uint8_t length = messageLength;
-                        printf("Message - ID : 0x%x Length : 0x%x ", messageID, messageLength);
+                        printf("Message - ID : 0x%lx Length : 0x%x ", messageID, messageLength);
                         printf("Message : ");
                         while(length)
                         {
@@ -136,7 +136,7 @@ int main ( void )
                     }
                     else
                     {
-                        printf("MCAN Tx Error : 0x%x\r\n", status);
+                        printf("MCAN Tx Error : 0x%lx\r\n", status);
                     }
                 }
                 else
@@ -146,7 +146,7 @@ int main ( void )
             }
             else
             {
-                printf("MCAN Rx Error : 0x%x\r\n", status);
+                printf("MCAN Rx Error : 0x%lx\r\n", status);
             }
         }
     }
