2eec45252ddb96e705d253f9807f4db9cfc32cc7
Rajat Batra <rajat.batra@microchip.com>
2019-06-17 16:31:01 +0530

    [ICAP] Regenerated demo for icap_icap_capture_mode_pic32mz_ef_sk MH3-14146
    Signed-off-by: Rajat Batra <rajat.batra@microchip.com>

--- apps/icap/icap_capture_mode/firmware/src/config/pic32mz_ef_sk/peripheral/clk/plib_clk.c
+++ apps/icap/icap_capture_mode/firmware/src/config/pic32mz_ef_sk/peripheral/clk/plib_clk.c
@@ -102,13 +102,13 @@ void CLK_Initialize( void )
   
 
     /* Peripheral Module Disable Configuration */
-    PMD1SET = 0xffffffff;
-    PMD2SET = 0xffffffff;
-    PMD3SET = 0xfffbfffe;
-    PMD4SET = 0xfffffffd;
-    PMD5SET = 0xfffffffd;
-    PMD6SET = 0xfffff0ff;
-    PMD7SET = 0xffffffef;
+    PMD1 = 0xffffffff;
+    PMD2 = 0xffffffff;
+    PMD3 = 0xfffbfffe;
+    PMD4 = 0xfffffffd;
+    PMD5 = 0xfffffffd;
+    PMD6 = 0xfffff0ff;
+    PMD7 = 0xffffffef;
 
     /* Lock system since done with clock configuration */
     int_flag = (bool)__builtin_disable_interrupts();
