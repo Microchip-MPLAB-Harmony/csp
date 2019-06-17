b247a5eae77eebdf81411b72afc42747c87b5867
Rajat Batra <rajat.batra@microchip.com>
2019-06-17 17:35:10 +0530

    [RTCC] Regenerated demo for rtcc_rtcc_alarm_pic32mz_ef_sk MH3-14449
    Signed-off-by: Rajat Batra <rajat.batra@microchip.com>

--- apps/rtcc/rtcc_alarm/firmware/src/config/pic32mz_ef_sk/peripheral/clk/plib_clk.c
+++ apps/rtcc/rtcc_alarm/firmware/src/config/pic32mz_ef_sk/peripheral/clk/plib_clk.c
@@ -102,13 +102,13 @@ void CLK_Initialize( void )
   
 
     /* Peripheral Module Disable Configuration */
-    PMD1SET = 0xffffffff;
-    PMD2SET = 0xffffffff;
-    PMD3SET = 0xffffffff;
-    PMD4SET = 0xffffffff;
-    PMD5SET = 0xfffffffd;
-    PMD6SET = 0xfffff0fe;
-    PMD7SET = 0xffffffef;
+    PMD1 = 0xffffffff;
+    PMD2 = 0xffffffff;
+    PMD3 = 0xffffffff;
+    PMD4 = 0xffffffff;
+    PMD5 = 0xfefffffd;
+    PMD6 = 0xfffff0fe;
+    PMD7 = 0xffffffef;
 
     /* Lock system since done with clock configuration */
     int_flag = (bool)__builtin_disable_interrupts();
