dc54da2edebcf962757379a9f76e08ce9a40fa71
Rajat <http://support.microchip.com>
2019-06-17 17:01:51 +0530

    [EFC] Regenerated demo for efc_flash_read_write_sam_g55_xpro MH3-13979

--- apps/efc/flash_read_write/firmware/src/config/sam_g55_xpro/startup_xc32.c
+++ apps/efc/flash_read_write/firmware/src/config/sam_g55_xpro/startup_xc32.c
@@ -82,10 +82,11 @@ __STATIC_INLINE void TCM_Configure(uint32_t tcmSize);
 __STATIC_INLINE void ICache_Enable(void);
 __STATIC_INLINE void DCache_Enable(void);
 
-/** Program CMCC CSIZESW bits for TCM and cache configuration */
+/** Program CMCC PRGCSIZE bits for TCM and cache configuration */
 __STATIC_INLINE void TCM_Configure(uint32_t tcmSize)
 {
-    }
+        CMCC_REGS->CMCC_CFG = CMCC_CFG_PRGCSIZE(tcmSize);
+}
 
 /** Enable TCM memory */
 __STATIC_INLINE void  __attribute__((optimize("-O1"))) TCM_Enable(void)
