12ad643c413935021ec19dbf2b41c49030979e5f
Rajat <http://support.microchip.com>
2019-06-17 17:19:06 +0530

    [SPI] Regenerated demo for spi_spi_self_loopback_blocking_pic32mz_ef_sk MH3-14670

--- apps/spi/spi_self_loopback_blocking/firmware/src/config/pic32mz_ef_sk/peripheral/clk/plib_clk.c
+++ apps/spi/spi_self_loopback_blocking/firmware/src/config/pic32mz_ef_sk/peripheral/clk/plib_clk.c
@@ -102,13 +102,13 @@ void CLK_Initialize( void )
   
 
     /* Peripheral Module Disable Configuration */
-    PMD1SET = 0xffffffff;
-    PMD2SET = 0xffffffff;
-    PMD3SET = 0xffffffff;
-    PMD4SET = 0xffffffff;
-    PMD5SET = 0xfffffbff;
-    PMD6SET = 0xfffff0ff;
-    PMD7SET = 0xffffffef;
+    PMD1 = 0xffffffff;
+    PMD2 = 0xffffffff;
+    PMD3 = 0xffffffff;
+    PMD4 = 0xffffffff;
+    PMD5 = 0xfefffbff;
+    PMD6 = 0xfffff0ff;
+    PMD7 = 0xffffffef;
 
     /* Lock system since done with clock configuration */
     int_flag = (bool)__builtin_disable_interrupts();
