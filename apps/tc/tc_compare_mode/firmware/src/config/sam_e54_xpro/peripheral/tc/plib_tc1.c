111cebb6a2119b0667f81fa9d4bf483250558f5a
Rajat <http://support.microchip.com>
2019-06-17 15:56:35 +0530

    [TC] Regenerated demo for tc_tc_compare_mode_sam_e54_xpro MH3-14843

--- apps/tc/tc_compare_mode/firmware/src/config/sam_e54_xpro/peripheral/tc/plib_tc1.c
+++ apps/tc/tc_compare_mode/firmware/src/config/sam_e54_xpro/peripheral/tc/plib_tc1.c
@@ -55,7 +55,6 @@
 
 #include "plib_tc1.h"
 
-
 // *****************************************************************************
 // *****************************************************************************
 // Section: Global Data
@@ -184,6 +183,6 @@ TC_COMPARE_STATUS TC1_CompareStatusGet( void )
     TC_COMPARE_STATUS compare_status;
     compare_status = ((TC1_REGS->COUNT16.TC_INTFLAG) & TC_COMPARE_STATUS_MSK);
     /* Clear timer overflow interrupt */
-    TC1_REGS->COUNT16.TC_INTFLAG = TC_COMPARE_STATUS_MSK;
+    TC1_REGS->COUNT16.TC_INTFLAG = compare_status;
     return compare_status;
 }
