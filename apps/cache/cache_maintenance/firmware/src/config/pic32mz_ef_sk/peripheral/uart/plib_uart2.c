7722ab65df9c1f54e8ff18b1a7c1e08b777988d5
Rajat Batra <rajat.batra@microchip.com>
2019-06-17 18:43:09 +0530

    [CACHE] Regenerated demo for cache_cache_maintenance_pic32mz_ef_sk MH3-13730
    Signed-off-by: Rajat Batra <rajat.batra@microchip.com>

--- apps/cache/cache_maintenance/firmware/src/config/pic32mz_ef_sk/peripheral/uart/plib_uart2.c
+++ apps/cache/cache_maintenance/firmware/src/config/pic32mz_ef_sk/peripheral/uart/plib_uart2.c
@@ -318,6 +318,8 @@ void UART2_FAULT_InterruptHandler (void)
 {
     /* Disable the fault interrupt */
     IEC4CLR = _IEC4_U2EIE_MASK;
+    /* Disable the receive interrupt */
+    IEC4CLR = _IEC4_U2RXIE_MASK;
 
     /* Clear rx status */
     uart2Obj.rxBusyStatus = false;
