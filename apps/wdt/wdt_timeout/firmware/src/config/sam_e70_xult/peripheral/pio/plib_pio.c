7d9ceef1e668e30191c3821ca15e841503e10145
Rajat <http://support.microchip.com>
2019-06-17 16:24:56 +0530

    [WDT] Regenerated demo for wdt_wdt_timeout_sam_e70_xult MH3-15007

--- apps/wdt/wdt_timeout/firmware/src/config/sam_e70_xult/peripheral/pio/plib_pio.c
+++ apps/wdt/wdt_timeout/firmware/src/config/sam_e70_xult/peripheral/pio/plib_pio.c
@@ -65,7 +65,7 @@ uint8_t portNumCb[PIO_MAX_NUM_OF_CHANNELS + 1] = {0, 1, 1, 1, 1, 1};
 void PIO_Initialize ( void )
 {
     /* Selected System IO pins are configured as GPIO */
-    MATRIX_REGS->CCFG_SYSIO |= 0x10;
+    MATRIX_REGS->CCFG_SYSIO |= 0xc10;
 
     /************************ PIO A Initialization ************************/
     /* PORTA PIO Disable and Peripheral Enable*/
@@ -114,6 +114,8 @@ void PIO_Initialize ( void )
     /* PORTB Output Direction Enable */
     ((pio_registers_t*)PIO_PORT_B)->PIO_OER = 0x0;
     ((pio_registers_t*)PIO_PORT_B)->PIO_ODR = ~0x0;
+    /* PORTB Initial state High */
+    ((pio_registers_t*)PIO_PORT_B)->PIO_ODSR = 0x0;
     /* PORTB drive control */
     ((pio_registers_t*)PIO_PORT_B)->PIO_DRIVER = 0x0;
 
@@ -129,6 +131,8 @@ void PIO_Initialize ( void )
     /* PORTC Output Direction Enable */
     ((pio_registers_t*)PIO_PORT_C)->PIO_OER = 0x0;
     ((pio_registers_t*)PIO_PORT_C)->PIO_ODR = ~0x0;
+    /* PORTC Initial state High */
+    ((pio_registers_t*)PIO_PORT_C)->PIO_ODSR = 0x0;
     /* PORTC drive control */
     ((pio_registers_t*)PIO_PORT_C)->PIO_DRIVER = 0x0;
 
@@ -144,6 +148,8 @@ void PIO_Initialize ( void )
     /* PORTD Output Direction Enable */
     ((pio_registers_t*)PIO_PORT_D)->PIO_OER = 0x0;
     ((pio_registers_t*)PIO_PORT_D)->PIO_ODR = ~0x0;
+    /* PORTD Initial state High */
+    ((pio_registers_t*)PIO_PORT_D)->PIO_ODSR = 0x0;
     /* PORTD drive control */
     ((pio_registers_t*)PIO_PORT_D)->PIO_DRIVER = 0x0;
 
@@ -159,6 +165,8 @@ void PIO_Initialize ( void )
     /* PORTE Output Direction Enable */
     ((pio_registers_t*)PIO_PORT_E)->PIO_OER = 0x0;
     ((pio_registers_t*)PIO_PORT_E)->PIO_ODR = ~0x0;
+    /* PORTE Initial state High */
+    ((pio_registers_t*)PIO_PORT_E)->PIO_ODSR = 0x0;
     /* PORTE drive control */
     ((pio_registers_t*)PIO_PORT_E)->PIO_DRIVER = 0x0;
 
