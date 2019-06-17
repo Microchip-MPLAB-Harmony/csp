c5a8879ed3d30b46c17e0971b0af8690d29a56c0
Rajat Batra <rajat.batra@microchip.com>
2019-06-17 16:04:55 +0530

    [SERCOM] Regenerated demo for sercom_i2c_master_i2c_eeprom_sam_l10_xpro MH3-14477
    Signed-off-by: Rajat Batra <rajat.batra@microchip.com>

--- apps/sercom/i2c/master/i2c_eeprom/firmware/src/config/sam_l10_xpro/definitions.h
+++ apps/sercom/i2c/master/i2c_eeprom/firmware/src/config/sam_l10_xpro/definitions.h
@@ -51,10 +51,10 @@
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/evsys/plib_evsys.h"
 #include "peripheral/sercom/i2cm/plib_sercom0_i2c.h"
-#include "peripheral/pm/plib_pm.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
+#include "peripheral/pm/plib_pm.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
