3cd36cc705fe5080cb5d866f7f93c270a809cbad
Rajat Batra <rajat.batra@microchip.com>
2019-06-17 18:49:30 +0530

    [TC] Regenerated demo for tc_tc_timer_mode_sam_l10_xpro
    MH3-14871
    
    Signed-off-by: Rajat Batra <rajat.batra@microchip.com>

--- apps/tc/tc_timer_mode/firmware/src/config/sam_l10_xpro/definitions.h
+++ apps/tc/tc_timer_mode/firmware/src/config/sam_l10_xpro/definitions.h
@@ -50,10 +50,10 @@
 #include <stdbool.h>
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/evsys/plib_evsys.h"
-#include "peripheral/pm/plib_pm.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
+#include "peripheral/pm/plib_pm.h"
 #include "peripheral/tc/plib_tc0.h"
 
 // DOM-IGNORE-BEGIN
