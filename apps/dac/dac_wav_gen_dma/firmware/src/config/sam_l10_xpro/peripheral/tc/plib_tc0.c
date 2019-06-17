de2d00b2826cf28a2e33a65d8864d0c6edae7c99
Rajat <http://support.microchip.com>
2019-06-17 18:22:18 +0530

    [DAC] Regenerated demo for dac_dac_wav_gen_dma_sam_l10_xpro MH3-13897

--- apps/dac/dac_wav_gen_dma/firmware/src/config/sam_l10_xpro/peripheral/tc/plib_tc0.c
+++ apps/dac/dac_wav_gen_dma/firmware/src/config/sam_l10_xpro/peripheral/tc/plib_tc0.c
@@ -88,7 +88,7 @@ void TC0_TimerInitialize( void )
     TC0_REGS->COUNT16.TC_WAVE = TC_WAVE_WAVEGEN_MPWM;
 
     /* Configure timer period */
-    TC0_REGS->COUNT16.TC_CC[0U] = 32000U;
+    TC0_REGS->COUNT16.TC_CC[0U] = 64U;
 
     /* Clear all interrupt flags */
     TC0_REGS->COUNT16.TC_INTFLAG = TC_INTFLAG_Msk;
