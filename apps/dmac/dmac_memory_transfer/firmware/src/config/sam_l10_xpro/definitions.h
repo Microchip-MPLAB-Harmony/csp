4843bf42fd7d54ec888a0fb3cdfa9907c66b6eeb
Rajat Batra <rajat.batra@microchip.com>
2019-06-17 16:14:53 +0530

    [DMAC] Regenerated demo for dmac_dmac_memory_transfer_sam_l10_xpro MH3-13929
    Signed-off-by: Rajat Batra <rajat.batra@microchip.com>

--- apps/dmac/dmac_memory_transfer/firmware/src/config/sam_l10_xpro/definitions.h
+++ apps/dmac/dmac_memory_transfer/firmware/src/config/sam_l10_xpro/definitions.h
@@ -52,12 +52,12 @@
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/evsys/plib_evsys.h"
 #include "peripheral/sercom/usart/plib_sercom0_usart.h"
-#include "peripheral/pm/plib_pm.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/systick/plib_systick.h"
 #include "peripheral/dmac/plib_dmac.h"
+#include "peripheral/pm/plib_pm.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
