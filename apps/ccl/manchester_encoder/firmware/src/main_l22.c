da1f03cdbb56a56ff2ebcc31bb35f01c1cdc8b59
David <http://support.microchip.com>
2019-06-28 08:16:39 -0700

    [CCL] Regenerated manchester_encoder app for sam_l21_xpro MH3-4341

---
+++ apps/ccl/manchester_encoder/firmware/src/main_l22.c
@@ -0,0 +1,73 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+
+/* Macro definitions */
+#define SIZE 10
+
+/* Global variables */
+char mybuffer[]="0123456789";
+
+/* This function will be called by SPI PLIB when transfer is completed */
+void SERCOM0_SPI_Callback(uintptr_t context )
+{
+    SERCOM0_SPI_Write(mybuffer, SIZE);
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+    
+    /* Register callback function   */
+    SERCOM0_SPI_CallbackRegister(SERCOM0_SPI_Callback, 0);
+
+    /* SPI Write */
+    SERCOM0_SPI_Write(mybuffer, SIZE);
+    while ( true )
+    {
+        /* Maintain state machines of all polled MPLAB Harmony modules. */
+        SYS_Tasks ( );
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+
