1dea94d34824fe2531d42f17d144a243084486c0
Bhargav <http://support.microchip.com>
2019-06-30 23:29:40 +0530

    [ARCH] Add initial support for SAMDA1 devices
    MH3-16803

--- arch/arm/config/devices_cortex_m0/ATSAMD21E18A.py
+++ arch/arm/config/devices_cortex_m0/ATSAMD21E18A.py
@@ -24,4 +24,4 @@
 
 print("Loading System Services for " + Variables.get("__PROCESSOR"))
 # load family specific configuration
-execfile(Variables.get("__ARCH_DIR") + "/SAM_D21.py")
+execfile(Variables.get("__ARCH_DIR") + "/SAM_D21_DA1.py")
