8e8c6d045c4c73c385a0cb54fe7fec6100c9fa53
Settu Duraisamy <settu.duraisamy@microchip.com>
2019-07-09 19:57:34 +0530

    [TC] Create tc_timer_mode_sam_d11_xpro configuration demo
    MH3-16948

---
+++ apps/tc/tc_timer_mode/firmware/src/main_d11.c
@@ -0,0 +1,70 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+
+
+/* This function is called after period expires */
+void TC1_Callback_InterruptHandler(TC_TIMER_STATUS status, uintptr_t context)
+{
+    /* Toggle LED */
+    LED_Toggle();
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+    
+    /* Register callback function for TC0 period interrupt */
+    TC1_TimerCallbackRegister(TC1_Callback_InterruptHandler, (uintptr_t)NULL);
+    
+    /* Start the timer channel 0*/
+    TC1_TimerStart();
+
+    while ( true )
+    {
+        /* Maintain state machines of all polled MPLAB Harmony modules. */
+        SYS_Tasks ( );
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+
