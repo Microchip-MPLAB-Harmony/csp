3955f0256b478ea5a533600e2e5bfd0d0d534257
Settu <http://support.microchip.com>
2019-07-10 20:34:54 +0530

    [PORT] Created port_led_on_off_sam_d10_xmini configuration demo
    MH3-16936

---
+++ apps/port/port_led_on_off_polling/firmware/src/main_d10.c
@@ -0,0 +1,64 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+
+#define SWITCH_PRESSED_STATE        0   // Active LOW switch
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+
+    while ( true )
+    {
+        if(SWITCH_Get() == SWITCH_PRESSED_STATE)
+        {
+            /* Turn ON LED */
+            LED_Set();
+        }
+        else
+        {
+            /* Turn OFF LED */
+            LED_Clear();
+        }
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+/*******************************************************************************
+ End of File
+*/
+
