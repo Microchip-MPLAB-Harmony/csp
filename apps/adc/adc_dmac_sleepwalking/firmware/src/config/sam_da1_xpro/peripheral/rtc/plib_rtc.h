e9a26cd9a4a1745d5e8a4f503a822f07eacbe711
Rajat <http://support.microchip.com>
2019-07-16 13:33:13 +0530

    [ADC] Created adc_dmac_sleepwalking demo MH3-17757

---
+++ apps/adc/adc_dmac_sleepwalking/firmware/src/config/sam_da1_xpro/peripheral/rtc/plib_rtc.h
@@ -0,0 +1,82 @@
+/*******************************************************************************
+  Real Time Counter (RTC) PLIB
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    plib_rtc.h
+
+  Summary:
+    RTC PLIB Header file
+
+  Description:
+    This file defines the interface to the RTC peripheral library. This
+    library provides access to and control of the associated peripheral
+    instance.
+
+*******************************************************************************/
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+#ifndef PLIB_RTC_H
+#define PLIB_RTC_H
+
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus // Provide C++ Compatibility
+extern "C" {
+#endif
+// DOM-IGNORE-END
+
+/* Frequency of Counter Clock for RTC */
+#define RTC_COUNTER_CLOCK_FREQUENCY        (1024 / (1 << (0x0)))
+
+
+
+
+void RTC_Initialize(void);
+
+bool RTC_Timer32CounterHasOverflowed ( void );
+bool RTC_Timer32CompareHasMatched(void);
+void RTC_Timer32Start ( void );
+void RTC_Timer32Stop ( void );
+void RTC_Timer32CounterSet ( uint32_t count );
+uint32_t RTC_Timer32CounterGet ( void );
+uint32_t RTC_Timer32FrequencyGet ( void );
+void RTC_Timer32CompareSet ( uint32_t compareValue );
+uint32_t RTC_Timer32PeriodGet ( void );
+
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+}
+#endif
+// DOM-IGNORE-END
+
+#endif /* PLIB_RTC_H */
