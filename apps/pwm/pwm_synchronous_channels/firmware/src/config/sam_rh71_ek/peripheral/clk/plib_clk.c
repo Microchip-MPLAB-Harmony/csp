1fd9699f0c040f31444f66580042f46b6f78d985
Prachi Phadke <prachi.phadke@microchip.com>
2019-08-22 16:48:40 +0530

    [PWM] Create pwm_synchronous_channels_sam_rh71_ek demo
    MH3-12497

---
+++ apps/pwm/pwm_synchronous_channels/firmware/src/config/sam_rh71_ek/peripheral/clk/plib_clk.c
@@ -0,0 +1,120 @@
+/*******************************************************************************
+* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+
+#include "device.h"
+#include "plib_clk.h"
+
+
+/*********************************************************************************
+Initialize Slow Clock (SLCK)
+*********************************************************************************/
+static void CLK_SlowClockInitialize(void)
+{
+    SUPC_REGS->SUPC_CR = SUPC_CR_KEY_PASSWD;
+}
+
+/*********************************************************************************
+Initialize Main Clock (MAINCK)
+*********************************************************************************/
+static void CLK_MainClockInitialize(void)
+{
+    /* Disable Main Crystal Oscillator and Enable External Clock Signal on XIN pin  */
+    PMC_REGS->CKGR_MOR = (PMC_REGS->CKGR_MOR & ~CKGR_MOR_MOSCXTEN_Msk) | CKGR_MOR_KEY_PASSWD | CKGR_MOR_MOSCXTBY_Msk;
+
+     /* External clock signal (XIN pin) is selected as the Main Clock (MAINCK) source.
+        Switch Main Clock (MAINCK) to External signal on XIN pin */
+    PMC_REGS->CKGR_MOR |= CKGR_MOR_KEY_PASSWD | CKGR_MOR_MOSCSEL_Msk;
+
+    /* Wait until MAINCK is switched to External Clock Signal (XIN pin) */
+    while ( (PMC_REGS->PMC_SR & PMC_SR_MOSCSELS_Msk) != PMC_SR_MOSCSELS_Msk);
+
+    /* Disable the RC Oscillator */
+    PMC_REGS->CKGR_MOR = CKGR_MOR_KEY_PASSWD | (PMC_REGS->CKGR_MOR & ~CKGR_MOR_MOSCRCEN_Msk);
+    
+}
+
+/*********************************************************************************
+Initialize PLLACK/PLLBCK
+*********************************************************************************/
+static void CLK_PLLxClockInitialize(void)
+{
+    /* Configure and Enable PLLA */
+    PMC_REGS->CKGR_PLLAR =  CKGR_PLLAR_ONE_Msk |
+                            CKGR_PLLAR_FREQ_VCO_VCO0 |
+                            CKGR_PLLAR_PLLACOUNT(0x3f) |
+                            CKGR_PLLAR_MULA(9) |
+                            CKGR_PLLAR_DIVA(1);
+
+    while ( (PMC_REGS->PMC_SR & PMC_SR_LOCKA_Msk) != PMC_SR_LOCKA_Msk);
+
+}
+
+/*********************************************************************************
+Initialize Master clock (MCK)
+*********************************************************************************/
+static void CLK_MasterClockInitialize(void)
+{
+    /* Program PMC_MCKR.PRES and wait for PMC_SR.MCKRDY to be set   */
+    PMC_REGS->PMC_MCKR = (PMC_REGS->PMC_MCKR & ~PMC_MCKR_PRES_Msk) | PMC_MCKR_PRES_CLK_1;
+    while ((PMC_REGS->PMC_SR & PMC_SR_MCKRDY_Msk) != PMC_SR_MCKRDY_Msk);
+
+    /* Program PMC_MCKR.MDIV and Wait for PMC_SR.MCKRDY to be set   */
+    PMC_REGS->PMC_MCKR = (PMC_REGS->PMC_MCKR & ~PMC_MCKR_MDIV_Msk) | PMC_MCKR_MDIV_PCK_DIV2;
+    while ((PMC_REGS->PMC_SR & PMC_SR_MCKRDY_Msk) != PMC_SR_MCKRDY_Msk);
+
+    /* Program PMC_MCKR.CSS and Wait for PMC_SR.MCKRDY to be set    */
+    PMC_REGS->PMC_MCKR = (PMC_REGS->PMC_MCKR & ~PMC_MCKR_CSS_Msk) | PMC_MCKR_CSS_PLLA_CLK;
+    while ((PMC_REGS->PMC_SR & PMC_SR_MCKRDY_Msk) != PMC_SR_MCKRDY_Msk);
+
+}
+
+/*********************************************************************************
+Initialize Peripheral Clock
+*********************************************************************************/
+static void CLK_PeripheralClockInitialize(void)
+{
+    PMC_REGS->PMC_PCR = PMC_PCR_EN_Msk | PMC_PCR_CMD_Msk | PMC_PCR_PID(10); /* PIOA */
+    PMC_REGS->PMC_PCR = PMC_PCR_EN_Msk | PMC_PCR_CMD_Msk | PMC_PCR_PID(31); /* PWM0 */
+}
+
+/*********************************************************************************
+Clock Initialize
+*********************************************************************************/
+void CLK_Initialize( void )
+{
+    /* Initialize Slow Clock */
+    CLK_SlowClockInitialize();
+
+    /* Initialize Main Clock */
+    CLK_MainClockInitialize();
+
+    /* Initialize PLLA/PLLB */
+    CLK_PLLxClockInitialize();
+
+    /* Initialize Master Clock */
+    CLK_MasterClockInitialize();
+
+    /* Initialize Peripheral Clock */
+    CLK_PeripheralClockInitialize();
+
+}
