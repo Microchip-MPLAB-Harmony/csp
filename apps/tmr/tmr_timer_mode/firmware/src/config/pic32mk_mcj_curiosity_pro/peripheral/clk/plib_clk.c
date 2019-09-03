03372caacd7aa793fdf177a35d06cd1fa3f47c0d
Rajat <http://support.microchip.com>
2019-09-03 21:06:56 +0530

    [TMR] Regenerated demo for tmr_tmr_timer_mode_pic32mk_mcj_curiosity_pro MH3-21747

--- apps/tmr/tmr_timer_mode/firmware/src/config/pic32mk_mcj_curiosity_pro/peripheral/clk/plib_clk.c
+++ apps/tmr/tmr_timer_mode/firmware/src/config/pic32mk_mcj_curiosity_pro/peripheral/clk/plib_clk.c
@@ -98,13 +98,11 @@ void CLK_Initialize( void )
         __builtin_mtc0(12, 0,(__builtin_mfc0(12, 0) | 0x0001)); /* enable interrupts */
     }
 
-
-
   
 
     /* Peripheral Module Disable Configuration */
     PMD1SET = 0xfffffffe;
-    PMD2SET = 0xffffffff;
+    PMD2SET = 0x17001f;
     PMD3SET = 0xffffffff;
     PMD4SET = 0xfffffffd;
     PMD5SET = 0xefffffff;
