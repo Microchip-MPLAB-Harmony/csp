d510dee1bb03b0a01f079e5b7568787e022a086b
Rajat Batra <rajat.batra@microchip.com>
2019-09-03 18:22:55 +0530

    [ADCHS] Regenerated demo for adchs_adchs_polled_pic32mz_ef_sk MH3-20132
    Signed-off-by: Rajat Batra <rajat.batra@microchip.com>

--- apps/adchs/adchs_polled/firmware/src/config/pic32mz_ef_sk/peripheral/clk/plib_clk.c
+++ apps/adchs/adchs_polled/firmware/src/config/pic32mz_ef_sk/peripheral/clk/plib_clk.c
@@ -102,13 +102,13 @@ void CLK_Initialize( void )
   
 
     /* Peripheral Module Disable Configuration */
-    PMD1 = 0xfffffffe;
-    PMD2 = 0xffffffff;
-    PMD3 = 0xffffffff;
-    PMD4 = 0xfffffffb;
-    PMD5 = 0xfefffffd;
-    PMD6 = 0xfffff0ff;
-    PMD7 = 0xffffffef;
+    PMD1 = 0x1000;
+    PMD2 = 0x3;
+    PMD3 = 0x1ff01ff;
+    PMD4 = 0x1fb;
+    PMD5 = 0x301f3f3d;
+    PMD6 = 0x10830001;
+    PMD7 = 0x500000;
 
     /* Lock system since done with clock configuration */
     int_flag = (bool)__builtin_disable_interrupts();
