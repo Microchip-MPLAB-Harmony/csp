cb76e0d047e1169980f576be8ac6cfae6c16b1aa
Rajat Batra <rajat.batra@microchip.com>
2019-09-03 18:49:20 +0530

    [EIC] Regenerated demo for eic_eic_led_on_off_sam_d20_xpro MH3-20560
    Signed-off-by: Rajat Batra <rajat.batra@microchip.com>

--- apps/eic/eic_led_on_off/firmware/src/config/sam_d20_xpro/peripheral/clock/plib_clock.c
+++ apps/eic/eic_led_on_off/firmware/src/config/sam_d20_xpro/peripheral/clock/plib_clock.c
@@ -78,6 +78,7 @@ static void DFLL_Initialize(void)
 
 static void GCLK0_Initialize(void)
 {
+    
     GCLK_REGS->GCLK_GENCTRL = GCLK_GENCTRL_SRC(7) | GCLK_GENCTRL_GENEN_Msk | GCLK_GENCTRL_ID(0);
 
     while((GCLK_REGS->GCLK_STATUS & GCLK_STATUS_SYNCBUSY_Msk) == GCLK_STATUS_SYNCBUSY_Msk)
@@ -105,7 +106,6 @@ void CLOCK_Initialize (void)
     DFLL_Initialize();
     GCLK1_Initialize();
     GCLK0_Initialize();
-
 
 
     /* Selection of the Generator and write Lock for EIC */
