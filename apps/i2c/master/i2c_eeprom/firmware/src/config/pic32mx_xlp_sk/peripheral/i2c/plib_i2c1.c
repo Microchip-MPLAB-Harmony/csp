14ea3ff7a06139a16338c96974032731f41068e5
Rajat Batra <rajat.batra@microchip.com>
2019-09-03 21:15:49 +0530

    [I2C] Regenerated demo for i2c_master_i2c_eeprom_pic32mx_xlp_sk MH3-20743
    Signed-off-by: Rajat Batra <rajat.batra@microchip.com>

--- apps/i2c/master/i2c_eeprom/firmware/src/config/pic32mx_xlp_sk/peripheral/i2c/plib_i2c1.c
+++ apps/i2c/master/i2c_eeprom/firmware/src/config/pic32mx_xlp_sk/peripheral/i2c/plib_i2c1.c
@@ -57,6 +57,7 @@
 // *****************************************************************************
 // *****************************************************************************
 
+
 static I2C_OBJ i2c1Obj;
 
 void I2C1_Initialize(void)
@@ -104,17 +105,10 @@ static void I2C1_TransferSM(void)
             {
                 if (i2c1Obj.address > 0x007F)
                 {
-                    /* Transmit the first byte of the 10-bit slave address */
-                    I2C1TRN = ( 0xF0 | (((uint8_t*)&i2c1Obj.address)[1] << 1) | (i2c1Obj.transferType) );
+                    /* Transmit the MSB 2 bits of the 10-bit slave address, with R/W = 0 */
+                    I2C1TRN = ( 0xF0 | (((uint8_t*)&i2c1Obj.address)[1] << 1));
 
-                    if (i2c1Obj.transferType == I2C_TRANSFER_TYPE_WRITE)
-                    {
-                        i2c1Obj.state = I2C_STATE_ADDR_BYTE_2_SEND;
-                    }
-                    else
-                    {
-                        i2c1Obj.state = I2C_STATE_READ;
-                    }
+                    i2c1Obj.state = I2C_STATE_ADDR_BYTE_2_SEND;
                 }
                 else
                 {
@@ -138,9 +132,50 @@ static void I2C1_TransferSM(void)
             {
                 if (!(I2C1STAT & _I2C1STAT_TBF_MASK))
                 {
+                    /* Transmit the remaining 8-bits of the 10-bit address */
                     I2C1TRN = i2c1Obj.address;
-                    i2c1Obj.state = I2C_STATE_WRITE;
+
+                    if (i2c1Obj.transferType == I2C_TRANSFER_TYPE_WRITE)
+                    {
+                        i2c1Obj.state = I2C_STATE_WRITE;
+                    }
+                    else
+                    {
+                        i2c1Obj.state = I2C_STATE_READ_10BIT_MODE;
+                    }
                 }
+            }
+            else
+            {
+                /* NAK received. Generate Stop Condition. */
+                i2c1Obj.error = I2C_ERROR_NACK;
+                I2C1CONSET = _I2C1CON_PEN_MASK;
+                i2c1Obj.state = I2C_STATE_WAIT_STOP_CONDITION_COMPLETE;
+            }
+            break;
+            
+        case I2C_STATE_READ_10BIT_MODE:
+            if (!(I2C1STAT & _I2C1STAT_ACKSTAT_MASK))
+            {
+                /* Generate repeated start condition */
+                I2C1CONSET = _I2C1CON_RSEN_MASK;
+                i2c1Obj.state = I2C_STATE_ADDR_BYTE_1_SEND_10BIT_ONLY;
+            }
+            else
+            {
+                /* NAK received. Generate Stop Condition. */
+                i2c1Obj.error = I2C_ERROR_NACK;
+                I2C1CONSET = _I2C1CON_PEN_MASK;
+                i2c1Obj.state = I2C_STATE_WAIT_STOP_CONDITION_COMPLETE;
+            }
+            break;
+        case I2C_STATE_ADDR_BYTE_1_SEND_10BIT_ONLY:
+            /* Is transmit buffer full? */
+            if (!(I2C1STAT & _I2C1STAT_TBF_MASK))
+            {
+                /* Transmit the first byte of the 10-bit slave address, with R/W = 1 */
+                I2C1TRN = ( 0xF1 | ((((uint8_t*)&i2c1Obj.address)[1] << 1)));
+                i2c1Obj.state = I2C_STATE_READ;
             }
             else
             {
@@ -168,9 +203,20 @@ static void I2C1_TransferSM(void)
                     {
                         /* Generate repeated start condition */
                         I2C1CONSET = _I2C1CON_RSEN_MASK;
+
                         i2c1Obj.transferType = I2C_TRANSFER_TYPE_READ;
-                        /* Send the I2C slave address with R/W = 1*/
-                        i2c1Obj.state = I2C_STATE_ADDR_BYTE_1_SEND;
+
+                        if (i2c1Obj.address > 0x007F)
+                        {
+                            /* Send the I2C slave address with R/W = 1 */
+                            i2c1Obj.state = I2C_STATE_ADDR_BYTE_1_SEND_10BIT_ONLY;
+                        }
+                        else
+                        {
+                            /* Send the I2C slave address with R/W = 1 */
+                            i2c1Obj.state = I2C_STATE_ADDR_BYTE_1_SEND;
+                        }
+
                     }
                     else
                     {
@@ -335,6 +381,7 @@ bool I2C1_Write(uint16_t address, uint8_t* wdata, size_t wlength)
     return true;
 }
 
+
 bool I2C1_WriteRead(uint16_t address, uint8_t* wdata, size_t wlength, uint8_t* rdata, size_t rlength)
 {
     /* State machine must be idle and I2C module should not have detected a start bit on the bus */
@@ -369,6 +416,53 @@ I2C_ERROR I2C1_ErrorGet(void)
     i2c1Obj.error = I2C_ERROR_NONE;
 
     return error;
+}
+
+bool I2C1_TransferSetup(I2C_TRANSFER_SETUP* setup, uint32_t srcClkFreq )
+{
+    uint32_t baudValue;
+    uint32_t i2cClkSpeed;
+
+    if (setup == NULL)
+    {
+        return false;
+    }
+
+    i2cClkSpeed = setup->clkSpeed;
+
+    /* Maximum I2C clock speed cannot be greater than 1 MHz */
+    if (i2cClkSpeed > 1000000)
+    {
+        return false;
+    }
+
+    if( srcClkFreq == 0)
+    {
+        srcClkFreq = 36000000UL;
+    }
+
+    baudValue = ((1.0/(2*i2cClkSpeed) - 104e-9)*(float)srcClkFreq) - 2;
+
+    /* I2CxBRG value cannot be 0 or 1 */
+    if ((baudValue == 0) || (baudValue == 1))
+    {
+        return false;
+    }
+
+    I2C1BRG = baudValue;
+
+    /* Enable slew rate for 400 kHz clock speed; disable for all other speeds */
+
+    if (i2cClkSpeed == 400000)
+    {
+        I2C1CONCLR = _I2C1CON_DISSLW_MASK;;
+    }
+    else
+    {
+        I2C1CONSET = _I2C1CON_DISSLW_MASK;;
+    }
+
+    return true;
 }
 
 static void I2C1_BUS_InterruptHandler(void)
--- apps/i2c/master/i2c_eeprom/firmware/src/config/pic32mx_xlp_sk/peripheral/i2c/plib_i2c1.h
+++ apps/i2c/master/i2c_eeprom/firmware/src/config/pic32mx_xlp_sk/peripheral/i2c/plib_i2c1.h
@@ -132,7 +132,7 @@ void I2C1_Initialize(void);
 
   Precondition:
     I2C1_Initialize must have been called for the associated
-    I2C instance. At least one TRB should be available.
+    I2C instance.
 
   Parameters:
     address - 7-bit / 10-bit slave address.
@@ -188,9 +188,9 @@ bool I2C1_Read(uint16_t address, uint8_t *pdata, size_t length);
     This function writes data to a slave on the bus. The function will attempt
     to write length number of bytes from pdata buffer to a slave whose address
     is specified by address. The I2C Master will generate a Start condition,
-    write the data and then generate a Stop Condition. If the slave NAKs the request 
-    or a bus error was encountered on the bus, the transfer is terminated. The 
-    application can call the I2C1_ErrorGet() function to know that 
+    write the data and then generate a Stop Condition. If the slave NAKs the request
+    or a bus error was encountered on the bus, the transfer is terminated. The
+    application can call the I2C1_ErrorGet() function to know that
     cause of the error.
 
     The function is non-blocking. It initiates bus activity and returns
@@ -204,12 +204,12 @@ bool I2C1_Read(uint16_t address, uint8_t *pdata, size_t length);
 
   Precondition:
     I2C1_Initialize must have been called for the associated
-    I2C instance.  At least one TRB should be available.
+    I2C instance.
 
   Parameters:
     address - 7-bit / 10-bit slave address.
 
-    data    - pointer to source data buffer that contains the data to be
+    pdata   - pointer to source data buffer that contains the data to be
               transmitted.
 
     length  - length of data buffer in number of bytes. Also the number of bytes
@@ -234,7 +234,7 @@ bool I2C1_Read(uint16_t address, uint8_t *pdata, size_t length);
         I2C1_Initialize();
         I2C1_CallbackRegister(MyI2CCallback, NULL);
 
-        if(!I2C1_Read( SLAVE_ADDR, &myData[0], NUM_BYTES ))
+        if(!I2C1_Write( SLAVE_ADDR, &myData[0], NUM_BYTES ))
         {
             // error handling
         }
@@ -246,6 +246,7 @@ bool I2C1_Read(uint16_t address, uint8_t *pdata, size_t length);
 */
 
 bool I2C1_Write(uint16_t address, uint8_t *pdata, size_t length);
+
 
 // *****************************************************************************
 /* Function:
@@ -264,7 +265,7 @@ bool I2C1_Write(uint16_t address, uint8_t *pdata, size_t length);
     bytes are stored in rdata buffer. A Stop condition is generated after the
     last byte has been received.
 
-    If the slave NAKs the request or a bus error was encountered on the bus, 
+    If the slave NAKs the request or a bus error was encountered on the bus,
     the transfer is terminated. The application can call I2C1_ErrorGet()
     function to know that cause of the error.
 
@@ -279,7 +280,7 @@ bool I2C1_Write(uint16_t address, uint8_t *pdata, size_t length);
 
   Precondition:
     I2C1_Initialize must have been called for the associated
-    I2C instance.  A minimum of two TRB's should be available.
+    I2C instance.
 
   Parameters:
     address - 7-bit / 10-bit slave address.
@@ -398,7 +399,7 @@ bool I2C1_IsBusy(void);
   Returns:
     Returns a I2C_ERROR type of status identifying the error that has
     occurred.
-    
+
     Example:
     <code>
     if(I2C_ERROR_NONE == I2C1_ErrorGet())
@@ -455,6 +456,49 @@ I2C_ERROR I2C1_ErrorGet(void);
 */
 
 void I2C1_CallbackRegister(I2C_CALLBACK callback, uintptr_t contextHandle);
+
+// *****************************************************************************
+/* Function:
+    bool I2C1_TransferSetup(I2C_TRANSFER_SETUP* setup, uint32_t srcClkFreq)
+
+   Summary:
+    Dynamic setup of I2C Peripheral.
+
+   Precondition:
+    I2C1_Initialize must have been called for the associated I2C instance.
+	The transfer status should not be busy.
+
+   Parameters:
+    setup - Pointer to the structure containing the transfer setup.
+    srcClkFreq - I2C Peripheral Clock Source Frequency.
+
+   Returns:
+    true - Transfer setup was updated Successfully.
+    false - Failure while updating transfer setup.
+
+   Example:
+    <code>
+
+    I2C_TRANSFER_SETUP setup;
+
+    setup.clkSpeed = 400000;
+
+    // Make sure that the I2C is not busy before changing the I2C clock frequency
+    if (I2C1_IsBusy() == false)
+    {
+        if (I2C1_TransferSetup( &setup, 0 ) == true)
+        {
+            // Transfer Setup updated successfully
+        }
+    }
+    </code>
+
+   Remarks:
+    srcClkFreq overrides any change in the peripheral clock frequency.
+    If configured to zero PLib takes the peripheral clock frequency from MHC.
+*/
+
+bool I2C1_TransferSetup(I2C_TRANSFER_SETUP* setup, uint32_t srcClkFreq );
 
 
 // DOM-IGNORE-BEGIN
