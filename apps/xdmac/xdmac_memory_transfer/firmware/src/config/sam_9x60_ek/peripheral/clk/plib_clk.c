8215b0f631292629dc195f4cea6264dcc6c1faf5
Rajat <http://support.microchip.com>
2019-09-03 20:22:44 +0530

    [XDMAC] Regenerated demo for xdmac_xdmac_memory_transfer_sam_9x60_ek MH3-19943

--- apps/xdmac/xdmac_memory_transfer/firmware/src/config/sam_9x60_ek/peripheral/clk/plib_clk.c
+++ apps/xdmac/xdmac_memory_transfer/firmware/src/config/sam_9x60_ek/peripheral/clk/plib_clk.c
@@ -1,58 +1,69 @@
 #include "device.h"
 #include "plib_clk.h"
 
+#define PLL_ID_PLLA     0U
+#define PLL_ID_UPLL     1U
 
-//TODO: To handle the delay break this into different functions that SYS_Init can call
-// and do the waitning in sys_init
 
+/*********************************************************************************
+Initialize Programmable clocks 
+*********************************************************************************/
 static void initProgrammableClk(void)
 {
     PMC_REGS->PMC_SCDR |= PMC_SCDR_PCK0_Msk | PMC_SCDR_PCK1_Msk;
 }
 
+/*********************************************************************************
+Initialize Peripheral clocks
+*********************************************************************************/
 static void initPeriphClk(void)
 {
     struct {
-        int id;
-        int gclk;
-        int css;
-        int div;
-    } periphList[] = {
-        { ID_PIOA, 0, 0, 0},
-        { ID_PIOB, 0, 0, 0},
-        { ID_PIOC, 0, 0, 0},
-        { ID_XDMAC, 0, 0, 0},
-        { ID_PIOD, 0, 0, 0},
-        { ID_DBGU, 1, PMC_PCR_GCLKCSS_MCK, 0 },
-        { ID_PERIPH_MAX + 1, 0, 0, 0}//end of list marker
+        uint8_t id;
+        uint8_t clken;
+        uint8_t gclken;
+        uint8_t css;
+        uint8_t div;
+    } periphList[] =
+    {
+        { ID_PIOA, 1, 0, 0, 0},
+        { ID_PIOB, 1, 0, 0, 0},
+        { ID_PIOC, 1, 0, 0, 0},
+        { ID_XDMAC, 1, 0, 0, 0},
+        { ID_PIOD, 1, 0, 0, 0},
+        { ID_DBGU, 1, 1, 0x3, 0},
+        { ID_PERIPH_MAX + 1, 0, 0, 0, 0}//end of list marker
     };
 
-    int i;
-    int count;
-
-    count = sizeof(periphList)/sizeof(periphList[0]);
-    for (i=0; i<count; i++) {
-        if (periphList[i].id == (ID_PERIPH_MAX + 1)) {
+    int count = sizeof(periphList)/sizeof(periphList[0]);
+    for (int i = 0; i < count; i++)
+    {
+        if (periphList[i].id == (ID_PERIPH_MAX + 1))
+        {
             break;
         }
-        PMC_REGS->PMC_PCR = PMC_PCR_PID(periphList[i].id) |\
-                            PMC_PCR_EN_Msk |\
-                            PMC_PCR_CMD_Msk |\
-                            PMC_PCR_GCLKEN(periphList[i].gclk) |\
-                            periphList[i].css |\
-                            PMC_PCR_GCLKDIV(periphList[i].div);
+
+        PMC_REGS->PMC_PCR = PMC_PCR_CMD_Msk |\
+                            PMC_PCR_GCLKEN(periphList[i].gclken) |\
+                            PMC_PCR_EN(periphList[i].clken) |\
+                            PMC_PCR_GCLKDIV(periphList[i].div) |\
+                            PMC_PCR_GCLKCSS(periphList[i].css) |\
+                            PMC_PCR_PID(periphList[i].id);                
     }
 
 }
 
-void initSysClks(void)
-{
 
-}
-
+/*********************************************************************************
+Clock Initialize
+*********************************************************************************/
 void CLK_Initialize( void )
 {
-initProgrammableClk();
-initPeriphClk();
-initSysClks();
+    
+    /* Initialize Programmable clock */
+    initProgrammableClk();
+
+    /* Initialize Peripheral clock */
+    initPeriphClk();
+ 
 }
