ae63de8d2efbb954a5dae55c8f1f66ecf4cc9419
Rajat <http://support.microchip.com>
2019-09-03 20:23:30 +0530

    [XDMAC] Regenerated demo for xdmac_xdmac_memory_transfer_sam_a5d2_xult MH3-19945

--- apps/xdmac/xdmac_memory_transfer/firmware/src/config/sam_a5d2_xult/peripheral/pio/plib_pio.c
+++ apps/xdmac/xdmac_memory_transfer/firmware/src/config/sam_a5d2_xult/peripheral/pio/plib_pio.c
@@ -59,9 +59,13 @@
 */
 void PIO_Initialize ( void )
 {
+ /* Port B Peripheral function GPIO configuration */
+	PIOB_REGS->PIO_MSKR = 0x1;
+	PIOB_REGS->PIO_CFGR = 0x0;
+	
  /* Port B Pin 0 configuration */
 	PIOB_REGS->PIO_MSKR = 0x1;
-	PIOB_REGS->PIO_CFGR |= 0x100;
+	PIOB_REGS->PIO_CFGR = (PIOB_REGS->PIO_CFGR & (PIO_CFGR_FUNC_Msk)) | 0x100;
 	
  /* Port B Latch configuration */
 	PIOB_REGS->PIO_SODR = 0x1;
@@ -69,30 +73,6 @@ void PIO_Initialize ( void )
  /* Port D Peripheral function A configuration */
 	PIOD_REGS->PIO_MSKR = 0x3c00cL;
 	PIOD_REGS->PIO_CFGR = 0x1;
-	
- /* Port D Peripheral function B configuration */
-	PIOD_REGS->PIO_MSKR = 0x0L;
-	PIOD_REGS->PIO_CFGR = 0x2;
-	
- /* Port D Peripheral function C configuration */
-	PIOD_REGS->PIO_MSKR = 0x0L;
-	PIOD_REGS->PIO_CFGR = 0x3;
-	
- /* Port D Peripheral function D configuration */
-	PIOD_REGS->PIO_MSKR = 0x0L;
-	PIOD_REGS->PIO_CFGR = 0x4;
-	
- /* Port D Peripheral function E configuration */
-	PIOD_REGS->PIO_MSKR = 0x0L;
-	PIOD_REGS->PIO_CFGR = 0x5;
-	
- /* Port D Peripheral function F configuration */
-	PIOD_REGS->PIO_MSKR = 0x0L;
-	PIOD_REGS->PIO_CFGR = 0x6;
-	
- /* Port D Peripheral function G configuration */
-	PIOD_REGS->PIO_MSKR = 0x0L;
-	PIOD_REGS->PIO_CFGR = 0x7;
 	
 }
 
