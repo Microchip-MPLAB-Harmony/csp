b9a7ac929c9feb51658e2dad602171b33586b8e6
Rajat <http://support.microchip.com>
2019-09-03 22:27:03 +0530

    [DAC] Regenerated demo for dac_dac_wav_gen_dma_sam_d11_xpro MH3-20359

--- apps/dac/dac_wav_gen_dma/firmware/src/packs/ATSAMD11D14AM_DFP/atsamd11d14am.h
+++ apps/dac/dac_wav_gen_dma/firmware/src/packs/ATSAMD11D14AM_DFP/atsamd11d14am.h
@@ -20,7 +20,7 @@
  *
  */
 
-/* file generated from device description version 2019-06-25T03:42:57Z */
+/* file generated from device description version 2019-06-28T15:51:48Z */
 #ifndef _SAMD11D14AM_H_
 #define _SAMD11D14AM_H_
 
@@ -150,6 +150,12 @@ typedef struct _DeviceVectors
   void* pfnPTC_Handler;                          /*  18 Peripheral Touch Controller (PTC) */
 } DeviceVectors;
 
+/* Defines for Deprecated Interrupt and Exceptions handler names */
+#define pfnMemManage_Handler      pfnMemoryManagement_Handler     /**< \deprecated  Backward compatibility for ASF*/
+#define pfnDebugMon_Handler       pfnDebugMonitor_Handler         /**< \deprecated  Backward compatibility for ASF*/
+#define pfnNMI_Handler            pfnNonMaskableInt_Handler       /**< \deprecated  Backward compatibility for ASF*/
+#define pfnSVC_Handler            pfnSVCall_Handler               /**< \deprecated  Backward compatibility for ASF*/
+
 #endif /* !(defined(__ASSEMBLER__) || defined(__IAR_SYSTEMS_ASM__)) */
 
 #if !(defined(__ASSEMBLER__) || defined(__IAR_SYSTEMS_ASM__))
@@ -185,6 +191,12 @@ void AC_Handler                    ( void );
 void DAC_Handler                   ( void );
 void PTC_Handler                   ( void );
 #endif /* DONT_USE_PREDEFINED_PERIPHERALS_HANDLERS */
+/* Defines for Deprecated Interrupt and Exceptions handler names */
+#define MemManage_Handler         MemoryManagement_Handler        /**< \deprecated  Backward compatibility*/
+#define DebugMon_Handler          DebugMonitor_Handler            /**< \deprecated  Backward compatibility*/
+#define NMI_Handler               NonMaskableInt_Handler          /**< \deprecated  Backward compatibility*/
+#define SVC_Handler               SVCall_Handler                  /**< \deprecated  Backward compatibility*/
+
 #endif /* !(defined(__ASSEMBLER__) || defined(__IAR_SYSTEMS_ASM__)) */
 
 /*
