3dd67cd3653c7608e106091dd5fd91872af30c5a
Rajat Batra <rajat.batra@microchip.com>
2019-09-03 23:41:51 +0530

    [AC] Regenerated demo for ac_ac_sleepwalk_singleshot_sam_da1_xpro MH3-20034
    Signed-off-by: Rajat Batra <rajat.batra@microchip.com>

--- apps/ac/ac_sleepwalk_singleshot/firmware/src/packs/ATSAMDA1J16B_DFP/component/mtb.h
+++ apps/ac/ac_sleepwalk_singleshot/firmware/src/packs/ATSAMDA1J16B_DFP/component/mtb.h
@@ -20,7 +20,7 @@
  *
  */
 
-/* file generated from device description version 2019-03-25T22:33:43Z */
+/* file generated from device description version 2019-08-23T08:32:38Z */
 #ifndef _SAMDA1_MTB_COMPONENT_H_
 #define _SAMDA1_MTB_COMPONENT_H_
 
