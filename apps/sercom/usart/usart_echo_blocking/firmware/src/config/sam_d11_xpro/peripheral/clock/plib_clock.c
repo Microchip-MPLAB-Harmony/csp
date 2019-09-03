ea15ab682601f3ba1fb6d666ccc4b1b2797fc51e
Rajat <http://support.microchip.com>
2019-09-03 18:57:12 +0530

    [SERCOM] Regenerated demo for sercom_usart_usart_echo_blocking_sam_d11_xpro MH3-21329

--- apps/sercom/usart/usart_echo_blocking/firmware/src/config/sam_d11_xpro/peripheral/clock/plib_clock.c
+++ apps/sercom/usart/usart_echo_blocking/firmware/src/config/sam_d11_xpro/peripheral/clock/plib_clock.c
@@ -92,6 +92,7 @@ static void DFLL_Initialize( void )
 
 static void GCLK0_Initialize( void )
 {
+    
     GCLK_REGS->GCLK_GENCTRL = GCLK_GENCTRL_SRC(7) | GCLK_GENCTRL_GENEN_Msk | GCLK_GENCTRL_ID(0);
 
     while((GCLK_REGS->GCLK_STATUS & GCLK_STATUS_SYNCBUSY_Msk) == GCLK_STATUS_SYNCBUSY_Msk)
@@ -107,7 +108,6 @@ void CLOCK_Initialize( void )
 
     DFLL_Initialize();
     GCLK0_Initialize();
-
 
 
     /* Selection of the Generator and write Lock for SERCOM2_CORE */
