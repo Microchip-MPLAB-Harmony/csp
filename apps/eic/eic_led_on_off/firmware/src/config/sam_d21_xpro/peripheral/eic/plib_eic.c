eccf6c335822147531ea009270fadb38dd80c42b
Rajat Batra <rajat.batra@microchip.com>
2019-09-03 18:49:54 +0530

    [EIC] Regenerated demo for eic_eic_led_on_off_sam_d21_xpro MH3-20562
    Signed-off-by: Rajat Batra <rajat.batra@microchip.com>

--- apps/eic/eic_led_on_off/firmware/src/config/sam_d21_xpro/peripheral/eic/plib_eic.c
+++ apps/eic/eic_led_on_off/firmware/src/config/sam_d21_xpro/peripheral/eic/plib_eic.c
@@ -60,15 +60,14 @@
 // Section: Global Data
 // *****************************************************************************
 // *****************************************************************************
-
 /* EIC Channel Callback object */
 EIC_CALLBACK_OBJ    eicCallbackObject[EXTINT_COUNT];
 
 
-void EIC_Initialize (void)
+void EIC_Initialize(void)
 {
     /* Reset all registers in the EIC module to their initial state and
-	   EIC will be disabled. */
+       EIC will be disabled. */
     EIC_REGS->EIC_CTRL |= EIC_CTRL_SWRST_Msk;
 
     while((EIC_REGS->EIC_STATUS & EIC_STATUS_SYNCBUSY_Msk) == EIC_STATUS_SYNCBUSY_Msk)
@@ -78,8 +77,8 @@ void EIC_Initialize (void)
 
     /* NMI Control register */
 
-    /* Interrupt sense type and filter control for EXTINT channels 0 to 7*/
-    EIC_REGS->EIC_CONFIG[0] =  EIC_CONFIG_SENSE0_NONE  |
+    /* Interrupt sense type and filter control for EXTINT channels 0 to 7 */
+    EIC_REGS->EIC_CONFIG[0] = EIC_CONFIG_SENSE0_NONE  |
                               EIC_CONFIG_SENSE1_NONE  |
                               EIC_CONFIG_SENSE2_NONE  |
                               EIC_CONFIG_SENSE3_NONE  |
@@ -89,7 +88,7 @@ void EIC_Initialize (void)
                               EIC_CONFIG_SENSE7_NONE ;
 
     /* Interrupt sense type and filter control for EXTINT channels 8 to 15 */
-    EIC_REGS->EIC_CONFIG[1] =  EIC_CONFIG_SENSE0_NONE  |
+    EIC_REGS->EIC_CONFIG[1] = EIC_CONFIG_SENSE0_NONE  |
                               EIC_CONFIG_SENSE1_NONE  |
                               EIC_CONFIG_SENSE2_NONE  |
                               EIC_CONFIG_SENSE3_NONE  |
@@ -97,8 +96,6 @@ void EIC_Initialize (void)
                               EIC_CONFIG_SENSE5_NONE  |
                               EIC_CONFIG_SENSE6_NONE  |
                               EIC_CONFIG_SENSE7_RISE ;
-
-
 
     /* External Interrupt enable*/
     EIC_REGS->EIC_INTENSET = 0x8000;
@@ -120,6 +117,7 @@ void EIC_Initialize (void)
     eicCallbackObject[13].eicPinNo = EIC_PIN_MAX;
     eicCallbackObject[14].eicPinNo = EIC_PIN_MAX;
     eicCallbackObject[15].eicPinNo = EIC_PIN_15;
+
     /* Enable the EIC */
     EIC_REGS->EIC_CTRL |= EIC_CTRL_ENABLE_Msk;
 
@@ -129,12 +127,12 @@ void EIC_Initialize (void)
     }
 }
 
-void EIC_InterruptEnable (EIC_PIN pin)
+void EIC_InterruptEnable(EIC_PIN pin)
 {
     EIC_REGS->EIC_INTENSET = (1UL << pin);
 }
 
-void EIC_InterruptDisable (EIC_PIN pin)
+void EIC_InterruptDisable(EIC_PIN pin)
 {
     EIC_REGS->EIC_INTENCLR = (1UL << pin);
 }
