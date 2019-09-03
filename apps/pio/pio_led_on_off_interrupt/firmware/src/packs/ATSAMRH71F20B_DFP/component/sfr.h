f7b386a3e4dc1a105e5f6ead60e46030c99f9f72
Rajat Batra <rajat.batra@microchip.com>
2019-09-03 19:04:54 +0530

    [PIO] Regenerated demo for pio_pio_led_on_off_interrupt_sam_rh71_ek MH3-20857
    Signed-off-by: Rajat Batra <rajat.batra@microchip.com>

--- apps/pio/pio_led_on_off_interrupt/firmware/src/packs/ATSAMRH71F20B_DFP/component/sfr.h
+++ apps/pio/pio_led_on_off_interrupt/firmware/src/packs/ATSAMRH71F20B_DFP/component/sfr.h
@@ -1,7 +1,7 @@
 /**
  * \brief Component description for SFR
  *
- * © 2019 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2019 Microchip Technology Inc. and its subsidiaries.
  *
  * Subject to your compliance with these terms, you may use Microchip software and any derivatives
  * exclusively with Microchip products. It is your responsibility to comply with third party license
@@ -20,7 +20,7 @@
  *
  */
 
-/* file generated from device description version 2019-06-11T20:50:29Z */
+/* file generated from device description version 2019-08-29T14:13:58Z */
 #ifndef _SAMRH71_SFR_COMPONENT_H_
 #define _SAMRH71_SFR_COMPONENT_H_
 
@@ -29,39 +29,40 @@
 /* ************************************************************************** */
 
 /* -------- SFR_SECURE : (SFR Offset: 0x28) (R/W 32) Security Configuration Register -------- */
-#define SFR_SECURE_ROM_Pos                    _U_(0)                                         /**< (SFR_SECURE) Disable Access to ROM Code Position */
-#define SFR_SECURE_ROM_Msk                    (_U_(0x1) << SFR_SECURE_ROM_Pos)               /**< (SFR_SECURE) Disable Access to ROM Code Mask */
-#define SFR_SECURE_Msk                        _U_(0x00000001)                                /**< (SFR_SECURE) Register Mask  */
+#define SFR_SECURE_ROM_Pos                    _U_(0)                                               /**< (SFR_SECURE) Disable Access to ROM Code Position */
+#define SFR_SECURE_ROM_Msk                    (_U_(0x1) << SFR_SECURE_ROM_Pos)                     /**< (SFR_SECURE) Disable Access to ROM Code Mask */
+#define SFR_SECURE_ROM(value)                 (SFR_SECURE_ROM_Msk & ((value) << SFR_SECURE_ROM_Pos))
+#define SFR_SECURE_Msk                        _U_(0x00000001)                                      /**< (SFR_SECURE) Register Mask  */
 
 
 /* -------- SFR_CAN0 : (SFR Offset: 0xA0) (R/W 32) CAN0 MSB Base Address 0 -------- */
-#define SFR_CAN0_EXT_MEM_ADDR_Pos             _U_(16)                                        /**< (SFR_CAN0) MSB Base Address Position */
-#define SFR_CAN0_EXT_MEM_ADDR_Msk             (_U_(0xFFFF) << SFR_CAN0_EXT_MEM_ADDR_Pos)     /**< (SFR_CAN0) MSB Base Address Mask */
+#define SFR_CAN0_EXT_MEM_ADDR_Pos             _U_(16)                                              /**< (SFR_CAN0) MSB Base Address Position */
+#define SFR_CAN0_EXT_MEM_ADDR_Msk             (_U_(0xFFFF) << SFR_CAN0_EXT_MEM_ADDR_Pos)           /**< (SFR_CAN0) MSB Base Address Mask */
 #define SFR_CAN0_EXT_MEM_ADDR(value)          (SFR_CAN0_EXT_MEM_ADDR_Msk & ((value) << SFR_CAN0_EXT_MEM_ADDR_Pos))
-#define SFR_CAN0_Msk                          _U_(0xFFFF0000)                                /**< (SFR_CAN0) Register Mask  */
+#define SFR_CAN0_Msk                          _U_(0xFFFF0000)                                      /**< (SFR_CAN0) Register Mask  */
 
 
 /* -------- SFR_CAN1 : (SFR Offset: 0xA4) (R/W 32) CAN0 MSB Base Address 1 -------- */
-#define SFR_CAN1_EXT_MEM_ADDR_Pos             _U_(16)                                        /**< (SFR_CAN1) MSB Base Address Position */
-#define SFR_CAN1_EXT_MEM_ADDR_Msk             (_U_(0xFFFF) << SFR_CAN1_EXT_MEM_ADDR_Pos)     /**< (SFR_CAN1) MSB Base Address Mask */
+#define SFR_CAN1_EXT_MEM_ADDR_Pos             _U_(16)                                              /**< (SFR_CAN1) MSB Base Address Position */
+#define SFR_CAN1_EXT_MEM_ADDR_Msk             (_U_(0xFFFF) << SFR_CAN1_EXT_MEM_ADDR_Pos)           /**< (SFR_CAN1) MSB Base Address Mask */
 #define SFR_CAN1_EXT_MEM_ADDR(value)          (SFR_CAN1_EXT_MEM_ADDR_Msk & ((value) << SFR_CAN1_EXT_MEM_ADDR_Pos))
-#define SFR_CAN1_Msk                          _U_(0xFFFF0000)                                /**< (SFR_CAN1) Register Mask  */
+#define SFR_CAN1_Msk                          _U_(0xFFFF0000)                                      /**< (SFR_CAN1) Register Mask  */
 
 
 /** \brief SFR register offsets definitions */
-#define SFR_SECURE_REG_OFST            (0x28)         /**< (SFR_SECURE) Security Configuration Register Offset */
-#define SFR_CAN0_REG_OFST              (0xA0)         /**< (SFR_CAN0) CAN0 MSB Base Address 0 Offset */
-#define SFR_CAN1_REG_OFST              (0xA4)         /**< (SFR_CAN1) CAN0 MSB Base Address 1 Offset */
+#define SFR_SECURE_REG_OFST            (0x28)              /**< (SFR_SECURE) Security Configuration Register Offset */
+#define SFR_CAN0_REG_OFST              (0xA0)              /**< (SFR_CAN0) CAN0 MSB Base Address 0 Offset */
+#define SFR_CAN1_REG_OFST              (0xA4)              /**< (SFR_CAN1) CAN0 MSB Base Address 1 Offset */
 
 #if !(defined(__ASSEMBLER__) || defined(__IAR_SYSTEMS_ASM__))
 /** \brief SFR register API structure */
 typedef struct
 {
   __I   uint8_t                        Reserved1[0x28];
-  __IO  uint32_t                       SFR_SECURE;      /**< Offset: 0x28 (R/W  32) Security Configuration Register */
+  __IO  uint32_t                       SFR_SECURE;         /**< Offset: 0x28 (R/W  32) Security Configuration Register */
   __I   uint8_t                        Reserved2[0x74];
-  __IO  uint32_t                       SFR_CAN0;        /**< Offset: 0xA0 (R/W  32) CAN0 MSB Base Address 0 */
-  __IO  uint32_t                       SFR_CAN1;        /**< Offset: 0xA4 (R/W  32) CAN0 MSB Base Address 1 */
+  __IO  uint32_t                       SFR_CAN0;           /**< Offset: 0xA0 (R/W  32) CAN0 MSB Base Address 0 */
+  __IO  uint32_t                       SFR_CAN1;           /**< Offset: 0xA4 (R/W  32) CAN0 MSB Base Address 1 */
 } sfr_registers_t;
 
 
