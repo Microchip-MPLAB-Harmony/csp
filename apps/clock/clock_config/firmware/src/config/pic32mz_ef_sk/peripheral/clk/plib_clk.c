3086e154c82fd1b82c684c8541090a98c949015c
Rajat Batra <rajat.batra@microchip.com>
2019-09-03 23:57:45 +0530

    [CLOCK] Regenerated demo for clock_clock_config_pic32mz_ef_sk MH3-20279
    Signed-off-by: Rajat Batra <rajat.batra@microchip.com>

--- apps/clock/clock_config/firmware/src/config/pic32mz_ef_sk/peripheral/clk/plib_clk.c
+++ apps/clock/clock_config/firmware/src/config/pic32mz_ef_sk/peripheral/clk/plib_clk.c
@@ -112,13 +112,13 @@ void CLK_Initialize( void )
   
 
     /* Peripheral Module Disable Configuration */
-    PMD1 = 0xffffffff;
-    PMD2 = 0xffffffff;
-    PMD3 = 0xffffffff;
-    PMD4 = 0xffffffff;
-    PMD5 = 0xfeffffff;
-    PMD6 = 0xfffff0ff;
-    PMD7 = 0xffffffef;
+    PMD1 = 0x1001;
+    PMD2 = 0x3;
+    PMD3 = 0x1ff01ff;
+    PMD4 = 0x1ff;
+    PMD5 = 0x301f3f3f;
+    PMD6 = 0x10830001;
+    PMD7 = 0x500000;
 
     /* Lock system since done with clock configuration */
     int_flag = (bool)__builtin_disable_interrupts();
