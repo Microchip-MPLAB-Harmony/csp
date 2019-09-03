4455dfeaaaaa2f2ded1aabe497197ca6cba592f4
Rajat <http://support.microchip.com>
2019-09-03 18:42:55 +0530

    [UART] Regenerated demo for uart_uart_echo_blocking_pic32mk_mcj_curiosity_pro MH3-21813

--- apps/uart/uart_echo_blocking/firmware/src/config/pic32mk_mcj_curiosity_pro/peripheral/clk/plib_clk.c
+++ apps/uart/uart_echo_blocking/firmware/src/config/pic32mk_mcj_curiosity_pro/peripheral/clk/plib_clk.c
@@ -98,15 +98,13 @@ void CLK_Initialize( void )
         __builtin_mtc0(12, 0,(__builtin_mfc0(12, 0) | 0x0001)); /* enable interrupts */
     }
 
-
-
   
 
     /* Peripheral Module Disable Configuration */
     PMD1SET = 0xfffffffe;
-    PMD2SET = 0xffffffff;
+    PMD2SET = 0x17001f;
     PMD3SET = 0xffffffff;
-    PMD4SET = 0xffffffff;
+    PMD4SET = 0xfff01ff;
     PMD5SET = 0xeffffffd;
     PMD6SET = 0xfffff0ff;
     PMD7SET = 0xffffffef;
