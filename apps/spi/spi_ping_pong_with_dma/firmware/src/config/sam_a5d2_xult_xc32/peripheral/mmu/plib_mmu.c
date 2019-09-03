90139f9e31becb656968d9b80aeee847f5b31ccc
Rajat Batra <rajat.batra@microchip.com>
2019-09-03 16:26:56 +0530

    [SPI] Regenerated demo for spi_spi_ping_pong_with_dma_sam_a5d2_xult_xc32 MH3-21460
    Signed-off-by: Rajat Batra <rajat.batra@microchip.com>

--- apps/spi/spi_ping_pong_with_dma/firmware/src/config/sam_a5d2_xult_xc32/peripheral/mmu/plib_mmu.c
+++ apps/spi/spi_ping_pong_with_dma/firmware/src/config/sam_a5d2_xult_xc32/peripheral/mmu/plib_mmu.c
@@ -143,7 +143,7 @@ void icache_Disable(void)
 
 static inline uint32_t cp15_read_sctlr(void)
 {
-    uint32_t sctlr;
+    uint32_t sctlr = 0;
     asm("mrc p15, 0, %0, c1, c0, 0" : "=r"(sctlr));
     return sctlr;
 }
