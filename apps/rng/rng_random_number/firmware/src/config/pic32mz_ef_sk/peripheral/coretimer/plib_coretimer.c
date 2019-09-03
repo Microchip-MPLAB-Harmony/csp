997d58f3a1befbbd0de9701e07bc63597dfcb0af
Rajat <http://support.microchip.com>
2019-09-03 20:28:22 +0530

    [RNG] Regenerated demo for rng_rng_random_number_pic32mz_ef_sk MH3-21044

--- apps/rng/rng_random_number/firmware/src/config/pic32mz_ef_sk/peripheral/coretimer/plib_coretimer.c
+++ apps/rng/rng_random_number/firmware/src/config/pic32mz_ef_sk/peripheral/coretimer/plib_coretimer.c
@@ -43,12 +43,74 @@
 
 
 
+static uint32_t compareValue = CORE_TIMER_COMPARE_VALUE;
 
 void CORETIMER_Initialize()
 {
+
+    // Clear Core Timer
+    _CP0_SET_COUNT(0);
+    _CP0_SET_COMPARE(compareValue);
+
     // Enable Timer by clearing Disable Count (DC) bit
     _CP0_SET_CAUSE(_CP0_GET_CAUSE() & (~_CP0_CAUSE_DC_MASK));
 }
+
+void CORETIMER_Start( void )
+{
+    // Disable Timer by setting Disable Count (DC) bit
+    _CP0_SET_CAUSE(_CP0_GET_CAUSE() | _CP0_CAUSE_DC_MASK);
+
+    // Clear Compare Timer Interrupt Flag
+    IFS0CLR=0x1;
+
+    // Clear Core Timer
+    _CP0_SET_COUNT(0);
+
+    _CP0_SET_COMPARE(compareValue);
+
+    // Enable Timer by clearing Disable Count (DC) bit
+    _CP0_SET_CAUSE(_CP0_GET_CAUSE() & (~_CP0_CAUSE_DC_MASK));
+
+}
+
+void CORETIMER_Stop( void )
+{
+    // Disable Timer by setting Disable Count (DC) bit
+    _CP0_SET_CAUSE(_CP0_GET_CAUSE() | _CP0_CAUSE_DC_MASK);
+}
+
+uint32_t CORETIMER_FrequencyGet ( void )
+{
+    return (CORE_TIMER_FREQUENCY);
+}
+
+void CORETIMER_CompareSet ( uint32_t compare )
+{
+    compareValue = compare;
+    _CP0_SET_COMPARE(compareValue);
+}
+
+uint32_t CORETIMER_CounterGet ( void )
+{
+    uint32_t count;
+    count = _CP0_GET_COUNT();
+    return count;
+}
+
+bool CORETIMER_CompareHasExpired( void )
+{
+    if (IFS0bits.CTIF != 0)
+    {
+        // Clear Compare Timer Interrupt Flag
+        IFS0CLR=0x1;
+
+        return true;
+    }
+
+    return false;
+}
+
 void CORETIMER_DelayMs ( uint32_t delay_ms)
 {
     uint32_t startCount, endCount;
