14abf5290be0c5ac1c52ee9071cd773e2dc93f6b
Rajat Batra <rajat.batra@microchip.com>
2019-09-04 00:17:43 +0530

    [TCM] Regenerated demo for tcm_data_tcm_sam_rh71_ek MH3-21739
    Signed-off-by: Rajat Batra <rajat.batra@microchip.com>

--- apps/tcm/data_tcm/firmware/src/packs/ATSAMRH71F20B_DFP/component/icm.h
+++ apps/tcm/data_tcm/firmware/src/packs/ATSAMRH71F20B_DFP/component/icm.h
@@ -20,7 +20,7 @@
  *
  */
 
-/* file generated from device description version 2019-06-11T20:50:29Z */
+/* file generated from device description version 2019-08-29T14:13:58Z */
 #ifndef _SAMRH71_ICM_COMPONENT_H_
 #define _SAMRH71_ICM_COMPONENT_H_
 
