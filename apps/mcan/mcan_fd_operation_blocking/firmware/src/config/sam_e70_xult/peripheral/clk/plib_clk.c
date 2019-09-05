a1a086a1876b122e2648b9b6504cc15ab2b8a5a5
Jignesh <http://support.microchip.com>
2019-09-05 18:13:40 +0530

    [MCAN] DFT regenerated and tested for mcan_mcan_fd_operation_blocking_sam_e70_xult
    MH3-20763

--- apps/mcan/mcan_fd_operation_blocking/firmware/src/config/sam_e70_xult/peripheral/clk/plib_clk.c
+++ apps/mcan/mcan_fd_operation_blocking/firmware/src/config/sam_e70_xult/peripheral/clk/plib_clk.c
@@ -97,7 +97,7 @@ static void CLK_ProgrammableClockInitialize(void)
 /*********************************************************************************
 Clock Initialize
 *********************************************************************************/
-void CLK_Initialize( void )
+void CLOCK_Initialize( void )
 {
 
 
