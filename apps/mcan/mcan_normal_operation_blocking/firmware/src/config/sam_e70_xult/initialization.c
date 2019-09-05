6d9c9a005b38f1d075c7d4ec013d971479cfd801
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2019-09-05 18:07:01 +0530

    [MCAN] DFT regenerated and tested for mcan_mcan_normal_operation_blocking_sam_e70_xult
    MH3-20771

--- apps/mcan/mcan_normal_operation_blocking/firmware/src/config/sam_e70_xult/initialization.c
+++ apps/mcan/mcan_normal_operation_blocking/firmware/src/config/sam_e70_xult/initialization.c
@@ -103,7 +103,7 @@ void SYS_Initialize ( void* data )
 
     EFC_Initialize();
   
-    CLK_Initialize();
+    CLOCK_Initialize();
 	PIO_Initialize();
 
 	RSWDT_REGS->RSWDT_MR = RSWDT_MR_WDDIS_Msk;	// Disable RSWDT 
