d243031c3c9f119ef644c0539b3f85f2a034719d
David FitzRandolph <david.fitzrandolph@microchip.com>
2019-10-29 12:55:50 -0700

    [CLOCK] Create demo for clock_clock_config_pic32mzw_curiosity MH3-23028

---
+++ apps/clock/clock_config/firmware/src/main_pic32mz_w1_curiosity.c
@@ -0,0 +1,61 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+    
+    /* Start system tick timer */
+    CORETIMER_Start();
+    
+    while ( true )
+    {
+        /* Toggle LED after every 1s */
+        LED_Toggle();
+        CORETIMER_DelayMs(1000);
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+
