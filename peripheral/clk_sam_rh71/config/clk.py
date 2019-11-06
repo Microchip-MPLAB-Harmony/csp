404ecd06534e01ed6e2b7e8f0fa29dac01cbf02c
Joffrey COFFINEAU <joffrey.coffineau@microchip.com>
2019-11-06 19:05:45 +0100

    [SAMRH71] Update clock configuration to add GCLK symbol for IP1553 peripheral
    MH3-24170
    
    Signed-off-by: Joffrey COFFINEAU <joffrey.coffineau@microchip.com>

--- peripheral/clk_sam_rh71/config/clk.py
+++ peripheral/clk_sam_rh71/config/clk.py
@@ -203,6 +203,14 @@ def upd_flexcom_freq(symbol,event):
     if frequency != previousFreq:
         symbol.setValue(frequency)
 
+global upd_1553_freq
+def upd_1553_freq(symbol, event):
+    frequency = 0
+    instance_name = symbol.getID().split("_CLOCK_FREQUENCY")[0]
+    if Database.getSymbolValue("core", instance_name + "_GCLK_ENABLE"):
+        frequency = Database.getSymbolValue("core", instance_name + "_GCLK_FREQUENCY")
+    symbol.setValue(frequency)
+
 global upd_mcan_freq
 def upd_mcan_freq(symbol, event):
     frequency = 0
@@ -358,7 +366,12 @@ def create_hefc_freq_sym(clock_id_name, clock_comp, clk_menu):
 
 def create_ip1553_freq_sym(clock_id_name, clock_comp, clk_menu):
     create_gclk_entries(clock_id_name, clock_comp, clk_menu)
-    create_default_freq_sym(clock_id_name, clock_comp, clk_menu)
+
+    freq_sym = clock_comp.createIntegerSymbol(clock_id_name + "_CLOCK_FREQUENCY", clk_menu)
+    freq_sym.setVisible(True)
+    freq_sym.setReadOnly(True)
+    freq_sym.setDefaultValue(Database.getSymbolValue("core", clock_id_name + "_GCLK_FREQUENCY"))
+    freq_sym.setDependencies(upd_1553_freq, [clock_id_name + "_GCLK_ENABLE", clock_id_name + "_GCLK_FREQUENCY"])
 
 global create_mcan_freq_sym
 def create_mcan_freq_sym(clock_id_name, clock_comp, clk_menu):
