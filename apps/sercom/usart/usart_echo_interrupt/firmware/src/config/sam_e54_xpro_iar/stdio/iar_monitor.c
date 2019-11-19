74d63c4086292d288302b4e4de454a1946e24635
Rajat Batra <rajat.batra@microchip.com>
2019-11-19 09:52:08 +0530

    [APPS] Added IAR configuration for usart_echo_interrupt demo for SAME54 MH3-24377
    Signed-off-by: Rajat batra <rajat.batra@microchip.com>

---
+++ apps/sercom/usart/usart_echo_interrupt/firmware/src/config/sam_e54_xpro_iar/stdio/iar_monitor.c
@@ -0,0 +1,117 @@
+/*******************************************************************************
+ Debug Console Source file 
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    iar_monitor.c
+
+  Summary:
+    iar monitor file with IAR compiler support
+
+  Description:
+    None
+
+*******************************************************************************/
+
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+#include <stddef.h>    // size_t
+
+/*******************
+ *
+ * Copyright 1998-2003 IAR Systems.  All rights reserved.
+ *
+ * $Revision: 38614 $
+ *
+ * This is a template implementation of the "__write" function used by
+ * the standard library.  Replace it with a system-specific
+ * implementation.
+ *
+ * The "__write" function should output "size" number of bytes from
+ * "buffer" in some application-specific way.  It should return the
+ * number of characters written, or _LLIO_ERROR on failure.
+ *
+ * If "buffer" is zero then __write should perform flushing of
+ * internal buffers, if any.  In this case "handle" can be -1 to
+ * indicate that all handles should be flushed.
+ *
+ * The template implementation below assumes that the application
+ * provides the function "MyLowLevelPutchar".  It should return the
+ * character written, or -1 on failure.
+ *
+ ********************/
+/*
+ * Note: "handle == -1" means that all handles should be flushed
+ *
+ * This code only writes to "standard out" and "standard err",
+ * for all other file handles it returns failure, (_LLIO_ERROR), 
+ * -- a low level IO error
+*/
+
+size_t 
+__write( int handle, const unsigned char * buffer, size_t size )
+{
+    (void) handle;      // avoid compiler warning
+    (void) buffer;      // avoid compiler warning
+    (void) size;        // avoid compiler warning
+    size_t nChars = 0;
+
+    return( nChars );
+}
+
+/*******************
+ *
+ * Copyright 1998-2003 IAR Systems.  All rights reserved.
+ *
+ * $Revision: 38614 $
+ *
+ * This is a template implementation of the "__read" function used by
+ * the standard library.  Replace it with a system-specific
+ * implementation.
+ *
+ * The "__read" function reads a number of bytes, at most "size" into
+ * the memory area pointed to by "buffer".  It returns the number of
+ * bytes read, 0 at the end of the file, or _LLIO_ERROR if failure
+ * occurs.
+ *
+ * The template implementation below assumes that the application
+ * provides the function "MyLowLevelGetchar".  It should return a
+ * character value, or -1 on failure.
+ *
+********************/
+/* Note: This template only reads from "standard in"; for all other file 
+ * handles it returns failure. 
+*/
+
+size_t
+__read( int handle, unsigned char * buffer, size_t size )
+{
+    (void) handle;      // avoid compiler warning
+    (void) buffer;      // avoid compiler warning
+    (void) size;        // avoid compiler warning
+    size_t nChars = 0;
+
+    return( nChars );
+}
