e3ea6448fb1c83742deccaec105a39631d7c7190
Prachi Phadke <prachi.phadke@microchip.com>
2019-11-22 11:02:20 +0530

    [AC] Created ac_sleepwalk_singleshot_sam_ha1_xpro application
    MH3-18232

---
+++ apps/ac/ac_sleepwalk_singleshot/firmware/src/main_ha1.c
@@ -0,0 +1,122 @@
+/*******************************************************************************
+* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+#include <string.h>
+#include <stdio.h>
+
+/* Value of RTC_COMPARE_VAL defines the interval at which AC is triggered
+ * by the RTC
+ */
+#define RTC_COMPARE_VAL 2000
+
+uintptr_t comparator_context;
+volatile bool change_detect = false;
+volatile bool ac_comparison_done = false;
+
+void ac_callBack(uint8_t int_flag, uintptr_t ac_context)
+{
+    ac_comparison_done = true;
+    
+    /* Indication that a comparison is done */
+    LED_Toggle();
+    
+    /* Check the comparator output state */
+    if(int_flag & AC_STATUSA_STATE0_Msk)
+    {
+        change_detect  = true;
+    }
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+    RTC_Timer32Start();
+    RTC_Timer32CompareSet(RTC_COMPARE_VAL);
+    AC_CallbackRegister(ac_callBack,comparator_context);
+    printf("\r\n\r\nAC Demo - RTC triggers AC comparison on PA04\r\n");
+
+    while ( true )
+    {
+        PM_StandbyModeEnter();
+        /* Maintain state machines of all polled MPLAB Harmony modules. */
+        SYS_Tasks ( );
+        if(ac_comparison_done)
+        {
+            ac_comparison_done = false;
+            if(change_detect)
+            {
+                change_detect = false;
+                printf("\r\nPA04 voltage is above detect level\r\n");
+            }
+            else
+            {
+                printf("\r\nPA04 voltage is below detect level\r\n");
+            }
+        }
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+
