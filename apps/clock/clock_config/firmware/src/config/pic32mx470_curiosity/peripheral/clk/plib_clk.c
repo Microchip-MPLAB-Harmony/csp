81cfcaa39b3827ea85e517cf137f88f8d376b349
Rajat Batra <rajat.batra@microchip.com>
2019-12-23 20:22:28 +0530

    [CLOCK] Regenerated demo for clock_clock_config_pic32mx470_curiosity MH3-25465
    Signed-off-by: Rajat batra <rajat.batra@microchip.com>

--- apps/clock/clock_config/firmware/src/config/pic32mx470_curiosity/peripheral/clk/plib_clk.c
+++ apps/clock/clock_config/firmware/src/config/pic32mx470_curiosity/peripheral/clk/plib_clk.c
@@ -117,10 +117,10 @@ void CLK_Initialize( void )
         __builtin_mtc0(12, 0,(__builtin_mfc0(12, 0) | 0x0001));
     }
     /* Peripheral Module Disable Configuration */
-    PMD1SET = 0x1101;
-    PMD2SET = 0x3;
-    PMD3SET = 0x1f001f;
-    PMD4SET = 0x1f;
-    PMD5SET = 0x103031f;
-    PMD6SET = 0x10001;
+    PMD1 = 0x1101;
+    PMD2 = 0x3;
+    PMD3 = 0x1f001f;
+    PMD4 = 0x1f;
+    PMD5 = 0x103031f;
+    PMD6 = 0x10001;
 }
