f57548664090a59c5e8e775f5fe5b86013361f4a
Rajat Batra <rajat.batra@microchip.com>
2019-12-23 23:42:55 +0530

    [SERCOM] Regenerated demo for sercom_spi_spi_ping_pong_with_dma_sam_d11_xpro MH3-27853
    Signed-off-by: Rajat batra <rajat.batra@microchip.com>

--- apps/sercom/spi/spi_ping_pong_with_dma/firmware/src/config/sam_d11_xpro/peripheral/port/plib_port.c
+++ apps/sercom/spi/spi_ping_pong_with_dma/firmware/src/config/sam_d11_xpro/peripheral/port/plib_port.c
@@ -73,6 +73,7 @@ void PORT_Initialize(void)
 {
    /************************** GROUP 0 Initialization *************************/
    PORT_REGS->GROUP[0].PORT_DIR = 0x10000;
+   PORT_REGS->GROUP[0].PORT_OUT = 0x10000;
    PORT_REGS->GROUP[0].PORT_PINCFG[6] = 0x1;
    PORT_REGS->GROUP[0].PORT_PINCFG[7] = 0x1;
    PORT_REGS->GROUP[0].PORT_PINCFG[9] = 0x1;
