c88be95a014355d68b7a7205db701b886071c3d4
Rajat Batra <rajat.batra@microchip.com>
2019-12-24 00:38:51 +0530

    [CCL] Regenerated demo for ccl_manchester_encoder_sam_e54_xpro MH3-25441
    Signed-off-by: Rajat batra <rajat.batra@microchip.com>

--- apps/ccl/manchester_encoder/firmware/src/config/sam_e54_xpro/definitions.h
+++ apps/ccl/manchester_encoder/firmware/src/config/sam_e54_xpro/definitions.h
@@ -55,6 +55,7 @@
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
+#include "peripheral/cmcc/plib_cmcc.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
