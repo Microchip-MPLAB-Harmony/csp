f826e350a24829a86639dd64d6180b113611d223
Rajat Batra <rajat.batra@microchip.com>
2019-12-24 03:18:25 +0530

    [SDRAMC] Regenerated demo for sdramc_sdramc_read_write_sam_v71_xult MH3-27498
    Signed-off-by: Rajat batra <rajat.batra@microchip.com>

--- apps/sdramc/sdramc_read_write/firmware/src/config/sam_v71_xult/initialization.c
+++ apps/sdramc/sdramc_read_write/firmware/src/config/sam_v71_xult/initialization.c
@@ -108,6 +108,8 @@ void SYS_Initialize ( void* data )
     SDRAMC_Initialize();
 
 
+
+
 	RSWDT_REGS->RSWDT_MR = RSWDT_MR_WDDIS_Msk;	// Disable RSWDT 
 
 	WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk; 		// Disable WDT 
