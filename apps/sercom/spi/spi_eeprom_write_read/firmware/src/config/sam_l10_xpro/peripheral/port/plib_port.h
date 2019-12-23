dde474850fea919115f456f1a276507f1e5564ee
Rajat Batra <rajat.batra@microchip.com>
2019-12-24 02:09:31 +0530

    [SERCOM] Regenerated demo for sercom_spi_spi_eeprom_write_read_sam_l10_xpro MH3-27845
    Signed-off-by: Rajat batra <rajat.batra@microchip.com>

--- apps/sercom/spi/spi_eeprom_write_read/firmware/src/config/sam_l10_xpro/peripheral/port/plib_port.h
+++ apps/sercom/spi/spi_eeprom_write_read/firmware/src/config/sam_l10_xpro/peripheral/port/plib_port.h
@@ -66,6 +66,7 @@
 // *****************************************************************************
 
 
+
 /*** Macros for EEPROM_CS pin ***/
 #define EEPROM_CS_Set()               (PORT_REGS->GROUP[0].PORT_OUTSET = 1 << 5)
 #define EEPROM_CS_Clear()             (PORT_REGS->GROUP[0].PORT_OUTCLR = 1 << 5)
@@ -924,7 +925,7 @@ void PORT_GroupOutputEnable(PORT_GROUP group, uint32_t mask);
 
 static inline void PORT_PinWrite(PORT_PIN pin, bool value)
 {
-    PORT_GroupWrite(PORT_BASE_ADDRESS + (0x80 * (pin>>5)), (uint32_t)(0x1) << (pin & 0x1f), (uint32_t)(value) << (pin & 0x1f));
+    PORT_GroupWrite((PORT_GROUP)(PORT_BASE_ADDRESS + (0x80 * (pin>>5))), (uint32_t)(0x1) << (pin & 0x1f), (uint32_t)(value) << (pin & 0x1f));
 }
 
 // *****************************************************************************
@@ -948,7 +949,7 @@ static inline void PORT_PinWrite(PORT_PIN pin, bool value)
 
 bool PORT_PinRead(PORT_PIN pin)
 {
-    return (bool)((PORT_GroupRead(PORT_BASE_ADDRESS + (0x80 * (pin>>5))) >> (pin & 0x1F)) & 0x1);
+    return (bool)((PORT_GroupRead((PORT_GROUP)(PORT_BASE_ADDRESS + (0x80 * (pin>>5)))) >> (pin & 0x1F)) & 0x1);
 }
 
 // *****************************************************************************
@@ -970,7 +971,7 @@ bool PORT_PinRead(PORT_PIN pin)
 
 bool PORT_PinLatchRead(PORT_PIN pin)
 {
-    return (bool)((PORT_GroupLatchRead(PORT_BASE_ADDRESS + (0x80 * (pin>>5))) >> (pin & 0x1F)) & 0x1);
+    return (bool)((PORT_GroupLatchRead((PORT_GROUP)(PORT_BASE_ADDRESS + (0x80 * (pin>>5)))) >> (pin & 0x1F)) & 0x1);
 }
 
 // *****************************************************************************
@@ -989,7 +990,7 @@ bool PORT_PinLatchRead(PORT_PIN pin)
 
 void PORT_PinToggle(PORT_PIN pin)
 {
-    PORT_GroupToggle(PORT_BASE_ADDRESS + (0x80 * (pin>>5)), 0x1 << (pin & 0x1F));
+    PORT_GroupToggle((PORT_GROUP)(PORT_BASE_ADDRESS + (0x80 * (pin>>5))), 0x1 << (pin & 0x1F));
 }
 
 // *****************************************************************************
@@ -1008,7 +1009,7 @@ void PORT_PinToggle(PORT_PIN pin)
 
 void PORT_PinSet(PORT_PIN pin)
 {
-    PORT_GroupSet(PORT_BASE_ADDRESS + (0x80 * (pin>>5)), 0x1 << (pin & 0x1F));
+    PORT_GroupSet((PORT_GROUP)(PORT_BASE_ADDRESS + (0x80 * (pin>>5))), 0x1 << (pin & 0x1F));
 }
 
 // *****************************************************************************
@@ -1027,7 +1028,7 @@ void PORT_PinSet(PORT_PIN pin)
 
 void PORT_PinClear(PORT_PIN pin)
 {
-    PORT_GroupClear(PORT_BASE_ADDRESS + (0x80 * (pin>>5)), 0x1 << (pin & 0x1F));
+    PORT_GroupClear((PORT_GROUP)(PORT_BASE_ADDRESS + (0x80 * (pin>>5))), 0x1 << (pin & 0x1F));
 }
 
 // *****************************************************************************
@@ -1047,7 +1048,7 @@ void PORT_PinClear(PORT_PIN pin)
 
 void PORT_PinInputEnable(PORT_PIN pin)
 {
-    PORT_GroupInputEnable(PORT_BASE_ADDRESS + (0x80 * (pin>>5)), 0x1 << (pin & 0x1F));
+    PORT_GroupInputEnable((PORT_GROUP)(PORT_BASE_ADDRESS + (0x80 * (pin>>5))), 0x1 << (pin & 0x1F));
 }
 
 // *****************************************************************************
@@ -1067,7 +1068,7 @@ void PORT_PinInputEnable(PORT_PIN pin)
 
 void PORT_PinOutputEnable(PORT_PIN pin)
 {
-    PORT_GroupOutputEnable(PORT_BASE_ADDRESS + (0x80 * (pin>>5)), 0x1 << (pin & 0x1F));
+    PORT_GroupOutputEnable((PORT_GROUP)(PORT_BASE_ADDRESS + (0x80 * (pin>>5))), 0x1 << (pin & 0x1F));
 }
 
 // DOM-IGNORE-BEGIN
