38ddbde761673b8f3bbf2b2f28025a287ffa7372
Rajat Batra <rajat.batra@microchip.com>
2019-12-24 02:08:31 +0530

    [SERCOM] Regenerated demo for sercom_i2c_master_i2c_eeprom_sam_l10_xpro MH3-27806
    Signed-off-by: Rajat batra <rajat.batra@microchip.com>

--- apps/sercom/i2c/master/i2c_eeprom/firmware/src/config/sam_l10_xpro/definitions.h
+++ apps/sercom/i2c/master/i2c_eeprom/firmware/src/config/sam_l10_xpro/definitions.h
@@ -50,7 +50,7 @@
 #include <stdbool.h>
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/evsys/plib_evsys.h"
-#include "peripheral/sercom/i2cm/plib_sercom0_i2c.h"
+#include "peripheral/sercom/i2c_master/plib_sercom0_i2c_master.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
