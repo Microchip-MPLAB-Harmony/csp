bb6296770c0cfd8e9201328cc6576fa0cd5e5e0c
Rajat Batra <rajat.batra@microchip.com>
2019-12-23 20:36:47 +0530

    [UART] Regenerated demo for uart_uart_echo_interrupt_pic32mz_daa_sk MH3-28462
    Signed-off-by: Rajat batra <rajat.batra@microchip.com>

--- apps/uart/uart_echo_interrupt/firmware/src/config/pic32mz_daa_sk/peripheral/gpio/plib_gpio.c
+++ apps/uart/uart_echo_interrupt/firmware/src/config/pic32mz_daa_sk/peripheral/gpio/plib_gpio.c
@@ -90,7 +90,7 @@ void GPIO_Initialize ( void )
     ANSELKCLR = 0xf9; /* Digital Mode Enable */
 
 
-    /* unlock system for PPS configuration */
+    /* Unlock system for PPS configuration */
     SYSKEY = 0x00000000;
     SYSKEY = 0xAA996655;
     SYSKEY = 0x556699AA;
