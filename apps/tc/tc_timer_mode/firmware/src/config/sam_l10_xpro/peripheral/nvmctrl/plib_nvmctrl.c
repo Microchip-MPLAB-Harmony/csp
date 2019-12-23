c6ee75ea6be4b9535bb9bfb935d9d5326f5b1bcf
Rajat Batra <rajat.batra@microchip.com>
2019-12-24 02:14:22 +0530

    [TC] Regenerated demo for tc_tc_timer_mode_sam_l10_xpro MH3-28093
    Signed-off-by: Rajat batra <rajat.batra@microchip.com>

--- apps/tc/tc_timer_mode/firmware/src/config/sam_l10_xpro/peripheral/nvmctrl/plib_nvmctrl.c
+++ apps/tc/tc_timer_mode/firmware/src/config/sam_l10_xpro/peripheral/nvmctrl/plib_nvmctrl.c
@@ -50,7 +50,6 @@
 #include <string.h>
 #include "plib_nvmctrl.h"
 
-
 // *****************************************************************************
 // *****************************************************************************
 // Section: NVMCTRL Implementation
@@ -116,7 +115,7 @@ bool NVMCTRL_RowErase( uint32_t address )
 
 NVMCTRL_ERROR NVMCTRL_ErrorGet( void )
 {
-    return ((uint32_t) ((NVMCTRL_REGS->NVMCTRL_INTFLAG &  ~(NVMCTRL_INTFLAG_DONE_Msk))));
+    return ((NVMCTRL_ERROR) ((NVMCTRL_REGS->NVMCTRL_INTFLAG &  ~(NVMCTRL_INTFLAG_DONE_Msk))));
 }
 
 bool NVMCTRL_IsBusy(void)
--- apps/tc/tc_timer_mode/firmware/src/packs/ATSAML10E16A_DFP/component/ccl.h
+++ apps/tc/tc_timer_mode/firmware/src/packs/ATSAML10E16A_DFP/component/ccl.h
@@ -20,7 +20,7 @@
  *
  */
 
-/* file generated from device description version 2019-06-07T05:54:14Z */
+/* file generated from device description version 2019-11-24T15:57:35Z */
 #ifndef _SAML10_CCL_COMPONENT_H_
 #define _SAML10_CCL_COMPONENT_H_
 
