78f661709861794f8fee074345bd849e5ad3b209
Rajat Batra <rajat.batra@microchip.com>
2019-12-23 22:37:05 +0530

    [UART] Regenerated demo for uart_uart_echo_blocking_sam_a5d2_xult MH3-28386
    Signed-off-by: Rajat batra <rajat.batra@microchip.com>

--- apps/uart/uart_echo_blocking/firmware/src/config/sam_a5d2_xult/interrupts.c
+++ apps/uart/uart_echo_blocking/firmware/src/config/sam_a5d2_xult/interrupts.c
@@ -68,6 +68,103 @@ void DefaultInterruptHandlerForSpurious( void )
 {
     ++spuriousEventCount;
 }
+/*  Weak definitions for default handlers.  Users may override these with
+    implementations of their own or provide alternate functions to the 
+    interrupt controller.  PLIB implementations provide alternate functions, 
+    with the name {periph}_InterruptHandler so as they will not collide with
+    user {periph}_Handler definitions.
+*/
+void SAIC_Handler( void )                __attribute__((weak, alias("DefaultInterruptHandler")));
+void PMU_Handler( void )                 __attribute__((weak, alias("DefaultInterruptHandler")));
+void PIT_Handler( void )                 __attribute__((weak, alias("DefaultInterruptHandler")));
+void WDT_Handler( void )                 __attribute__((weak, alias("DefaultInterruptHandler")));
+void GMAC_Handler( void )                __attribute__((weak, alias("DefaultInterruptHandler")));
+void XDMAC0_Handler( void )              __attribute__((weak, alias("DefaultInterruptHandler")));
+void XDMAC1_Handler( void )              __attribute__((weak, alias("DefaultInterruptHandler")));
+void ICM_Handler( void )                 __attribute__((weak, alias("DefaultInterruptHandler")));
+void AES_Handler( void )                 __attribute__((weak, alias("DefaultInterruptHandler")));
+void AESB_Handler( void )                __attribute__((weak, alias("DefaultInterruptHandler")));
+void TDES_Handler( void )                __attribute__((weak, alias("DefaultInterruptHandler")));
+void SHA_Handler( void )                 __attribute__((weak, alias("DefaultInterruptHandler")));
+void MPDDRC_Handler( void )              __attribute__((weak, alias("DefaultInterruptHandler")));
+void MATRIX1_Handler( void )             __attribute__((weak, alias("DefaultInterruptHandler")));
+void MATRIX0_Handler( void )             __attribute__((weak, alias("DefaultInterruptHandler")));
+void SECUMOD_Handler( void )             __attribute__((weak, alias("DefaultInterruptHandler")));
+void HSMC_Handler( void )                __attribute__((weak, alias("DefaultInterruptHandler")));
+void PIOA_Handler( void )                __attribute__((weak, alias("DefaultInterruptHandler")));
+void FLEXCOM0_Handler( void )            __attribute__((weak, alias("DefaultInterruptHandler")));
+void FLEXCOM1_Handler( void )            __attribute__((weak, alias("DefaultInterruptHandler")));
+void FLEXCOM2_Handler( void )            __attribute__((weak, alias("DefaultInterruptHandler")));
+void FLEXCOM3_Handler( void )            __attribute__((weak, alias("DefaultInterruptHandler")));
+void FLEXCOM4_Handler( void )            __attribute__((weak, alias("DefaultInterruptHandler")));
+void UART0_Handler( void )               __attribute__((weak, alias("DefaultInterruptHandler")));
+void UART1_Handler( void )               __attribute__((weak, alias("DefaultInterruptHandler")));
+void UART2_Handler( void )               __attribute__((weak, alias("DefaultInterruptHandler")));
+void UART3_Handler( void )               __attribute__((weak, alias("DefaultInterruptHandler")));
+void UART4_Handler( void )               __attribute__((weak, alias("DefaultInterruptHandler")));
+void TWIHS0_Handler( void )              __attribute__((weak, alias("DefaultInterruptHandler")));
+void TWIHS1_Handler( void )              __attribute__((weak, alias("DefaultInterruptHandler")));
+void SDMMC0_Handler( void )              __attribute__((weak, alias("DefaultInterruptHandler")));
+void SDMMC1_Handler( void )              __attribute__((weak, alias("DefaultInterruptHandler")));
+void SPI0_Handler( void )                __attribute__((weak, alias("DefaultInterruptHandler")));
+void SPI1_Handler( void )                __attribute__((weak, alias("DefaultInterruptHandler")));
+void TC0_Handler( void )                 __attribute__((weak, alias("DefaultInterruptHandler")));
+void TC1_Handler( void )                 __attribute__((weak, alias("DefaultInterruptHandler")));
+void PWM_Handler( void )                 __attribute__((weak, alias("DefaultInterruptHandler")));
+void ADC_Handler( void )                 __attribute__((weak, alias("DefaultInterruptHandler")));
+void UHPHS_Handler( void )               __attribute__((weak, alias("DefaultInterruptHandler")));
+void UDPHS_Handler( void )               __attribute__((weak, alias("DefaultInterruptHandler")));
+void SSC0_Handler( void )                __attribute__((weak, alias("DefaultInterruptHandler")));
+void SSC1_Handler( void )                __attribute__((weak, alias("DefaultInterruptHandler")));
+void LCDC_Handler( void )                __attribute__((weak, alias("DefaultInterruptHandler")));
+void ISC_Handler( void )                 __attribute__((weak, alias("DefaultInterruptHandler")));
+void TRNG_Handler( void )                __attribute__((weak, alias("DefaultInterruptHandler")));
+void PDMIC_Handler( void )               __attribute__((weak, alias("DefaultInterruptHandler")));
+void AIC_Handler( void )                 __attribute__((weak, alias("DefaultInterruptHandler")));
+void SFC_Handler( void )                 __attribute__((weak, alias("DefaultInterruptHandler")));
+void SECURAM_Handler( void )             __attribute__((weak, alias("DefaultInterruptHandler")));
+void QSPI0_Handler( void )               __attribute__((weak, alias("DefaultInterruptHandler")));
+void QSPI1_Handler( void )               __attribute__((weak, alias("DefaultInterruptHandler")));
+void I2SC0_Handler( void )               __attribute__((weak, alias("DefaultInterruptHandler")));
+void I2SC1_Handler( void )               __attribute__((weak, alias("DefaultInterruptHandler")));
+void MCAN0_INT0_Handler( void )          __attribute__((weak, alias("DefaultInterruptHandler")));
+void MCAN1_INT0_Handler( void )          __attribute__((weak, alias("DefaultInterruptHandler")));
+void PTC_Handler( void )                 __attribute__((weak, alias("DefaultInterruptHandler")));
+void CLASSD_Handler( void )              __attribute__((weak, alias("DefaultInterruptHandler")));
+void L2CC_Handler( void )                __attribute__((weak, alias("DefaultInterruptHandler")));
+void MCAN0_INT1_Handler( void )          __attribute__((weak, alias("DefaultInterruptHandler")));
+void MCAN1_INT1_Handler( void )          __attribute__((weak, alias("DefaultInterruptHandler")));
+void GMAC_Q1_Handler( void )             __attribute__((weak, alias("DefaultInterruptHandler")));
+void GMAC_Q2_Handler( void )             __attribute__((weak, alias("DefaultInterruptHandler")));
+void PIOB_Handler( void )                __attribute__((weak, alias("DefaultInterruptHandler")));
+void PIOC_Handler( void )                __attribute__((weak, alias("DefaultInterruptHandler")));
+void PIOD_Handler( void )                __attribute__((weak, alias("DefaultInterruptHandler")));
+void SDMMC0_TIMER_Handler( void )        __attribute__((weak, alias("DefaultInterruptHandler")));
+void SDMMC1_TIMER_Handler( void )        __attribute__((weak, alias("DefaultInterruptHandler")));
+void SYSC_Handler( void )                __attribute__((weak, alias("DefaultInterruptHandler")));
+void ACC_Handler( void )                 __attribute__((weak, alias("DefaultInterruptHandler")));
+void RXLP_Handler( void )                __attribute__((weak, alias("DefaultInterruptHandler")));
+
+
+/* Weak definitions for sub-handlers in vectors shared by multiple interrupts */
+void PMC_Handler( void )                 __attribute__((weak, alias("DefaultInterruptHandler")));
+void RSTC_Handler( void )                __attribute__((weak, alias("DefaultInterruptHandler")));
+void RTC_Handler( void )                 __attribute__((weak, alias("DefaultInterruptHandler")));
+
+
+/* Handlers for vectors that are shared by multiple interrupts */
+void SYSC_SharedHandler( void )
+{
+}
+
+/* data for irq register initialization */
+IrqData irqData[] = {
+    { 25,  (uint32_t) AIC_REGS,    UART1_Handler,              AIC_SMR_SRCTYPE_INT_LEVEL_SENSITIVE_Val,  0x0 },
+};
+
+uint32_t irqDataEntryCount = sizeof( irqData ) / sizeof( irqData[ 0 ]);
+
+
 
 /*******************************************************************************
  End of File
--- apps/uart/uart_echo_blocking/firmware/src/config/sam_a5d2_xult/peripheral/aic/plib_aic.c
+++ apps/uart/uart_echo_blocking/firmware/src/config/sam_a5d2_xult/peripheral/aic/plib_aic.c
@@ -49,10 +49,49 @@
 // *****************************************************************************
 extern IrqData  irqData[2];
 extern uint32_t irqDataEntryCount;
+void DefaultInterruptHandlerForSpurious( void );
 
 void
 INT_Initialize( void )
 {   
+    const uint32_t      keyGuard = 0xb6d81c4d;
+    const unsigned      MaxNumPeripherals = 77;
+    const unsigned      MaxInterruptDepth = 8;
+    uint32_t            ii;
+    aic_registers_t *   aicPtr;
+
+    __disable_irq();
+    __DMB();                                                // Data Memory Barrier
+    __ISB();                                                // Instruction Synchronization Barrier
+    ////// secure to nonSecure redirection
+    SFR_REGS->SFR_AICREDIR = (keyGuard ^ SFR_REGS->SFR_SN1) | SFR_AICREDIR_NSAIC_Msk;
+    //////
+    aicPtr = (aic_registers_t *) AIC_REGS;
+    for( ii= 0; ii < MaxNumPeripherals; ++ii )
+    {
+        aicPtr->AIC_SSR = AIC_SSR_INTSEL( ii );
+        aicPtr->AIC_IDCR = AIC_IDCR_Msk;
+        __DSB();
+        __ISB();
+        aicPtr->AIC_ICCR = AIC_ICCR_INTCLR_Msk;
+    }
+    for( ii = 0; ii < MaxInterruptDepth; ++ii )
+    {   // pop all possible nested interrupts from internal hw stack
+        aicPtr->AIC_EOICR = AIC_EOICR_ENDIT_Msk;
+    }
+
+    for( ii = 0; ii < irqDataEntryCount; ++ii )
+    {   // inspect irqData array in interrupts.c to see the configuration data 
+        aicPtr = (aic_registers_t *) irqData[ ii ].targetRegisters;
+        aicPtr->AIC_SSR = AIC_SSR_INTSEL( irqData[ ii ].peripheralId );
+        aicPtr->AIC_SMR = (aicPtr->AIC_SMR & ~AIC_SMR_SRCTYPE_Msk)  | AIC_SMR_SRCTYPE( irqData[ ii ].srcType );
+        aicPtr->AIC_SMR = (aicPtr->AIC_SMR & ~AIC_SMR_PRIORITY_Msk) | (irqData[ ii ].priority << AIC_SMR_PRIORITY_Pos);
+        aicPtr->AIC_SPU = (uint32_t) DefaultInterruptHandlerForSpurious;
+        aicPtr->AIC_SVR = (uint32_t) irqData[ ii ].handler;
+        aicPtr->AIC_IECR = AIC_IECR_Msk;
+    }
+    //////
+    __DSB();                                                // Data Synchronization Barrier
     __enable_irq();
     __ISB();                                                // Allow pended interrupts to be recognized immediately
 }
