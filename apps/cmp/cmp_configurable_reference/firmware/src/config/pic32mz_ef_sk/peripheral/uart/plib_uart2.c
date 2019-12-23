4a721bf4a09489100ac637ccfc69f3ed21fcbad8
Rajat Batra <rajat.batra@microchip.com>
2019-12-23 21:08:36 +0530

    [CMP] Regenerated demo for cmp_cmp_configurable_reference_pic32mz_ef_sk MH3-25561
    Signed-off-by: Rajat batra <rajat.batra@microchip.com>

--- apps/cmp/cmp_configurable_reference/firmware/src/config/pic32mz_ef_sk/peripheral/uart/plib_uart2.c
+++ apps/cmp/cmp_configurable_reference/firmware/src/config/pic32mz_ef_sk/peripheral/uart/plib_uart2.c
@@ -86,13 +86,13 @@ void UART2_Initialize( void )
     /* STSEL  = 0 */
     /* PDSEL = 0 */
 
-    U2MODE = 0x8;
+    U2MODE = 0x0;
 
     /* Enable UART2 Receiver and Transmitter */
     U2STASET = (_U2STA_UTXEN_MASK | _U2STA_URXEN_MASK);
 
     /* BAUD Rate register Setup */
-    U2BRG = 216;
+    U2BRG = 53;
 
     /* Turn ON UART2 */
     U2MODESET = _U2MODE_ON_MASK;
@@ -266,6 +266,18 @@ bool UART2_TransmitterIsReady( void )
     }
 
     return status;
+}
+
+bool UART2_TransmitComplete( void )
+{
+    bool transmitComplete = false;
+
+    if((U2STA & _U2STA_TRMT_MASK))
+    {
+        transmitComplete = true;
+    }
+
+    return transmitComplete;
 }
 
 int UART2_ReadByte( void )
