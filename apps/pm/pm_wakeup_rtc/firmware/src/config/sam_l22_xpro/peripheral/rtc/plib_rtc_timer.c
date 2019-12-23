6ee9e8b21dd6e206f10bca7b2bfdaf69c1861be1
Rajat Batra <rajat.batra@microchip.com>
2019-12-24 02:40:38 +0530

    [PM] Regenerated demo for pm_pm_wakeup_rtc_sam_l22_xpro MH3-27148
    Signed-off-by: Rajat batra <rajat.batra@microchip.com>

--- apps/pm/pm_wakeup_rtc/firmware/src/config/sam_l22_xpro/peripheral/rtc/plib_rtc_timer.c
+++ apps/pm/pm_wakeup_rtc/firmware/src/config/sam_l22_xpro/peripheral/rtc/plib_rtc_timer.c
@@ -148,7 +148,7 @@ uint32_t RTC_BackupRegisterGet( BACKUP_REGISTER reg )
 }
  TAMPER_CHANNEL RTC_TamperSourceGet( void )
 {
-    return((RTC_REGS->MODE0.RTC_TAMPID) & (0xFF));
+    return((TAMPER_CHANNEL) ((RTC_REGS->MODE0.RTC_TAMPID) & (0xFF)));
 }
 
 uint32_t RTC_Timer32TimeStampGet( void )
@@ -164,13 +164,12 @@ void RTC_Timer32CallbackRegister ( RTC_TIMER32_CALLBACK callback, uintptr_t cont
 
 void RTC_InterruptHandler( void )
 {
-    rtcObj.timer32intCause = RTC_REGS->MODE0.RTC_INTFLAG;
-
+    rtcObj.timer32intCause = (RTC_TIMER32_INT_MASK) RTC_REGS->MODE0.RTC_INTFLAG;
+    RTC_REGS->MODE0.RTC_INTFLAG = RTC_MODE0_INTFLAG_Msk;
+        
     /* Invoke registered Callback function */
     if(rtcObj.timer32BitCallback != NULL)
     {
         rtcObj.timer32BitCallback( rtcObj.timer32intCause, rtcObj.context );
     }
-
-    RTC_REGS->MODE0.RTC_INTFLAG = RTC_MODE0_INTFLAG_Msk;
 }
