614e068916d969ac07a827c000c14ce2ae4c7935
Rajat Batra <rajat.batra@microchip.com>
2019-12-23 22:52:09 +0530

    [PWM] Regenerated demo for pwm_pwm_synchronous_channels_sam_a5d2_xult_xc32 MH3-27325
    Signed-off-by: Rajat batra <rajat.batra@microchip.com>

--- apps/pwm/pwm_synchronous_channels/firmware/src/config/sam_a5d2_xult_xc32/peripheral/clk/plib_clk.c
+++ apps/pwm/pwm_synchronous_channels/firmware/src/config/sam_a5d2_xult_xc32/peripheral/clk/plib_clk.c
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -19,7 +19,7 @@
 * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
 * ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
 * THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
- *******************************************************************************/
+*******************************************************************************/
 
 #include "device.h"
 #include "plib_clk.h"
