6f78f201e8197f1f269bb102a08eb2f784ac0d29
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2020-01-06 01:35:59 +0530

    [CAN] Regenerated demo for can_normal_operation_interrupt_timestamp_pic32mz_w1_curiosity
    MH3-25425

--- apps/can/can_normal_operation_interrupt_timestamp/firmware/src/config/pic32mz_w1_curiosity/peripheral/can/plib_can_common.h
+++ apps/can/can_normal_operation_interrupt_timestamp/firmware/src/config/pic32mz_w1_curiosity/peripheral/can/plib_can_common.h
@@ -122,38 +122,6 @@ typedef enum
 } CAN_ERROR;
 
 // *****************************************************************************
-/* CAN State.
-
-   Summary:
-    CAN PLib Task State.
-
-   Description:
-    This data type defines the CAN Task State.
-
-   Remarks:
-    None.
-
-*/
-typedef enum {
-
-    /* CAN PLib Task Error State */
-    CAN_STATE_ERROR = -1,
-
-    /* CAN PLib Task Idle State */
-    CAN_STATE_IDLE,
-
-    /* CAN PLib Task Transfer Transmit State */
-    CAN_STATE_TRANSFER_TRANSMIT,
-
-    /* CAN PLib Task Transfer Receive State */
-    CAN_STATE_TRANSFER_RECEIVE,
-
-    /* CAN PLib Task Transfer Done State */
-    CAN_STATE_TRANSFER_DONE
-
-} CAN_STATE;
-
-// *****************************************************************************
 /* CAN Callback
 
    Summary:
@@ -216,6 +184,27 @@ typedef struct
 } CAN_TX_RX_MSG_BUFFER;
 
 // *****************************************************************************
+/* CAN Callback Object
+
+   Summary:
+    CAN transfer event callback structure.
+
+   Description:
+    This data structure stores transfer event callback and it's context.
+
+   Remarks:
+    None.
+*/
+typedef struct
+{
+    /* Transfer Event Callback */
+    CAN_CALLBACK callback;
+
+    /* Transfer Event Callback Context */
+    uintptr_t context;
+} CAN_CALLBACK_OBJ;
+
+// *****************************************************************************
 /* CAN PLib Instance Object
 
    Summary:
@@ -229,15 +218,6 @@ typedef struct
 */
 typedef struct
 {
-    /* Transfer State */
-    CAN_STATE state;
-
-    /* Transfer Event Callback */
-    CAN_CALLBACK callback;
-
-    /* Transfer Event Callback Context */
-    uintptr_t context;
-
     /* CAN Error Status */
     uint32_t errorStatus;
 
