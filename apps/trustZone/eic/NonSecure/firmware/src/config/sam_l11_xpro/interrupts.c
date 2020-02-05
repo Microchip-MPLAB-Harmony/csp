57b4eece0178aa7e4bba68c9d215d3b73d09e024
Rajat batra <rajat.batra@microchip.com>
2020-02-05 14:44:35 +0530

    [APPS] Added eic_pin_toggle demo MH3-29744
    Signed-off-by: Rajat batra <rajat.batra@microchip.com>

---
+++ apps/trustZone/eic/NonSecure/firmware/src/config/sam_l11_xpro/interrupts.c
@@ -0,0 +1,110 @@
+/*******************************************************************************
+ System Interrupts File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    interrupt.c
+
+  Summary:
+    Interrupt vectors mapping
+
+  Description:
+    This file maps all the interrupt vectors to their corresponding
+    implementations. If a particular module interrupt is used, then its ISR
+    definition can be found in corresponding PLIB source file. If a module
+    interrupt is not used, then its ISR implementation is mapped to dummy
+    handler.
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include "definitions.h"
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Interrupt Vector Functions
+// *****************************************************************************
+// *****************************************************************************
+
+extern uint32_t _stack;
+
+/* Brief default interrupt handler for unused IRQs.*/
+void __attribute__((optimize("-O1"),section(".text.Dummy_Handler"),long_call))Dummy_Handler(void)
+{
+#if defined(__DEBUG) || defined(__DEBUG_D) && defined(__XC32)
+    __builtin_software_breakpoint();
+#endif
+    while (1)
+    {
+    }
+}
+/* Device vectors list dummy definition*/
+void Reset_Handler              ( void ) __attribute__((weak, alias("Dummy_Handler")));
+void NonMaskableInt_Handler     ( void ) __attribute__((weak, alias("Dummy_Handler")));
+void HardFault_Handler          ( void ) __attribute__((weak, alias("Dummy_Handler")));
+void SVCall_Handler             ( void ) __attribute__((weak, alias("Dummy_Handler")));
+void PendSV_Handler             ( void ) __attribute__((weak, alias("Dummy_Handler")));
+void SysTick_Handler            ( void ) __attribute__((weak, alias("Dummy_Handler")));
+void RTC_InterruptHandler       ( void ) __attribute__((weak, alias("Dummy_Handler")));
+void EIC_EXTINT_0_InterruptHandler     ( void ) __attribute__((weak, alias("Dummy_Handler")));
+void EVSYS_0_Handler            ( void ) __attribute__((weak, alias("Dummy_Handler")));
+
+
+
+/* Mutiple handlers for vector */
+
+
+
+__attribute__ ((section(".vectors")))
+const DeviceVectors exception_table=
+{
+    /* Configure Initial Stack Pointer, using linker-generated symbols */
+    .pvStack = (void*) (&_stack),
+
+    .pfnReset_Handler              = ( void * ) Reset_Handler,
+    .pfnNonMaskableInt_Handler     = ( void * ) NonMaskableInt_Handler,
+    .pfnHardFault_Handler          = ( void * ) HardFault_Handler,
+    .pfnSVCall_Handler             = ( void * ) SVCall_Handler,
+    .pfnPendSV_Handler             = ( void * ) PendSV_Handler,
+    .pfnSysTick_Handler            = ( void * ) SysTick_Handler,
+    .pfnRTC_Handler                = ( void * ) RTC_InterruptHandler,
+    .pfnEIC_0_Handler              = ( void * ) EIC_EXTINT_0_InterruptHandler,
+    .pfnEVSYS_0_Handler            = ( void * ) EVSYS_0_Handler,
+
+
+};
+
+/*******************************************************************************
+ End of File
+*/
---
+++ apps/trustZone/eic/NonSecure/firmware/src/main.c
@@ -0,0 +1,62 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+void SWITCH_handler(uintptr_t context)
+{
+	NON_SECURE_PIN_Toggle();
+    
+}
+int main ( void )
+{
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+    EIC_CallbackRegister(EIC_PIN_0, SWITCH_handler,  (uintptr_t) NULL);
+    RTC_Timer32Start();
+    while ( true )
+    {
+        /* Maintain state machines of all polled MPLAB Harmony modules. */
+        SYS_Tasks ( );
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+
