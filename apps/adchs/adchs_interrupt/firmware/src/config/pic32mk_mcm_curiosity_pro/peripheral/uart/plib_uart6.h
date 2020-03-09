3e86b164460639b04fa36a4d4c30bae32c12d3de
Prachi Phadke <prachi.phadke@microchip.com>
2020-03-09 14:48:42 +0530

    [ADCHS] Added interrupt mode application for pic32mk_mcm_curiosity_pro
    MH3-24346

---
+++ apps/adchs/adchs_interrupt/firmware/src/config/pic32mk_mcm_curiosity_pro/peripheral/uart/plib_uart6.h
@@ -0,0 +1,100 @@
+/*******************************************************************************
+  UART6 PLIB
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    plib_uart6.h
+
+  Summary:
+    UART6 PLIB Header File
+
+  Description:
+    None
+
+*******************************************************************************/
+
+/*******************************************************************************
+* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+
+#ifndef PLIB_UART6_H
+#define PLIB_UART6_H
+
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "device.h"
+#include "plib_uart_common.h"
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+    extern "C" {
+
+#endif
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Interface
+// *****************************************************************************
+// *****************************************************************************
+
+#define UART6_FrequencyGet()    (uint32_t)(60000000UL)
+
+/****************************** UART6 API *********************************/
+
+void UART6_Initialize( void );
+
+bool UART6_SerialSetup( UART_SERIAL_SETUP *setup, uint32_t srcClkFreq );
+
+bool UART6_Write( void *buffer, const size_t size );
+
+bool UART6_Read( void *buffer, const size_t size );
+
+UART_ERROR UART6_ErrorGet( void );
+
+bool UART6_AutoBaudQuery( void );
+
+void UART6_AutoBaudSet( bool enable );
+
+int UART6_ReadByte( void );
+
+bool UART6_ReceiverIsReady( void );
+
+void UART6_WriteByte( int data );
+
+bool UART6_TransmitterIsReady( void );
+
+bool UART6_TransmitComplete( void );
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+    }
+
+#endif
+// DOM-IGNORE-END
+
+#endif // PLIB_UART6_H
