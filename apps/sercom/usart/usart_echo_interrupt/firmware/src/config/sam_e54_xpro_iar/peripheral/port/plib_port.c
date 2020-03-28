3911677e7075695d0e18a118d69b040cbf6e4cdc
Syam Nair <syam.nair@microchip.com>
2020-03-29 00:05:06 +0530

    [APPS] Regenerate IAR application usart_echo_interrupt_sam_e54_xpro
    MH3-31345

--- apps/sercom/usart/usart_echo_interrupt/firmware/src/config/sam_e54_xpro_iar/peripheral/port/plib_port.c
+++ apps/sercom/usart/usart_echo_interrupt/firmware/src/config/sam_e54_xpro_iar/peripheral/port/plib_port.c
@@ -73,16 +73,20 @@ void PORT_Initialize(void)
 {
    /************************** GROUP 0 Initialization *************************/
 
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_PINCFG[24] = 0x1;
    PORT_REGS->GROUP[1].PORT_PINCFG[25] = 0x1;
 
    PORT_REGS->GROUP[1].PORT_PMUX[12] = 0x33;
+                
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x40000;
 
+                
    /************************** GROUP 3 Initialization *************************/
 
+                
 }
 
 // *****************************************************************************
