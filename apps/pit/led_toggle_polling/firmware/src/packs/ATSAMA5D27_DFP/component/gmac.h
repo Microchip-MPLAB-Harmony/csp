757c000f0a61a633b452472494b96f31e9cf63aa
Syam Nair <syam.nair@microchip.com>
2020-03-28 23:30:03 +0530

    [APPS] Regenerate IAR application led_toggle_polling_sam_a5d2_xult
    MH3-31345

--- apps/pit/led_toggle_polling/firmware/src/packs/ATSAMA5D27_DFP/component/gmac.h
+++ apps/pit/led_toggle_polling/firmware/src/packs/ATSAMA5D27_DFP/component/gmac.h
@@ -1,7 +1,7 @@
 /**
  * \brief Component description for GMAC
  *
- * Copyright (c) 2019 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2020 Microchip Technology Inc. and its subsidiaries.
  *
  * Subject to your compliance with these terms, you may use Microchip software and any derivatives
  * exclusively with Microchip products. It is your responsibility to comply with third party license
@@ -20,7 +20,7 @@
  *
  */
 
-/* file generated from device description version 2019-12-06T14:18:30Z */
+/* file generated from device description version 2020-03-18T12:53:15Z */
 #ifndef _SAMA5D2_GMAC_COMPONENT_H_
 #define _SAMA5D2_GMAC_COMPONENT_H_
 
