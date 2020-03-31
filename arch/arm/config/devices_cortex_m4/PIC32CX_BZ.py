e54d7f957947b2ee6f4572f476c4381a097aeeea
Arpan Kumar <arpan.kumar@microchip.com>
2020-03-31 23:48:40 +0530

    [PIC32CX Arch] arch files for PIC32CX_BZ support
    MH3-29460

---
+++ arch/arm/config/devices_cortex_m4/PIC32CX_BZ.py
@@ -0,0 +1,413 @@
+# coding: utf-8
+"""*****************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*****************************************************************************"""
+
+global xc32StackInTCMSym
+global xc32DTCMSizeSym
+global xc32ITCMSizeSym
+global tcmSize
+global tcmEnable
+global fuseMenu
+
+# ///////////////////////Symbol Creation for Fuses ////////////////////////// #
+global _find_default_value
+
+def _find_default_value(bitfieldNode, initialRegValue):
+    '''
+    Helper function to lookup default value for a particular bitfield within a given register from atdf node
+    Arguments:
+      bitfieldNode - the particular <bitfield > entry in the atdf file being looked at
+      initialRegValue - initval of <register > entry in the atdf file.  Holds initial values of
+                        all fields for this register.
+    '''
+    # make initialRegValue an integer
+    registerValue = int(initialRegValue[2:],16)   # get rid of the '0x'
+
+    # find bitshift of lsb of field
+    maskStr = bitfieldNode.getAttribute('mask').strip('L')
+    if(maskStr.find('0x') != -1):
+        mask = int(maskStr[2:],16)
+    else:
+        mask = int(maskStr)
+    shift = 0
+    for ii in range(0,32):
+        if( ((mask >> ii) & 1) != 0):
+            shift = ii
+            break
+    return((registerValue & mask) >> shift)
+
+global _find_key
+
+def _find_key(value, keypairs):
+    '''
+    Helper function that finds the keyname for the given value.  Needed for setting up combo symbol value.
+          value - the value to be looked for in the dictionary
+          keypairs - the dictionary to be searched over
+    '''
+    for keyname, val in keypairs.items():
+        if(val == str(value)):
+            return keyname
+    print("_find_key: could not find value in dictionary",val,str(value), keyname) # should never get here
+    return ""
+
+global _process_valuegroup_entry
+
+def _process_valuegroup_entry(node):
+    '''
+    Looks at input node and returns key name, description, and value for it.
+       node:  <value ...> in atdf file.  A particular bitfield value for a particular register.
+    '''
+    stringval = node.getAttribute('value')
+    newstring = stringval.replace('L','')
+    value = int(newstring,16)
+    return str(value)
+
+def getMaxValue(mask):
+    if mask == 0 :
+        return hex(0)
+
+    mask = int(mask, 16)
+    while (mask % 2) == 0:
+        mask = mask >> 1
+
+    return mask
+
+def xc32SetTcmSize(symbol, event):
+    symObj=event["symbol"]
+    if (symObj.getSelectedKey() == "0KB"):
+        xc32DTCMSizeSym.setValue("")
+        xc32ITCMSizeSym.setValue("")
+    elif (symObj.getSelectedKey() == "2KB"):
+        xc32DTCMSizeSym.setValue("0x800")
+        xc32ITCMSizeSym.setValue("0x800")
+    elif (symObj.getSelectedKey() == "3KB"):
+        xc32DTCMSizeSym.setValue("0xC00")
+        xc32ITCMSizeSym.setValue("0xC00")
+    elif (symObj.getSelectedKey() == "4KB"):
+        xc32DTCMSizeSym.setValue("0x1000")
+        xc32ITCMSizeSym.setValue("0x1000")
+
+def xc32SetStackInTcm(symbol, event):
+    if (event["value"] == True):
+        xc32StackInTCMSym.setValue("true")
+    else:
+        xc32StackInTCMSym.setValue("false")
+
+def populate_config_items(basenode, baseLabel, moduleNode, component, parentMenu, visibility):
+    no_symbol_list = ['CFGCON0','CFGCON1','CFGCON2','CFGCON4','CFGPG']  # not needed to populate symbols that user does not control
+    register = basenode.getChildren() # these are <register > fields for fuse or cfg sections
+    for ii in range(len(register)):
+        porValue = register[ii].getAttribute('initval')
+        if((porValue != None) and (register[ii].getAttribute('name') not in no_symbol_list)):
+            symbolName = register[ii].getAttribute('name')
+            menuitem = component.createMenuSymbol(symbolName, parentMenu)
+            menuitem.setVisible(visibility)
+            menuitem.setLabel(symbolName)
+            bitfields = register[ii].getChildren()
+            for jj in range(len(bitfields)):
+                bitfieldName = bitfields[jj].getAttribute('name')
+                if bitfields[jj].getAttribute('values') is None:
+                    bitfielditem = component.createHexSymbol(baseLabel+bitfieldName,menuitem) # symbol ID must match ftl file symbol
+                else: # key value type symbol
+                    submodnode = moduleNode.getChildren()   # <value-group > entries
+                    for kk in range(len(submodnode)): # scan over all <value-group ..> attributes (i.e., all bitfields) for our bitfieldName
+                        # extract field names from <value-group > items.  The part after the '__' is what we need here.
+                        temp = submodnode[kk].getAttribute('name')
+                        posn = temp.find('__')
+                        name = temp[posn+2:]
+
+                        if(name == bitfieldName):  # if we have a matching <value-group >, look at all children <value > fields
+                            valuenode = submodnode[kk].getChildren()  # look at all the <value ..> entries underneath <value-group >
+                            keyVals = {}
+                            for ll in range(len(valuenode)):  # do this for each child <value ..> attribute for this bitfield
+                                keyVals[valuenode[ll].getAttribute("name")] = _process_valuegroup_entry(valuenode[ll])
+                    bitfielditem = component.createComboSymbol(baseLabel+bitfieldName, menuitem, sorted(keyVals.keys()))
+                    bitfielditem.setDefaultValue(_find_key(_find_default_value(bitfields[jj], porValue),keyVals))
+
+                bitfielditem.setVisible(visibility)
+
+                if bitfields[jj].getAttribute('values') is None:
+                    bitfielditem.setDefaultValue(_find_default_value(bitfields[jj], porValue))
+
+                label = bitfields[jj].getAttribute('caption')+' ('+bitfields[jj].getAttribute('name')+')'
+                bitfielditem.setLabel(label)
+                bitfielditem.setDescription(bitfields[jj].getAttribute('caption'))
+
+    # by default ATDF sets POSCMOD value to OFF, change it here
+    if Database.getSymbolValue("core", "CONFIG_POSCMOD") != None:
+        Database.setSymbolValue("core", "CONFIG_POSCMOD", "HS")
+
+global getWaitStates
+def getWaitStates():
+    sysclk = int(Database.getSymbolValue("core", "CPU_CLOCK_FREQUENCY"))
+    if sysclk <= 33000000:
+        ws = 0
+    else:
+        ws = 1
+    return ws
+
+def calcWaitStates(symbol, event):
+    symbol.setValue(getWaitStates(), 2)
+
+# load family specific configurations
+print("Loading System Services for " + Variables.get("__PROCESSOR"))
+
+# loaded from atdf file
+node = ATDF.getNode("/avr-tools-device-file/modules/module@[name=\"FUSES\"]/register-group")
+populate_config_items(node, 'CONFIG_', ATDF.getNode("/avr-tools-device-file/modules/module@[name=\"FUSES\"]"), coreComponent, devCfgMenu, True)
+
+
+# SysTick External Clock Source
+systickExternal = coreComponent.createBooleanSymbol("SYSTICK_EXTERNAL_CLOCK", devCfgMenu)
+systickExternal.setLabel("External Clock Source for SysTick Available")
+systickExternal.setDefaultValue(False)
+systickExternal.setVisible(False)
+
+coreFPU = coreComponent.createBooleanSymbol("FPU_Available", devCfgMenu)
+coreFPU.setLabel("FPU Available")
+coreFPU.setDefaultValue(True)
+coreFPU.setReadOnly(True)
+coreFPU.setVisible(False)
+
+deviceFamily = coreComponent.createStringSymbol("DeviceFamily", devCfgMenu)
+deviceFamily.setLabel("Device Family")
+deviceFamily.setDefaultValue("PIC32CX_BZ")
+deviceFamily.setReadOnly(True)
+deviceFamily.setVisible(False)
+
+cortexMenu = coreComponent.createMenuSymbol("CORTEX_MENU", None)
+cortexMenu.setLabel("Cortex-M4 Configuration")
+cortexMenu.setDescription("Configuration for Cortex M4")
+
+cacheMenu = coreComponent.createMenuSymbol("CACHE_MENU", cortexMenu)
+cacheMenu.setLabel("CMCC Configuration")
+cacheMenu.setDescription("CACHE Configuration")
+
+# TCM exists on PIC3CX_BZ and cannot be disabled. Only its size can be  configured.
+# We need this symbol defined so that the FTL will emit the code associated
+# with TCM configuration.
+tcmEnable = coreComponent.createBooleanSymbol("TCM_ENABLE", cacheMenu)
+tcmEnable.setLabel("Enable TCM")
+tcmEnable.setDefaultValue(True)
+tcmEnable.setReadOnly(True)
+tcmEnable.setVisible(False)
+
+deviceTCMsize = coreComponent.createKeyValueSetSymbol("DEVICE_TCM_SIZE", cacheMenu)
+deviceTCMsize.setLabel("TCM and cache Size")
+deviceTCMsize.setOutputMode("Value")
+deviceTCMsize.setDisplayMode("Description")
+deviceTCMsize.addKey("0KB", "2", "TCM: 0 KB, Cache: 4 KB" )
+deviceTCMsize.addKey("2KB", "1", "TCM: 2 KB, Cache: 2 KB")
+deviceTCMsize.addKey("3KB", "0", "TCM: 3 KB, Cache: 1 KB")
+deviceTCMsize.addKey("4KB", "3", "TCM: 4 KB,  Cache: 0 KB")
+deviceTCMsize.setSelectedKey("0KB")
+
+
+dcacheEnable = coreComponent.createBooleanSymbol("DATA_CACHE_ENABLE", cacheMenu)
+dcacheEnable.setLabel("Enable Data Cache")
+dcacheEnable.setDefaultValue(False)
+
+icacheEnable = coreComponent.createBooleanSymbol("INSTRUCTION_CACHE_ENABLE", cacheMenu)
+icacheEnable.setLabel("Enable Instruction Cache")
+icacheEnable.setDefaultValue(True)
+
+stackTCM = coreComponent.createBooleanSymbol("STACK_IN_TCM", cacheMenu)
+stackTCM.setLabel("Locate Stack in TCM")
+stackTCM.setDefaultValue(False)
+stackTCM.setVisible(False)
+
+cacheAlign = coreComponent.createIntegerSymbol("CACHE_ALIGN", cacheMenu)
+cacheAlign.setLabel("Cache Alignment Length")
+cacheAlign.setVisible(False)
+cacheAlign.setDefaultValue(16)
+
+periInstanceMultiVectorSupport = coreComponent.createBooleanSymbol("PERIPHERAL_MULTI_VECTOR", None)
+periInstanceMultiVectorSupport.setDefaultValue(True)
+periInstanceMultiVectorSupport.setVisible(False)
+
+def getCorePeripherals():
+
+    # Components which are creating critical section
+    corePeripherals = ["DMAC", "RTC", "TC", "SERCOM"]
+
+    return corePeripherals
+
+def setDMACDefaultSettings():
+
+    triggerSettings = {
+                        "Software Trigger"  : ["BLOCK", "INCREMENTED_AM", "INCREMENTED_AM", "WORD"],
+                        "Standard_Transmit" : ["BEAT", "INCREMENTED_AM", "FIXED_AM", "BYTE"],
+                        "Standard_Receive"  : ["BEAT", "FIXED_AM", "INCREMENTED_AM", "BYTE"]
+                    }
+
+    return triggerSettings
+
+
+def setMPUDefaultSettings():
+    mpuRegions = 8
+    mpuSettings = {"FLASH"              : ["MPU_ATTR_NORMAL_WT",           "MPU_RASR_AP_READWRITE_Val",    "",     "",     "0x00000000",   "4MB"   ],
+                    "SRAM"              : ["MPU_ATTR_NORMAL_WB_WA",     "MPU_RASR_AP_READWRITE_Val",    "True",     "",     "0x20000000",   "8MB"],
+                    "PERIPHERALS"       : ["MPU_ATTR_DEVICE",           "MPU_RASR_AP_READWRITE_Val",    "",         "",     "0x40000000",   "256MB" ],
+                    "SYSTEM"            : ["MPU_ATTR_STRONGLY_ORDERED", "MPU_RASR_AP_READWRITE_Val",    "",         "",     "0xE0000000",   "1MB"   ],
+                    "QSPI"              : ["MPU_ATTR_STRONGLY_ORDERED", "MPU_RASR_AP_READWRITE_Val",    "True",     "",     "0x04000000",   "256MB"],}
+    mpuSetUpLogicList = ["FLASH", "SRAM", "PERIPHERALS", "SYSTEM", "QSPI"]
+
+    return mpuRegions, mpuSettings, mpuSetUpLogicList
+
+prefetchMenu = coreComponent.createMenuSymbol("PREFETCH_MENU", None)
+prefetchMenu.setLabel("Prefetch and Flash Configuration")
+prefetchMenu.setDescription("Configure Prefetch and Flash")
+
+SYM_REFEN = coreComponent.createKeyValueSetSymbol("CONFIG_CHECON_PREFEN", prefetchMenu)
+SYM_REFEN.setLabel("Predictive Prefetch Configuration")
+SYM_REFEN.addKey("DISABLE", "0", "Disable predictive prefetch")
+SYM_REFEN.addKey("ENABLE", "1", "Enable predictive prefetch for CPU instructions only")
+SYM_REFEN.setOutputMode("Value")
+SYM_REFEN.setDisplayMode("Description")
+SYM_REFEN.setDefaultValue(1)
+
+# load clock manager information
+execfile(Variables.get("__CORE_DIR") + "/../peripheral/clk_pic32cx_bz/config/clk.py")
+coreComponent.addPlugin("../peripheral/clk_pic32cx_bz/plugin/clockmanager.jar")
+
+SYM_PFMWS = coreComponent.createIntegerSymbol("CONFIG_CHECON_PFMWS", prefetchMenu)
+SYM_PFMWS.setLabel("Program Flash memory Wait states")
+SYM_PFMWS.setDefaultValue(getWaitStates())
+SYM_PFMWS.setMin(0)
+SYM_PFMWS.setMax(1)
+SYM_PFMWS.setReadOnly(False)
+SYM_PFMWS.setDependencies(calcWaitStates, ["CPU_CLOCK_FREQUENCY"])
+
+periphNode = ATDF.getNode("/avr-tools-device-file/devices/device/peripherals/module@[name=\"NVM\"]")
+if periphNode is not None:
+    modules = periphNode.getChildren()
+    components = []
+    for nvmctrl_instance in range (0, len(modules)):
+        components.append(str(modules[nvmctrl_instance].getAttribute("name")).lower())
+    Database.activateComponents(components)
+
+# load pin manager information
+execfile(Variables.get("__CORE_DIR") + "/../peripheral/gpio_02467/config/gpio_pic32c.py")
+coreComponent.addPlugin("../peripheral/gpio_02467/plugin/gpio_02467.jar")
+
+# # load NVIC
+execfile(Variables.get("__CORE_DIR") + "/../peripheral/nvic/config/nvic.py")
+coreComponent.addPlugin("../peripheral/nvic/plugin/nvic.jar")
+
+# #load mpu
+execfile(Variables.get("__CORE_DIR") + "/../peripheral/mpu/config/mpu.py")
+coreComponent.addPlugin("../peripheral/mpu/plugin/mpu.jar")
+
+# #load systick
+execfile(Variables.get("__CORE_DIR") + "/../peripheral/systick/config/systick.py")
+
+# # load dma manager information
+execfile(Variables.get("__CORE_DIR") + "/../peripheral/dmac_u2503/config/dmac.py")
+coreComponent.addPlugin("../peripheral/dmac_u2503/plugin/dmamanager.jar")
+
+# # load wdt
+periphNode = ATDF.getNode("/avr-tools-device-file/modules/module@[name=\"WDT\"]")
+id = periphNode.getAttribute("id").lower()
+execfile(Variables.get("__CORE_DIR") + "/../peripheral/wdt_" + id + "/config/wdt_pic32c.py")
+
+# load dmt
+execfile(Variables.get("__CORE_DIR") + "/../peripheral/dmt_01520/config/dmt.py")
+
+# load PAC
+execfile(Variables.get("__CORE_DIR") + "/../peripheral/pac_u2120/config/pac.py")
+
+#  load CMCC
+execfile(Variables.get("__CORE_DIR") + "/../peripheral/cmcc_u2015/config/cmcc.py")
+
+# Activate Event System
+periphNode = ATDF.getNode("/avr-tools-device-file/devices/device/peripherals/module@[name=\"EVSYS\"]")
+modules = periphNode.getChildren()
+components = []
+for evsys_instance in range (0, len(modules)):
+    components.append(str(modules[evsys_instance].getAttribute("name")).lower())
+Database.activateComponents(components)
+
+global armLibCSourceFile
+global devconSystemInitFile
+global compilerSpecifics
+
+compilerSelected = compilerChoice.getSelectedKey().lower()
+
+armSysStartSourceFile = coreComponent.createFileSymbol("STARTUP_C", None)
+armSysStartSourceFile.setSourcePath("../arch/arm/templates/" + compilerSelected + "/cortex_m/startup/startup_" + compilerSelected + ".c.ftl")
+armSysStartSourceFile.setOutputName("startup_" + compilerSelected + ".c")
+armSysStartSourceFile.setMarkup(True)
+armSysStartSourceFile.setOverwrite(True)
+armSysStartSourceFile.setDestPath("")
+armSysStartSourceFile.setProjectPath("config/" + configName + "/")
+armSysStartSourceFile.setType("SOURCE")
+armSysStartSourceFile.setDependencies(
+    genSysSourceFile, ["CoreSysStartupFile", "CoreSysFiles"])
+
+# generate libc_syscalls.c file
+armLibCSourceFile = coreComponent.createFileSymbol("LIBC_SYSCALLS_C", None)
+armLibCSourceFile.setSourcePath("arm/templates/xc32/libc_syscalls.c.ftl")
+armLibCSourceFile.setOutputName("libc_syscalls.c")
+armLibCSourceFile.setMarkup(True)
+armLibCSourceFile.setOverwrite(True)
+armLibCSourceFile.setDestPath("")
+armLibCSourceFile.setProjectPath("config/" + configName + "/")
+armLibCSourceFile.setType("SOURCE")
+armLibCSourceFile.setDependencies(genSysSourceFile, ["CoreSysCallsFile", "CoreSysFiles"])
+
+# set XC32 ITCM Size
+xc32ITCMSizeSym = coreComponent.createSettingSymbol("XC32_ITCM_SIZE", None)
+xc32ITCMSizeSym.setCategory("C32Global")
+xc32ITCMSizeSym.setKey("mitcm")
+xc32ITCMSizeSym.setValue("")
+xc32ITCMSizeSym.setDependencies(xc32SetTcmSize, ["DEVICE_TCM_SIZE"])
+
+# set XC32 DTCM Size
+xc32DTCMSizeSym = coreComponent.createSettingSymbol("XC32_DTCM_SIZE", None)
+xc32DTCMSizeSym.setCategory("C32Global")
+xc32DTCMSizeSym.setKey("mdtcm")
+xc32DTCMSizeSym.setValue("")
+xc32DTCMSizeSym.setDependencies(xc32SetTcmSize, ["DEVICE_TCM_SIZE"])
+
+# set XC32 Stack in TCM: True or False
+xc32StackInTCMSym = coreComponent.createSettingSymbol("XC32_STACK_IN_TCM", None)
+xc32StackInTCMSym.setCategory("C32Global")
+xc32StackInTCMSym.setKey("mstacktcm")
+xc32StackInTCMSym.setValue("false")
+xc32StackInTCMSym.setDependencies(xc32SetStackInTcm, ["STACK_IN_TCM"])
+
+# fuse configuration generation
+devconSystemInitFile = coreComponent.createFileSymbol("DEVICE_CONFIG_SYSTEM_INIT", None)
+devconSystemInitFile.setType("STRING")
+devconSystemInitFile.setOutputName("core.LIST_SYSTEM_INIT_C_CONFIG_BITS_INITIALIZATION")
+devconSystemInitFile.setSourcePath("arm/templates/common/fuses/PIC32CX_BZ.c.ftl")
+devconSystemInitFile.setMarkup(True)
+
+# wait state and prefetch cache setting
+devconInitFile = coreComponent.createFileSymbol("DEVCON_INIT", None)
+devconInitFile.setType("STRING")
+devconInitFile.setOutputName("core.LIST_SYSTEM_INIT_C_SYS_INITIALIZE_CORE")
+devconInitFile.setSourcePath("arm/templates/common/PIC32CX_BZ_DEVCON.c.ftl")
+devconInitFile.setMarkup(True)
+
+compilerSpecifics = [armSysStartSourceFile]
---
+++ arch/arm/templates/common/fuses/PIC32CX_BZ.c.ftl
@@ -0,0 +1,279 @@
+<#if LIST_SYSTEM_INIT_C_CONFIG_BITS_INITIALIZATION?has_content>
+<@mhc_expand_list list=LIST_SYSTEM_INIT_C_CONFIG_BITS_INITIALIZATION/>
+</#if>
+
+/*** FUSERID ***/
+<#if CONFIG_USER_ID?has_content>
+#pragma config USER_ID =      0x${CONFIG_USER_ID}
+</#if>
+
+/*** DEVCFG0 ***/
+<#if CONFIG_TDOEN?has_content>
+#pragma config TDOEN =      ${CONFIG_TDOEN}
+</#if>
+<#if CONFIG_SWOEN?has_content>
+#pragma config SWOEN =      ${CONFIG_SWOEN}
+</#if>
+<#if CONFIG_TROEN?has_content>
+#pragma config TROEN =      ${CONFIG_TROEN}
+</#if>
+<#if CONFIG_JTAGEN?has_content>
+#pragma config JTAGEN =      ${CONFIG_JTAGEN}
+</#if>
+<#if CONFIG_ADCPOVR?has_content>
+#pragma config ADCPOVR =      ${CONFIG_ADCPOVR}
+</#if>
+<#if CONFIG_GPSOSCE?has_content>
+#pragma config GPSOSCE =      ${CONFIG_GPSOSCE}
+</#if>
+<#if CONFIG_ACCMP1_ALTEN?has_content>
+#pragma config ACCMP1_ALTEN =      ${CONFIG_ACCMP1_ALTEN}
+</#if>
+<#if CONFIG_CPENFILT?has_content>
+#pragma config CPENFILT =      ${CONFIG_CPENFILT}
+</#if>
+<#if CONFIG_RTCIN0_ALTEN?has_content>
+#pragma config RTCIN0_ALTEN =      ${CONFIG_RTCIN0_ALTEN}
+</#if>
+<#if CONFIG_RTCOUT_ALTEN?has_content>
+#pragma config RTCOUT_ALTEN =      ${CONFIG_RTCOUT_ALTEN}
+</#if>
+<#if CONFIG_PMULOCK?has_content>
+#pragma config PMULOCK =      ${CONFIG_PMULOCK}
+</#if>
+<#if CONFIG_PGLOCK?has_content>
+#pragma config PGLOCK =      ${CONFIG_PGLOCK}
+</#if>
+<#if CONFIG_PMDLOCK?has_content>
+#pragma config PMDLOCK =      ${CONFIG_PMDLOCK}
+</#if>
+<#if CONFIG_IOLOCK?has_content>
+#pragma config IOLOCK =      ${CONFIG_IOLOCK}
+</#if>
+<#if CONFIG_CFGLOCK?has_content>
+#pragma config CFGLOCK =      ${CONFIG_CFGLOCK}
+</#if>
+<#if CONFIG_VBCMODE?has_content>
+#pragma config VBCMODE =      ${CONFIG_VBCMODE}
+</#if>
+<#if CONFIG_SMBUSEN0?has_content>
+#pragma config SMBUSEN0 =      ${CONFIG_SMBUSEN0}
+</#if>
+<#if CONFIG_SMBUSEN1?has_content>
+#pragma config SMBUSEN1 =      ${CONFIG_SMBUSEN1}
+</#if>
+<#if CONFIG_SMBUSEN2?has_content>
+#pragma config SMBUSEN2 =      ${CONFIG_SMBUSEN2}
+</#if>
+<#if CONFIG_HPLUGDIS?has_content>
+#pragma config HPLUGDIS =      ${CONFIG_HPLUGDIS}
+</#if>
+<#if CONFIG_SLRTEN0?has_content>
+#pragma config SLRTEN0 =      ${CONFIG_SLRTEN0}
+</#if>
+<#if CONFIG_SLRTEN1?has_content>
+#pragma config SLRTEN1 =      ${CONFIG_SLRTEN1}
+</#if>
+<#if CONFIG_SLRTEN2?has_content>
+#pragma config SLRTEN2 =      ${CONFIG_SLRTEN2}
+</#if>
+<#if CONFIG_PCM?has_content>
+#pragma config PCM =      ${CONFIG_PCM}
+</#if>
+<#if CONFIG_INT0E?has_content>
+#pragma config INT0E =      ${CONFIG_INT0E}
+</#if>
+<#if CONFIG_INT0P?has_content>
+#pragma config INT0P =      ${CONFIG_INT0P}
+</#if>
+<#if CONFIG_ADCFCEN?has_content>
+#pragma config ADCFCEN =      ${CONFIG_ADCFCEN}
+</#if>
+<#if CONFIG_FECCCON?has_content>
+#pragma config FECCCON =         ${CONFIG_FECCCON}
+</#if>
+<#if CONFIG_FRECCDIS?has_content>
+#pragma config FRECCDIS =      ${CONFIG_FRECCDIS}
+</#if>
+<#if CONFIG_EPGMCLK?has_content>
+#pragma config EPGMCLK =      ${CONFIG_EPGMCLK}
+</#if>
+
+
+/*** DEVCFG1 ***/
+<#if CONFIG_DEBUG?has_content>
+#pragma config DEBUG =      ${CONFIG_DEBUG}
+</#if>
+<#if CONFIG_ICESEL?has_content>
+#pragma config ICESEL =      ${CONFIG_ICESEL}
+</#if>
+<#if CONFIG_TRCEN?has_content>
+#pragma config TRCEN =      ${CONFIG_TRCEN}
+</#if>
+<#if CONFIG_ZBTWKSYS?has_content>
+#pragma config ZBTWKSYS =      ${CONFIG_ZBTWKSYS}
+</#if>
+<#if CONFIG_CMP0_OE?has_content>
+#pragma config CMP0_OE =      ${CONFIG_CMP0_OE}
+</#if>
+<#if CONFIG_CMP1_OE?has_content>
+#pragma config CMP1_OE =      ${CONFIG_CMP1_OE}
+</#if>
+<#if CONFIG_CLASSBDIS?has_content>
+#pragma config CLASSBDIS =      ${CONFIG_CLASSBDIS}
+</#if>
+<#if CONFIG_SLRCTRL0?has_content>
+#pragma config SLRCTRL0 =      ${CONFIG_SLRCTRL0}
+</#if>
+<#if CONFIG_SLRCTRL1?has_content>
+#pragma config SLRCTRL1 =      ${CONFIG_SLRCTRL1}
+</#if>
+<#if CONFIG_SLRCTRL2?has_content>
+#pragma config SLRCTRL2 =      ${CONFIG_SLRCTRL2}
+</#if>
+<#if CONFIG_SMCLR?has_content>
+#pragma config SMCLR =      ${CONFIG_SMCLR}
+</#if>
+<#if CONFIG_QSCHE_EN?has_content>
+#pragma config QSCHE_EN =      ${CONFIG_QSCHE_EN}
+</#if>
+<#if CONFIG_QSPI_HSEN?has_content>
+#pragma config QSPI_HSEN =      ${CONFIG_QSPI_HSEN}
+</#if>
+<#if CONFIG_SCOM0_HSEN?has_content>
+#pragma config SCOM0_HSEN =      ${CONFIG_SCOM0_HSEN}
+</#if>
+<#if CONFIG_SCOM1_HSEN?has_content>
+#pragma config SCOM1_HSEN =      ${CONFIG_SCOM1_HSEN}
+</#if>
+<#if CONFIG_SCOM2_HSEN?has_content>
+#pragma config SCOM2_HSEN =      ${CONFIG_SCOM2_HSEN}
+</#if>
+<#if CONFIG_CCL_OE?has_content>
+#pragma config CCL_OE =      ${CONFIG_CCL_OE}
+</#if>
+<#if CONFIG_I2CDSEL0?has_content>
+#pragma config I2CDSEL0 =      ${CONFIG_I2CDSEL0}
+</#if>
+<#if CONFIG_I2CDSEL1?has_content>
+#pragma config I2CDSEL1 =      ${CONFIG_I2CDSEL1}
+</#if>
+<#if CONFIG_I2CDSEL2?has_content>
+#pragma config I2CDSEL2 =      ${CONFIG_I2CDSEL2}
+</#if>
+<#if CONFIG_WDTPSS?has_content>
+#pragma config WDTPSS =      ${CONFIG_WDTPSS}
+</#if>
+<#if CONFIG_QSPIDDRM?has_content>
+#pragma config QSPIDDRM =      ${CONFIG_QSPIDDRM}
+</#if>
+<#if CONFIG_CLKZBREF?has_content>
+#pragma config CLKZBREF =      ${CONFIG_CLKZBREF}
+</#if>
+<#if CONFIG_FMPDAEN?has_content>
+#pragma config FMPDAEN =      ${CONFIG_FMPDAEN}
+</#if>
+
+/*** DEVCFG2 ***/
+<#if CONFIG_ACMP_CYCLE?has_content>
+#pragma config ACMP_CYCLE =      ${CONFIG_ACMP_CYCLE}
+</#if>
+<#if CONFIG_DMTINTV?has_content>
+#pragma config DMTINTV =      ${CONFIG_DMTINTV}
+</#if>
+<#if CONFIG_PMUTEST_VDD_EN?has_content>
+#pragma config PMUTEST_VDD_EN =      ${CONFIG_PMUTEST_VDD_EN}
+</#if>
+<#if CONFIG_POSCMOD?has_content>
+#pragma config POSCMOD =      ${CONFIG_POSCMOD}
+</#if>
+<#if CONFIG_WDTRMCS?has_content>
+#pragma config WDTRMCS =      ${CONFIG_WDTRMCS}
+</#if>
+<#if CONFIG_SOSCSEL?has_content>
+#pragma config SOSCSEL =      ${CONFIG_SOSCSEL}
+</#if>
+<#if CONFIG_WAKE2SPD?has_content>
+#pragma config WAKE2SPD =      ${CONFIG_WAKE2SPD}
+</#if>
+<#if CONFIG_CKSWEN?has_content>
+#pragma config CKSWEN =      ${CONFIG_CKSWEN}
+</#if>
+<#if CONFIG_FSCMEN?has_content>
+#pragma config FSCMEN =      ${CONFIG_FSCMEN}
+</#if>
+<#if CONFIG_WDTPSR?has_content>
+#pragma config WDTPSR =      ${CONFIG_WDTPSR}
+</#if>
+<#if CONFIG_WDTSPGM?has_content>
+#pragma config WDTSPGM =      ${CONFIG_WDTSPGM}
+</#if>
+<#if CONFIG_WINDIS?has_content>
+#pragma config WINDIS =      ${CONFIG_WINDIS}
+</#if>
+<#if CONFIG_WDTEN?has_content>
+#pragma config WDTEN =      ${CONFIG_WDTEN}
+</#if>
+<#if CONFIG_WDTWINSZ?has_content>
+#pragma config WDTWINSZ =    ${CONFIG_WDTWINSZ}
+</#if>
+<#if CONFIG_DMTCNT?has_content>
+#pragma config DMTCNT =      ${CONFIG_DMTCNT}
+</#if>
+<#if CONFIG_DMTEN?has_content>
+#pragma config DMTEN =      ${CONFIG_DMTEN}
+</#if>
+
+
+/*** DEVCFG4 ***/
+<#if CONFIG_SOSCCFG?has_content>
+#pragma config SOSCCFG =    0x${CONFIG_SOSCCFG}
+</#if>
+<#if CONFIG_RTCEVENT_SEL?has_content>
+#pragma config RTCEVENT_SEL =      ${CONFIG_RTCEVENT_SEL}
+</#if>
+<#if CONFIG_RTCEVENT_EN?has_content>
+#pragma config RTCEVENT_EN =      ${CONFIG_RTCEVENT_EN}
+</#if>
+<#if CONFIG_VBKP_1KCSEL?has_content>
+#pragma config VBKP_1KCSEL =      ${CONFIG_VBKP_1KCSEL}
+</#if>
+<#if CONFIG_VBKP_32KCSEL?has_content>
+#pragma config VBKP_32KCSEL =      ${CONFIG_VBKP_32KCSEL}
+</#if>
+<#if CONFIG_VBKP_DIVSEL?has_content>
+#pragma config VBKP_DIVSEL =      ${CONFIG_VBKP_DIVSEL}
+</#if>
+<#if CONFIG_LPCLK_MOD?has_content>
+#pragma config LPCLK_MOD =      ${CONFIG_LPCLK_MOD}
+</#if>
+<#if CONFIG_RTCEVTYPE?has_content>
+#pragma config RTCEVTYPE =      ${CONFIG_RTCEVTYPE}
+</#if>
+<#if CONFIG_CPEN_DLY?has_content>
+#pragma config CPEN_DLY =      ${CONFIG_CPEN_DLY}
+</#if>
+<#if CONFIG_DSZPBOREN?has_content>
+#pragma config DSZPBOREN =      ${CONFIG_DSZPBOREN}
+</#if>
+<#if CONFIG_DSWDTPS?has_content>
+#pragma config DSWDTPS =      ${CONFIG_DSWDTPS}
+</#if>
+<#if CONFIG_DSWDTOSC?has_content>
+#pragma config DSWDTOSC =    ${CONFIG_DSWDTOSC}
+</#if>
+<#if CONFIG_DSWDTEN?has_content>
+#pragma config DSWDTEN =      ${CONFIG_DSWDTEN}
+</#if>
+<#if CONFIG_DSEN?has_content>
+#pragma config DSEN =    ${CONFIG_DSEN}
+</#if>
+<#if CONFIG_UVREGROVR?has_content>
+#pragma config UVREGROVR =      ${CONFIG_UVREGROVR}
+</#if>
+<#if CONFIG_LPOSCEN?has_content>
+#pragma config LPOSCEN =      ${CONFIG_LPOSCEN}
+</#if>
+<#if CONFIG_RTCNTM_CSEL?has_content>
+#pragma config RTCNTM_CSEL =      ${CONFIG_RTCNTM_CSEL}
+</#if>
