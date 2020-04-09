f943ce7b090f4cbba0e2ed410d036220801996e6
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2020-04-09 19:42:18 +0530

    [MCAN] Regenerated demo for mcan_mcan_fd_operation_blocking_sam_rh71_ek MH3-31401
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/mcan/mcan_fd_operation_blocking/firmware/src/config/sam_rh71_ek/peripheral/mcan/plib_mcan0.c
+++ apps/mcan/mcan_fd_operation_blocking/firmware/src/config/sam_rh71_ek/peripheral/mcan/plib_mcan0.c
@@ -64,14 +64,14 @@ static const mcan_sidfe_registers_t mcan0StdFilter[] =
 {
     {
         .MCAN_SIDFE_0 = MCAN_SIDFE_0_SFT(0) |
-                  MCAN_SIDFE_0_SFID1(1129) |
-                  MCAN_SIDFE_0_SFID2(1129) |
+                  MCAN_SIDFE_0_SFID1(0x0) |
+                  MCAN_SIDFE_0_SFID2(0x0) |
                   MCAN_SIDFE_0_SFEC(1)
     },
     {
         .MCAN_SIDFE_0 = MCAN_SIDFE_0_SFT(0) |
-                  MCAN_SIDFE_0_SFID1(1114) |
-                  MCAN_SIDFE_0_SFID2(0) |
+                  MCAN_SIDFE_0_SFID1(0x0) |
+                  MCAN_SIDFE_0_SFID2(0x0) |
                   MCAN_SIDFE_0_SFEC(7)
     },
 };
@@ -79,12 +79,12 @@ static const mcan_sidfe_registers_t mcan0StdFilter[] =
 static const mcan_xidfe_registers_t mcan0ExtFilter[] =
 {
     {
-        .MCAN_XIDFE_0 = MCAN_XIDFE_0_EFID1(268435621) | MCAN_XIDFE_0_EFEC(7),
-        .MCAN_XIDFE_1 = MCAN_XIDFE_1_EFID2(0) | MCAN_XIDFE_1_EFT(0),
+        .MCAN_XIDFE_0 = MCAN_XIDFE_0_EFID1(0x0) | MCAN_XIDFE_0_EFEC(7),
+        .MCAN_XIDFE_1 = MCAN_XIDFE_1_EFID2(0x0) | MCAN_XIDFE_1_EFT(0),
     },
     {
-        .MCAN_XIDFE_0 = MCAN_XIDFE_0_EFID1(268435606) | MCAN_XIDFE_0_EFEC(2),
-        .MCAN_XIDFE_1 = MCAN_XIDFE_1_EFID2(268435606) | MCAN_XIDFE_1_EFT(0),
+        .MCAN_XIDFE_0 = MCAN_XIDFE_0_EFID1(0x0) | MCAN_XIDFE_0_EFEC(2),
+        .MCAN_XIDFE_1 = MCAN_XIDFE_1_EFID2(0x0) | MCAN_XIDFE_1_EFT(0),
     },
 };
 
@@ -282,7 +282,8 @@ bool MCAN0_MessageTransmit(uint32_t id, uint8_t length, uint8_t* data, MCAN_MODE
 
 // *****************************************************************************
 /* Function:
-    bool MCAN0_MessageReceive(uint32_t *id, uint8_t *length, uint8_t *data, uint16_t *timestamp, MCAN_MSG_RX_ATTRIBUTE msgAttr)
+    bool MCAN0_MessageReceive(uint32_t *id, uint8_t *length, uint8_t *data, uint16_t *timestamp,
+                                              MCAN_MSG_RX_ATTRIBUTE msgAttr, MCAN_MSG_RX_FRAME_ATTRIBUTE *msgFrameAttr)
 
    Summary:
     Receives a message from CAN bus.
@@ -291,18 +292,20 @@ bool MCAN0_MessageTransmit(uint32_t id, uint8_t length, uint8_t* data, MCAN_MODE
     MCAN0_Initialize must have been called for the associated MCAN instance.
 
    Parameters:
-    id        - Pointer to 11-bit / 29-bit identifier (ID) to be received.
-    length    - Pointer to data length in number of bytes to be received.
-    data      - pointer to destination data buffer
-    timestamp - Pointer to Rx message timestamp, timestamp value is 0 if timestamp is disabled
-    msgAttr   - Message to be read from Rx FIFO0 or Rx FIFO1 or Rx Buffer
+    id           - Pointer to 11-bit / 29-bit identifier (ID) to be received.
+    length       - Pointer to data length in number of bytes to be received.
+    data         - pointer to destination data buffer
+    timestamp    - Pointer to Rx message timestamp, timestamp value is 0 if timestamp is disabled
+    msgAttr      - Message to be read from Rx FIFO0 or Rx FIFO1 or Rx Buffer
+    msgFrameAttr - Data frame or Remote frame to be received
 
    Returns:
     Request status.
     true  - Request was successful.
     false - Request has failed.
 */
-bool MCAN0_MessageReceive(uint32_t *id, uint8_t *length, uint8_t *data, uint16_t *timestamp, MCAN_MSG_RX_ATTRIBUTE msgAttr)
+bool MCAN0_MessageReceive(uint32_t *id, uint8_t *length, uint8_t *data, uint16_t *timestamp,
+                                          MCAN_MSG_RX_ATTRIBUTE msgAttr, MCAN_MSG_RX_FRAME_ATTRIBUTE *msgFrameAttr)
 {
     uint8_t msgLength = 0;
     uint8_t rxgi = 0;
@@ -351,6 +354,16 @@ bool MCAN0_MessageReceive(uint32_t *id, uint8_t *length, uint8_t *data, uint16_t
                 *id = (rxbeFifo->MCAN_RXBE_0 >> 18) & MCAN_STD_ID_Msk;
             }
 
+            /* Check RTR and FDF bits for Remote/Data Frame */
+            if ((rxbeFifo->MCAN_RXBE_0 & MCAN_RXBE_0_RTR_Msk) && ((rxbeFifo->MCAN_RXBE_1 & MCAN_RXBE_1_FDF_Msk) == 0))
+            {
+                *msgFrameAttr = MCAN_MSG_RX_REMOTE_FRAME;
+            }
+            else
+            {
+                *msgFrameAttr = MCAN_MSG_RX_DATA_FRAME;
+            }
+
             /* Get received data length */
             msgLength = MCANDlcToLengthGet(((rxbeFifo->MCAN_RXBE_1 & MCAN_RXBE_1_DLC_Msk) >> MCAN_RXBE_1_DLC_Pos));
 
@@ -389,6 +402,16 @@ bool MCAN0_MessageReceive(uint32_t *id, uint8_t *length, uint8_t *data, uint16_t
                 *id = (rxf0eFifo->MCAN_RXF0E_0 >> 18) & MCAN_STD_ID_Msk;
             }
 
+            /* Check RTR and FDF bits for Remote/Data Frame */
+            if ((rxf0eFifo->MCAN_RXF0E_0 & MCAN_RXF0E_0_RTR_Msk) && ((rxf0eFifo->MCAN_RXF0E_1 & MCAN_RXF0E_1_FDF_Msk) == 0))
+            {
+                *msgFrameAttr = MCAN_MSG_RX_REMOTE_FRAME;
+            }
+            else
+            {
+                *msgFrameAttr = MCAN_MSG_RX_DATA_FRAME;
+            }
+
             /* Get received data length */
             msgLength = MCANDlcToLengthGet(((rxf0eFifo->MCAN_RXF0E_1 & MCAN_RXF0E_1_DLC_Msk) >> MCAN_RXF0E_1_DLC_Pos));
 
@@ -418,6 +441,16 @@ bool MCAN0_MessageReceive(uint32_t *id, uint8_t *length, uint8_t *data, uint16_t
             else
             {
                 *id = (rxf1eFifo->MCAN_RXF1E_0 >> 18) & MCAN_STD_ID_Msk;
+            }
+
+            /* Check RTR and FDF bits for Remote/Data Frame */
+            if ((rxf1eFifo->MCAN_RXF1E_0 & MCAN_RXF1E_0_RTR_Msk) && ((rxf1eFifo->MCAN_RXF1E_1 & MCAN_RXF1E_1_FDF_Msk) == 0))
+            {
+                *msgFrameAttr = MCAN_MSG_RX_REMOTE_FRAME;
+            }
+            else
+            {
+                *msgFrameAttr = MCAN_MSG_RX_DATA_FRAME;
             }
 
             /* Get received data length */
@@ -595,6 +628,28 @@ bool MCAN0_InterruptGet(MCAN_INTERRUPT_MASK interruptMask)
 void MCAN0_InterruptClear(MCAN_INTERRUPT_MASK interruptMask)
 {
     MCAN0_REGS->MCAN_IR = interruptMask;
+}
+
+// *****************************************************************************
+/* Function:
+    bool MCAN0_TxFIFOIsFull(void)
+
+   Summary:
+    Returns true if Tx FIFO is full otherwise false.
+
+   Precondition:
+    MCAN0_Initialize must have been called for the associated MCAN instance.
+
+   Parameters:
+    None
+
+   Returns:
+    true  - Tx FIFO is full.
+    false - Tx FIFO is not full.
+*/
+bool MCAN0_TxFIFOIsFull(void)
+{
+    return (MCAN0_REGS->MCAN_TXFQS & MCAN_TXFQS_TFQF_Msk);
 }
 
 // *****************************************************************************
