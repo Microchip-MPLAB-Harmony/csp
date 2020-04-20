decff800aba8abd935a4f7d3c1ff73d9b22b68fd
Arpan Kumar <arpan.kumar@microchip.com>
2020-04-21 02:17:50 +0530

    [SERCOM] Regenerated demo for sercom_spi_spi_eeprom_write_read_sam_ha1_xpro
    MH3-33409
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/sercom/spi/spi_eeprom_write_read/firmware/src/config/sam_ha1_xpro/peripheral/port/plib_port.c
+++ apps/sercom/spi/spi_eeprom_write_read/firmware/src/config/sam_ha1_xpro/peripheral/port/plib_port.c
@@ -80,10 +80,12 @@ void PORT_Initialize(void)
 
    PORT_REGS->GROUP[0].PORT_PMUX[2] = 0x33;
    PORT_REGS->GROUP[0].PORT_PMUX[5] = 0x20;
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0x40;
 
    PORT_REGS->GROUP[1].PORT_PMUX[3] = 0x20;
+                
 }
 
 // *****************************************************************************
