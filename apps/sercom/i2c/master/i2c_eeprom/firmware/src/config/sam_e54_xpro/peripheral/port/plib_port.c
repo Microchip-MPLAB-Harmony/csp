f34f4b42bbb7e0da8bbb02ceef5b524c8ad254aa
Arpan Kumar <arpan.kumar@microchip.com>
2020-04-20 20:22:29 +0530

    [SERCOM] Regenerated demo for sercom_i2c_master_i2c_eeprom_sam_e54_xpro
    MH3-33371
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/sercom/i2c/master/i2c_eeprom/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.c
+++ apps/sercom/i2c/master/i2c_eeprom/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.c
@@ -73,17 +73,21 @@ void PORT_Initialize(void)
 {
    /************************** GROUP 0 Initialization *************************/
 
+                
    /************************** GROUP 1 Initialization *************************/
 
+                
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x40000;
    PORT_REGS->GROUP[2].PORT_OUT = 0x40000;
 
+                
    /************************** GROUP 3 Initialization *************************/
    PORT_REGS->GROUP[3].PORT_PINCFG[8] = 0x1;
    PORT_REGS->GROUP[3].PORT_PINCFG[9] = 0x1;
 
    PORT_REGS->GROUP[3].PORT_PMUX[4] = 0x22;
+                
 }
 
 // *****************************************************************************
