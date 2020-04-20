00e49a29206524d87a11d0deca6b67ebdbea2dfd
Arpan Kumar <arpan.kumar@microchip.com>
2020-04-20 20:17:59 +0530

    [QSPI] Regenerated demo for qspi_spi_read_write_sam_e54_xpro
    MH3-35871
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/qspi/spi_read_write/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.c
+++ apps/qspi/spi_read_write/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.c
@@ -78,18 +78,22 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_PINCFG[9] = 0x1;
 
    PORT_REGS->GROUP[0].PORT_PMUX[4] = 0x77;
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0x800;
    PORT_REGS->GROUP[1].PORT_OUT = 0x800;
    PORT_REGS->GROUP[1].PORT_PINCFG[10] = 0x1;
 
    PORT_REGS->GROUP[1].PORT_PMUX[5] = 0x7;
+                
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x40000;
    PORT_REGS->GROUP[2].PORT_OUT = 0x40000;
 
+                
    /************************** GROUP 3 Initialization *************************/
 
+                
 }
 
 // *****************************************************************************
--- apps/qspi/spi_read_write/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.h
+++ apps/qspi/spi_read_write/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.h
@@ -65,8 +65,7 @@
 // *****************************************************************************
 // *****************************************************************************
 
-
-
+  
 /*** Macros for WRITE_PROTECT pin ***/
 #define WRITE_PROTECT_Set()               (PORT_REGS->GROUP[0].PORT_OUTSET = 1 << 10)
 #define WRITE_PROTECT_Clear()             (PORT_REGS->GROUP[0].PORT_OUTCLR = 1 << 10)
