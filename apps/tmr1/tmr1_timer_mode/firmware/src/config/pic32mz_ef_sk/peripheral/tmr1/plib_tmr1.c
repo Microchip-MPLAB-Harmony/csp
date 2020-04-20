1233916896fc0508dfdbcde2fb25e525136d7a4b
Arpan Kumar <arpan.kumar@microchip.com>
2020-04-20 23:04:47 +0530

    [TMR1] Regenerated demo for tmr1_tmr1_timer_mode_pic32mz_ef_sk
    MH3-33953
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/tmr1/tmr1_timer_mode/firmware/src/config/pic32mz_ef_sk/peripheral/tmr1/plib_tmr1.c
+++ apps/tmr1/tmr1_timer_mode/firmware/src/config/pic32mz_ef_sk/peripheral/tmr1/plib_tmr1.c
@@ -72,7 +72,7 @@ void TMR1_Initialize(void)
     TMR1 = 0x0;
 
     /*Set period */
-    PR1 = 13020;
+    PR1 = 13019;
 
     /* Setup TMR1 Interrupt */
     TMR1_InterruptEnable();  /* Enable interrupt on the way out */
