c8af8c71f911184448b73ea670f3c20677088f10
Prachi Phadke <prachi.phadke@microchip.com>
2020-04-20 20:39:22 +0530

    [TMR1] Regenerated demo for tmr1_tmr1_timer_mode_pic32mk_mcj_curiosity_pro
    MH3-33939
    
    Signed-off-by: Prachi Phadke <prachi.phadke@microchip.com>

--- apps/tmr1/tmr1_timer_mode/firmware/src/config/pic32mk_mcj_curiosity_pro/peripheral/tmr1/plib_tmr1.c
+++ apps/tmr1/tmr1_timer_mode/firmware/src/config/pic32mk_mcj_curiosity_pro/peripheral/tmr1/plib_tmr1.c
@@ -73,7 +73,7 @@ void TMR1_Initialize(void)
     TMR1 = 0x0;
 
     /*Set period */
-    PR1 = 23437;
+    PR1 = 23436;
 
     /* Setup TMR1 Interrupt */
     TMR1_InterruptEnable();  /* Enable interrupt on the way out */
