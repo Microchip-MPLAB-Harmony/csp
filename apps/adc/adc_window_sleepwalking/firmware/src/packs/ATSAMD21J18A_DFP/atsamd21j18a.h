d988764ea76898f3d468c7874f99d83f4e3d24ef
Arpan Kumar <arpan.kumar@microchip.com>
2020-04-20 19:49:15 +0530

    [ADC] Regenerated demo for adc_adc_window_sleepwalking_sam_d21_xpro
    MH3-32037
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/adc/adc_window_sleepwalking/firmware/src/packs/ATSAMD21J18A_DFP/atsamd21j18a.h
+++ apps/adc/adc_window_sleepwalking/firmware/src/packs/ATSAMD21J18A_DFP/atsamd21j18a.h
@@ -1,7 +1,7 @@
 /**
  * \brief Header file for ATSAMD21J18A
  *
- * Copyright (c) 2019 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2020 Microchip Technology Inc. and its subsidiaries.
  *
  * Subject to your compliance with these terms, you may use Microchip software and any derivatives
  * exclusively with Microchip products. It is your responsibility to comply with third party license
@@ -481,7 +481,7 @@ void I2S_Handler                   ( void );
 /* ************************************************************************** */
 /**  DEVICE SIGNATURES FOR SAMD21J18A                                         */
 /* ************************************************************************** */
-#define DSU_DID                        _UL_(0X10010300)
+#define CHIP_DSU_DID                   _UL_(0X10010300)
 
 /* ************************************************************************** */
 /**  ELECTRICAL DEFINITIONS FOR SAMD21J18A                                    */
