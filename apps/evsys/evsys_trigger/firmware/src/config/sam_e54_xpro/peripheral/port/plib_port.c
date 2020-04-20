eaf8e9c7a7b481481edbf7372f5bd704afac03b6
Arpan Kumar <arpan.kumar@microchip.com>
2020-04-20 20:20:12 +0530

    [EVSYS] Regenerated demo for evsys_evsys_trigger_sam_e54_xpro
    MH3-32563
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/evsys/evsys_trigger/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.c
+++ apps/evsys/evsys_trigger/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.c
@@ -73,17 +73,21 @@ void PORT_Initialize(void)
 {
    /************************** GROUP 0 Initialization *************************/
 
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_OUT = 0x80000000;
    PORT_REGS->GROUP[1].PORT_PINCFG[31] = 0x7;
 
+                
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x40000;
    PORT_REGS->GROUP[2].PORT_OUT = 0x40000;
 
    PORT_REGS->GROUP[2].PORT_EVCTRL = 0xf2;
+                
    /************************** GROUP 3 Initialization *************************/
 
+                
 }
 
 // *****************************************************************************
