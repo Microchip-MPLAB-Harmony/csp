ca78411c9110ea98f0259cb62be3c077650f745b
Arpan Kumar <arpan.kumar@microchip.com>
2020-04-20 20:44:47 +0530

    [AC] Regenerated demo for ac_ac_sleepwalk_singleshot_sam_e54_xpro
    MH3-31951
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/ac/ac_sleepwalk_singleshot/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.c
+++ apps/ac/ac_sleepwalk_singleshot/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.c
@@ -75,16 +75,20 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_PINCFG[4] = 0x1;
 
    PORT_REGS->GROUP[0].PORT_PMUX[2] = 0x1;
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_PINCFG[24] = 0x1;
    PORT_REGS->GROUP[1].PORT_PINCFG[25] = 0x1;
 
    PORT_REGS->GROUP[1].PORT_PMUX[12] = 0x33;
+                
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x40000;
 
+                
    /************************** GROUP 3 Initialization *************************/
 
+                
 }
 
 // *****************************************************************************
