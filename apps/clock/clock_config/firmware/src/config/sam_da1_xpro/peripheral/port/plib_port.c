2ba65a534e5ba04337bcfa53308a7fc0e0974c01
Arpan Kumar <arpan.kumar@microchip.com>
2020-04-20 20:16:47 +0530

    [CLOCK] Regenerated demo for clock_clock_config_sam_da1_xpro
    MH3-32254
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/clock/clock_config/firmware/src/config/sam_da1_xpro/peripheral/port/plib_port.c
+++ apps/clock/clock_config/firmware/src/config/sam_da1_xpro/peripheral/port/plib_port.c
@@ -73,11 +73,13 @@ void PORT_Initialize(void)
 {
    /************************** GROUP 0 Initialization *************************/
 
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0x40000000;
    PORT_REGS->GROUP[1].PORT_PINCFG[15] = 0x1;
 
    PORT_REGS->GROUP[1].PORT_PMUX[7] = 0x70;
+                
 }
 
 // *****************************************************************************
