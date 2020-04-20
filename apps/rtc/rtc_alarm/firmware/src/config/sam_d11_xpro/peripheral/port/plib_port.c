2a3dba780478cbb306698e5bf99b2e449920974f
Arpan Kumar <arpan.kumar@microchip.com>
2020-04-20 19:32:08 +0530

    [RTC] Regenerated demo for rtc_rtc_alarm_sam_d11_xpro
    MH3-33190
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/rtc/rtc_alarm/firmware/src/config/sam_d11_xpro/peripheral/port/plib_port.c
+++ apps/rtc/rtc_alarm/firmware/src/config/sam_d11_xpro/peripheral/port/plib_port.c
@@ -78,6 +78,7 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_PINCFG[11] = 0x1;
 
    PORT_REGS->GROUP[0].PORT_PMUX[5] = 0x33;
+                
 }
 
 // *****************************************************************************
