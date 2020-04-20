0077a967a008a107297c77b78b759702ad50420e
Arpan Kumar <arpan.kumar@microchip.com>
2020-04-20 19:50:11 +0530

    [SERCOM] Regenerated demo for sercom_spi_spi_self_loopback_interrupt_sam_d21_xpro
    MH3-33457
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/sercom/spi/spi_self_loopback_interrupt/firmware/src/config/sam_d21_xpro/peripheral/port/plib_port.c
+++ apps/sercom/spi/spi_self_loopback_interrupt/firmware/src/config/sam_d21_xpro/peripheral/port/plib_port.c
@@ -78,10 +78,12 @@ void PORT_Initialize(void)
 
    PORT_REGS->GROUP[0].PORT_PMUX[8] = 0x2;
    PORT_REGS->GROUP[0].PORT_PMUX[9] = 0x22;
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0x40000000;
    PORT_REGS->GROUP[1].PORT_OUT = 0x40000000;
 
+                
 }
 
 // *****************************************************************************
