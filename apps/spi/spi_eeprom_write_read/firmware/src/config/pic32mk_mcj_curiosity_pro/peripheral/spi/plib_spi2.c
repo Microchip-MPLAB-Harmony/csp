fd3e3db271aa849bdf6c701a0107a87e8fc5a518
Prachi Phadke <prachi.phadke@microchip.com>
2020-04-20 20:47:20 +0530

    [SPI] Regenerated demo for spi_spi_eeprom_write_read_pic32mk_mcj_curiosity_pro
    MH3-33519
    
    Signed-off-by: Prachi Phadke <prachi.phadke@microchip.com>

--- apps/spi/spi_eeprom_write_read/firmware/src/config/pic32mk_mcj_curiosity_pro/peripheral/spi/plib_spi2.c
+++ apps/spi/spi_eeprom_write_read/firmware/src/config/pic32mk_mcj_curiosity_pro/peripheral/spi/plib_spi2.c
@@ -57,6 +57,7 @@ SPI_OBJECT spi2Obj;
 #define SPI2_CON_ENHBUF                     (1 << _SPI2CON_ENHBUF_POSITION)
 #define SPI2_CON_MCLKSEL                    (0 << _SPI2CON_MCLKSEL_POSITION)
 #define SPI2_CON_MSSEN                      (0 << _SPI2CON_MSSEN_POSITION)
+#define SPI2_CON_SMP                        (0 << _SPI2CON_SMP_POSITION)
 
 void SPI2_Initialize ( void )
 {
@@ -94,7 +95,7 @@ void SPI2_Initialize ( void )
     MSSEN = 0
     MCLKSEL = 0
     */
-    SPI2CONSET = (SPI2_CON_MSSEN | SPI2_CON_MCLKSEL | SPI2_CON_ENHBUF | SPI2_CON_MODE_32_MODE_16 | SPI2_CON_CKE | SPI2_CON_CKP | SPI2_CON_MSTEN);
+    SPI2CONSET = (SPI2_CON_MSSEN | SPI2_CON_MCLKSEL | SPI2_CON_ENHBUF | SPI2_CON_MODE_32_MODE_16 | SPI2_CON_CKE | SPI2_CON_CKP | SPI2_CON_MSTEN | SPI2_CON_SMP);
 
     /* Enable transmit interrupt when transmit buffer is completely empty (STXISEL = '01') */
     /* Enable receive interrupt when the receive buffer is not empty (SRXISEL = '01') */
