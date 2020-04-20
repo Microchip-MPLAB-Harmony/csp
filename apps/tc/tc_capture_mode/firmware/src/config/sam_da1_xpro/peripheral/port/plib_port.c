2a01141b5a64c8e8371b2b36150e0594f7866e6f
Arpan Kumar <arpan.kumar@microchip.com>
2020-04-20 20:14:44 +0530

    [TC] Regenerated demo for tc_tc_capture_mode_sam_da1_xpro
    MH3-33726
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/tc/tc_capture_mode/firmware/src/config/sam_da1_xpro/peripheral/port/plib_port.c
+++ apps/tc/tc_capture_mode/firmware/src/config/sam_da1_xpro/peripheral/port/plib_port.c
@@ -79,8 +79,10 @@ void PORT_Initialize(void)
 
    PORT_REGS->GROUP[0].PORT_PMUX[9] = 0x40;
    PORT_REGS->GROUP[0].PORT_PMUX[11] = 0x22;
+                
    /************************** GROUP 1 Initialization *************************/
 
+                
 }
 
 // *****************************************************************************
