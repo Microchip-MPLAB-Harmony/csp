401b67a559a6bfe2bfd7fcfdbc2a61e8662aadde
Arpan Kumar <arpan.kumar@microchip.com>
2020-04-20 19:21:53 +0530

    [TMR1] Regenerated demo for tmr1_tmr1_timer_mode_pic32mz_w1_curiosity
    MH3-33955
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/tmr1/tmr1_timer_mode/firmware/src/config/pic32mz_w1_curiosity/peripheral/tmr1/plib_tmr1.c
+++ apps/tmr1/tmr1_timer_mode/firmware/src/config/pic32mz_w1_curiosity/peripheral/tmr1/plib_tmr1.c
@@ -73,7 +73,7 @@ void TMR1_Initialize(void)
     TMR1 = 0x0;
 
     /*Set period */
-    PR1 = 7812;
+    PR1 = 7811;
 
     /* Setup TMR1 Interrupt */
     TMR1_InterruptEnable();  /* Enable interrupt on the way out */
