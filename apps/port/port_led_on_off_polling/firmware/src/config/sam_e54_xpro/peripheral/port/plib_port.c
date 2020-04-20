f4a345e04a7e70d8333d4697667fade3b950de70
Arpan Kumar <arpan.kumar@microchip.com>
2020-04-20 20:40:36 +0530

    [PORT] Regenerated demo for port_port_led_on_off_polling_sam_e54_xpro
    MH3-33037
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/port/port_led_on_off_polling/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.c
+++ apps/port/port_led_on_off_polling/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.c
@@ -73,16 +73,20 @@ void PORT_Initialize(void)
 {
    /************************** GROUP 0 Initialization *************************/
 
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_OUT = 0x80000000;
    PORT_REGS->GROUP[1].PORT_PINCFG[31] = 0x6;
 
+                
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x40000;
    PORT_REGS->GROUP[2].PORT_OUT = 0x40000;
 
+                
    /************************** GROUP 3 Initialization *************************/
 
+                
 }
 
 // *****************************************************************************
