be2c6c9840346319910d1ea083facda3614062b7
Arpan Kumar <arpan.kumar@microchip.com>
2020-04-20 19:35:25 +0530

    [ADC] Regenerated demo for adc_adc_window_sleepwalking_sam_d11_xpro
    MH3-32033
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/adc/adc_window_sleepwalking/firmware/src/config/sam_d11_xpro/peripheral/port/plib_port.c
+++ apps/adc/adc_window_sleepwalking/firmware/src/config/sam_d11_xpro/peripheral/port/plib_port.c
@@ -78,6 +78,7 @@ void PORT_Initialize(void)
 
    PORT_REGS->GROUP[0].PORT_PMUX[2] = 0x1;
    PORT_REGS->GROUP[0].PORT_PMUX[5] = 0x33;
+                
 }
 
 // *****************************************************************************
