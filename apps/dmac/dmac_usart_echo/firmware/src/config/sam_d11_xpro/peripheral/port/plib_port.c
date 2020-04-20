f67250a1406f7dc9fb86918fb6ff1c147c8b29d9
Arpan Kumar <arpan.kumar@microchip.com>
2020-04-20 19:32:56 +0530

    [DMAC] Regenerated demo for dmac_dmac_usart_echo_sam_d11_xpro
    MH3-32485
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/dmac/dmac_usart_echo/firmware/src/config/sam_d11_xpro/peripheral/port/plib_port.c
+++ apps/dmac/dmac_usart_echo/firmware/src/config/sam_d11_xpro/peripheral/port/plib_port.c
@@ -77,6 +77,7 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_PINCFG[11] = 0x1;
 
    PORT_REGS->GROUP[0].PORT_PMUX[5] = 0x33;
+                
 }
 
 // *****************************************************************************
