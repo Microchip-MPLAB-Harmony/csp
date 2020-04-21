378398298913dfa623fee09dd5869243a3398a78
Prachi Phadke <prachi.phadke@microchip.com>
2020-04-21 11:51:54 +0530

    [SERCOM] Regenerated demo for sercom_i2c_master_i2c_eeprom_sam_l21_xpro
    MH3-33377
    
    Signed-off-by: Prachi Phadke <prachi.phadke@microchip.com>

--- apps/sercom/i2c/master/i2c_eeprom/firmware/src/config/sam_l21_xpro/peripheral/port/plib_port.c
+++ apps/sercom/i2c/master/i2c_eeprom/firmware/src/config/sam_l21_xpro/peripheral/port/plib_port.c
@@ -76,10 +76,12 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_PINCFG[9] = 0x1;
 
    PORT_REGS->GROUP[0].PORT_PMUX[4] = 0x33;
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0x400;
    PORT_REGS->GROUP[1].PORT_OUT = 0x400;
 
+                
 }
 
 // *****************************************************************************
