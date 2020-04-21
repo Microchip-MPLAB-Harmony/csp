410a72706b6c4608b2afbf1cf9c89f0411f09e24
Prachi Phadke <prachi.phadke@microchip.com>
2020-04-21 10:48:43 +0530

    [WDT] Regenerated demo for wdt_wdt_timeout_sam_d20_xpro
    MH3-34099
    
    Signed-off-by: Prachi Phadke <prachi.phadke@microchip.com>

--- apps/wdt/wdt_timeout/firmware/src/config/sam_d20_xpro/peripheral/port/plib_port.c
+++ apps/wdt/wdt_timeout/firmware/src/config/sam_d20_xpro/peripheral/port/plib_port.c
@@ -79,8 +79,10 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_PINCFG[25] = 0x1;
 
    PORT_REGS->GROUP[0].PORT_PMUX[12] = 0x22;
+                
    /************************** GROUP 1 Initialization *************************/
 
+                
 }
 
 // *****************************************************************************
