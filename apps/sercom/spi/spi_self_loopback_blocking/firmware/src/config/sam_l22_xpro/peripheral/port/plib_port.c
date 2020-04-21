79e43025a1198d42307f69fd3d7943b9738e9aa3
Prachi Phadke <prachi.phadke@microchip.com>
2020-04-21 12:10:35 +0530

    [SERCOM] Regenerated demo for sercom_spi_spi_self_loopback_blocking_sam_l22_xpro
    MH3-33449
    
    Signed-off-by: Prachi Phadke <prachi.phadke@microchip.com>

--- apps/sercom/spi/spi_self_loopback_blocking/firmware/src/config/sam_l22_xpro/peripheral/port/plib_port.c
+++ apps/sercom/spi/spi_self_loopback_blocking/firmware/src/config/sam_l22_xpro/peripheral/port/plib_port.c
@@ -78,12 +78,15 @@ void PORT_Initialize(void)
 
    PORT_REGS->GROUP[0].PORT_PMUX[8] = 0x2;
    PORT_REGS->GROUP[0].PORT_PMUX[9] = 0x22;
+                
    /************************** GROUP 1 Initialization *************************/
 
+                
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x8000000;
    PORT_REGS->GROUP[2].PORT_PINCFG[27] = 0x2;
 
+                
 }
 
 // *****************************************************************************
