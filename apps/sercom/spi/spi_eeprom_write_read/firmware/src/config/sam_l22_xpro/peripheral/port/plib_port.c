ed4471147f3ab3714c4236c24eece07dc0e23f54
Prachi Phadke <prachi.phadke@microchip.com>
2020-04-21 12:32:16 +0530

    [SERCOM] Regenerated demo for sercom_spi_spi_eeprom_write_read_sam_l22_xpro
    MH3-33415
    
    Signed-off-by: Prachi Phadke <prachi.phadke@microchip.com>

--- apps/sercom/spi/spi_eeprom_write_read/firmware/src/config/sam_l22_xpro/peripheral/port/plib_port.c
+++ apps/sercom/spi/spi_eeprom_write_read/firmware/src/config/sam_l22_xpro/peripheral/port/plib_port.c
@@ -80,13 +80,16 @@ void PORT_Initialize(void)
 
    PORT_REGS->GROUP[0].PORT_PMUX[8] = 0x2;
    PORT_REGS->GROUP[0].PORT_PMUX[9] = 0x22;
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0x10;
    PORT_REGS->GROUP[1].PORT_OUT = 0x10;
 
+                
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x18000000;
 
+                
 }
 
 // *****************************************************************************
