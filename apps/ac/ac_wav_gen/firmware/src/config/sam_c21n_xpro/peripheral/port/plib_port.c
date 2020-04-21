5ae7ab3a773100ef3e48d0ae6619f1f7a7323953
Prachi Phadke <prachi.phadke@microchip.com>
2020-04-21 10:28:02 +0530

    [AC] Regenerated demo for ac_ac_wav_gen_sam_c21n_xpro
    MH3-31961
    
    Signed-off-by: Prachi Phadke <prachi.phadke@microchip.com>

--- apps/ac/ac_wav_gen/firmware/src/config/sam_c21n_xpro/peripheral/port/plib_port.c
+++ apps/ac/ac_wav_gen/firmware/src/config/sam_c21n_xpro/peripheral/port/plib_port.c
@@ -80,6 +80,7 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_PMUX[1] = 0x1;
    PORT_REGS->GROUP[0].PORT_PMUX[2] = 0x1;
    PORT_REGS->GROUP[0].PORT_PMUX[6] = 0x7;
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_OUT = 0x80000;
    PORT_REGS->GROUP[1].PORT_PINCFG[10] = 0x1;
@@ -87,8 +88,10 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[1].PORT_PINCFG[19] = 0x7;
 
    PORT_REGS->GROUP[1].PORT_PMUX[5] = 0x33;
+                
    /************************** GROUP 2 Initialization *************************/
 
+                
 }
 
 // *****************************************************************************
