6e74f84d6dc71ef29d1b6abf00a673435cb58da6
Prachi Phadke <prachi.phadke@microchip.com>
2020-04-21 10:24:07 +0530

    [RSTC] Regenerated demo for rstc_rstc_reset_cause_sam_c21n_xpro
    MH3-33170
    
    Signed-off-by: Prachi Phadke <prachi.phadke@microchip.com>

--- apps/rstc/rstc_reset_cause/firmware/src/config/sam_c21n_xpro/peripheral/port/plib_port.c
+++ apps/rstc/rstc_reset_cause/firmware/src/config/sam_c21n_xpro/peripheral/port/plib_port.c
@@ -73,6 +73,7 @@ void PORT_Initialize(void)
 {
    /************************** GROUP 0 Initialization *************************/
 
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_OUT = 0x80000;
    PORT_REGS->GROUP[1].PORT_PINCFG[10] = 0x1;
@@ -80,10 +81,12 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[1].PORT_PINCFG[19] = 0x7;
 
    PORT_REGS->GROUP[1].PORT_PMUX[5] = 0x33;
+                
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x20;
    PORT_REGS->GROUP[2].PORT_OUT = 0x20;
 
+                
 }
 
 // *****************************************************************************
