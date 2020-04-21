5aa6b8d5408972f7c2ea27e424b0530d5250d3e4
Prachi Phadke <prachi.phadke@microchip.com>
2020-04-21 11:56:39 +0530

    [ADC] Regenerated demo for adc_adc_sample_sam_l21_xpro
    MH3-32023
    
    Signed-off-by: Prachi Phadke <prachi.phadke@microchip.com>

--- apps/adc/adc_sample/firmware/src/config/sam_l21_xpro/peripheral/port/plib_port.c
+++ apps/adc/adc_sample/firmware/src/config/sam_l21_xpro/peripheral/port/plib_port.c
@@ -82,8 +82,10 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_PMUX[1] = 0x10;
    PORT_REGS->GROUP[0].PORT_PMUX[2] = 0x10;
    PORT_REGS->GROUP[0].PORT_PMUX[11] = 0x22;
+                
    /************************** GROUP 1 Initialization *************************/
 
+                
 }
 
 // *****************************************************************************
