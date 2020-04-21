20a31c8e6517d7ae2d57b45ea44991ef44a56e59
Prachi Phadke <prachi.phadke@microchip.com>
2020-04-21 12:03:46 +0530

    [SERCOM] Regenerated demo for sercom_spi_spi_eeprom_write_read_sam_l21_xpro
    MH3-33413
    
    Signed-off-by: Prachi Phadke <prachi.phadke@microchip.com>

--- apps/sercom/spi/spi_eeprom_write_read/firmware/src/config/sam_l21_xpro/peripheral/port/plib_port.c
+++ apps/sercom/spi/spi_eeprom_write_read/firmware/src/config/sam_l21_xpro/peripheral/port/plib_port.c
@@ -75,6 +75,7 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_DIR = 0x120000;
    PORT_REGS->GROUP[0].PORT_OUT = 0x120000;
 
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0x1400;
    PORT_REGS->GROUP[1].PORT_OUT = 0x1000;
@@ -84,6 +85,7 @@ void PORT_Initialize(void)
 
    PORT_REGS->GROUP[1].PORT_PMUX[8] = 0x2;
    PORT_REGS->GROUP[1].PORT_PMUX[11] = 0x33;
+                
 }
 
 // *****************************************************************************
