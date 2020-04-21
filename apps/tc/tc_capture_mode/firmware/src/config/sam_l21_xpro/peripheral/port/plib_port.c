ae9d8d73015c96e086e9e55a340da4c60496dfea
Prachi Phadke <prachi.phadke@microchip.com>
2020-04-21 12:06:48 +0530

    [TC] Regenerated demo for tc_tc_capture_mode_sam_l21_xpro
    MH3-33738
    
    Signed-off-by: Prachi Phadke <prachi.phadke@microchip.com>

--- apps/tc/tc_capture_mode/firmware/src/config/sam_l21_xpro/peripheral/port/plib_port.c
+++ apps/tc/tc_capture_mode/firmware/src/config/sam_l21_xpro/peripheral/port/plib_port.c
@@ -79,10 +79,12 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_PINCFG[23] = 0x1;
 
    PORT_REGS->GROUP[0].PORT_PMUX[11] = 0x22;
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_PINCFG[9] = 0x1;
 
    PORT_REGS->GROUP[1].PORT_PMUX[4] = 0x40;
+                
 }
 
 // *****************************************************************************
