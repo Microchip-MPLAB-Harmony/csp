28e4b0e9891c524e41a6124ab9a79f3d3626454f
Prachi Phadke <prachi.phadke@microchip.com>
2020-04-21 11:12:51 +0530

    [DAC] Regenerated demo for dac_dac_wav_gen_dma_sam_l10_xpro
    MH3-32417
    
    Signed-off-by: Prachi Phadke <prachi.phadke@microchip.com>

--- apps/dac/dac_wav_gen_dma/firmware/src/packs/ATSAML10E16A_DFP/component/ccl.h
+++ apps/dac/dac_wav_gen_dma/firmware/src/packs/ATSAML10E16A_DFP/component/ccl.h
@@ -1,7 +1,7 @@
 /**
  * \brief Component description for CCL
  *
- * Copyright (c) 2019 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2020 Microchip Technology Inc. and its subsidiaries.
  *
  * Subject to your compliance with these terms, you may use Microchip software and any derivatives
  * exclusively with Microchip products. It is your responsibility to comply with third party license
@@ -20,7 +20,7 @@
  *
  */
 
-/* file generated from device description version 2019-11-24T15:57:35Z */
+/* file generated from device description version 2020-02-04T13:16:04Z */
 #ifndef _SAML10_CCL_COMPONENT_H_
 #define _SAML10_CCL_COMPONENT_H_
 
