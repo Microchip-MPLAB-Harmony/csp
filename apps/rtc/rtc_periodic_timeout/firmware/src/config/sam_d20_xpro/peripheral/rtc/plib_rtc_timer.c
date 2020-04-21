a7d27f5fad05c7755167215e6522cebd73eb80b9
Prachi Phadke <prachi.phadke@microchip.com>
2020-04-21 10:52:50 +0530

    [RTC] Regenerated demo for rtc_rtc_periodic_timeout_sam_d20_xpro
    MH3-33220
    
    Signed-off-by: Prachi Phadke <prachi.phadke@microchip.com>

--- apps/rtc/rtc_periodic_timeout/firmware/src/config/sam_d20_xpro/peripheral/rtc/plib_rtc_timer.c
+++ apps/rtc/rtc_periodic_timeout/firmware/src/config/sam_d20_xpro/peripheral/rtc/plib_rtc_timer.c
@@ -117,7 +117,7 @@ uint32_t RTC_Timer32CounterGet ( void )
         /* Wait for Synchronization before reading value from Count Register */
     }
 
-    return(RTC_REGS->MODE0.RTC_COUNT) + 3;
+    return(RTC_REGS->MODE0.RTC_COUNT) + 6;
 }
 
 uint32_t RTC_Timer32PeriodGet ( void )
