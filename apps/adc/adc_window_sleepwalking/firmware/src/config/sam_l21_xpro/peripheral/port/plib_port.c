52b411dd6a1affea09aa66e44bcef24d27f92f8c
Prachi Phadke <prachi.phadke@microchip.com>
2020-04-21 11:57:14 +0530

    [ADC] Regenerated demo for adc_adc_window_sleepwalking_sam_l21_xpro
    MH3-32047
    
    Signed-off-by: Prachi Phadke <prachi.phadke@microchip.com>

--- apps/adc/adc_window_sleepwalking/firmware/src/config/sam_l21_xpro/peripheral/port/plib_port.c
+++ apps/adc/adc_window_sleepwalking/firmware/src/config/sam_l21_xpro/peripheral/port/plib_port.c
@@ -78,8 +78,10 @@ void PORT_Initialize(void)
 
    PORT_REGS->GROUP[0].PORT_PMUX[1] = 0x10;
    PORT_REGS->GROUP[0].PORT_PMUX[11] = 0x22;
+                
    /************************** GROUP 1 Initialization *************************/
 
+                
 }
 
 // *****************************************************************************
