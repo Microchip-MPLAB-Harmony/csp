9e8366c64f8e1dd91e791cdb0ba1293dc24c2131
Prachi Phadke <prachi.phadke@microchip.com>
2020-04-21 10:23:29 +0530

    [TC] Regenerated demo for tc_tc_compare_mode_sam_c21n_xpro
    MH3-33750
    
    Signed-off-by: Prachi Phadke <prachi.phadke@microchip.com>

--- apps/tc/tc_compare_mode/firmware/src/config/sam_c21n_xpro/peripheral/port/plib_port.c
+++ apps/tc/tc_compare_mode/firmware/src/config/sam_c21n_xpro/peripheral/port/plib_port.c
@@ -75,14 +75,17 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_PINCFG[15] = 0x1;
 
    PORT_REGS->GROUP[0].PORT_PMUX[7] = 0x40;
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_PINCFG[12] = 0x1;
    PORT_REGS->GROUP[1].PORT_PINCFG[31] = 0x1;
 
    PORT_REGS->GROUP[1].PORT_PMUX[6] = 0x4;
    PORT_REGS->GROUP[1].PORT_PMUX[15] = 0x40;
+                
    /************************** GROUP 2 Initialization *************************/
 
+                
 }
 
 // *****************************************************************************
