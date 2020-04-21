b8c59305a105b5171e2db92e349eabaae34b3f32
Prachi Phadke <prachi.phadke@microchip.com>
2020-04-21 12:04:25 +0530

    [PM] Regenerated demo for pm_pm_wakeup_rtc_sam_l21_xpro
    MH3-33017
    
    Signed-off-by: Prachi Phadke <prachi.phadke@microchip.com>

--- apps/pm/pm_wakeup_rtc/firmware/src/config/sam_l21_xpro/peripheral/port/plib_port.c
+++ apps/pm/pm_wakeup_rtc/firmware/src/config/sam_l21_xpro/peripheral/port/plib_port.c
@@ -76,9 +76,11 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_PINCFG[23] = 0x1;
 
    PORT_REGS->GROUP[0].PORT_PMUX[11] = 0x22;
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0x400;
 
+                
 }
 
 // *****************************************************************************
