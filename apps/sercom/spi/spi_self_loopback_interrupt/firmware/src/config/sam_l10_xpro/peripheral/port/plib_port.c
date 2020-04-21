d8bfc9645601ea63e38b3d7eafebde5d7be76d37
Prachi Phadke <prachi.phadke@microchip.com>
2020-04-21 11:07:58 +0530

    [SERCOM] Regenerated demo for sercom_spi_spi_self_loopback_interrupt_sam_l10_xpro
    MH3-33465
    
    Signed-off-by: Prachi Phadke <prachi.phadke@microchip.com>

--- apps/sercom/spi/spi_self_loopback_interrupt/firmware/src/config/sam_l10_xpro/peripheral/port/plib_port.c
+++ apps/sercom/spi/spi_self_loopback_interrupt/firmware/src/config/sam_l10_xpro/peripheral/port/plib_port.c
@@ -81,6 +81,7 @@ void PORT_Initialize(void)
 
    PORT_REGS->GROUP[0].PORT_PMUX[2] = 0x3;
    PORT_REGS->GROUP[0].PORT_PMUX[7] = 0x33;
+                
 }
 
 // *****************************************************************************
