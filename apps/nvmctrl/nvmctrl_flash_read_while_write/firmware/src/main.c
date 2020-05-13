9fd079269a7ba1ff52198b725c5c56fd19cf8347
Prachi Phadke <prachi.phadke@microchip.com>
2020-05-13 17:46:12 +0530

    [NVMCTRL] Updated main.c file of nvmctrl_flash_read_while_write application
    MH3-36337

--- apps/nvmctrl/nvmctrl_flash_read_while_write/firmware/src/main.c
+++ apps/nvmctrl/nvmctrl_flash_read_while_write/firmware/src/main.c
@@ -55,8 +55,8 @@
 #define LED_OFF                     LED_Set
 #define LED_TOGGLE                  LED_Toggle
 
-uint32_t nvm_user_start_address = (NVMCTRL_FLASH_START_ADDRESS+0x20000);
-uint32_t nvm_rwwee_user_start_address = NVMCTRL_RWWEEPROM_START_ADDRESS;
+const uint8_t nvm_user_start_address[NVMCTRL_FLASH_ROWSIZE] __attribute__((address(NVMCTRL_FLASH_START_ADDRESS+0x20000)))= {0};
+const uint8_t nvm_rwwee_user_start_address[NVMCTRL_RWWEEPROM_ROWSIZE] __attribute__((address(NVMCTRL_RWWEEPROM_START_ADDRESS)))= {0};
 
 // *****************************************************************************
 // *****************************************************************************
