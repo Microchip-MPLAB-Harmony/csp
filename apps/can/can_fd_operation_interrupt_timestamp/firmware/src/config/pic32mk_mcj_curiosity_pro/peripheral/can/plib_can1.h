e64c66f203d611dbf91a9c157a2d38b0f7fb425b
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2020-05-14 17:42:59 +0530

    [CAN] Regenerated can_can_fd_operation_interrupt_timestamp_pic32mk_mcj_curiosity_pro demo
    MH3-36355

--- apps/can/can_fd_operation_interrupt_timestamp/firmware/src/config/pic32mk_mcj_curiosity_pro/peripheral/can/plib_can1.h
+++ apps/can/can_fd_operation_interrupt_timestamp/firmware/src/config/pic32mk_mcj_curiosity_pro/peripheral/can/plib_can1.h
@@ -87,6 +87,7 @@ bool CAN1_InterruptGet(uint8_t fifoQueueNum, CAN_FIFO_INTERRUPT_FLAG_MASK fifoIn
 bool CAN1_TxFIFOQueueIsFull(uint8_t fifoQueueNum);
 bool CAN1_AutoRTRResponseSet(uint32_t id, uint8_t length, uint8_t* data, uint8_t fifoNum);
 void CAN1_CallbackRegister(CAN_CALLBACK callback, uintptr_t contextHandle, uint8_t fifoQueueNum);
+void CAN1_ErrorCallbackRegister(CAN_CALLBACK callback, uintptr_t contextHandle);
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
     }
