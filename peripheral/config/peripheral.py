7f223d58fe2abf60f382606acc95d141641cdba5
Vishal Gandhi <vishal.gandhi@microchip.com>
2020-07-18 00:45:40 +0530

    [CONFIG] Updated python to publish Memory capability for the NVMCTRL PLIB
    MH3-38178

--- peripheral/config/peripheral.py
+++ peripheral/config/peripheral.py
@@ -29,6 +29,7 @@ peripherals = {
                 "NVMCTRL_U2409" : ["MEMORY"],
                 "NVMCTRL_U2207" : ["MEMORY"],
                 "NVMCTRL_U2802" : ["MEMORY"],
+                "NVMCTRL_03929" : ["MEMORY"],
                 "FLEXCOM_11268" : ["UART", "SPI", "I2C"],
                 "FLEXCOM_11277" : ["UART", "SPI", "I2C"],
                 "SERCOM_U2201"  : ["UART", "SPI", "I2C"],
