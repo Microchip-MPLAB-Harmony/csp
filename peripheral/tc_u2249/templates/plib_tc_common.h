2fbe3ca230c0d086e7430d64cf2d13108677d630
Syam Nair <syam.nair@microchip.com>
2021-01-05 12:41:57 +0530

    [TC] Convert status enums to typedefs
    MH3-47063

--- peripheral/tc_u2249/templates/plib_tc_common.h
+++ peripheral/tc_u2249/templates/plib_tc_common.h
@@ -63,6 +63,68 @@
 
 #endif
 // DOM-IGNORE-END
+// *****************************************************************************
+// *****************************************************************************
+// Section:Preprocessor macros
+// *****************************************************************************
+// *****************************************************************************
+// *****************************************************************************
+
+// *****************************************************************************
+/* Convenience macros for TC compare status */
+// *****************************************************************************
+
+#define TC_CAPTURE_STATUS_NONE              0U
+
+/* Capture status overflow */
+#define TC_CAPTURE_STATUS_OVERFLOW          TC_INTFLAG_OVF_Msk
+
+/* Capture status error */
+#define TC_CAPTURE_STATUS_ERROR             TC_INTFLAG_ERR_Msk
+
+/* Capture status ready for channel 0 */
+#define TC_CAPTURE_STATUS_CAPTURE0_READY    TC_INTFLAG_MC0_Msk
+
+/* Capture status ready for channel 1 */
+#define TC_CAPTURE_STATUS_CAPTURE1_READY    TC_INTFLAG_MC1_Msk
+
+#define TC_CAPTURE_STATUS_MSK               (TC_CAPTURE_STATUS_OVERFLOW | TC_CAPTURE_STATUS_ERROR | TC_CAPTURE_STATUS_CAPTURE0_READY | TC_CAPTURE_STATUS_CAPTURE1_READY) 
+
+/* Invalid compare status */
+#define TC_CAPTURE_STATUS_INVALID           0xFFFFFFFFU
+
+// *****************************************************************************
+/* Convenience macros for TC capture status */
+// *****************************************************************************
+
+#define TC_COMPARE_STATUS_NONE          0U
+/*  overflow */
+#define TC_COMPARE_STATUS_OVERFLOW      TC_INTFLAG_OVF_Msk
+/* match compare 0 */
+#define TC_COMPARE_STATUS_MATCH0        TC_INTFLAG_MC0_Msk
+/* match compare 1 */
+#define TC_COMPARE_STATUS_MATCH1        TC_INTFLAG_MC1_Msk
+
+#define TC_COMPARE_STATUS_MSK           (TC_COMPARE_STATUS_OVERFLOW | TC_COMPARE_STATUS_MATCH0 | TC_COMPARE_STATUS_MATCH1)
+
+/* Invalid capture status */
+#define TC_COMPARE_STATUS_INVALID       0xFFFFFFFFU
+
+// *****************************************************************************
+/* Convenience macros for TC capture status */
+// *****************************************************************************
+
+#define TC_TIMER_STATUS_NONE        0U
+/*  overflow */
+#define TC_TIMER_STATUS_OVERFLOW    TC_INTFLAG_OVF_Msk
+
+/* match compare 1 */
+#define TC_TIMER_STATUS_MATCH1      TC_INTFLAG_MC1_Msk
+
+#define TC_TIMER_STATUS_MSK         (TC_TIMER_STATUS_OVERFLOW | TC_TIMER_STATUS_MATCH1)
+
+/* Invalid timer status */
+#define TC_TIMER_STATUS_INVALID     0xFFFFFFFFU
 
 // *****************************************************************************
 // *****************************************************************************
@@ -75,58 +137,11 @@
 
 // *****************************************************************************
 
-typedef enum
-{
-    TC_CAPTURE_STATUS_NONE = 0,
-    
-    /* Capture status overflow */
-    TC_CAPTURE_STATUS_OVERFLOW = TC_INTFLAG_OVF_Msk,
+typedef uint32_t TC_CAPTURE_STATUS;
 
-    /* Capture status error */
-    TC_CAPTURE_STATUS_ERROR = TC_INTFLAG_ERR_Msk,
+typedef uint32_t TC_COMPARE_STATUS;
 
-    /* Capture status ready for channel 0 */
-    TC_CAPTURE_STATUS_CAPTURE0_READY = TC_INTFLAG_MC0_Msk,
-
-    /* Capture status ready for channel 1 */
-    TC_CAPTURE_STATUS_CAPTURE1_READY = TC_INTFLAG_MC1_Msk,
-    
-    TC_CAPTURE_STATUS_MSK = TC_CAPTURE_STATUS_OVERFLOW | TC_CAPTURE_STATUS_ERROR | TC_CAPTURE_STATUS_CAPTURE0_READY | TC_CAPTURE_STATUS_CAPTURE1_READY, 
-    
-    /* Force the compiler to reserve 32-bit memory for enum */
-    TC_CAPTURE_STATUS_INVALID = 0xFFFFFFFF
-
-} TC_CAPTURE_STATUS;
-
-typedef enum
-{
-    TC_COMPARE_STATUS_NONE = 0,
-    /*  overflow */
-    TC_COMPARE_STATUS_OVERFLOW = TC_INTFLAG_OVF_Msk,
-    /* match compare 0 */
-    TC_COMPARE_STATUS_MATCH0 = TC_INTFLAG_MC0_Msk,
-    /* match compare 1 */
-    TC_COMPARE_STATUS_MATCH1 = TC_INTFLAG_MC1_Msk,
-    TC_COMPARE_STATUS_MSK = TC_COMPARE_STATUS_OVERFLOW | TC_COMPARE_STATUS_MATCH0 | TC_COMPARE_STATUS_MATCH1,
-
-    /* Force the compiler to reserve 32-bit memory for enum */
-    TC_COMPARE_STATUS_INVALID = 0xFFFFFFFF
-}TC_COMPARE_STATUS;
-
-typedef enum
-{
-    TC_TIMER_STATUS_NONE = 0, 
-    /*  overflow */
-    TC_TIMER_STATUS_OVERFLOW = TC_INTFLAG_OVF_Msk,
-
-    /* match compare 1 */
-    TC_TIMER_STATUS_MATCH1 = TC_INTFLAG_MC1_Msk,
-    
-    TC_TIMER_STATUS_MSK = TC_TIMER_STATUS_OVERFLOW | TC_TIMER_STATUS_MATCH1, 
-
-    /* Force the compiler to reserve 32-bit memory for enum */
-    TC_TIMER_STATUS_INVALID = 0xFFFFFFFF
-} TC_TIMER_STATUS;
+typedef uint32_t TC_TIMER_STATUS;
 
 typedef enum 
 {
