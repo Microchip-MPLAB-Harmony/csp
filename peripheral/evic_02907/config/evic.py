2bb6ac0f86f8b074521f6f1ae62c64ef3a8c13d3
Syam Nair <syam.nair@microchip.com>
2022-09-29 18:52:31 +0530

    [EVIC] Fix MISRA-C 2012 required rule violations
    MH3-62567

--- peripheral/evic_02907/config/evic.py
+++ peripheral/evic_02907/config/evic.py
@@ -630,8 +630,8 @@ evicSystemInitStartFile.setMarkup(True)
 
 evicSystemIntWeakHandleFile = coreComponent.createFileSymbol("EVIC_INT_HANDLER", None)
 evicSystemIntWeakHandleFile.setType("STRING")
-evicSystemIntWeakHandleFile.setOutputName("core.LIST_SYSTEM_INTERRUPT_WEAK_HANDLERS")
-evicSystemIntWeakHandleFile.setSourcePath("../peripheral/evic_02907/templates/system/interrupts_weak_handlers.h.ftl")
+evicSystemIntWeakHandleFile.setOutputName("core.LIST_SYSTEM_INTERRUPT_HANDLER_DECLS")
+evicSystemIntWeakHandleFile.setSourcePath("../peripheral/evic_02907/templates/system/interrupts_handlers_decls.h.ftl")
 evicSystemIntWeakHandleFile.setMarkup(True)
 
 evicSystemIntTableFile = coreComponent.createFileSymbol("EVIC_INT_TABLE", None)
