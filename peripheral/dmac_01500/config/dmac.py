51bcb57b3730dcdd4ec89498ea24ef399b54e691
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-02-20 18:15:20 +0530

    [DMA] Fixed default value of DMA channel priority for dmac_01500 PLIB
    MH3-74429

--- peripheral/dmac_01500/config/dmac.py
+++ peripheral/dmac_01500/config/dmac.py
@@ -632,7 +632,7 @@ for dmaChannel in range(0, numDMAChans):
     dmacPrioritySymId = "DMAC_" + str(dmaChannel) + "_PRIORITY"
     symbol = coreComponent.createComboSymbol(dmacPrioritySymId, dmacChannelEnable, sorted(per_priority.keys()))
     symbol.setLabel(dmaValGrp_DCH0CON_CHPRI.getAttribute("caption"))
-    symbol.setDefaultValue("0")
+    symbol.setDefaultValue("CHPRI0")
 
     symbol = coreComponent.createBooleanSymbol("DMAC_" + str(dmaChannel) + "_SOURCE_HALF_EMPTY_INT_ENABLE", dmacChannelEnable)
     symbol.setLabel("Source Half Empty Interrupt Enable")
