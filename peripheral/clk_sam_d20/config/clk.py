a4bc049eee1741bd3181f478bb3ce391c955fa98
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-03-07 14:55:43 +0530

    [CLK_SAM_D20] Updated calibration values address to be ATDF driven
    MH3-74642

--- peripheral/clk_sam_d20/config/clk.py
+++ peripheral/clk_sam_d20/config/clk.py
@@ -151,7 +151,10 @@ def updateBODVisibleProperty(symbol, event):
 ################################################################################
 #######          SYSCTRL Database Components      ##############################
 ################################################################################
-
+calibRowAddr = ATDF.getNode("/avr-tools-device-file/devices/device/address-spaces/address-space/memory-segment@[name=\"OTP4\"]").getAttribute("start")
+swCalibRowAddr = coreComponent.createStringSymbol("SW_CALIB_ROW_ADDR", None)
+swCalibRowAddr.setDefaultValue(calibRowAddr)
+swCalibRowAddr.setVisible(False)
 ##############################OSC8M#############################################
 
 osc8MEnable = coreComponent.createBooleanSymbol("CONFIG_CLOCK_OSC8M_ENABLE", sysctrl_OSC8M)
