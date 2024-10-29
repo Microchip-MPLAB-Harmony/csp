dd81f9c9fa23b84f4010b777f6bdca7c4b3f0f8e
Kevin Abishek S <kevinabishek.s@microchip.com>
2024-10-29 09:50:35 +0530

    [OTPC] sethelp updated in otpc.py MH3-104965

--- peripheral/otpc_44105/config/otpc.py
+++ peripheral/otpc_44105/config/otpc.py
@@ -33,7 +33,7 @@ def instantiateComponent(otpcComponent):
 
     #OTPC emulation
     otpcEmulation = otpcComponent.createBooleanSymbol("ENABLE_OTPC_EMULATION", None)
-    otpcEmulation.setHelp("atmel;device:" + Variables.get("__PROCESSOR") + ";comp:otpc_44105;register:%NOREGISTER%")
+    otpcEmulation.setHelp("atmel;device:" + Variables.get("__PROCESSOR") + ";comp:otpc_44105;register:OTPC_MR")
     otpcEmulation.setLabel("Enable Emulation mode")
     otpcEmulation.setDefaultValue(True)
 
