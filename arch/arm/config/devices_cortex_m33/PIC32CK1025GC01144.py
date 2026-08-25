280ff6aa8ec628fc37a1565c75b43b22bd806fcb
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2026-08-25 16:04:28 +0530

    [ARCH] Added PIC32CK1025GC01144 part support in harmony
    MH3C-1079

---
+++ arch/arm/config/devices_cortex_m33/PIC32CK1025GC01144.py
@@ -0,0 +1,3 @@
+Log.writeInfoMessage("Loading System Services for " + Variables.get("__PROCESSOR"))
+# load family specific configuration
+execfile(Variables.get("__ARCH_DIR") + "/PIC32CK_GC_SG.py")
